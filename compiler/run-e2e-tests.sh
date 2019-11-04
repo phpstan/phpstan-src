de3cb86a3fd84af2931be55f7f195d66fdf1586e
Ondrej Mirtes <ondrej@mirtes.cz>
2019-11-04 19:08:16 +0100

    Fix compiler tests

--- compiler/run-e2e-tests.sh
+++ compiler/run-e2e-tests.sh
@@ -2,17 +2,15 @@
 set -ev
 
 # compile phar
-if [ ! -f tmp/phpstan.phar ]; then
-    php bin/compile
-fi
+php bin/compile
 
 # setup
 cd e2e
 rm -rf vendor
 rm -f composer.lock
 composer install --no-interaction
-cp -f ../tmp/phpstan.phar vendor/phpstan/phpstan/phpstan.phar
-cp -f ../tmp/phpstan.phar vendor/phpstan/phpstan/phpstan
+cp -f ../../tmp/phpstan.phar vendor/phpstan/phpstan/phpstan.phar
+cp -f ../../tmp/phpstan.phar vendor/phpstan/phpstan/phpstan
 
 # test that the phar autoloader works
 php testPharAutoloader.php

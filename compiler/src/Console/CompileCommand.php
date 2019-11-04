8c618fbbeb5fa0d115d0ff967b93523b52e5d2e6
Ondrej Mirtes <ondrej@mirtes.cz>
2019-11-04 18:44:25 +0100

    Fix CS

--- compiler/src/Console/CompileCommand.php
+++ compiler/src/Console/CompileCommand.php
@@ -5,7 +5,6 @@ namespace PHPStan\Compiler\Console;
 use PHPStan\Compiler\Filesystem\Filesystem;
 use PHPStan\Compiler\Process\ProcessFactory;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 

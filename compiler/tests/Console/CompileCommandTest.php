e6eca5e2ce0193d4f83a35a306afc76839efd39c
Ondrej Mirtes <ondrej@mirtes.cz>
2019-11-04 18:52:27 +0100

    Fixed compiler test

--- compiler/tests/Console/CompileCommandTest.php
+++ compiler/tests/Console/CompileCommandTest.php
@@ -15,9 +15,6 @@ final class CompileCommandTest extends TestCase
 	public function testCommand(): void
 	{
 		$filesystem = $this->createMock(Filesystem::class);
-		$filesystem->expects(self::once())->method('exists')->with('bar')->willReturn(true);
-		$filesystem->expects(self::once())->method('remove')->with('bar');
-		$filesystem->expects(self::once())->method('mkdir')->with('bar');
 		$filesystem->expects(self::once())->method('read')->with('bar/composer.json')->willReturn('{"name":"phpstan/phpstan-src","replace":{"phpstan/phpstan": "self.version"},"require":{"php":"~7.1"},"require-dev":1,"autoload-dev":2,"autoload":{"psr-4":{"PHPStan\\\\":[3]}}}');
 		$filesystem->expects(self::once())->method('write')->with('bar/composer.json', <<<EOT
 {
@@ -38,11 +35,9 @@ EOT
 
 		$processFactory = $this->createMock(ProcessFactory::class);
 		$processFactory->expects(self::at(0))->method('setOutput');
-		$processFactory->expects(self::at(1))->method('create')->with(['git', 'clone', 'https://github.com/phpstan/phpstan-src.git', '.'], 'bar')->willReturn($process);
-		$processFactory->expects(self::at(2))->method('create')->with(['git', 'checkout', '--force', 'master'], 'bar')->willReturn($process);
-		$processFactory->expects(self::at(3))->method('create')->with(['composer', 'require', '--no-update', 'dg/composer-cleaner:^2.0'], 'bar')->willReturn($process);
-		$processFactory->expects(self::at(4))->method('create')->with(['composer', 'update', '--no-dev', '--classmap-authoritative'], 'bar')->willReturn($process);
-		$processFactory->expects(self::at(5))->method('create')->with(['php', 'box.phar', 'compile'], 'foo')->willReturn($process);
+		$processFactory->expects(self::at(1))->method('create')->with(['composer', 'require', '--no-update', 'dg/composer-cleaner:^2.0'], 'bar')->willReturn($process);
+		$processFactory->expects(self::at(2))->method('create')->with(['composer', 'update', '--no-dev', '--classmap-authoritative'], 'bar')->willReturn($process);
+		$processFactory->expects(self::at(3))->method('create')->with(['php', 'box.phar', 'compile'], 'foo')->willReturn($process);
 
 		$application = new Application();
 		$application->add(new CompileCommand($filesystem, $processFactory, 'foo', 'bar'));

cf9bde298cd0384bd234a161a00b07a73def29c1
Markus Staab <markus.staab@redaxo.de>
2019-11-10 20:29:30 +0100

    fix CS

--- src/Command/ErrorFormatter/BaselineNeonErrorFormatter.php
+++ src/Command/ErrorFormatter/BaselineNeonErrorFormatter.php
@@ -57,7 +57,7 @@ class BaselineNeonErrorFormatter implements ErrorFormatter
 			foreach ($fileErrorsCounts as $message => $count) {
 				$message = preg_replace('/\r\n|\r|\n/', "\n", $message);
 				if ($message === null) {
-					throw new \Exception('Error while executing regex: '. preg_last_error());
+					throw new \Exception('Error while executing regex: ' . preg_last_error());
 				}
 
 				$errorsToOutput[] = [

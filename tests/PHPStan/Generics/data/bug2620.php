472556ff13f3bea3307fcf0ddd525ac5b553e793
Arnaud Le Blanc <arnaud.lb@gmail.com>
2019-11-28 19:09:07 +0100

    Make isSuperTypeOf() a little stricter on GenericObjectType (fixes #2620)

---
+++ tests/PHPStan/Generics/data/bug2620.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Generics\Bug2620;
+
+class Foo {
+    public function someMethod() : void {}
+}
+class Bar {}
+
+/**
+ * @implements \IteratorAggregate<int, Foo>
+ */
+class SomeIterator implements \IteratorAggregate {
+    /**
+     * @return \Traversable<int, Bar>
+     */
+    public function getIterator() {
+         yield new Bar;
+    }
+}
+
+/**
+ * @param \IteratorAggregate<int, Foo> $i
+ */
+function takesIteratorAggregate(\IteratorAggregate $i): void {
+    foreach ($i as $foo) {
+        $foo->someMethod();
+    }
+}
+
+function test(): void {
+	takesIteratorAggregate(new SomeIterator());
+}

3a2351242dc5ccce5f057bb2ba4f0a220bd6fcb5
Adam Žurek <thejabko@gmail.com>
2019-12-04 16:48:08 +0100

    TypeWithClassName::getClassName returns class-string

--- src/Type/TypeWithClassName.php
+++ src/Type/TypeWithClassName.php
@@ -5,6 +5,7 @@ namespace PHPStan\Type;
 interface TypeWithClassName extends Type
 {
 
+	/** @phpstan-return class-string */
 	public function getClassName(): string;
 
 	public function getAncestorWithClassName(string $className): ?ObjectType;

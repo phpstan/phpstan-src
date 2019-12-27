d5499e9ab9fa1a4a19f16932f2236592316378dc
Ondrej Mirtes <ondrej@mirtes.cz>
2019-12-27 14:05:09 +0100

    Fixed build after nette/neon 3.1.0 release

--- compiler/build/scoper.inc.php
+++ compiler/build/scoper.inc.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-require_once __DIR__ . '/../vendor/nette/neon/src/neon.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 $stubs = [
 	'../../src/Reflection/SignatureMap/functionMap.php',

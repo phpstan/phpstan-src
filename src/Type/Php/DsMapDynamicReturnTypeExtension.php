26d27be37a049ad750e69fa1d4737a0d17fbb9ce
Jáchym Toušek <enumag@gmail.com>
2020-01-02 17:05:59 +0100

    Implement ext-ds generics

---
+++ src/Type/Php/DsMapDynamicReturnTypeExtension.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Type\Php;
+
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\Type;
+use PHPStan\Type\UnionType;
+
+final class DsMapDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
+{
+
+	public function getClass(): string
+	{
+		return 'Ds\Map';
+	}
+
+	public function isMethodSupported(MethodReflection $methodReflection): bool
+	{
+		return $methodReflection->getName() === 'get' || $methodReflection->getName() === 'remove';
+	}
+
+	public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
+	{
+		$returnType = $methodReflection->getVariants()[0]->getReturnType();
+
+		if (count($methodCall->args) > 1) {
+			return $returnType;
+		}
+
+		if (! $returnType instanceof UnionType) {
+			return $returnType;
+		}
+
+		return $returnType->getTypes()[0];
+	}
+
+}

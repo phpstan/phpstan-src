phpstan/phpstan-src

commit 9665e165d50737c53d68521936cf8ab2f2c07109

Fixed expression invalidation when ++ is involved

src/Analyser/MutatingScope.php
@@ -2574,26 +2574,31 @@ class MutatingScope implements Scope
 		return $scope->specifyExpressionType($expr, $type);
 	}
 
-	public function invalidateExpression(Expr $expressionToInvalidate, bool $requireMoreCharacters = false): self
+	public function invalidateExpression(Expr $expressionToInvalidate): self
 	{
 		$exprStringToInvalidate = $this->printer->prettyPrintExpr($expressionToInvalidate);
 		$moreSpecificTypeHolders = $this->moreSpecificTypes;
 		foreach (array_keys($moreSpecificTypeHolders) as $exprString) {
 			$exprString = (string) $exprString;
-			if ($requireMoreCharacters && $exprString === $exprStringToInvalidate) {
-				continue;
+			if (Strings::startsWith($exprString, $exprStringToInvalidate)) {
+				if ($exprString === $exprStringToInvalidate) {
+					unset($moreSpecificTypeHolders[$exprString]);
+					continue;
+				}
+				$nextLetter = substr($exprString, strlen($exprStringToInvalidate), 1);
+				if (Strings::match($nextLetter, '#[a-zA-Z_0-9\x7f-\xff]#') === null) {
+					unset($moreSpecificTypeHolders[$exprString]);
+					continue;
+				}
 			}
-			if (!Strings::startsWith($exprString, $exprStringToInvalidate)) {
+			$matches = \Nette\Utils\Strings::matchAll($exprString, '#\$[a-zA-Z_\x7f-\xff][a-zA-Z_0-9\x7f-\xff]*#');
+			if ($matches === []) {
 				continue;
 			}
 
-			if ($exprString === $exprStringToInvalidate) {
-				unset($moreSpecificTypeHolders[$exprString]);
-				continue;
-			}
+			$matches = array_column($matches, 0);
 
-			$nextLetter = substr($exprString, strlen($exprStringToInvalidate), 1);
-			if (Strings::match($nextLetter, '#[a-zA-Z_0-9\x7f-\xff]#') !== null) {
+			if (!in_array($exprStringToInvalidate, $matches, true)) {
 				continue;
 			}
 

src/Analyser/NodeScopeResolver.php
@@ -1492,7 +1492,7 @@ class NodeScopeResolver
 			$result = $this->processArgs($methodReflection, $parametersAcceptor, $expr->args, $scope, $nodeCallback, $context);
 			$scope = $result->getScope();
 			if ($methodReflection !== null && $methodReflection->hasSideEffects()->yes()) {
-				$scope = $scope->invalidateExpression($expr->var, true);
+				$scope = $scope->invalidateExpression($expr->var);
 			}
 			$hasYield = $hasYield || $result->hasYield();
 		} elseif ($expr instanceof StaticCall) {
@@ -1829,6 +1829,8 @@ class NodeScopeResolver
 						},
 						false
 					)->getScope();
+				} else {
+					$scope = $scope->invalidateExpression($expr->var);
 				}
 			}
 		} elseif ($expr instanceof Ternary) {

tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -9752,6 +9752,11 @@ class NodeScopeResolverTest extends \PHPStan\Testing\TestCase
 		return $this->gatherAssertTypes(__DIR__ . '/data/list-type.php');
 	}
 
+	public function dataBug2835(): array
+	{
+		return $this->gatherAssertTypes(__DIR__ . '/data/bug-2835.php');
+	}
+
 	/**
 	 * @dataProvider dataBug2574
 	 * @dataProvider dataBug2577
@@ -9775,6 +9780,7 @@ class NodeScopeResolverTest extends \PHPStan\Testing\TestCase
 	 * @dataProvider dataPhpDocInheritanceConstructors
 	 * @dataProvider dataListType
 	 * @dataProvider dataBug2822
+	 * @dataProvider dataBug2835
 	 * @param ConstantStringType $expectedType
 	 * @param Type $actualType
 	 */

tests/PHPStan/Analyser/data/bug-2835.php
@@ -1,0 +1,41 @@
+<?php
+
+namespace Bug2835AssertTypes;
+
+use function PHPStan\Analyser\assertType;
+
+class Foo
+{
+
+	/**
+	 * @param array<int, array> $tokens
+	 * @return bool
+	 */
+	public function doFoo(array $tokens): bool {
+		$i = 0;
+		while (isset($tokens[$i])) {
+			assertType('int', $i);
+			if ($tokens[$i]['code'] !== 1) {
+				assertType('mixed~1', $tokens[$i]['code']);
+				$i++;
+				assertType('int', $i);
+				assertType('mixed', $tokens[$i]['code']);
+				continue;
+			}
+			assertType('1', $tokens[$i]['code']);
+			$i++;
+			assertType('int', $i);
+			assertType('mixed', $tokens[$i]['code']);
+			if ($tokens[$i]['code'] !== 2) {
+				assertType('mixed~2', $tokens[$i]['code']);
+				$i++;
+				assertType('int', $i);
+				continue;
+			}
+			assertType('2', $tokens[$i]['code']);
+			return true;
+		}
+		return false;
+	}
+
+}

tests/PHPStan/Rules/Comparison/StrictComparisonOfDifferentTypesRuleTest.php
@@ -413,4 +413,10 @@ class StrictComparisonOfDifferentTypesRuleTest extends \PHPStan\Testing\RuleTest
 		]);
 	}
 
+	public function testBug2835(): void
+	{
+		$this->checkAlwaysTrueStrictComparison = true;
+		$this->analyse([__DIR__ . '/data/bug-2835.php'], []);
+	}
+
 }

tests/PHPStan/Rules/Comparison/data/bug-2835.php
@@ -1,0 +1,29 @@
+<?php
+
+namespace Bug2835;
+
+class Foo
+{
+
+	/**
+	 * @param array<int, array> $tokens
+	 * @return bool
+	 */
+	public function doFoo(array $tokens): bool {
+		$i = 0;
+		while (isset($tokens[$i])) {
+			if ($tokens[$i]['code'] !== 1) {
+				$i++;
+				continue;
+			}
+			$i++;
+			if ($tokens[$i]['code'] !== 2) {
+				$i++;
+				continue;
+			}
+			return true;
+		}
+		return false;
+	}
+
+}

commit c981d894b73c117192ce715e663cda5a381f7882

Fixed regression from commit 9665e165d50737c53d68521936cf8ab2f2c07109

src/Analyser/MutatingScope.php
@@ -2574,15 +2574,14 @@ class MutatingScope implements Scope
 		return $scope->specifyExpressionType($expr, $type);
 	}
 
-	public function invalidateExpression(Expr $expressionToInvalidate): self
+	public function invalidateExpression(Expr $expressionToInvalidate, bool $requireMoreCharacters = false): self
 	{
 		$exprStringToInvalidate = $this->printer->prettyPrintExpr($expressionToInvalidate);
 		$moreSpecificTypeHolders = $this->moreSpecificTypes;
 		foreach (array_keys($moreSpecificTypeHolders) as $exprString) {
 			$exprString = (string) $exprString;
 			if (Strings::startsWith($exprString, $exprStringToInvalidate)) {
-				if ($exprString === $exprStringToInvalidate) {
-					unset($moreSpecificTypeHolders[$exprString]);
+				if ($exprString === $exprStringToInvalidate && $requireMoreCharacters) {
 					continue;
 				}
 				$nextLetter = substr($exprString, strlen($exprStringToInvalidate), 1);

src/Analyser/NodeScopeResolver.php
@@ -1492,7 +1492,7 @@ class NodeScopeResolver
 			$result = $this->processArgs($methodReflection, $parametersAcceptor, $expr->args, $scope, $nodeCallback, $context);
 			$scope = $result->getScope();
 			if ($methodReflection !== null && $methodReflection->hasSideEffects()->yes()) {
-				$scope = $scope->invalidateExpression($expr->var);
+				$scope = $scope->invalidateExpression($expr->var, true);
 			}
 			$hasYield = $hasYield || $result->hasYield();
 		} elseif ($expr instanceof StaticCall) {

tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -9772,6 +9772,11 @@ class NodeScopeResolverTest extends \PHPStan\Testing\TestCase
 		return $this->gatherAssertTypes(__DIR__ . '/data/bug-2750.php');
 	}
 
+	public function dataBug2850(): array
+	{
+		return $this->gatherAssertTypes(__DIR__ . '/data/bug-2850.php');
+	}
+
 	/**
 	 * @dataProvider dataBug2574
 	 * @dataProvider dataBug2577
@@ -9799,6 +9804,7 @@ class NodeScopeResolverTest extends \PHPStan\Testing\TestCase
 	 * @dataProvider dataBug2835
 	 * @dataProvider dataBug2443
 	 * @dataProvider dataBug2750
+	 * @dataProvider dataBug2850
 	 * @param ConstantStringType $expectedType
 	 * @param Type $actualType
 	 */

tests/PHPStan/Analyser/data/bug-2850.php
@@ -1,0 +1,34 @@
+<?php
+
+namespace Bug2850;
+
+use function PHPStan\Analyser\assertType;
+
+class Foo
+{
+	public function y(): void {}
+}
+
+class Bar
+{
+	/** @var Foo|null */
+	private $x;
+
+	public function getFoo(): Foo
+	{
+		if ($this->x === null) {
+			$this->x = new Foo();
+			assertType(Foo::class, $this->x);
+			$this->x->y();
+			assertType(Foo::class, $this->x);
+			$this->y();
+			assertType(Foo::class . '|null', $this->x);
+		}
+		return $this->x;
+	}
+
+	public function y(): void
+	{
+		$this->x = null;
+	}
+}
137c7f96b78abfa965f891b8a84833265d8e1bdb
Chris Smith <chris@cs278.org>
2020-01-29 00:45:55 +0000

    Invalidate json_last_error{,_msg}() type on json_{en,de}code() call

---
+++ tests/PHPStan/Analyser/data/bug-2863.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Bug2863;
+
+use function PHPStan\Analyser\assertType;
+
+$result = json_decode('{"a":5}');
+assertType('int', json_last_error());
+assertType('string', json_last_error_msg());
+
+if (json_last_error() !== JSON_ERROR_NONE || json_last_error_msg() !== 'No error') {
+	throw new Exception(json_last_error_msg());
+}
+
+assertType('0', json_last_error());
+assertType("'No error'", json_last_error_msg());
+
+//
+$result2 = json_decode('');
+assertType('int', json_last_error());
+assertType('string', json_last_error_msg());
+
+if (json_last_error() !== JSON_ERROR_NONE || json_last_error_msg() !== 'No error') {
+	throw new Exception(json_last_error_msg());
+}
+
+assertType('0', json_last_error());
+assertType("'No error'", json_last_error_msg());
+
+//
+$result3 = json_encode([]);
+assertType('int', json_last_error());
+assertType('string', json_last_error_msg());
+
+if (json_last_error() !== JSON_ERROR_NONE || json_last_error_msg() !== 'No error') {
+	throw new Exception(json_last_error_msg());
+}
+
+assertType('0', json_last_error());
+assertType("'No error'", json_last_error_msg());

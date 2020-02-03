c4bae9fe996c14ee14d775d2acb8fb2c6e61b103
Ondrej Mirtes <ondrej@mirtes.cz>
2020-02-03 05:58:46 +0100

    Process timeout timer

--- src/Parallel/Process.php
+++ src/Parallel/Process.php
@@ -5,6 +5,7 @@ namespace PHPStan\Parallel;
 use Clue\React\NDJson\Decoder;
 use Clue\React\NDJson\Encoder;
 use React\EventLoop\LoopInterface;
+use React\EventLoop\TimerInterface;
 
 class Process
 {
@@ -24,6 +25,12 @@ class Process
 	/** @var StreamBuffer */
 	private $stdErr;
 
+	/** @var callable */
+	private $onError;
+
+	/** @var TimerInterface|null */
+	private $timer;
+
 	public function __construct(
 		\React\ChildProcess\Process $process,
 		LoopInterface $loop
@@ -37,15 +44,36 @@ class Process
 	{
 		$this->process->start($this->loop);
 		$this->stdin = new Encoder($this->process->stdin);
-		$this->stdin->on('error', $onError);
+		$this->stdin->on('error', function (\Throwable $error) use ($onError): void {
+			$this->cancelTimer();
+			$onError($error);
+		});
 		$this->stdout = new Decoder($this->process->stdout, true, 512, 0, 4 * 1024 * 1024);
-		$this->stdout->on('data', $onData);
-		$this->stdout->on('error', $onError);
+		$this->stdout->on('data', function (array $data) use ($onData): void {
+			$this->cancelTimer();
+			$onData($data);
+		});
+		$this->stdout->on('error', function (\Throwable $error) use ($onError): void {
+			$this->cancelTimer();
+			$onError($error);
+		});
+		$this->onError = $onError;
 
 		$this->stdErr = new StreamBuffer($this->process->stderr);
 		$this->process->on('exit', function ($exitCode) use ($onExit): void {
+			$this->cancelTimer();
 			$onExit($exitCode, $this->stdErr->getBuffer());
 		});
+	}
+
+	private function cancelTimer(): void
+	{
+		if ($this->timer === null) {
+			return;
+		}
+
+		$this->loop->cancelTimer($this->timer);
+		$this->timer = null;
 	}
 
 	/**
@@ -53,11 +81,17 @@ class Process
 	 */
 	public function request(array $data): void
 	{
+		$this->cancelTimer();
 		$this->stdin->write($data);
+		$this->timer = $this->loop->addTimer(60.0, function (): void {
+			$onError = $this->onError;
+			$onError(new \Exception('Child process timed out after 60 seconds.'));
+		});
 	}
 
 	public function quit(): void
 	{
+		$this->cancelTimer();
 		if (!$this->process->isRunning()) {
 			return;
 		}

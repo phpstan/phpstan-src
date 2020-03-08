6b18a98cd9e621435aca652cfcb2863cbda42653
Ondrej Mirtes <ondrej@mirtes.cz>
2020-03-08 21:23:00 +0100

    Intentionally break th result cache - part I.

--- src/Analyser/FileAnalyser.php
+++ src/Analyser/FileAnalyser.php
@@ -9,7 +9,6 @@ use PHPStan\Parser\Parser;
 use PHPStan\Rules\FileRuleError;
 use PHPStan\Rules\IdentifierRuleError;
 use PHPStan\Rules\LineRuleError;
-use PHPStan\Rules\MetadataRuleError;
 use PHPStan\Rules\Registry;
 use PHPStan\Rules\TipRuleError;
 use Roave\BetterReflection\Reflector\Exception\IdentifierNotFound;
@@ -97,7 +96,6 @@ class FileAnalyser
 							$traitFilePath = null;
 							$tip = null;
 							$identifier = null;
-							$metadata = [];
 							if ($scope->isInTrait()) {
 								$traitReflection = $scope->getTraitReflection();
 								if ($traitReflection->getFileName() !== false) {
@@ -130,10 +128,6 @@ class FileAnalyser
 								if ($ruleError instanceof IdentifierRuleError) {
 									$identifier = $ruleError->getIdentifier();
 								}
-
-								if ($ruleError instanceof MetadataRuleError) {
-									$metadata = $ruleError->getMetadata();
-								}
 							}
 							$fileErrors[] = new Error(
 								$message,
@@ -146,8 +140,7 @@ class FileAnalyser
 								false,
 								$nodeLine,
 								$nodeType,
-								$identifier,
-								$metadata
+								$identifier
 							);
 						}
 					}

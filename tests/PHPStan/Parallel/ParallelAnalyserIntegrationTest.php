09733b5f4b048d73f85f87c7b92bfde198ed05bb
Ondrej Mirtes <ondrej@mirtes.cz>
2020-03-15 16:07:25 +0100

    Fixed ParallelAnalyserIntegrationTest on Windows

--- tests/PHPStan/Parallel/ParallelAnalyserIntegrationTest.php
+++ tests/PHPStan/Parallel/ParallelAnalyserIntegrationTest.php
@@ -3,6 +3,7 @@
 namespace PHPStan\Parallel;
 
 use Nette\Utils\Json;
+use PHPStan\File\FileHelper;
 use PHPUnit\Framework\TestCase;
 
 class ParallelAnalyserIntegrationTest extends TestCase
@@ -22,13 +23,13 @@ class ParallelAnalyserIntegrationTest extends TestCase
 	 */
 	public function testRun(string $command): void
 	{
-		exec(sprintf('%s %s clear-result-cache --configuration %s', escapeshellarg(PHP_BINARY), escapeshellarg(__DIR__ . '/../../../bin/phpstan'), escapeshellarg(__DIR__ . '/parallel-analyser.neon')), $clearResultCacheOutputLines, $clearResultCacheExitCode);
+		exec(sprintf('%s %s clear-result-cache --configuration %s -q', escapeshellarg(PHP_BINARY), escapeshellarg(__DIR__ . '/../../../bin/phpstan'), escapeshellarg(__DIR__ . '/parallel-analyser.neon')), $clearResultCacheOutputLines, $clearResultCacheExitCode);
 		if ($clearResultCacheExitCode !== 0) {
 			throw new \PHPStan\ShouldNotHappenException('Could not clear result cache.');
 		}
 
 		exec(sprintf(
-			'%s %s %s -l 8 -c %s --error-format json %s',
+			'%s %s %s -l 8 -c %s --error-format json --no-progress %s',
 			escapeshellarg(PHP_BINARY),
 			escapeshellarg(__DIR__ . '/../../../bin/phpstan'),
 			$command,
@@ -41,13 +42,16 @@ class ParallelAnalyserIntegrationTest extends TestCase
 			]))
 		), $outputLines, $exitCode);
 		$output = implode("\n", $outputLines);
+
+		$fileHelper = new FileHelper(__DIR__);
+		$filePath = $fileHelper->normalizePath(__DIR__ . '/data/trait-definition.php');
 		$this->assertJsonStringEqualsJsonString(Json::encode([
 			'totals' => [
 				'errors' => 0,
 				'file_errors' => 3,
 			],
 			'files' => [
-				sprintf('%s/data/trait-definition.php (in context of class ParallelAnalyserIntegrationTest\\Bar)', __DIR__) => [
+				sprintf('%s (in context of class ParallelAnalyserIntegrationTest\\Bar)', $filePath) => [
 					'errors' => 1,
 					'messages' => [
 						[
@@ -57,7 +61,7 @@ class ParallelAnalyserIntegrationTest extends TestCase
 						],
 					],
 				],
-				sprintf('%s/data/trait-definition.php (in context of class ParallelAnalyserIntegrationTest\\Foo)', __DIR__) => [
+				sprintf('%s (in context of class ParallelAnalyserIntegrationTest\\Foo)', $filePath) => [
 					'errors' => 2,
 					'messages' => [
 						[

98f8b0e23a87e0f6d55bc286dcc0d076570bf17d
Ondrej Mirtes <ondrej@mirtes.cz>
2020-03-27 16:45:29 +0100

    Reproduce timecop issue

---
+++ tests/e2e/data/timecop.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+namespace TimecopRegressionTest;
+
+use DateTimeImmutable;
+
+class TimecopRegression
+{
+
+	/** @var DateTimeImmutable */
+	private $bar;
+
+	public function __construct(DateTimeImmutable $bar)
+	{
+		$this->bar = $bar;
+	}
+
+	public static function create(): self
+	{
+		return new self(new DateTimeImmutable());
+	}
+
+}

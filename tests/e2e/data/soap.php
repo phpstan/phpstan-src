640d950eb0719bcefbbbb30be9a6eb056eb27d8c
Ondrej Mirtes <ondrej@mirtes.cz>
2020-04-18 09:43:00 +0200

    Reproduce SOAP extension issues
    https://github.com/phpstan/phpstan/issues/618

---
+++ tests/e2e/data/soap.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace SoapTests;
+
+use SoapFault;
+
+class MySoapClient extends \SoapClient
+{
+
+}
+
+class MySoapClient2 extends \SoapClient
+{
+
+	/**
+	 * @param string|null $wsdl
+	 * @param mixed[]|null $options
+	 */
+	public function __construct($wsdl, array $options = null)
+	{
+		parent::__construct($wsdl, $options);
+	}
+}
+
+class MySoapClient3 extends \SoapClient
+{
+
+	/**
+	 * @param string|null $wsdl
+	 * @param mixed[]|null $options
+	 */
+	public function __construct($wsdl, array $options = null)
+	{
+		parent::SoapClient($wsdl, $options);
+	}
+}
+
+function () {
+	$soap = new MySoapClient('some.wsdl', ['soap_version' => SOAP_1_2]);
+	$soap = new MySoapClient2('some.wsdl', ['soap_version' => SOAP_1_2]);
+	$soap = new MySoapClient3('some.wsdl', ['soap_version' => SOAP_1_2]);
+};
+
+class MySoapHeader extends \SoapHeader
+{
+
+	public function __construct(string $username, string $password)
+	{
+		parent::__construct($username, $password);
+	}
+
+}
+
+function () {
+	$header = new MySoapHeader('user', 'passw0rd');
+};
+
+function (\SoapFault $fault) {
+	echo $fault->faultcode;
+	echo $fault->faultstring;
+};

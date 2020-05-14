9fd30bf6a92ba6a7e17ed9cdaafe21098d4b2115
Ondrej Mirtes <ondrej@mirtes.cz>
2020-05-14 20:41:45 +0200

    FileReader - assert is_file() before reading

--- src/File/FileReader.php
+++ src/File/FileReader.php
@@ -9,6 +9,9 @@ class FileReader
 
 	public static function read(string $fileName): string
 	{
+		if (!is_file($fileName)) {
+			throw new \PHPStan\File\CouldNotReadFileException($fileName);
+		}
 		$contents = @file_get_contents($fileName);
 		if ($contents === false) {
 			throw new \PHPStan\File\CouldNotReadFileException($fileName);

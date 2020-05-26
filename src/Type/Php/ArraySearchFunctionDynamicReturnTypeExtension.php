e9b941341065203a58d038101adea065ab9250fe
Vincent Langlet <vincentlanglet@hotmail.fr>
2020-05-26 20:14:16 +0200

    Remove Benevolent check

--- src/Type/Php/ArraySearchFunctionDynamicReturnTypeExtension.php
+++ src/Type/Php/ArraySearchFunctionDynamicReturnTypeExtension.php
@@ -7,7 +7,6 @@ use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Reflection\ParametersAcceptorSelector;
 use PHPStan\Type\ArrayType;
-use PHPStan\Type\BenevolentUnionType;
 use PHPStan\Type\Constant\ConstantArrayType;
 use PHPStan\Type\Constant\ConstantBooleanType;
 use PHPStan\Type\ConstantScalarType;
@@ -87,9 +86,6 @@ final class ArraySearchFunctionDynamicReturnTypeExtension implements DynamicFunc
 		}
 
 		$iterableKeyType = TypeCombinator::union(...$arrays)->getIterableKeyType();
-		if ($iterableKeyType instanceof BenevolentUnionType) {
-			$iterableKeyType = new MixedType();
-		}
 
 		return TypeCombinator::union(
 			$iterableKeyType,

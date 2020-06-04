093ae3fae5c3349b4798dd57d2cc4efd550b6473
Ondrej Mirtes <ondrej@mirtes.cz>
2020-06-04 23:21:36 +0200

    OverridingMethodRule - RuleErrorBuilder

--- src/Rules/Methods/OverridingMethodRule.php
+++ src/Rules/Methods/OverridingMethodRule.php
@@ -8,6 +8,7 @@ use PHPStan\Node\InClassMethodNode;
 use PHPStan\Reflection\MethodPrototypeReflection;
 use PHPStan\Reflection\Php\PhpMethodFromParserNodeReflection;
 use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
 
 /**
  * @implements Rule<InClassMethodNode>
@@ -37,35 +38,35 @@ class OverridingMethodRule implements Rule
 
 		$messages = [];
 		if ($prototype->isFinal()) {
-			$messages[] = sprintf(
+			$messages[] = RuleErrorBuilder::message(sprintf(
 				'Method %s::%s() overrides final method %s::%s().',
 				$method->getDeclaringClass()->getName(),
 				$method->getName(),
 				$prototype->getDeclaringClass()->getName(),
 				$prototype->getName()
-			);
+			))->nonIgnorable()->build();
 		}
 
 		if ($prototype->isPublic()) {
 			if (!$method->isPublic()) {
-				$messages[] = sprintf(
+				$messages[] = RuleErrorBuilder::message(sprintf(
 					'%s method %s::%s() overriding public method %s::%s() should also be public.',
 					$method->isPrivate() ? 'Private' : 'Protected',
 					$method->getDeclaringClass()->getName(),
 					$method->getName(),
 					$prototype->getDeclaringClass()->getName(),
 					$prototype->getName()
-				);
+				))->nonIgnorable()->build();
 			}
 		} else {
 			if ($method->isPrivate()) {
-				$messages[] = sprintf(
+				$messages[] = RuleErrorBuilder::message(sprintf(
 					'Private method %s::%s() overriding protected method %s::%s() should be protected or public.',
 					$method->getDeclaringClass()->getName(),
 					$method->getName(),
 					$prototype->getDeclaringClass()->getName(),
 					$prototype->getName()
-				);
+				))->nonIgnorable()->build();
 			}
 		}
 

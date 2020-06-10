f8512a7ba44a660589c67e7bf426f881fdb581d7
Ondrej Mirtes <ondrej@mirtes.cz>
2020-06-10 06:59:50 +0200

    Use runtime reflection for Generator

--- src/Reflection/ReflectionProvider/ClassBlacklistReflectionProvider.php
+++ src/Reflection/ReflectionProvider/ClassBlacklistReflectionProvider.php
@@ -37,7 +37,7 @@ class ClassBlacklistReflectionProvider implements ReflectionProvider
 
 	public function hasClass(string $className): bool
 	{
-		if ($this->phpStormStubsSourceStubber->hasClass($className)) {
+		if ($this->phpStormStubsSourceStubber->hasClass($className) && $className !== \Generator::class) {
 			// check that userland class isn't aliased to the same name as a class from stubs
 			if (!class_exists($className, false)) {
 				return false;

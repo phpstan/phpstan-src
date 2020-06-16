4d878d9eb88596a3bc77942e4566c0304b3f1131
Alain Schlesser <alain.schlesser@gmail.com>
2020-06-16 12:03:11 +0200

    Add test case to assert failure

--- tests/PHPStan/Analyser/data/bug-3468.php
+++ tests/PHPStan/Analyser/data/bug-3468.php
@@ -9,3 +9,11 @@ class NewInterval extends \DateInterval
 function (NewInterval $ni): void {
 	$ni->f = 0.1;
 };
+
+class NewDocument extends \DOMDocument
+{
+}
+
+function (NewDocument $nd): void {
+	$element = $nd->documentElement;
+};

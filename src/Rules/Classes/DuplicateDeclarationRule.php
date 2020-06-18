5e2ba1a2649681eaf2d878f77a7704b90c9754b3
Dylan K. Taylor <odigiman@gmail.com>
2020-06-18 13:38:17 +0100

    implemented DuplicateDeclarationRule, fixes phpstan/phpstan#3475

---
+++ src/Rules/Classes/DuplicateDeclarationRule.php
@@ -0,0 +1,79 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Classes;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Node\InClassNode;
+use PHPStan\Rules\RuleErrorBuilder;
+use function array_key_exists;
+use function sprintf;
+use function strtolower;
+
+/**
+ * @implements \PHPStan\Rules\Rule<\PHPStan\Node\InClassNode>
+ */
+class DuplicateDeclarationRule implements \PHPStan\Rules\Rule
+{
+
+	public function getNodeType(): string
+	{
+		return InClassNode::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		$classReflection = $scope->getClassReflection();
+		if ($classReflection === null) {
+			throw new \PHPStan\ShouldNotHappenException();
+		}
+
+		$errors = [];
+
+		$declaredClassConstants = [];
+		foreach ($node->getOriginalNode()->getConstants() as $constDecl) {
+			foreach ($constDecl->consts as $const) {
+				if (array_key_exists($const->name->name, $declaredClassConstants)) {
+					$errors[] = RuleErrorBuilder::message(sprintf(
+						'Cannot redeclare constant %s::%s.',
+						$classReflection->getDisplayName(),
+						$const->name->name
+					))->line($const->getLine())->nonIgnorable()->build();
+				} else {
+					$declaredClassConstants[$const->name->name] = true;
+				}
+			}
+		}
+
+		$declaredProperties = [];
+		foreach ($node->getOriginalNode()->getProperties() as $propertyDecl) {
+			foreach ($propertyDecl->props as $property) {
+				if (array_key_exists($property->name->name, $declaredProperties)) {
+					$errors[] = RuleErrorBuilder::message(sprintf(
+						'Cannot redeclare property %s::$%s.',
+						$classReflection->getDisplayName(),
+						$property->name->name
+					))->line($property->getLine())->nonIgnorable()->build();
+				} else {
+					$declaredProperties[$property->name->name] = true;
+				}
+			}
+		}
+
+		$declaredFunctions = [];
+		foreach ($node->getOriginalNode()->getMethods() as $method) {
+			if (array_key_exists(strtolower($method->name->name), $declaredFunctions)) {
+				$errors[] = RuleErrorBuilder::message(sprintf(
+					'Cannot redeclare method %s::%s().',
+					$classReflection->getDisplayName(),
+					$method->name->name
+				))->line($method->getStartLine())->nonIgnorable()->build();
+			} else {
+				$declaredFunctions[strtolower($method->name->name)] = true;
+			}
+		}
+
+		return $errors;
+	}
+
+}
---
+++ tests/PHPStan/Rules/Classes/DuplicateDeclarationRuleTest.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Classes;
+
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+
+/**
+ * @extends \PHPStan\Testing\RuleTestCase<\PHPStan\Rules\Classes\DuplicateDeclarationRule>
+ */
+class DuplicateDeclarationRuleTest extends RuleTestCase
+{
+
+	protected function getRule(): Rule
+	{
+		return new DuplicateDeclarationRule();
+	}
+
+	public function testDuplicateDeclarations(): void
+	{
+		if (!self::$useStaticReflectionProvider) {
+			$this->markTestSkipped('This test needs static reflection');
+		}
+
+		$this->analyse(
+			[
+				__DIR__ . '/data/duplicate-declarations.php',
+			],
+			[
+				[
+					'Cannot redeclare constant DuplicateDeclarations\Foo::CONST1.',
+					8,
+				],
+				[
+					'Cannot redeclare constant DuplicateDeclarations\Foo::CONST2.',
+					10,
+				],
+				[
+					'Cannot redeclare property DuplicateDeclarations\Foo::$prop1.',
+					17,
+				],
+				[
+					'Cannot redeclare property DuplicateDeclarations\Foo::$prop2.',
+					20,
+				],
+				[
+					'Cannot redeclare method DuplicateDeclarations\Foo::func1().',
+					27,
+				],
+				[
+					'Cannot redeclare method DuplicateDeclarations\Foo::Func1().',
+					35,
+				],
+			]
+		);
+	}
+
+}

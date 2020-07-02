c9fcb555689d0da30771f94589818dcf66387e11
Ben Harcourt <ben.harcourt@myvouchercodes.co.uk>
2020-07-02 16:20:14 +0000

    Correct handling of GD outputs with no `filename` arg.
    When the function is called as `imagepng($image)`, the output is returned,
    and thus the expected return type is `string|false`.
    The previous commits had the second parameter optional for both variants,
    resulting in PHPStan inferring a return of `string|bool`.
    
    Additionally, this parameter has been marked as optional for `imagexbm`,
    in line with the PHP documentation.

--- tests/PHPStan/Analyser/data/graphics-draw-return-types.php
+++ tests/PHPStan/Analyser/data/graphics-draw-return-types.php
@@ -5,6 +5,7 @@ use function PHPStan\Analyser\assertType;
 $image = imagecreatetruecolor(1, 1);
 $memoryHandle = fopen('php://memory', 'w');
 
+assertType('string|false', imagegd($image));
+assertType('string|false', imagegd($image, null));
 assertType('bool', imagegd($image, 'php://memory'));
 assertType('bool', imagegd($image, $memoryHandle));
-assertType('string|false', imagegd($image, null));

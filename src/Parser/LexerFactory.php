9f53ac586a0bfcae993d63b3e402c0cf445c4804
Ondrej Mirtes <ondrej@mirtes.cz>
2020-07-16 15:09:06 +0200

    Use non-emulative Lexer if the desired PHP version matches the runtime one

---
+++ src/Parser/LexerFactory.php
@@ -0,0 +1,27 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Parser;
+
+use PhpParser\Lexer;
+use PHPStan\Php\PhpVersion;
+
+class LexerFactory
+{
+
+	private PhpVersion $phpVersion;
+
+	public function __construct(PhpVersion $phpVersion)
+	{
+		$this->phpVersion = $phpVersion;
+	}
+
+	public function create(): Lexer
+	{
+		if ($this->phpVersion->getVersionId() === PHP_VERSION_ID) {
+			return new Lexer();
+		}
+
+		return new Lexer\Emulative();
+	}
+
+}

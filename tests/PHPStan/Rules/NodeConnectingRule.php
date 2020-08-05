e72ef39e1519b0e0c6d0cd89884d2e8a39d3c071
Ondrej Mirtes <ondrej@mirtes.cz>
2020-08-05 10:01:23 +0200

    Introduced NodeConnectingVisitor to be able to look around the current node in rules

---
+++ tests/PHPStan/Rules/NodeConnectingRule.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+
+/**
+ * @implements Rule<Node\Stmt\Echo_>
+ */
+class NodeConnectingRule implements Rule
+{
+
+	public function getNodeType(): string
+	{
+		return Node\Stmt\Echo_::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		return [
+			sprintf(
+				'Parent: %s, previous: %s, next: %s',
+				get_class($node->getAttribute('parent')),
+				get_class($node->getAttribute('previous')),
+				get_class($node->getAttribute('next')),
+			),
+		];
+	}
+
+}

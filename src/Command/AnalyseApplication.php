efbe009cf1b5d0f07e6dcb02fdd354b53abc06f6
Ondrej Mirtes <ondrej@mirtes.cz>
2020-08-30 16:23:30 +0200

    Refactored part of AnalyseApplication into AnalyserRunner

--- src/Command/AnalyseApplication.php
+++ src/Command/AnalyseApplication.php
@@ -2,26 +2,18 @@
 
 namespace PHPStan\Command;
 
-use PHPStan\Analyser\Analyser;
 use PHPStan\Analyser\AnalyserResult;
 use PHPStan\Analyser\IgnoredErrorHelper;
 use PHPStan\Analyser\ResultCache\ResultCacheManager;
-use PHPStan\Parallel\ParallelAnalyser;
-use PHPStan\Parallel\Scheduler;
 use PHPStan\PhpDoc\StubValidator;
 use Symfony\Component\Console\Input\InputInterface;
-use function file_exists;
 
 class AnalyseApplication
 {
 
-	private \PHPStan\Analyser\Analyser $analyser;
+	private AnalyserRunner $analyserRunner;
 
 	private \PHPStan\PhpDoc\StubValidator $stubValidator;
-
-	private ParallelAnalyser $parallelAnalyser;
-
-	private Scheduler $scheduler;
 
 	private \PHPStan\Analyser\ResultCache\ResultCacheManager $resultCacheManager;
 
@@ -32,20 +24,16 @@ class AnalyseApplication
 	private int $internalErrorsCountLimit;
 
 	public function __construct(
-		Analyser $analyser,
+		AnalyserRunner $analyserRunner,
 		StubValidator $stubValidator,
-		ParallelAnalyser $parallelAnalyser,
-		Scheduler $scheduler,
 		ResultCacheManager $resultCacheManager,
 		IgnoredErrorHelper $ignoredErrorHelper,
 		string $memoryLimitFile,
 		int $internalErrorsCountLimit
 	)
 	{
-		$this->analyser = $analyser;
+		$this->analyserRunner = $analyserRunner;
 		$this->stubValidator = $stubValidator;
-		$this->parallelAnalyser = $parallelAnalyser;
-		$this->scheduler = $scheduler;
 		$this->resultCacheManager = $resultCacheManager;
 		$this->ignoredErrorHelper = $ignoredErrorHelper;
 		$this->memoryLimitFile = $memoryLimitFile;
@@ -175,17 +163,13 @@ class AnalyseApplication
 			$progressStarted = false;
 			$fileOrder = 0;
 			$preFileCallback = null;
-			$postFileCallback = function (int $step) use ($errorOutput, &$progressStarted, $allAnalysedFilesCount, $filesCount, &$fileOrder, &$runningInParallel): void {
+			$postFileCallback = function (int $step) use ($errorOutput, &$progressStarted, $allAnalysedFilesCount, $filesCount, &$fileOrder): void {
 				if (!$progressStarted) {
 					$errorOutput->getStyle()->progressStart($allAnalysedFilesCount);
 					$errorOutput->getStyle()->progressAdvance($allAnalysedFilesCount - $filesCount);
 					$progressStarted = true;
 				}
 				$errorOutput->getStyle()->progressAdvance($step);
-
-				if ($runningInParallel) {
-					return;
-				}
 
 				if ($fileOrder >= 100) {
 					$this->updateMemoryLimitFile();
@@ -200,29 +184,7 @@ class AnalyseApplication
 			$postFileCallback = null;
 		}
 
-		// todo what about hyperthreading? should I divide CPU cores by 2?
-		$schedule = $this->scheduler->scheduleWork($this->getNumberOfCpuCores(), $files);
-		$mainScript = null;
-		if (isset($_SERVER['argv'][0]) && file_exists($_SERVER['argv'][0])) {
-			$mainScript = $_SERVER['argv'][0];
-		}
-
-		if (
-			!$debug
-			&& $mainScript !== null
-			&& $schedule->getNumberOfProcesses() > 1
-		) {
-			$runningInParallel = true;
-			$analyserResult = $this->parallelAnalyser->analyse($schedule, $mainScript, $postFileCallback, $projectConfigFile, $input);
-		} else {
-			$analyserResult = $this->analyser->analyse(
-				$files,
-				$preFileCallback,
-				$postFileCallback,
-				$debug,
-				$allAnalysedFiles
-			);
-		}
+		$analyserResult = $this->analyserRunner->runAnalyser($files, $allAnalysedFiles, $preFileCallback, $postFileCallback, $debug, $projectConfigFile, $input);
 
 		if (isset($progressStarted) && $progressStarted) {
 			$errorOutput->getStyle()->progressFinish();
@@ -236,41 +198,6 @@ class AnalyseApplication
 		$bytes = memory_get_peak_usage(true);
 		$megabytes = ceil($bytes / 1024 / 1024);
 		file_put_contents($this->memoryLimitFile, sprintf('%d MB', $megabytes));
-	}
-
-	private function getNumberOfCpuCores(): int
-	{
-		// from brianium/paratest
-		$cores = 2;
-		if (is_file('/proc/cpuinfo')) {
-			// Linux (and potentially Windows with linux sub systems)
-			$cpuinfo = @file_get_contents('/proc/cpuinfo');
-			if ($cpuinfo !== false) {
-				preg_match_all('/^processor/m', $cpuinfo, $matches);
-				return count($matches[0]);
-			}
-		}
-
-		if (\DIRECTORY_SEPARATOR === '\\') {
-			// Windows
-			$process = @popen('wmic cpu get NumberOfLogicalProcessors', 'rb');
-			if ($process !== false) {
-				fgets($process);
-				$cores = (int) fgets($process);
-				pclose($process);
-			}
-
-			return $cores;
-		}
-
-		$process = @\popen('sysctl -n hw.ncpu', 'rb');
-		if ($process !== false) {
-			// *nix (Linux, BSD and Mac)
-			$cores = (int) fgets($process);
-			pclose($process);
-		}
-
-		return $cores;
 	}
 
 }
---
+++ src/Command/AnalyserRunner.php
@@ -0,0 +1,115 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Command;
+
+use PHPStan\Analyser\Analyser;
+use PHPStan\Analyser\AnalyserResult;
+use PHPStan\Parallel\ParallelAnalyser;
+use PHPStan\Parallel\Scheduler;
+use Symfony\Component\Console\Input\InputInterface;
+
+class AnalyserRunner
+{
+
+	private Scheduler $scheduler;
+
+	private Analyser $analyser;
+
+	private ParallelAnalyser $parallelAnalyser;
+
+	public function __construct(
+		Scheduler $scheduler,
+		Analyser $analyser,
+		ParallelAnalyser $parallelAnalyser
+	)
+	{
+		$this->scheduler = $scheduler;
+		$this->analyser = $analyser;
+		$this->parallelAnalyser = $parallelAnalyser;
+	}
+
+	/**
+	 * @param string[] $files
+	 * @param string[] $allAnalysedFiles
+	 * @param \Closure|null $preFileCallback
+	 * @param \Closure|null $postFileCallback
+	 * @param bool $debug
+	 * @param string|null $projectConfigFile
+	 * @param InputInterface $input
+	 * @return AnalyserResult
+	 * @throws \Throwable
+	 */
+	public function runAnalyser(
+		array $files,
+		array $allAnalysedFiles,
+		?\Closure $preFileCallback,
+		?\Closure $postFileCallback,
+		bool $debug,
+		?string $projectConfigFile,
+		InputInterface $input
+	): AnalyserResult
+	{
+		$filesCount = count($files);
+		if ($filesCount === 0) {
+			return new AnalyserResult([], [], [], false);
+		}
+
+		$schedule = $this->scheduler->scheduleWork($this->getNumberOfCpuCores(), $files);
+		$mainScript = null;
+		if (isset($_SERVER['argv'][0]) && file_exists($_SERVER['argv'][0])) {
+			$mainScript = $_SERVER['argv'][0];
+		}
+
+		if (
+			!$debug
+			&& $mainScript !== null
+			&& $schedule->getNumberOfProcesses() > 1
+		) {
+			return $this->parallelAnalyser->analyse($schedule, $mainScript, $postFileCallback, $projectConfigFile, $input);
+		}
+
+		return $this->analyser->analyse(
+			$files,
+			$preFileCallback,
+			$postFileCallback,
+			$debug,
+			$allAnalysedFiles
+		);
+	}
+
+	private function getNumberOfCpuCores(): int
+	{
+		// from brianium/paratest
+		$cores = 2;
+		if (is_file('/proc/cpuinfo')) {
+			// Linux (and potentially Windows with linux sub systems)
+			$cpuinfo = @file_get_contents('/proc/cpuinfo');
+			if ($cpuinfo !== false) {
+				preg_match_all('/^processor/m', $cpuinfo, $matches);
+				return count($matches[0]);
+			}
+		}
+
+		if (\DIRECTORY_SEPARATOR === '\\') {
+			// Windows
+			$process = @popen('wmic cpu get NumberOfLogicalProcessors', 'rb');
+			if ($process !== false) {
+				fgets($process);
+				$cores = (int) fgets($process);
+				pclose($process);
+			}
+
+			return $cores;
+		}
+
+		$process = @\popen('sysctl -n hw.ncpu', 'rb');
+		if ($process !== false) {
+			// *nix (Linux, BSD and Mac)
+			$cores = (int) fgets($process);
+			pclose($process);
+		}
+
+		return $cores;
+	}
+
+}

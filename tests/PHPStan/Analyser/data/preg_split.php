1be28f37250ce8a74fa6338be8ae3abe9b487ea1
Michal Špaček <mail@michalspacek.cz>
2020-09-20 03:45:29 +0200

    preg_split sometimes returns array of arrays
    When PREG_SPLIT_OFFSET_CAPTURE is given, "this changes the return value in an array where every element is an array consisting of the matched string at offset 0 and its string offset into subject at offset 1."

---
+++ tests/PHPStan/Analyser/data/preg_split.php
@@ -0,0 +1,8 @@
+<?php
+
+use function PHPStan\Analyser\assertType;
+
+assertType('array<int, string>|false', preg_split('/-/', '1-2-3'));
+assertType('array<int, string>|false', preg_split('/-/', '1-2-3', -1, PREG_SPLIT_NO_EMPTY));
+assertType('array<int, array(string, int)>|false', preg_split('/-/', '1-2-3', -1, PREG_SPLIT_OFFSET_CAPTURE));
+assertType('array<int, array(string, int)>|false', preg_split('/-/', '1-2-3', -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_OFFSET_CAPTURE));

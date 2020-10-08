f32a30fa7d11c54d10ecaa13a73daa83e86db4da
Jean-Luc Herren <jlh@gmx.ch>
2020-10-08 21:52:05 +0200

    Add test for fixed bug #2611
    Fixes #2611

---
+++ tests/PHPStan/Analyser/data/bug-2611.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Bug2611;
+
+use function PHPStan\Analyser\assertType;
+
+/**
+ * @param \Traversable|array $collection
+ * @return array
+ */
+function flatten($collection)
+{
+	$stack = [$collection];
+	$result = [];
+
+	while (!empty($stack)) {
+		$item = \array_shift($stack);
+		assertType('mixed', $item);
+
+		if (\is_array($item) || $item instanceof \Traversable) {
+			foreach ($item as $element) {
+				\array_unshift($stack, $element);
+			}
+		} else {
+			\array_unshift($result, $item);
+		}
+	}
+
+	return $result;
+}

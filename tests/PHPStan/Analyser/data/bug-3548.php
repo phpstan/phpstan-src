47214d5b1a2739ce5761051e952fd9cdf84d0021
Jean-Luc Herren <jlh@gmx.ch>
2020-10-08 22:14:41 +0200

    Add test for fixed bug #3548
    Fixes #3548

---
+++ tests/PHPStan/Analyser/data/bug-3548.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Bug3548;
+
+use function PHPStan\Analyser\assertType;
+
+class HelloWorld
+{
+	/**
+	 * @param int[] $arr
+	 */
+	public function shift(array $arr): int {
+		if (count($arr) === 0) {
+			throw new \Exception("oops");
+		}
+		$name = array_shift($arr);
+		assertType('int', $name);
+		return $name;
+	}
+}

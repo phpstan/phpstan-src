9b5a09d2d4b11c8c1055b77c69d1cb2089a96b11
Viktor Szépe <viktor@szepe.net>
2020-11-03 20:02:30 +0000

    Add debugger function

---
+++ src/Rules/Functions/DebuggerFunctionRule.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Functions;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\FuncCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Rules\RuleErrorBuilder;
+
+/**
+ * @implements \PHPStan\Rules\Rule<\PhpParser\Node\Expr\FuncCall>
+ */
+class DebuggerFunctionRule implements \PHPStan\Rules\Rule
+{
+
+	public function getNodeType(): string
+	{
+		return FuncCall::class
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		if ($node->name !== 'PHPStan\dumpType') {
+			return [];
+		}
+
+		return [$scope->getType($node->args[0]->value)->describe(VerbosityLevel::precise())];
+	}
+}

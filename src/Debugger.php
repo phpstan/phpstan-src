b995ac6061286b76f2930b8855acb32fe2231edf
Viktor Szépe <viktor@szepe.net>
2020-11-03 20:37:11 +0100

    Add debugger function
    Fixes https://github.com/phpstan/phpstan/issues/3765

---
+++ src/Debugger.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan;
+
+class Debugger
+{
+    /**
+     * @var mixed $thing
+     */
+    public function logType($thing)
+    {
+        //
+    }
+}

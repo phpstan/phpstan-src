031621d0d06784a9c590d2a46cb5c55c2fad3894
Ondrej Mirtes <ondrej@mirtes.cz>
2020-11-21 09:11:45 +0100

    Attempt to fix PHP 8.0

--- stubs/runtime/ReflectionUnionType.php
+++ stubs/runtime/ReflectionUnionType.php
@@ -1,16 +1,18 @@
 <?php
 
-if (class_exists('ReflectionUnionType', false)) {
-	return;
-}
-
-class ReflectionUnionType extends ReflectionType
-{
-
-	/** @return ReflectionType[] */
-	public function getTypes()
-	{
-		return [];
+if (\PHP_VERSION_ID < 80000) {
+	if (class_exists('ReflectionUnionType', false)) {
+		return;
 	}
 
+	class ReflectionUnionType extends ReflectionType
+	{
+
+		/** @return ReflectionType[] */
+		public function getTypes()
+		{
+			return [];
+		}
+
+	}
 }

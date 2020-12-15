43e53990703f7c0b0d2ff0249570ac7ed1beed97
Ondrej Mirtes <ondrej@mirtes.cz>
2020-12-15 14:36:57 +0100

    Update functionMap with functions that return false on both PHP 7 and PHP 8

--- tests/PHPStan/Analyser/data/bug-3336.php
+++ tests/PHPStan/Analyser/data/bug-3336.php
@@ -5,6 +5,6 @@ namespace Bug3336;
 function (array $arr, string $str, $mixed): void {
 	\PHPStan\Analyser\assertType('array<int, string>', mb_convert_encoding($arr));
 	\PHPStan\Analyser\assertType('string', mb_convert_encoding($str));
-	\PHPStan\Analyser\assertType('array<int, string>|string', mb_convert_encoding($mixed));
-	\PHPStan\Analyser\assertType('array<int, string>|string', mb_convert_encoding());
+	\PHPStan\Analyser\assertType('array<int, string>|string|false', mb_convert_encoding($mixed));
+	\PHPStan\Analyser\assertType('array<int, string>|string|false', mb_convert_encoding());
 };

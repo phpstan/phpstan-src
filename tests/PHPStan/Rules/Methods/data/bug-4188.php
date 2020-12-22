0af0399a9c877b3a40a8ad89f62b623c87538a90
Anatoly Pashin <anatoly.pashin@gmail.com>
2020-12-22 19:38:53 +1000

    Add another test

---
+++ tests/PHPStan/Rules/Methods/data/bug-4188.php
@@ -0,0 +1,32 @@
+<?php // lint >= 7.4
+
+namespace Bug4188;
+
+interface A {}
+interface B {}
+
+class Test
+{
+	/** @param array<A|B> $data */
+	public function set(array $data): void
+	{
+		$this->onlyB(array_filter(
+			$data,
+			function ($param): bool {
+				return $param instanceof B;
+			},
+		));
+	}
+
+	/** @param array<A|B> $data */
+	public function setShort(array $data): void
+	{
+		$this->onlyB(array_filter(
+			$data,
+			fn($param): bool => $param instanceof B,
+		));
+	}
+
+	/** @param B[] $data */
+	public function onlyB(array $data): void {}
+}

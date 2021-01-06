930560e0adafb267f5d286dd5c11bd7fdbed8cc5
Vincent Langlet <vincentlanglet@hotmail.fr>
2021-01-06 14:53:39 +0100

    Add array_rand return type extension

---
+++ src/Type/Php/ArrayRandFunctionReturnTypeExtension.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Type\Php;
+
+use PhpParser\Node\Expr\FuncCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\FunctionReflection;
+use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\Type\ArrayType;
+use PHPStan\Type\Constant\ConstantIntegerType;
+use PHPStan\Type\IntegerType;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
+use PHPStan\Type\UnionType;
+
+class ArrayRandFunctionReturnTypeExtension implements \PHPStan\Type\DynamicFunctionReturnTypeExtension
+{
+
+	public function isFunctionSupported(FunctionReflection $functionReflection): bool
+	{
+		return $functionReflection->getName() === 'array_rand';
+	}
+
+	public function getTypeFromFunctionCall(FunctionReflection $functionReflection, FuncCall $functionCall, Scope $scope): Type
+	{
+		$argsCount = count($functionCall->args);
+		if (count($functionCall->args) < 1) {
+			return ParametersAcceptorSelector::selectSingle($functionReflection->getVariants())->getReturnType();
+		}
+
+		$firstArgType = $scope->getType($functionCall->args[0]->value);
+		$isInteger = (new IntegerType())->isSuperTypeOf($firstArgType->getIterableKeyType());
+		$isString = (new StringType())->isSuperTypeOf($firstArgType->getIterableKeyType());
+
+		if ($isInteger->yes()) {
+			$valueType = new IntegerType();
+		} elseif ($isString->yes()) {
+			$valueType = new StringType();
+		} else {
+			$valueType = new UnionType([new IntegerType(), new StringType()]);
+		}
+
+		if ($argsCount < 2) {
+			return $valueType;
+		}
+
+		$secondArgType = $scope->getType($functionCall->args[1]->value);
+
+		if ($secondArgType instanceof ConstantIntegerType) {
+			if ($secondArgType->getValue() === 1) {
+				return $valueType;
+			}
+
+			if ($secondArgType->getValue() >= 2) {
+				return new ArrayType(new IntegerType(), $valueType);
+			}
+		}
+
+		return TypeCombinator::union($valueType, new ArrayType(new IntegerType(), $valueType));
+	}
+
+}

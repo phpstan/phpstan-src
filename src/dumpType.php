a475826713ac07d809b7c125cad20cf2c4a2d0d2
Michael Voříšek <mvorisek@mvorisek.cz>
2021-01-26 11:59:53 +0100

    \PHPStan\dumpType() function can be used only with running PHPStan

--- src/dumpType.php
+++ src/dumpType.php
@@ -7,4 +7,5 @@ namespace PHPStan;
  */
 function dumpType($value): void // phpcs:ignore Squiz.Functions.GlobalFunction.Found
 {
+	throw new \Exception('\PHPStan\dumpType() function can be used only with running PHPStan');
 }

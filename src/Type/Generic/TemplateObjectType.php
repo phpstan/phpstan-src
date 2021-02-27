d0e6683e2fa7eab044da22c18c5075ec9c5c9888
Ondrej Mirtes <ondrej@mirtes.cz>
2021-02-27 10:28:27 +0100

    Simplification

--- src/Type/Generic/TemplateObjectType.php
+++ src/Type/Generic/TemplateObjectType.php
@@ -2,9 +2,7 @@
 
 namespace PHPStan\Type\Generic;
 
-use PHPStan\TrinaryLogic;
 use PHPStan\Type\ObjectType;
-use PHPStan\Type\ObjectWithoutClassType;
 use PHPStan\Type\Traits\UndecidedComparisonCompoundTypeTrait;
 use PHPStan\Type\Type;
 
@@ -12,9 +10,7 @@ final class TemplateObjectType extends ObjectType implements TemplateType
 {
 
 	use UndecidedComparisonCompoundTypeTrait;
-	use TemplateTypeTrait {
-		isSubTypeOf as isSubTypeOfTrait;
-	}
+	use TemplateTypeTrait;
 
 	public function __construct(
 		TemplateTypeScope $scope,
@@ -31,15 +27,6 @@ final class TemplateObjectType extends ObjectType implements TemplateType
 		$this->variance = $templateTypeVariance;
 		$this->name = $name;
 		$this->bound = new ObjectType($class);
-	}
-
-	public function isSubTypeOf(Type $type): TrinaryLogic
-	{
-		if ($type instanceof ObjectWithoutClassType) {
-			return TrinaryLogic::createYes();
-		}
-
-		return $this->isSubTypeOfTrait($type);
 	}
 
 	public function toArgument(): TemplateType

33af0a9647ec1b25e160cd5bbb0fc27372dc6050
Ondrej Mirtes <ondrej@mirtes.cz>
2021-03-06 15:16:50 +0100

    Fix

--- build/ignore-by-php-version.neon.php
+++ build/ignore-by-php-version.neon.php
@@ -1,14 +1,16 @@
 <?php declare(strict_types = 1);
 
-use Nette\Neon\Neon;
+use PHPStan\DependencyInjection\NeonAdapter;
+
+$adapter = new NeonAdapter();
 
 $config = [];
 if (PHP_VERSION_ID >= 80000) {
-	$config = array_merge($config, Neon::decode(file_get_contents(__DIR__ . '/baseline-8.0.neon')));
+	$config = array_merge($config, $adapter->load(__DIR__ . '/baseline-8.0.neon'));
 }
 
 if (PHP_VERSION_ID >= 70400) {
-	$config = array_merge($config, Neon::decode(file_get_contents(__DIR__ . '/ignore-gte-php7.4-errors.neon')));
+	$config = array_merge($config, $adapter->load(__DIR__ . '/ignore-gte-php7.4-errors.neon'));
 }
 
 return $config;

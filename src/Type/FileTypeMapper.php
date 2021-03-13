b40eb72d9151a7f0ce3ad8ac3947132b837392ca
Alex Deleyn <alex.deleyn@teamleader.eu>
2021-03-13 18:48:04 +0100

    describe the version change

--- src/Type/FileTypeMapper.php
+++ src/Type/FileTypeMapper.php
@@ -216,7 +216,7 @@ class FileTypeMapper
 	private function getResolvedPhpDocMap(string $fileName): array
 	{
 		if (!isset($this->memoryCache[$fileName])) {
-			$cacheKey = sprintf('%s-phpdocstring-v9-generic-traits', $fileName);
+			$cacheKey = sprintf('%s-phpdocstring-v8-alias-collision', $fileName);
 			$variableCacheKey = implode(',', array_map(static function (array $file): string {
 				return sprintf('%s-%d', $file['filename'], $file['modifiedTime']);
 			}, $this->getCachedDependentFilesWithTimestamps($fileName)));

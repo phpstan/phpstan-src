ae9baaa2baed6c8fc14b52d34e1463fff4b066e5
Michael Voříšek <mvorisek@mvorisek.cz>
2021-05-18 15:52:55 +0200

    fix test (use really two backslashes)

--- tests/PHPStan/Rules/Classes/data/bug-5002.php
+++ tests/PHPStan/Rules/Classes/data/bug-5002.php
@@ -8,11 +8,21 @@ class Foo
 }
 
 function (): void {
+	$name = 'Test\Foo';
+	new $name();
+};
+
+function (): void {
 	$name = '\Test\Foo';
 	new $name();
 };
 
 function (): void {
-	$name = '\\Test\Foo';
+	$name = '\\\\Test\Foo';
+	new $name();
+};
+
+function (): void {
+	$name = 'Test\\\\Foo';
 	new $name();
 };

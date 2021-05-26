3b95855d31601aa7c8aa521cc18264c3c00c2e10
Ondrej Mirtes <ondrej@mirtes.cz>
2021-05-26 10:09:16 +0200

    BC promise - disallow using PHPStan namespace in 3rd party package

---
+++ src/Rules/Api/PhpStanNamespaceIn3rdPartyPackageRule.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Api;
+
+use Nette\Utils\Json;
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\File\FileReader;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+
+/**
+ * @implements Rule<Node\Stmt\Namespace_>
+ */
+class PhpStanNamespaceIn3rdPartyPackageRule implements Rule
+{
+
+	private ApiRuleHelper $apiRuleHelper;
+
+	public function __construct(ApiRuleHelper $apiRuleHelper)
+	{
+		$this->apiRuleHelper = $apiRuleHelper;
+	}
+
+	public function getNodeType(): string
+	{
+		return Node\Stmt\Namespace_::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		$namespace = null;
+		if ($node->name !== null) {
+			$namespace = $node->name->toString();
+		}
+		if (!$this->apiRuleHelper->isInPhpStanNamespace($namespace)) {
+			return [];
+		}
+
+		$composerJson = $this->findComposerJsonContents(dirname($scope->getFile()));
+		if ($composerJson === null) {
+			return [];
+		}
+
+		$packageName = $composerJson['name'] ?? null;
+		if ($packageName !== null && strpos($packageName, 'phpstan/') === 0) {
+			return [];
+		}
+
+		return [
+			RuleErrorBuilder::message('Declaring PHPStan namespace is not allowed in 3rd party packages.')
+				->tip("See:\n   https://phpstan.org/developing-extensions/backward-compatibility-promise")
+				->build(),
+		];
+	}
+
+	/**
+	 * @return mixed[]|null
+	 */
+	private function findComposerJsonContents(string $fromDirectory): ?array
+	{
+		if (!is_dir($fromDirectory)) {
+			return null;
+		}
+
+		$composerJsonPath = $fromDirectory . '/composer.json';
+		if (!is_file($composerJsonPath)) {
+			return $this->findComposerJsonContents(dirname($fromDirectory));
+		}
+
+		try {
+			return Json::decode(FileReader::read($composerJsonPath), Json::FORCE_ARRAY);
+		} catch (\Nette\Utils\JsonException $e) {
+			return null;
+		} catch (\PHPStan\File\CouldNotReadFileException $e) {
+			return null;
+		}
+	}
+
+}
---
+++ tests/PHPStan/Rules/Api/PhpStanNamespaceIn3rdPartyPackageRuleTest.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Api;
+
+use Nette\Utils\Json;
+use PHPStan\File\FileWriter;
+use PHPStan\Testing\RuleTestCase;
+
+/**
+ * @extends RuleTestCase<PhpStanNamespaceIn3rdPartyPackageRule>
+ */
+class PhpStanNamespaceIn3rdPartyPackageRuleTest extends RuleTestCase
+{
+
+	protected function getRule(): \PHPStan\Rules\Rule
+	{
+		return new PhpStanNamespaceIn3rdPartyPackageRule(new ApiRuleHelper());
+	}
+
+	protected function tearDown(): void
+	{
+		@unlink(__DIR__ . '/composer.json');
+	}
+
+	public function testRulePhpStanNamespaceInPhpStanPackage(): void
+	{
+		$this->createComposerJson('phpstan/foo');
+		$this->analyse([__DIR__ . '/data/phpstan-namespace.php'], []);
+	}
+
+	public function testRulePhpStanNamespaceIn3rdPartyPackage(): void
+	{
+		$this->createComposerJson('my/foo');
+		$this->analyse([__DIR__ . '/data/phpstan-namespace.php'], [
+			[
+				'Declaring PHPStan namespace is not allowed in 3rd party packages.',
+				3,
+				"See:\n   https://phpstan.org/developing-extensions/backward-compatibility-promise",
+			],
+		]);
+	}
+
+	public function testRuleCustomNamespaceInPhpStanPackage(): void
+	{
+		$this->createComposerJson('phpstan/foo');
+		$this->analyse([__DIR__ . '/data/custom-namespace.php'], []);
+	}
+
+	public function testRuleCustomNamespaceIn3rdPartyPackage(): void
+	{
+		$this->createComposerJson('my/foo');
+		$this->analyse([__DIR__ . '/data/custom-namespace.php'], []);
+	}
+
+	private function createComposerJson(string $packageName): void
+	{
+		FileWriter::write(__DIR__ . '/composer.json', Json::encode(['name' => $packageName], Json::PRETTY));
+	}
+
+}

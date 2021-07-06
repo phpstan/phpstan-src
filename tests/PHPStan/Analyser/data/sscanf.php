deabae1a15aa706d110cee669914866cdb8b64e2
fluffycondor <62219548+fluffycondor@users.noreply.github.com>
2021-07-06 15:06:01 +0300

    Fix a typo

--- tests/PHPStan/Analyser/data/sscanf.php
+++ tests/PHPStan/Analyser/data/sscanf.php
@@ -2,5 +2,5 @@
 
 use function PHPStan\Testing\assertType;
 
-assertType('int|null', sccanf('20-20', '%d-%d', $first, $second));
-assertType('array|null', sccanf('20-20', '%d-%d'));
+assertType('int|null', sscanf('20-20', '%d-%d', $first, $second));
+assertType('array|null', sscanf('20-20', '%d-%d'));

62b202ed4ca875d29422b385867396c10e4082a5
Markus Staab <markus.staab@redaxo.de>
2021-07-15 21:03:01 +0200

    Added url-functions in NonEmptyStringFunctionsReturnTypeExtension

--- src/Type/Php/NonEmptyStringFunctionsReturnTypeExtension.php
+++ src/Type/Php/NonEmptyStringFunctionsReturnTypeExtension.php
@@ -25,6 +25,10 @@ class NonEmptyStringFunctionsReturnTypeExtension implements DynamicFunctionRetur
 			'ucfirst',
 			'ucwords',
 			'htmlspecialchars',
+			'urlencode',
+			'urldecode',
+			'rawurlencode',
+			'rawurldecode',
 			'vsprintf',
 		], true);
 	}

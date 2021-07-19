6553b7c8b817be9205dedd2080c14feac00701eb
Markus Staab <m.staab@complex-it.de>
2021-07-19 10:08:45 +0200

    fix CS

--- src/Type/Php/SubstrDynamicReturnTypeExtension.php
+++ src/Type/Php/SubstrDynamicReturnTypeExtension.php
@@ -7,12 +7,12 @@ use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Reflection\ParametersAcceptorSelector;
 use PHPStan\Type\Accessory\AccessoryNonEmptyStringType;
+use PHPStan\Type\Constant\ConstantIntegerType;
 use PHPStan\Type\DynamicFunctionReturnTypeExtension;
+use PHPStan\Type\IntegerRangeType;
 use PHPStan\Type\IntersectionType;
 use PHPStan\Type\StringType;
 use PHPStan\Type\Type;
-use PHPStan\Type\IntegerRangeType;
-use PHPStan\Type\Constant\ConstantIntegerType;
 
 class SubstrDynamicReturnTypeExtension implements DynamicFunctionReturnTypeExtension
 {
@@ -33,41 +33,39 @@ class SubstrDynamicReturnTypeExtension implements DynamicFunctionReturnTypeExten
 			return ParametersAcceptorSelector::selectSingle($functionReflection->getVariants())->getReturnType();
 		}
 
-    if (count($args) >= 2) {
-      $string = $scope->getType($args[0]->value);
-      $offset = $scope->getType($args[1]->value);
-		
-		$negativeOffset = $this->getIntValue($offset) < 0;
-		$zeroOffset = $this->getIntValue($offset) === 0;
-		$positiveLength = false;
-		
-		if (count($args) === 3) {
-			$length = $scope->getType($args[2]->value);
-			$positiveLength = $this->getIntValue($length) > 0;
+		if (count($args) >= 2) {
+			$string = $scope->getType($args[0]->value);
+			$offset = $scope->getType($args[1]->value);
+
+			$negativeOffset = $this->getIntValue($offset) < 0;
+			$zeroOffset = $this->getIntValue($offset) === 0;
+			$positiveLength = false;
+
+			if (count($args) === 3) {
+				$length = $scope->getType($args[2]->value);
+				$positiveLength = $this->getIntValue($length) > 0;
+			}
+
+			if ($string->isNonEmptyString()->yes() && ($negativeOffset || $zeroOffset && $positiveLength)) {
+				  return new IntersectionType([
+					  new StringType(),
+					  new AccessoryNonEmptyStringType(),
+				  ]);
+			}
 		}
-      
-	  	if ($string->isNonEmptyString()->yes() && ($negativeOffset || $zeroOffset && $positiveLength)) {
-			  return new IntersectionType([
-		  		new StringType(),
-			  	new AccessoryNonEmptyStringType(),
-		  	]);
-	  	}
-	}
-		
 
 		return new StringType();
 	}
 
-private function getIntValue(Type $type): ?int {
-				if ($type instanceof IntegerRangeType) {
-					return $type->getMin();
-				}
-				if ($type instanceof ConstantIntegerType) {
-					return $type->getValue();
-				}
+	private function getIntValue(Type $type): ?int
+	{
+		if ($type instanceof IntegerRangeType) {
+			return $type->getMin();
+		}
+		if ($type instanceof ConstantIntegerType) {
+			return $type->getValue();
+		}
 				return null;
-			
-
-}
+	}
 
 }

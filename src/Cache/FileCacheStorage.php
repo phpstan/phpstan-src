29e1400fed31a341cadf422725a9691b39219637
Markus Staab <markus.staab@redaxo.de>
2021-07-26 16:59:29 +0200

    fix CS

--- src/Cache/FileCacheStorage.php
+++ src/Cache/FileCacheStorage.php
@@ -41,7 +41,7 @@ class FileCacheStorage implements CacheStorage
 	public function load(string $key, string $variableKey)
 	{
 		[,, $filePath] = $this->getFilePaths($key);
-		
+
 		return (static function () use ($variableKey, $filePath) {
 			if (!is_file($filePath)) {
 				return null;

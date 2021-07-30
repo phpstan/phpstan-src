4b95eee44bc22f5ffb2966557c398c128e85d2be
Lctrs <jerome@prmntr.me>
2021-07-31 01:06:03 +0200

    Cover non-empty-array in array_map()

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -265,6 +265,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/offset-value-after-assign.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-2112.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/array-flip.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/array-map.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/array-map-closure.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/array-sum.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-4573.php');
---
+++ tests/PHPStan/Analyser/data/array-map.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace ArrayMap;
+
+use function array_map;
+use function PHPStan\Testing\assertType;
+
+/**
+ * @param array<int, string> $array
+ */
+function foo(array $array): void {
+	$mapped = array_map(
+		static function(string $string): string {
+			return (string) $string;
+		},
+		$array
+	);
+
+	assertType('array<int, string>', $mapped);
+}
+
+/**
+ * @param non-empty-array<int, string> $array
+ */
+function foo2(array $array): void {
+	$mapped = array_map(
+		static function(string $string): string {
+			return (string) $string;
+		},
+		$array
+	);
+
+	assertType('array<int, string>&nonEmpty', $mapped);
+}
+
+/**
+ * @param list<string> $array
+ */
+function foo3(array $array): void {
+	$mapped = array_map(
+		static function(string $string): string {
+			return (string) $string;
+		},
+		$array
+	);
+
+	assertType('array<int, string>', $mapped);
+}
+
+/**
+ * @param non-empty-list<string> $array
+ */
+function foo4(array $array): void {
+	$mapped = array_map(
+		static function(string $string): string {
+			return (string) $string;
+		},
+		$array
+	);
+
+	assertType('array<int, string>&nonEmpty', $mapped);
+}

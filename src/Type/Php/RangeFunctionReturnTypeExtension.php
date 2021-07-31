193811d2fccc4293736dc0e740ab165c136ab3e9
Markus Staab <markus.staab@redaxo.de>
2021-07-31 13:44:20 +0200

    fix expectations

--- src/Type/Php/RangeFunctionReturnTypeExtension.php
+++ src/Type/Php/RangeFunctionReturnTypeExtension.php
@@ -45,7 +45,10 @@ class RangeFunctionReturnTypeExtension implements \PHPStan\Type\DynamicFunctionR
 		$stepType = count($functionCall->args) >= 3 ? $scope->getType($functionCall->args[2]->value) : new ConstantIntegerType(1);
 
 		if ($startType instanceof ConstantIntegerType && $endType instanceof ConstantIntegerType && $stepType instanceof ConstantIntegerType && $stepType->getValue() === 1) {
-			return IntegerRangeType::fromInterval($stepType->getValue(), $endType->getValue());
+			return new ArrayType(
+				new IntegerType(),
+				IntegerRangeType::fromInterval($stepType->getValue(), $endType->getValue());
+			);
 		}
 
 		$constantReturnTypes = [];
--- tests/PHPStan/Analyser/data/bug-4207.php
+++ tests/PHPStan/Analyser/data/bug-4207.php
@@ -5,5 +5,5 @@ namespace Bug4207;
 use function PHPStan\Testing\assertType;
 
 function (): void {
-	assertType('int<1, 10000>', range(1, 10000));
+	assertType('array<int, int<1, 10000>>', range(1, 10000));
 };

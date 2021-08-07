43cbc9fc8bd8bfae6541a17f1436a5a72e6e6b59
Markus Staab <markus.staab@redaxo.de>
2021-08-07 21:08:47 +0200

    cover more cases

--- tests/PHPStan/Analyser/data/substr-php8.php
+++ tests/PHPStan/Analyser/data/substr-php8.php
@@ -10,10 +10,15 @@ class Substr
 	{
 		assertType('string', substr(null, true));
 		assertType('string', substr(true, true));
-		assertType('string', substr([], true));
+		assertType('*NEVER*', substr([], true));
+		assertType('*NEVER*', substr(new \stdClass(), true));
 		assertType('string', substr('a', 2));
 		assertType('string', substr($i, 2));
 		assertType('string', substr($s, $i));
 		assertType('string', substr(3.1, $i));
+		assertType('"lo"', substr("hallo", 3));
+		assertType('"l"', substr("hallo", 3, 1));
+		assertType('string', substr("hallo", 10));
+		assertType('string', substr("hallo", 10, 1));
 	}
 }
--- tests/PHPStan/Analyser/data/substr.php
+++ tests/PHPStan/Analyser/data/substr.php
@@ -10,10 +10,15 @@ class Substr
 	{
 		assertType('false', substr(null, true));
 		assertType('false', substr(true, true));
-		assertType('false', substr([], true));
+		assertType('null', substr([], true));
+		assertType('null', substr(new \stdClass(), true));
 		assertType('false', substr('a', 2));
 		assertType('string|false', substr($i, 2));
 		assertType('string|false', substr($s, $i));
 		assertType('string|false', substr(3.1, $i));
+		assertType('"lo"', substr("hallo", 3));
+		assertType('"l"', substr("hallo", 3, 1));
+		assertType('false', substr("hallo", 10));
+		assertType('false', substr("hallo", 10, 1));
 	}
 }

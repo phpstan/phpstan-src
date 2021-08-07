a69730ac2d679eb4cb561c64c75ed4b14ca813f0
Markus Staab <markus.staab@redaxo.de>
2021-08-07 20:56:14 +0200

    cover more cases

--- tests/PHPStan/Analyser/data/substr-php8.php
+++ tests/PHPStan/Analyser/data/substr-php8.php
@@ -9,8 +9,11 @@ class Substr
 	public function doFoo(string $s, int $i)
 	{
 		assertType('string', substr(null, true));
+		assertType('string', substr(true, true));
+		assertType('string', substr([], true));
 		assertType('string', substr('a', 2));
 		assertType('string', substr($i, 2));
 		assertType('string', substr($s, $i));
+		assertType('string', substr(3.1, $i));
 	}
 }
--- tests/PHPStan/Analyser/data/substr.php
+++ tests/PHPStan/Analyser/data/substr.php
@@ -9,8 +9,11 @@ class Substr
 	public function doFoo(string $s, int $i)
 	{
 		assertType('false', substr(null, true));
+		assertType('false', substr(true, true));
+		assertType('false', substr([], true));
 		assertType('false', substr('a', 2));
 		assertType('string|false', substr($i, 2));
 		assertType('string|false', substr($s, $i));
+		assertType('string', substr(3.1, $i));
 	}
 }

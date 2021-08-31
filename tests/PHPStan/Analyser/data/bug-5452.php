8eabb7d6ceb441ac6afd61260554c7d11d3115f1
Markus Staab <markus.staab@redaxo.de>
2021-08-31 20:08:32 +0200

    added failling test

---
+++ tests/PHPStan/Analyser/data/bug-5452.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types = 1);
+
+use function PHPStan\Testing\assertType;
+
+class Collector {
+	/** @var array<string, object> */
+	private array $collection = [];
+	
+	public function rewind(): void { 
+		if (reset($this->collection) !== false) {
+			assertType('array<string, object>&nonEmpty', $this->collection);
+		}
+	}
+}

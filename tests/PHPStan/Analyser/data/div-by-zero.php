c53fb9a2887e9e0e7c84228a0a83995160708b81
Ondrej Mirtes <ondrej@mirtes.cz>
2021-08-31 15:00:12 +0200

    This is fine

--- tests/PHPStan/Analyser/data/div-by-zero.php
+++ tests/PHPStan/Analyser/data/div-by-zero.php
@@ -11,10 +11,11 @@ class Foo
 	 * @param int<0, max> $range1
 	 * @param int<min, 0> $range2
 	 */
-	public function doFoo(int $range1, int $range2): void
+	public function doFoo(int $range1, int $range2, int $int): void
 	{
-		assertType('(float|int)', 5 / $range1);
-		assertType('(float|int)', 5 / $range2);
+		assertType('float|int', 5 / $range1);
+		assertType('float|int', 5 / $range2);
+		assertType('(float|int)', 5 / $int);
 		assertType('*ERROR*', 5 / 0);
 	}
 

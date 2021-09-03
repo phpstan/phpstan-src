ddd520fd89d628ac9f0b0b029f8d5e3a92b1851a
Markus Staab <markus.staab@redaxo.de>
2021-09-03 10:17:57 +0200

    Fix false-positive when merging unions with plus operator

---
+++ tests/PHPStan/Analyser/data/bug-5584.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Analyser\Bug5584;
+
+use function PHPStan\Testing\assertType;
+
+class Foo {
+	public function unionSum(): void
+	{
+		$a = [];
+
+		if (rand(0,1) === 0) {
+			$a = ['a' => 5];
+		}
+
+		$b = [];
+
+		if (rand(0,1) === 0) {
+			$b = ['b' => 6];
+		}
+
+		assertType("array()|array(?'b' => 6, ?'a' => 5)", $a + $b);
+	}
+}

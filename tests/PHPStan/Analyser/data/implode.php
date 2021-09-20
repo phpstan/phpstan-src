1b156c0090ad67d7d09de5ab872fbd69254657f3
Markus Staab <maggus.staab@googlemail.com>
2021-09-21 00:12:32 +0200

    Added test

--- tests/PHPStan/Analyser/data/implode.php
+++ tests/PHPStan/Analyser/data/implode.php
@@ -9,5 +9,6 @@ class Foo
 	public function invalidArgs(): void
 	{
 		assertType('*ERROR*', implode('',['12','123',['1234','12345']]));
+                assertType('*ERROR*', implode('',[['1234','12345']]));
 	}
 }

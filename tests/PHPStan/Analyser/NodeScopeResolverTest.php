74f1462819a9497f1f8ce390931e7adb25edea45
Markus Staab <maggus.staab@googlemail.com>
2021-09-21 15:22:17 +0200

    Update NodeScopeResolverTest.php

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -511,7 +511,6 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/eval-implicit-throw.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-5628.php');
 
-		yield from $this->gatherAssertTypes(__DIR__ . '/data/implode.php');
 	}
 
 	/**

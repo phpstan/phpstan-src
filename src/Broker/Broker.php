c7755948c124cfbcb1220ab912a6eaa863cdf5fb
Ondrej Mirtes <ondrej@mirtes.cz>
2021-09-23 10:55:37 +0200

    Deprecated PHPStan\Broker\Broker. Use PHPStan\Reflection\ReflectionProvider instead.

--- src/Broker/Broker.php
+++ src/Broker/Broker.php
@@ -45,62 +45,97 @@ class Broker implements ReflectionProvider
 		return self::$instance;
 	}
 
+	/**
+	 * @deprecated Use PHPStan\Reflection\ReflectionProvider instead
+	 */
 	public function hasClass(string $className): bool
 	{
 		return $this->reflectionProvider->hasClass($className);
 	}
 
+	/**
+	 * @deprecated Use PHPStan\Reflection\ReflectionProvider instead
+	 */
 	public function getClass(string $className): ClassReflection
 	{
 		return $this->reflectionProvider->getClass($className);
 	}
 
+	/**
+	 * @deprecated Use PHPStan\Reflection\ReflectionProvider instead
+	 */
 	public function getClassName(string $className): string
 	{
 		return $this->reflectionProvider->getClassName($className);
 	}
 
+	/**
+	 * @deprecated Use PHPStan\Reflection\ReflectionProvider instead
+	 */
 	public function supportsAnonymousClasses(): bool
 	{
 		return $this->reflectionProvider->supportsAnonymousClasses();
 	}
 
+	/**
+	 * @deprecated Use PHPStan\Reflection\ReflectionProvider instead
+	 */
 	public function getAnonymousClassReflection(\PhpParser\Node\Stmt\Class_ $classNode, Scope $scope): ClassReflection
 	{
 		return $this->reflectionProvider->getAnonymousClassReflection($classNode, $scope);
 	}
 
+	/**
+	 * @deprecated Use PHPStan\Reflection\ReflectionProvider instead
+	 */
 	public function hasFunction(\PhpParser\Node\Name $nameNode, ?Scope $scope): bool
 	{
 		return $this->reflectionProvider->hasFunction($nameNode, $scope);
 	}
 
+	/**
+	 * @deprecated Use PHPStan\Reflection\ReflectionProvider instead
+	 */
 	public function getFunction(\PhpParser\Node\Name $nameNode, ?Scope $scope): FunctionReflection
 	{
 		return $this->reflectionProvider->getFunction($nameNode, $scope);
 	}
 
+	/**
+	 * @deprecated Use PHPStan\Reflection\ReflectionProvider instead
+	 */
 	public function resolveFunctionName(\PhpParser\Node\Name $nameNode, ?Scope $scope): ?string
 	{
 		return $this->reflectionProvider->resolveFunctionName($nameNode, $scope);
 	}
 
+	/**
+	 * @deprecated Use PHPStan\Reflection\ReflectionProvider instead
+	 */
 	public function hasConstant(\PhpParser\Node\Name $nameNode, ?Scope $scope): bool
 	{
 		return $this->reflectionProvider->hasConstant($nameNode, $scope);
 	}
 
+	/**
+	 * @deprecated Use PHPStan\Reflection\ReflectionProvider instead
+	 */
 	public function getConstant(\PhpParser\Node\Name $nameNode, ?Scope $scope): GlobalConstantReflection
 	{
 		return $this->reflectionProvider->getConstant($nameNode, $scope);
 	}
 
+	/**
+	 * @deprecated Use PHPStan\Reflection\ReflectionProvider instead
+	 */
 	public function resolveConstantName(\PhpParser\Node\Name $nameNode, ?Scope $scope): ?string
 	{
 		return $this->reflectionProvider->resolveConstantName($nameNode, $scope);
 	}
 
 	/**
+	 * @deprecated Inject %universalObjectCratesClasses% parameter instead.
+	 *
 	 * @return string[]
 	 */
 	public function getUniversalObjectCratesClasses(): array

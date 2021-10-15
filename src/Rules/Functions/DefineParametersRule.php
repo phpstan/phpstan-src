732d03389e801cf3949988f65297fb6b9450f907
Matt Glaman <nmd.matt@gmail.com>
2021-10-15 05:42:24 -0500

    define: case_insensitive is ignored, case-insensitive constants are no longer supported

---
+++ src/Rules/Functions/DefineParametersRule.php
@@ -0,0 +1,54 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Functions;
+
+use PhpParser\Node;
+use PhpParser\Node\Expr\FuncCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Php\PhpVersion;
+use PHPStan\Rules\RuleErrorBuilder;
+
+/**
+ * @implements \PHPStan\Rules\Rule<\PhpParser\Node\Expr\FuncCall>
+ */
+class DefineParametersRule implements \PHPStan\Rules\Rule
+{
+
+	private PhpVersion $phpVersion;
+
+	public function __construct(PhpVersion $phpVersion)
+	{
+		$this->phpVersion = $phpVersion;
+	}
+
+	public function getNodeType(): string
+	{
+		return FuncCall::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		if (!($node->name instanceof \PhpParser\Node\Name)) {
+			return [];
+		}
+		if ($this->phpVersion->supportsCaseInsensitiveConstantNames()) {
+			return [];
+		}
+		$name = strtolower((string) $node->name);
+		if ($name !== 'define') {
+			return [];
+		}
+		$args = $node->getArgs();
+		$argsCount = count($args);
+		// Expects 2 or 3, 1 arg is caught by CallToFunctionParametersRule
+		if ($argsCount < 3) {
+			return [];
+		}
+		return [
+			RuleErrorBuilder::message(
+				'Argument #3 ($case_insensitive) is ignored since declaration of case-insensitive constants is no longer supported.'
+			)->line($node->getLine())->build(),
+		];
+	}
+
+}

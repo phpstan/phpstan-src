ae3b3403616d0e40291bc27269123f8be52eab0a
John Blackbourn <john@humanmade.com>
2021-10-23 22:14:15 +0200

    These functions can also return boolean false.

---
+++ src/Type/Php/RoundFunctionReturnTypeExtension.php
@@ -0,0 +1,44 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Type\Php;
+
+use PhpParser\Node\Expr\FuncCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\FunctionReflection;
+use PHPStan\Type\ArrayType;
+use PHPStan\Type\DynamicFunctionReturnTypeExtension;
+use PHPStan\Type\FloatType;
+use PHPStan\Type\Constant\ConstantBooleanType;
+use PHPStan\Type\Type;
+
+class RoundFunctionReturnTypeExtension implements DynamicFunctionReturnTypeExtension
+{
+
+	public function isFunctionSupported(FunctionReflection $functionReflection): bool
+	{
+		return in_array(
+			$functionReflection->getName(),
+			[
+				'round',
+				'ceil',
+				'floor',
+			]
+		);
+	}
+
+	public function getTypeFromFunctionCall(FunctionReflection $functionReflection, FuncCall $functionCall, Scope $scope): Type
+	{
+		$defaultReturnType = new FloatType();
+		if (count($functionCall->getArgs()) < 1) {
+			return $defaultReturnType;
+		}
+
+		$firstArgType = $scope->getType($functionCall->getArgs()[0]->value);
+		if ($firstArgType instanceof ArrayType) {
+			return new ConstantBooleanType( false );
+		}
+
+		return $defaultReturnType;
+	}
+
+}

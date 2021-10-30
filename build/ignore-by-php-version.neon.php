27ba3bad43c7c94acebf8aa1f615d0a1e5e90fc4
Ondrej Mirtes <ondrej@mirtes.cz>
2021-10-30 16:19:36 +0200

    PHP 8.1 baseline

--- build/ignore-by-php-version.neon.php
+++ build/ignore-by-php-version.neon.php
@@ -13,6 +13,9 @@ if (PHP_VERSION_ID < 70300) {
 if (PHP_VERSION_ID >= 80000) {
 	$config = array_merge_recursive($config, $adapter->load(__DIR__ . '/baseline-8.0.neon'));
 }
+if (PHP_VERSION_ID >= 80100) {
+	$config = array_merge_recursive($config, $adapter->load(__DIR__ . '/baseline-8.1.neon'));
+}
 
 if (PHP_VERSION_ID >= 70400) {
 	$config = array_merge_recursive($config, $adapter->load(__DIR__ . '/ignore-gte-php7.4-errors.neon'));

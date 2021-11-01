659c55dbca89fffc4d66e50928d0aea994e18c60
Markus Staab <maggus.staab@googlemail.com>
2021-11-01 17:25:17 +0100

    Update bug-5866.php

--- tests/PHPStan/Rules/Exceptions/data/bug-5866.php
+++ tests/PHPStan/Rules/Exceptions/data/bug-5866.php
@@ -24,7 +24,7 @@ class Foo
 	 */
 	public function decode2($contents) {
 		try {
-			$parsed = json_decode($contents, flags: JSON_BIGINT_AS_STRING | JSON_OBJECT_AS_ARRAY | JSON_THROW_ON_ERROR, depth: 123, associative: true);
+			$parsed = json_decode($contents, depth: 123, associative: true, flags: JSON_BIGINT_AS_STRING | JSON_OBJECT_AS_ARRAY | JSON_THROW_ON_ERROR);
 		} catch (JsonException $exception) {
 			throw new InvalidArgumentException('Unable to decode contents');
 		}

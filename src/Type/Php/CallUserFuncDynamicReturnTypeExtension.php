2e9f4ff02499ebb2824c6f0e6a14e6e7a3338902
Markus Staab <m.staab@complex-it.de>
2021-11-05 17:46:24 +0100

    cs

--- src/Type/Php/CallUserFuncDynamicReturnTypeExtension.php
+++ src/Type/Php/CallUserFuncDynamicReturnTypeExtension.php
@@ -3,25 +3,18 @@
 namespace PHPStan\Type\Php;
 
 use PhpParser\Node\Expr\FuncCall;
-use PhpParser\Node\Name;
-use PhpParser\Node\Name\FullyQualified;
-use PhpParser\Node\Name\Relative;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Reflection\ParametersAcceptorSelector;
-use PHPStan\Type\BooleanType;
-use PHPStan\Type\Constant\ConstantArrayType;
 use PHPStan\Type\Constant\ConstantBooleanType;
-use PHPStan\Type\Constant\ConstantStringType;
 use PHPStan\Type\DynamicFunctionReturnTypeExtension;
 use PHPStan\Type\NeverType;
 use PHPStan\Type\Type;
-use PHPStan\Type\TypeCombinator;
-use PHPStan\Type\TypeUtils;
 use PHPStan\Type\UnionType;
 
 class CallUserFuncDynamicReturnTypeExtension implements DynamicFunctionReturnTypeExtension
 {
+
 	public function isFunctionSupported(FunctionReflection $functionReflection): bool
 	{
 		return in_array($functionReflection->getName(), ['call_user_func_array', 'call_user_func'], true);

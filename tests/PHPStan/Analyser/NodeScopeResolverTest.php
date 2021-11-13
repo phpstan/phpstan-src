0e9c43165ed62abcb6f5873b5895438b13ce3ef5
Markus Staab <m.staab@complex-it.de>
2021-11-13 11:12:01 +0100

    added failling test

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -527,6 +527,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-5501.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-4743.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-5017.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-5992.php');
 
 		if (PHP_VERSION_ID >= 70400) {
 			yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-5458.php');
---
+++ tests/PHPStan/Analyser/data/bug-5992.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types = 1);
+
+namespace Bug5992;
+
+use function PHPStan\Testing\assertType;
+
+function StringValue() : string
+{
+	$values = [ 1, 'one', true, null, [] ];
+	$value = $values[rand(0,4)];
+	if ( ! is_string( $value ) )
+	{
+		assertType('bool', trigger_error("just a soft warning", E_USER_WARNING));
+		assertType('*NEVER*', trigger_error("error which halts the script", E_USER_ERROR));
+	}
+	return $value;
+}

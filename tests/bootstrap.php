9095efe5cef069d2d5cdb64efb7a5e019d807da2
Ondrej Mirtes <ondrej@mirtes.cz>
2021-11-17 16:15:53 +0100

    Debug weird PHP 8 issuet

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,6 +3,7 @@
 error_reporting(E_ALL);
 
 require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../NetteReflection.php';
 require_once __DIR__ . '/PHPStan/Rules/AlwaysFailRule.php';
 require_once __DIR__ . '/PHPStan/Rules/DummyRule.php';
 require_once __DIR__ . '/phpstan-bootstrap.php';

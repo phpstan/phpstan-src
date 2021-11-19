e377854d00e19aa66b539eb39a2d7c3170c743ea
Markus Staab <m.staab@complex-it.de>
2021-11-19 21:32:43 +0100

    added generic failling test

--- tests/PHPStan/Analyser/data/call-user-func-array.php
+++ tests/PHPStan/Analyser/data/call-user-func-array.php
@@ -4,6 +4,15 @@ namespace CallUserFuncArray;
 
 use function PHPStan\Testing\assertType;
 
+/**
+ * @template T
+ * @param T $t
+ * @return T
+ */
+function generic($t) {
+	return $t;
+}
+
 function fun(): int
 {
 	return 3;
@@ -25,18 +34,20 @@ class Foo {
 	function doArray() {
 		assertType('*NEVER*', call_user_func_array('single-arg-only'));
 
+		assertType('bool', call_user_func_array('CallUserFuncArray\generic', [true]));
+		assertType('string', call_user_func_array('CallUserFuncArray\generic', ['hello']));
+
 		assertType('int', call_user_func_array('CallUserFuncArray\fun', []));
-
 		assertType('int', call_user_func_array('CallUserFuncArray\fun3', [1 ,2 ,3]));
-
 		assertType('string', call_user_func_array(['CallUserFuncArray\c', 'm'], []));
 	}
 
 	function doFunc() {
+		assertType('bool', call_user_func('CallUserFuncArray\generic', true));
+		assertType('string', call_user_func('CallUserFuncArray\generic', 'hello'));
+
 		assertType('int', call_user_func('CallUserFuncArray\fun'));
-
 		assertType('int', call_user_func('CallUserFuncArray\fun3', 1 ,2 ,3));
-
 		assertType('string', call_user_func(['CallUserFuncArray\c', 'm']));
 	}
 }

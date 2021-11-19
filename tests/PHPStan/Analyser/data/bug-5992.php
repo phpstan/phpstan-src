96bf58876a869ee4227e7fac48be85c3eacbf006
Markus Staab <m.staab@complex-it.de>
2021-11-19 12:28:13 +0100

    added failling test

--- tests/PHPStan/Analyser/data/bug-5992.php
+++ tests/PHPStan/Analyser/data/bug-5992.php
@@ -1,17 +1,24 @@
-<?php declare(strict_types = 1);
+<?php declare(strict_types=1);
 
 namespace Bug5992;
 
 use function PHPStan\Testing\assertType;
 
-function StringValue() : string
+function StringValue(): string
 {
-	$values = [ 1, 'one', true, null, [] ];
-	$value = $values[rand(0,4)];
-	if ( ! is_string( $value ) )
-	{
+	$values = [1, 'one', true, null, []];
+	$value = $values[rand(0, 4)];
+	if (!is_string($value)) {
 		assertType('bool', trigger_error("just a soft warning", E_USER_WARNING));
 		assertType('*NEVER*', trigger_error("error which halts the script", E_USER_ERROR));
 	}
 	return $value;
+}
+
+function foo(?string $s): void
+{
+	if ($s === null) {
+		trigger_error('foo', E_USER_ERROR);
+	}
+	assertType('string', $s);
 }

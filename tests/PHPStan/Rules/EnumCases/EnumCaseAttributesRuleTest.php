a345704f8ddf83f905e44f227266ca1db2c591e9
Jaroslav Hanslík <kukulich@kukulich.cz>
2021-11-30 20:34:20 +0100

    Added EnumCaseAttributesRule

---
+++ tests/PHPStan/Rules/EnumCases/EnumCaseAttributesRuleTest.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\EnumCases;
+
+use PHPStan\Php\PhpVersion;
+use PHPStan\Rules\AttributesCheck;
+use PHPStan\Rules\ClassCaseSensitivityCheck;
+use PHPStan\Rules\FunctionCallParametersCheck;
+use PHPStan\Rules\NullsafeCheck;
+use PHPStan\Rules\PhpDoc\UnresolvableTypeHelper;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleLevelHelper;
+use PHPStan\Testing\RuleTestCase;
+
+/**
+ * @extends RuleTestCase<EnumCaseAttributesRule>
+ */
+class EnumCaseAttributesRuleTest extends RuleTestCase
+{
+
+	protected function getRule(): Rule
+	{
+		$reflectionProvider = $this->createReflectionProvider();
+		return new EnumCaseAttributesRule(
+			new AttributesCheck(
+				$reflectionProvider,
+				new FunctionCallParametersCheck(
+					new RuleLevelHelper($reflectionProvider, true, false, true, false),
+					new NullsafeCheck(),
+					new PhpVersion(80100),
+					new UnresolvableTypeHelper(),
+					true,
+					true,
+					true,
+					true
+				),
+				new ClassCaseSensitivityCheck($reflectionProvider, false)
+			)
+		);
+	}
+
+	public function testRule(): void
+	{
+		if (!self::$useStaticReflectionProvider && PHP_VERSION_ID < 80100) {
+			$this->markTestSkipped('Test requires PHP 8.1.');
+		}
+
+		$this->analyse([__DIR__ . '/data/enum-case-attributes.php'], [
+			[
+				'Attribute class EnumCaseAttributes\AttributeWithPropertyTarget does not have the class constant target.',
+				26,
+			],
+		]);
+	}
+
+}

9271c5d3ca296475c2b6af0e12bb68d5b2f1543a
Martin Herndl <martin@herndl.org>
2021-12-01 14:58:28 +0100

    Handle non-empty-array cases

--- tests/PHPStan/Analyser/data/bug-5287.php
+++ tests/PHPStan/Analyser/data/bug-5287.php
@@ -14,6 +14,42 @@ function foo(array $arr): void
 }
 
 /**
+ * @param list<non-empty-array> $arr
+ */
+function foo2(array $arr): void
+{
+	$arrSpread = [...$arr];
+	assertType('non-empty-array<int, non-empty-array>', $arrSpread);
+}
+
+/**
+ * @param non-empty-list<string> $arr
+ */
+function foo3(array $arr): void
+{
+	$arrSpread = [...$arr];
+	assertType('non-empty-array<int, string>', $arrSpread);
+}
+
+/**
+ * @param non-empty-array<string, int> $arr
+ */
+function foo3(array $arr): void
+{
+	$arrSpread = [...$arr];
+	assertType('non-empty-array<int, int>', $arrSpread);
+}
+
+/**
+ * @param non-empty-array<mixed, bool|int> $arr
+ */
+function foo4(array $arr): void
+{
+	$arrSpread = [...$arr];
+	assertType('non-empty-array<int, bool|int>', $arrSpread);
+}
+
+/**
  * @param array{foo: 17, bar: 19} $arr
  */
 function bar(array $arr): void

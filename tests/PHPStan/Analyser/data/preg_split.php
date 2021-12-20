b22f3bb57098427b757a46d407baf6db7e16c0f7
Markus Staab <maggus.staab@googlemail.com>
2021-12-20 15:11:25 +0100

    put top-level asserts into method

--- tests/PHPStan/Analyser/data/preg_split.php
+++ tests/PHPStan/Analyser/data/preg_split.php
@@ -2,13 +2,15 @@
 
 use function PHPStan\Testing\assertType;
 
-assertType('array<int, string>|false', preg_split('/-/', '1-2-3'));
-assertType('array<int, string>|false', preg_split('/-/', '1-2-3', -1, PREG_SPLIT_NO_EMPTY));
-assertType('array<int, array{string, int<0, max>}>|false', preg_split('/-/', '1-2-3', -1, PREG_SPLIT_OFFSET_CAPTURE));
-assertType('array<int, array{string, int<0, max>}>|false', preg_split('/-/', '1-2-3', -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_OFFSET_CAPTURE));
-
 class HelloWorld
 {
+	public function doFoo() {
+		assertType('array<int, string>|false', preg_split('/-/', '1-2-3'));
+		assertType('array<int, string>|false', preg_split('/-/', '1-2-3', -1, PREG_SPLIT_NO_EMPTY));
+		assertType('array<int, array{string, int<0, max>}>|false', preg_split('/-/', '1-2-3', -1, PREG_SPLIT_OFFSET_CAPTURE));
+		assertType('array<int, array{string, int<0, max>}>|false', preg_split('/-/', '1-2-3', -1, PREG_SPLIT_NO_EMPTY | PREG_SPLIT_OFFSET_CAPTURE));
+	}
+
     /**
      * @param string $pattern
      * @param string $subject

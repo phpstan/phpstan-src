146d266c661d82e3d39f4c2322c388a87291b145
Ondrej Mirtes <ondrej@mirtes.cz>
2021-12-28 14:45:09 +0100

    Fix

--- tests/PHPStan/Rules/Comparison/MatchExpressionRuleTest.php
+++ tests/PHPStan/Rules/Comparison/MatchExpressionRuleTest.php
@@ -126,7 +126,7 @@ class MatchExpressionRuleTest extends RuleTestCase
 
 	public function testBug5454(): void
 	{
-		if (PHP_VERSION_ID < 80000 && !self::$useStaticReflectionProvider) {
+		if (PHP_VERSION_ID < 80000) {
 			$this->markTestSkipped('Test requires PHP 8.0.');
 		}
 		$this->analyse([__DIR__ . '/data/bug-5454.php'], []);

e70142f23a673a8f33168193d325faf1773b9730
Ondrej Mirtes <ondrej@mirtes.cz>
2021-12-29 11:11:21 +0100

    Enums cannot be generic

---
+++ src/Rules/Generics/EnumTemplateTypeRule.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Generics;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Node\InClassNode;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+use function count;
+use function sprintf;
+
+/**
+ * @implements Rule<InClassNode>
+ */
+class EnumTemplateTypeRule implements Rule
+{
+
+	public function getNodeType(): string
+	{
+		return InClassNode::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		if (!$scope->isInClass()) {
+			return [];
+		}
+		$classReflection = $scope->getClassReflection();
+		if (!$classReflection->isEnum()) {
+			return [];
+		}
+
+		$templateTagsCount = count($classReflection->getTemplateTags());
+		if ($templateTagsCount === 0) {
+			return [];
+		}
+
+		$className = $classReflection->getDisplayName();
+
+		return [
+			RuleErrorBuilder::message(sprintf('Enum %s has PHPDoc @template tag%s but enums cannot be generic.', $className, $templateTagsCount === 1 ? '' : 's'))->build(),
+		];
+	}
+
+}
---
+++ tests/PHPStan/Rules/Generics/data/enum-template.php
@@ -0,0 +1,25 @@
+<?php // lint >= 8.1
+
+namespace EnumTemplate;
+
+/**
+ * @template T
+ */
+enum Foo
+{
+
+}
+
+/**
+ * @template T
+ * @template U
+ */
+enum Bar
+{
+
+}
+
+enum Baz
+{
+
+}

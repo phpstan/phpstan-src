2f4db0ec445de224e06c86138534c00aedd517f6
Ondrej Mirtes <ondrej@mirtes.cz>
2022-01-03 22:31:56 +0100

    Fixes

--- tests/PHPStan/Rules/Methods/CallMethodsRuleTest.php
+++ tests/PHPStan/Rules/Methods/CallMethodsRuleTest.php
@@ -2275,6 +2275,10 @@ class CallMethodsRuleTest extends RuleTestCase
 
 	public function testBug6239(): void
 	{
+		if (PHP_VERSION_ID < 80000) {
+			$this->markTestSkipped('This test needs PHP 8.0');
+		}
+
 		$this->checkThisOnly = false;
 		$this->checkNullables = true;
 		$this->checkUnionTypes = true;

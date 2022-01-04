b60f022c874528e065b38d6e4eebea9ce49111c1
Ondrej Mirtes <ondrej@mirtes.cz>
2022-01-04 17:48:36 +0100

    Fix

--- tests/PHPStan/Rules/TooWideTypehints/TooWideMethodReturnTypehintRuleTest.php
+++ tests/PHPStan/Rules/TooWideTypehints/TooWideMethodReturnTypehintRuleTest.php
@@ -90,6 +90,9 @@ class TooWideMethodReturnTypehintRuleTest extends RuleTestCase
 
 	public function testBug6158(): void
 	{
+		if (PHP_VERSION_ID < 80000) {
+			$this->markTestSkipped('Test requires PHP 8.0.');
+		}
 		$this->analyse([__DIR__ . '/data/bug-6158.php'], []);
 	}
 

b535392e798de098fc3f4bff78195c1ba9a1ff0e
Ondrej Mirtes <ondrej@mirtes.cz>
2022-01-04 15:05:34 +0100

    Fix

--- tests/PHPStan/Rules/PhpDoc/InvalidPhpDocVarTagTypeRuleTest.php
+++ tests/PHPStan/Rules/PhpDoc/InvalidPhpDocVarTagTypeRuleTest.php
@@ -8,6 +8,7 @@ use PHPStan\Rules\MissingTypehintCheck;
 use PHPStan\Rules\Rule;
 use PHPStan\Testing\RuleTestCase;
 use PHPStan\Type\FileTypeMapper;
+use const PHP_VERSION_ID;
 
 /**
  * @extends RuleTestCase<InvalidPhpDocVarTagTypeRule>
@@ -139,6 +140,9 @@ class InvalidPhpDocVarTagTypeRuleTest extends RuleTestCase
 
 	public function testBug6252(): void
 	{
+		if (PHP_VERSION_ID < 80000) {
+			$this->markTestSkipped('Test requires PHP 8.0.');
+		}
 		$this->analyse([__DIR__ . '/data/bug-6252.php'], []);
 	}
 

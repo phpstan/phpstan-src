11817175c9977a835d8a0dcc6f8edf2d87744b47
Ondrej Mirtes <ondrej@mirtes.cz>
2022-01-06 16:47:31 +0100

    Improve Exception::getCode() type
    See discussion: https://github.com/phpstan/phpstan-src/pull/795#issuecomment-1003166361

--- src/Type/Php/ThrowableReturnTypeExtension.php
+++ src/Type/Php/ThrowableReturnTypeExtension.php
@@ -2,15 +2,23 @@
 
 namespace PHPStan\Type\Php;
 
+use PDOException;
 use PhpParser\Node\Expr\MethodCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\MethodReflection;
 use PHPStan\Type\BenevolentUnionType;
 use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\ErrorType;
 use PHPStan\Type\IntegerType;
+use PHPStan\Type\ObjectType;
 use PHPStan\Type\StringType;
 use PHPStan\Type\Type;
+use PHPStan\Type\TypeCombinator;
+use PHPStan\Type\TypeUtils;
 use Throwable;
+use function count;
+use function in_array;
+use function strtolower;
 
 final class ThrowableReturnTypeExtension implements DynamicMethodReturnTypeExtension
 {
@@ -27,7 +35,32 @@ final class ThrowableReturnTypeExtension implements DynamicMethodReturnTypeExten
 
 	public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type
 	{
-		return new BenevolentUnionType([new IntegerType(), new StringType()]);
+		$type = $scope->getType($methodCall->var);
+		$types = [];
+		$pdoException = new ObjectType(PDOException::class);
+		foreach (TypeUtils::getDirectClassNames($type) as $class) {
+			$classType = new ObjectType($class);
+			if ($pdoException->isSuperTypeOf($classType)->yes()) {
+				$types[] = new StringType();
+				continue;
+			}
+
+			if (in_array(strtolower($class), [
+				'throwable',
+				'exception',
+			], true)) {
+				$types[] = new BenevolentUnionType([new IntegerType(), new StringType()]);
+				continue;
+			}
+
+			$types[] = new IntegerType();
+		}
+
+		if (count($types) === 0) {
+			return new ErrorType();
+		}
+
+		return TypeCombinator::union(...$types);
 	}
 
 }
--- tests/PHPStan/Analyser/data/bug-6001.php
+++ tests/PHPStan/Analyser/data/bug-6001.php
@@ -4,8 +4,36 @@ namespace Bug6001;
 
 use function PHPStan\Testing\assertType;
 
-assertType('(int|string)', (new \Exception())->getCode());
+class Foo
+{
 
-assertType('(int|string)', (new \RuntimeException())->getCode());
+	public function doFoo(\Throwable $t): void
+	{
+		assertType('(int|string)', (new \Exception())->getCode());
+		assertType('(int|string)', $t->getCode());
+		assertType('int', (new \RuntimeException())->getCode());
+		assertType('string', (new \PDOException())->getCode());
+		assertType('int', (new MyException())->getCode());
+		assertType('string', (new SubPDOException())->getCode());
+	}
 
-assertType('(int|string)', (new \PDOException())->getCode());
+	/**
+	 * @param \PDOException|MyException $exception
+	 * @return void
+	 */
+	public function doBar($exception): void
+	{
+		assertType('int|string', $exception->getCode());
+	}
+
+}
+
+class MyException extends \Exception
+{
+
+}
+
+class SubPDOException extends \PDOException
+{
+
+}

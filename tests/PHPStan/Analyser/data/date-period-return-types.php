c91556d685e7cfad8b79a56007168f62cada60e7
Alban Baixas <albanbaixas@gmail.com>
2022-01-30 09:21:04 +0100

    Add DatePeriod test

--- tests/PHPStan/Analyser/data/date-period-return-types.php
+++ tests/PHPStan/Analyser/data/date-period-return-types.php
@@ -8,14 +8,24 @@ $end = new DateTime('2012-07-31');
 $recurrences = 4;
 $iso = 'R4/2012-07-01T00:00:00Z/P7D';
 
+$datePeriodList = [];
+
 $datePeriod = new DatePeriod($start, $interval, $end);
 assertType(\DateTimeInterface::class, $datePeriod->getEndDate());
 assertType('null', $datePeriod->getRecurrences());
+$datePeriodList[] = $datePeriod;
 
 $datePeriod = new DatePeriod($start, $interval, $recurrences);
 assertType('null', $datePeriod->getEndDate());
 assertType('int', $datePeriod->getRecurrences());
+$datePeriodList[] = $datePeriod;
 
 $datePeriod = new DatePeriod($iso);
 assertType('null', $datePeriod->getEndDate());
 assertType('int', $datePeriod->getRecurrences());
+$datePeriodList[] = $datePeriod;
+
+/** @var DatePeriod $datePeriod */
+$datePeriod = $datePeriodList[random_int(0, 2)];
+assertType(\DateTimeInterface::class . '|null', $datePeriod->getEndDate());
+assertType('int|null', $datePeriod->getRecurrences());

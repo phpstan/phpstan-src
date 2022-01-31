1361afafefc947654c50f1266ff7f81d2c4df519
Arnaud Le Blanc <arnaud.lb@gmail.com>
2022-01-31 18:30:01 +0100

    Fix tests

--- tests/PHPStan/Analyser/data/bug-1597.php
+++ tests/PHPStan/Analyser/data/bug-1597.php
@@ -7,6 +7,9 @@ use function PHPStan\Testing\assertType;
 $date = '';
 
 try {
+	if (rand(0,1) === 0) {
+		throw new \Exception();
+	}
 	$date = new \DateTime($date);
 } catch (\Exception $e) {
 	assertType('\'\'', $date);

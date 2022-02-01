6cac830fff64aa92393d99aaa040e96b54b3e950
Markus Staab <m.staab@complex-it.de>
2022-02-01 16:50:00 +0100

    cs

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -683,10 +683,10 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 			yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-6308.php');
 		}
 
-		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-6500.php');
 		if (PHP_VERSION_ID >= 70400) {
 			yield from $this->gatherAssertTypes(__DIR__ . '/../Rules/Comparison/data/bug-6473.php');
 		}
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-6500.php');
 	}
 
 	/**

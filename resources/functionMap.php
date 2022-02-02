c59e58d85eb36c432abcf6cc1534fd71ae99f213
Markus Staab <m.staab@complex-it.de>
2022-02-02 10:12:47 +0100

    Update functionMap.php

--- resources/functionMap.php
+++ resources/functionMap.php
@@ -12780,7 +12780,7 @@ return [
 'VarnishStat::__construct' => ['void', 'args='=>'array'],
 'VarnishStat::getSnapshot' => ['array'],
 'version_compare' => ['-1|0|1', 'version1'=>'string', 'version2'=>'string'],
-'version_compare\'1' => ['bool', 'version1'=>'string', 'version2'=>'string', 'operator'=>'string|null'],
+'version_compare\'1' => ['bool|null', 'version1'=>'string', 'version2'=>'string', 'operator'=>'string|null'],
 'vfprintf' => ['int', 'stream'=>'resource', 'format'=>'string', 'args'=>'array'],
 'virtual' => ['bool', 'uri'=>'string'],
 'Volatile::__construct' => ['void'],

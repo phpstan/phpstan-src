d36c13269e5e8e1ec044429e3e274bbac0e71acb
Markus Staab <m.staab@complex-it.de>
2022-02-10 18:40:53 +0100

    added failling test-case

---
+++ tests/PHPStan/Rules/Comparison/ImpossibleCheckTypeGenericOverwriteRuleTest.php
@@ -0,0 +1,45 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Comparison;
+
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+
+/**
+ * @extends RuleTestCase<ImpossibleCheckTypeMethodCallRule>
+ */
+class ImpossibleCheckTypeGenericOverwriteRuleTest extends RuleTestCase
+{
+
+	private bool $treatPhpDocTypesAsCertain;
+
+	public function getRule(): Rule
+	{
+		return new ImpossibleCheckTypeMethodCallRule(
+			new ImpossibleCheckTypeHelper(
+				$this->createReflectionProvider(),
+				$this->getTypeSpecifier(),
+				[],
+				$this->treatPhpDocTypesAsCertain,
+			),
+			true,
+			$this->treatPhpDocTypesAsCertain,
+		);
+	}
+
+	public function testNoReportedErrorOnOverwrite(): void
+	{
+		$this->treatPhpDocTypesAsCertain = false;
+
+		$this->analyse([__DIR__ . '/data/generic-type-override.php'], [
+		]);
+	}
+
+	public static function getAdditionalConfigFiles(): array
+	{
+		return [
+			__DIR__ . '/impossible-check-type-generic-overwrite.neon',
+		];
+	}
+
+}

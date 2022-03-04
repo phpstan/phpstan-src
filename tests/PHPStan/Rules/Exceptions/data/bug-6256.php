f574f72d081198de3d20c279120978eb056863d1
Yohta Kimura <kitakita7617@gmail.com>
2022-03-04 22:43:06 +0900

    enable editorconfig, fix spaces to tab

--- tests/PHPStan/Rules/Exceptions/data/bug-6256.php
+++ tests/PHPStan/Rules/Exceptions/data/bug-6256.php
@@ -4,39 +4,39 @@ namespace Bug6256;
 
 final class A
 {
-    public int $intVal = 1;
+	public int $intVal = 1;
 
-    function doFoo()
-    {
-        try {
-            $this->intVal = "string";
-        } catch (\TypeError $e) {
-        }
-    }
+	function doFoo()
+	{
+		try {
+			$this->intVal = "string";
+		} catch (\TypeError $e) {
+		}
+	}
 }
 
 final class B
 {
-    public $noType;
+	public $noType;
 
-    function doFoo()
-    {
-        try {
-            $this->noType = "string";
-        } catch (\TypeError $e) {
-        }
-    }
+	function doFoo()
+	{
+		try {
+			$this->noType = "string";
+		} catch (\TypeError $e) {
+		}
+	}
 }
 
 final class C
 {
-    public string $stringType;
+	public string $stringType;
 
-    function doFoo()
-    {
-        try {
-            $this->stringType = "string";
-        } catch (\TypeError $e) {
-        }
-    }
+	function doFoo()
+	{
+		try {
+			$this->stringType = "string";
+		} catch (\TypeError $e) {
+		}
+	}
 }

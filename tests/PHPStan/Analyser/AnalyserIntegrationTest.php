9bb07e1768d60702d7ef380c28d000fa81d3d1ad
Martin Herndl <martin@herndl.org>
2022-03-05 08:57:02 +0100

    Add missing newline between tests

--- tests/PHPStan/Analyser/AnalyserIntegrationTest.php
+++ tests/PHPStan/Analyser/AnalyserIntegrationTest.php
@@ -562,6 +562,7 @@ class AnalyserIntegrationTest extends PHPStanTestCase
 		$errors = $this->runAnalyse(__DIR__ . '/data/bug-6212.php');
 		$this->assertNoErrors($errors);
 	}
+
 	public function testBug6740(): void
 	{
 		$errors = $this->runAnalyse(__DIR__ . '/data/bug-6740-b.php');

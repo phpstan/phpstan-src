d0ab72ac815f4a8716fa110d5c1e6caba6ceeb47
Ondrej Mirtes <ondrej@mirtes.cz>
2022-03-08 15:07:21 +0100

    Fix

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -774,8 +774,12 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		if (PHP_VERSION_ID >= 80000) {
 			yield from $this->gatherAssertTypes(__DIR__ . '/../Rules/Methods/data/bug-6635.php');
 		}
+
+		if (PHP_VERSION_ID >= 70400) {
+			yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-6591.php');
+		}
+
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-6584.php');
-		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-6591.php');
 	}
 
 	/**

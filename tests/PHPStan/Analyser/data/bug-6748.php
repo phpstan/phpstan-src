89252562d5e14b8368d6556b7ea9243e9ab4523d
Matthew Krauss <m.d.krauss@gmail.com>
2022-03-10 02:06:39 -0500

    Tabs not spaces

--- tests/PHPStan/Analyser/data/bug-6748.php
+++ tests/PHPStan/Analyser/data/bug-6748.php
@@ -6,17 +6,17 @@ use function PHPStan\Testing\assertType;
 
 class Foo
 {
-    /** @param \DOMNodeList<\DOMNode> $list */
-    public function iterateNodes ($list): void
-    {
-        foreach($list as $item) {
-            assertType('DOMNode', $item);
-        }
-    }
+	/** @param \DOMNodeList<\DOMNode> $list */
+	public function iterateNodes ($list): void
+	{
+		foreach($list as $item) {
+			assertType('DOMNode', $item);
+		}
+	}
 
-    /** @param \DOMXPath $path */
-    public function xPathQuery ($path)
-    {
-        assertType('DOMNodeList<DOMNode>|false', $path->query(''));
-    }
+	/** @param \DOMXPath $path */
+	public function xPathQuery ($path)
+	{
+		assertType('DOMNodeList<DOMNode>|false', $path->query(''));
+	}
 }

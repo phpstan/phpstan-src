dfc5d25348034e943a1982a3d5c587edc511e590
Yohta Kimura <kitakita7617@gmail.com>
2022-03-11 22:32:07 +0900

    add regression tests for union type

--- tests/PHPStan/Rules/Exceptions/CatchWithUnthrownExceptionRuleTest.php
+++ tests/PHPStan/Rules/Exceptions/CatchWithUnthrownExceptionRuleTest.php
@@ -298,4 +298,22 @@ class CatchWithUnthrownExceptionRuleTest extends RuleTestCase
 		$this->analyse([__DIR__ . '/data/bug-6786.php'], []);
 	}
 
+	public function testUnionTypeError(): void
+	{
+		if (PHP_VERSION_ID < 80000) {
+			self::markTestSkipped('Test requires PHP 8.0.');
+		}
+
+		$this->analyse([__DIR__ . '/data/union-type-error.php'], [
+			[
+				'Dead catch - TypeError is never thrown in the try block.',
+				14,
+			],
+			[
+				'Dead catch - TypeError is never thrown in the try block.',
+				22,
+			],
+		]);
+	}
+
 }

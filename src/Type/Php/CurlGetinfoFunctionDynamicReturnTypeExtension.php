611c1111ad6c7122759ab92e1c7560952021e9cf
Frank Prins <25006490+PrinsFrank@users.noreply.github.com>
2022-03-14 22:58:55 +0100

    Don't rely on constants from ext-curl but use reflectionProvider instead

--- src/Type/Php/CurlGetinfoFunctionDynamicReturnTypeExtension.php
+++ src/Type/Php/CurlGetinfoFunctionDynamicReturnTypeExtension.php
@@ -3,9 +3,11 @@
 namespace PHPStan\Type\Php;
 
 use PhpParser\Node\Expr\FuncCall;
+use PhpParser\Node\Name;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\Reflection\ReflectionProvider;
 use PHPStan\ShouldNotHappenException;
 use PHPStan\Type\ArrayType;
 use PHPStan\Type\Constant\ConstantArrayTypeBuilder;
@@ -22,55 +24,15 @@ use PHPStan\Type\Type;
 use PHPStan\Type\TypeCombinator;
 use function count;
 use function strtolower;
-use const CURLINFO_APPCONNECT_TIME;
-use const CURLINFO_CERTINFO;
-use const CURLINFO_CONDITION_UNMET;
-use const CURLINFO_CONNECT_TIME;
-use const CURLINFO_CONTENT_LENGTH_DOWNLOAD;
-use const CURLINFO_CONTENT_LENGTH_UPLOAD;
-use const CURLINFO_CONTENT_TYPE;
-use const CURLINFO_COOKIELIST;
-use const CURLINFO_EFFECTIVE_URL;
-use const CURLINFO_FILETIME;
-use const CURLINFO_FTP_ENTRY_PATH;
-use const CURLINFO_HEADER_OUT;
-use const CURLINFO_HEADER_SIZE;
-use const CURLINFO_HTTP_CONNECTCODE;
-use const CURLINFO_HTTP_VERSION;
-use const CURLINFO_HTTPAUTH_AVAIL;
-use const CURLINFO_LOCAL_IP;
-use const CURLINFO_LOCAL_PORT;
-use const CURLINFO_NAMELOOKUP_TIME;
-use const CURLINFO_NUM_CONNECTS;
-use const CURLINFO_OS_ERRNO;
-use const CURLINFO_PRETRANSFER_TIME;
-use const CURLINFO_PRIMARY_IP;
-use const CURLINFO_PRIMARY_PORT;
-use const CURLINFO_PRIVATE;
-use const CURLINFO_PROTOCOL;
-use const CURLINFO_PROXY_SSL_VERIFYRESULT;
-use const CURLINFO_PROXYAUTH_AVAIL;
-use const CURLINFO_REDIRECT_COUNT;
-use const CURLINFO_REDIRECT_TIME;
-use const CURLINFO_REDIRECT_URL;
-use const CURLINFO_REQUEST_SIZE;
-use const CURLINFO_RESPONSE_CODE;
-use const CURLINFO_RTSP_CLIENT_CSEQ;
-use const CURLINFO_RTSP_CSEQ_RECV;
-use const CURLINFO_RTSP_SERVER_CSEQ;
-use const CURLINFO_RTSP_SESSION_ID;
-use const CURLINFO_SCHEME;
-use const CURLINFO_SIZE_DOWNLOAD;
-use const CURLINFO_SIZE_UPLOAD;
-use const CURLINFO_SPEED_DOWNLOAD;
-use const CURLINFO_SPEED_UPLOAD;
-use const CURLINFO_SSL_ENGINES;
-use const CURLINFO_SSL_VERIFYRESULT;
-use const CURLINFO_STARTTRANSFER_TIME;
-use const CURLINFO_TOTAL_TIME;
 
 final class CurlGetinfoFunctionDynamicReturnTypeExtension implements DynamicFunctionReturnTypeExtension
 {
+
+	public function __construct(
+		private ReflectionProvider $reflectionProvider,
+	)
+	{
+	}
 
 	public function isFunctionSupported(FunctionReflection $functionReflection): bool
 	{
@@ -108,55 +70,62 @@ final class CurlGetinfoFunctionDynamicReturnTypeExtension implements DynamicFunc
 		}
 
 		$componentTypesPairedConstants = [
-			CURLINFO_EFFECTIVE_URL => new StringType(),
-			CURLINFO_FILETIME => new IntegerType(),
-			CURLINFO_TOTAL_TIME => new FloatType(),
-			CURLINFO_NAMELOOKUP_TIME => new FloatType(),
-			CURLINFO_CONNECT_TIME => new FloatType(),
-			CURLINFO_PRETRANSFER_TIME => new FloatType(),
-			CURLINFO_STARTTRANSFER_TIME => new FloatType(),
-			CURLINFO_REDIRECT_COUNT => new IntegerType(),
-			CURLINFO_REDIRECT_TIME => new FloatType(),
-			CURLINFO_REDIRECT_URL => new StringType(),
-			CURLINFO_PRIMARY_IP => new StringType(),
-			CURLINFO_PRIMARY_PORT => new IntegerType(),
-			CURLINFO_LOCAL_IP => new StringType(),
-			CURLINFO_LOCAL_PORT => new IntegerType(),
-			CURLINFO_SIZE_UPLOAD => new FloatType(),
-			CURLINFO_SIZE_DOWNLOAD => new FloatType(),
-			CURLINFO_SPEED_DOWNLOAD => new FloatType(),
-			CURLINFO_SPEED_UPLOAD => new FloatType(),
-			CURLINFO_HEADER_SIZE => new FloatType(),
-			CURLINFO_HEADER_OUT => TypeCombinator::union(new StringType(), new ConstantBooleanType(false)),
-			CURLINFO_REQUEST_SIZE => new FloatType(),
-			CURLINFO_SSL_VERIFYRESULT => new IntegerType(),
-			CURLINFO_CONTENT_LENGTH_DOWNLOAD => new FloatType(),
-			CURLINFO_CONTENT_LENGTH_UPLOAD => new FloatType(),
-			CURLINFO_CONTENT_TYPE => new StringType(),
-			CURLINFO_PRIVATE => TypeCombinator::union(new StringType(), new ConstantBooleanType(false)),
-			CURLINFO_RESPONSE_CODE => new IntegerType(),
-			CURLINFO_HTTP_CONNECTCODE => new IntegerType(),
-			CURLINFO_HTTPAUTH_AVAIL => new IntegerType(),
-			CURLINFO_PROXYAUTH_AVAIL => new IntegerType(),
-			CURLINFO_OS_ERRNO => new IntegerType(),
-			CURLINFO_NUM_CONNECTS => new IntegerType(),
-			CURLINFO_SSL_ENGINES => new ArrayType(new IntegerType(), new StringType()),
-			CURLINFO_COOKIELIST => new ArrayType(new IntegerType(), new StringType()),
-			CURLINFO_FTP_ENTRY_PATH => TypeCombinator::union(new StringType(), new ConstantBooleanType(false)),
-			CURLINFO_APPCONNECT_TIME => new FloatType(),
-			CURLINFO_CERTINFO => new ArrayType(new IntegerType(), new ArrayType(new IntegerType(), new StringType())),
-			CURLINFO_CONDITION_UNMET => new IntegerType(),
-			CURLINFO_RTSP_CLIENT_CSEQ => new IntegerType(),
-			CURLINFO_RTSP_CSEQ_RECV => new IntegerType(),
-			CURLINFO_RTSP_SERVER_CSEQ => new IntegerType(),
-			CURLINFO_RTSP_SESSION_ID => new IntegerType(),
-			CURLINFO_HTTP_VERSION => new IntegerType(),
-			CURLINFO_PROTOCOL => new StringType(),
-			CURLINFO_PROXY_SSL_VERIFYRESULT => new IntegerType(),
-			CURLINFO_SCHEME => new StringType(),
+			'CURLINFO_EFFECTIVE_URL' => new StringType(),
+			'CURLINFO_FILETIME' => new IntegerType(),
+			'CURLINFO_TOTAL_TIME' => new FloatType(),
+			'CURLINFO_NAMELOOKUP_TIME' => new FloatType(),
+			'CURLINFO_CONNECT_TIME' => new FloatType(),
+			'CURLINFO_PRETRANSFER_TIME' => new FloatType(),
+			'CURLINFO_STARTTRANSFER_TIME' => new FloatType(),
+			'CURLINFO_REDIRECT_COUNT' => new IntegerType(),
+			'CURLINFO_REDIRECT_TIME' => new FloatType(),
+			'CURLINFO_REDIRECT_URL' => new StringType(),
+			'CURLINFO_PRIMARY_IP' => new StringType(),
+			'CURLINFO_PRIMARY_PORT' => new IntegerType(),
+			'CURLINFO_LOCAL_IP' => new StringType(),
+			'CURLINFO_LOCAL_PORT' => new IntegerType(),
+			'CURLINFO_SIZE_UPLOAD' => new FloatType(),
+			'CURLINFO_SIZE_DOWNLOAD' => new FloatType(),
+			'CURLINFO_SPEED_DOWNLOAD' => new FloatType(),
+			'CURLINFO_SPEED_UPLOAD' => new FloatType(),
+			'CURLINFO_HEADER_SIZE' => new FloatType(),
+			'CURLINFO_HEADER_OUT' => TypeCombinator::union(new StringType(), new ConstantBooleanType(false)),
+			'CURLINFO_REQUEST_SIZE' => new FloatType(),
+			'CURLINFO_SSL_VERIFYRESULT' => new IntegerType(),
+			'CURLINFO_CONTENT_LENGTH_DOWNLOAD' => new FloatType(),
+			'CURLINFO_CONTENT_LENGTH_UPLOAD' => new FloatType(),
+			'CURLINFO_CONTENT_TYPE' => new StringType(),
+			'CURLINFO_PRIVATE' => TypeCombinator::union(new StringType(), new ConstantBooleanType(false)),
+			'CURLINFO_RESPONSE_CODE' => new IntegerType(),
+			'CURLINFO_HTTP_CONNECTCODE' => new IntegerType(),
+			'CURLINFO_HTTPAUTH_AVAIL' => new IntegerType(),
+			'CURLINFO_PROXYAUTH_AVAIL' => new IntegerType(),
+			'CURLINFO_OS_ERRNO' => new IntegerType(),
+			'CURLINFO_NUM_CONNECTS' => new IntegerType(),
+			'CURLINFO_SSL_ENGINES' => new ArrayType(new IntegerType(), new StringType()),
+			'CURLINFO_COOKIELIST' => new ArrayType(new IntegerType(), new StringType()),
+			'CURLINFO_FTP_ENTRY_PATH' => TypeCombinator::union(new StringType(), new ConstantBooleanType(false)),
+			'CURLINFO_APPCONNECT_TIME' => new FloatType(),
+			'CURLINFO_CERTINFO' => new ArrayType(new IntegerType(), new ArrayType(new IntegerType(), new StringType())),
+			'CURLINFO_CONDITION_UNMET' => new IntegerType(),
+			'CURLINFO_RTSP_CLIENT_CSEQ' => new IntegerType(),
+			'CURLINFO_RTSP_CSEQ_RECV' => new IntegerType(),
+			'CURLINFO_RTSP_SERVER_CSEQ' => new IntegerType(),
+			'CURLINFO_RTSP_SESSION_ID' => new IntegerType(),
+			'CURLINFO_HTTP_VERSION' => new IntegerType(),
+			'CURLINFO_PROTOCOL' => new StringType(),
+			'CURLINFO_PROXY_SSL_VERIFYRESULT' => new IntegerType(),
+			'CURLINFO_SCHEME' => new StringType(),
 		];
 
-		return $componentTypesPairedConstants[$componentType->getValue()] ?? new ConstantBooleanType(false);
+		foreach ($componentTypesPairedConstants as $constantName => $type) {
+			$constantNameNode = new Name($constantName);
+			if ($this->reflectionProvider->hasConstant($constantNameNode, $scope) && $this->reflectionProvider->getConstant($constantNameNode, $scope)->getValueType()->getValue() === $componentType->getValue()) {
+				 return $type;
+			}
+		}
+
+		return new ConstantBooleanType(false);
 	}
 
 	/**

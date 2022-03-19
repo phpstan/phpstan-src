8515c01f9813a1fc7308a7ff31d319f39df666fc
Markus Staab <m.staab@complex-it.de>
2022-03-19 10:09:03 +0100

    added failling test-cases

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -802,6 +802,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-6584.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-6439.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-6748.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/array-search-type-specifying.php');
 	}
 
 	/**

2b9d2128db146f1bc2972ee68fdaafbc94e651f6
rajyan <38206553+rajyan@users.noreply.github.com>
2022-03-24 01:26:49 +0900

    Feature/match expression throws tag

--- tests/PHPStan/Rules/Comparison/MatchExpressionRuleTest.php
+++ tests/PHPStan/Rules/Comparison/MatchExpressionRuleTest.php
@@ -96,6 +96,10 @@ class MatchExpressionRuleTest extends RuleTestCase
 				'Match expression does not handle remaining value: true',
 				90,
 			],
+			[
+				'Match expression does not handle remaining values: int<min, 0>|int<2, max>',
+				168,
+			],
 		]);
 	}
 
@@ -176,7 +180,7 @@ class MatchExpressionRuleTest extends RuleTestCase
 		$this->analyse([__DIR__ . '/data/bug-6115.php'], [
 			[
 				'Match expression does not handle remaining value: 3',
-				28,
+				32,
 			],
 		]);
 	}

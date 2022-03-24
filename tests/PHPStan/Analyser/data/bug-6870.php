abaca6d12ba1f5cc64b4ef50f903c9add86e8b58
rajyan <38206553+rajyan@users.noreply.github.com>
2022-03-24 17:03:29 +0900

    Fix/issue 6870 expr early termination for `BinaryOp`

---
+++ tests/PHPStan/Analyser/data/bug-6870.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types = 1); // lint >= 8.0
+
+namespace Bug6870;
+
+use function PHPStan\Testing\assertType;
+
+class HelloWorld
+{
+	public function foo(?string $data): void
+	{
+		$data === null ? throw new \Exception() : $data;
+		assertType('string', $data);
+	}
+
+	public function buz(?string $data): void
+	{
+		$data !== null ? $data : throw new \Exception();
+		assertType('string', $data);
+	}
+
+	public function bar(?string $data): void
+	{
+		$data || throw new \Exception();
+		assertType('non-empty-string', $data);
+	}
+
+	public function bar2(?string $data): void
+	{
+		$data or throw new \Exception();
+		assertType('non-empty-string', $data);
+	}
+
+	public function baz(?string $data): void
+	{
+		!$data && throw new \Exception();
+		assertType('non-empty-string', $data);
+	}
+
+	public function baz2(?string $data): void
+	{
+		!$data and throw new \Exception();
+		assertType('non-empty-string', $data);
+	}
+
+	public function boo(?string $data): void
+	{
+		$data ?? throw new \Exception();
+		assertType('string', $data);
+	}
+}

9d4e8a5121c01560e1ea9d8631c7fbd75f44cf13
Ondrej Mirtes <ondrej@mirtes.cz>
2022-03-28 07:05:59 +0200

    Try different Scheduler

--- src/Parallel/Scheduler.php
+++ src/Parallel/Scheduler.php
@@ -2,11 +2,17 @@
 
 namespace PHPStan\Parallel;
 
-use function array_chunk;
+use PHPStan\ShouldNotHappenException;
+use function array_keys;
+use function array_pop;
+use function asort;
+use function ceil;
 use function count;
+use function filesize;
 use function floor;
 use function max;
 use function min;
+use function shuffle;
 
 class Scheduler
 {
@@ -30,9 +36,48 @@ class Scheduler
 		array $files,
 	): Schedule
 	{
-		$jobs = array_chunk($files, $this->jobSize);
+		$sizeByFile = [];
+		foreach ($files as $file) {
+			$size = @filesize($file);
+			if ($size === false) {
+				$size = 0;
+			}
+
+			$sizeByFile[$file] = $size;
+		}
+		$jobsCount = (int) ceil(count($files) / $this->jobSize);
+		$jobs = [];
+
+		asort($sizeByFile);
+		$files = array_keys($sizeByFile);
+
+		for ($i = 0; $i < $jobsCount; $i++) {
+			$file = array_pop($files);
+			if ($file === null) {
+				break;
+			}
+			$jobs[] = [$file];
+		}
+
+		shuffle($files);
+
+		foreach ($jobs as $i => $job) {
+			while (count($job) < $this->jobSize) {
+				$file = array_pop($files);
+				if ($file === null) {
+					break;
+				}
+				$job[] = $file;
+			}
+			$jobs[$i] = $job;
+		}
+
+		if (count($files) > 0) {
+			throw new ShouldNotHappenException();
+		}
+
 		$numberOfProcesses = min(
-			max((int) floor(count($jobs) / $this->minimumNumberOfJobsPerProcess), 1),
+			max((int) floor($jobsCount / $this->minimumNumberOfJobsPerProcess), 1),
 			$cpuCores,
 		);
 

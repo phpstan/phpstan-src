66a2bf38990ef9bcb74dce83575e55b051ba4a0e
Markus Staab <m.staab@complex-it.de>
2022-03-30 10:09:53 +0200

    fix

--- tests/PHPStan/Analyser/data/bug-6936-limit.php
+++ tests/PHPStan/Analyser/data/bug-6936-limit.php
@@ -40,10 +40,13 @@ class Foo
 		if (rand(0,1)) {
 			$arr[] = 'h';
 		}
+
+		assertType("array{0: 1|'a'|'b'|'c'|'d'|'e'|'f'|'g'|'h', 1: 2|'b', 2: 3|'c', 3?: 'd', 4?: 'e', 5?: 'f', 6?: 'g', 7?: 'h'}", $arr + $arr2);
 		if (rand(0,1)) {
 			$arr[] = 'i';
 		}
 
+		// fallback to a less precise form, which reduces the union-type size
 		assertType("non-empty-array<0|1|2|3|4|5|6|7|8, 1|2|3|'a'|'b'|'c'|'d'|'e'|'f'|'g'|'h'|'i'>", $arr + $arr2);
 	}
 }

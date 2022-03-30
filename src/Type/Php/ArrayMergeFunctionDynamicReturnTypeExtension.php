565248deba4f553011f77ee8f512e53ff453a795
Markus Staab <maggus.staab@googlemail.com>
2022-03-30 16:57:14 +0200

    array_merge lost non-empty-string keys type

--- src/Type/Php/ArrayMergeFunctionDynamicReturnTypeExtension.php
+++ src/Type/Php/ArrayMergeFunctionDynamicReturnTypeExtension.php
@@ -6,15 +6,17 @@ use PhpParser\Node\Expr\FuncCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\ShouldNotHappenException;
 use PHPStan\Type\Accessory\NonEmptyArrayType;
 use PHPStan\Type\ArrayType;
 use PHPStan\Type\Constant\ConstantArrayType;
+use PHPStan\Type\Constant\ConstantArrayTypeBuilder;
 use PHPStan\Type\DynamicFunctionReturnTypeExtension;
-use PHPStan\Type\GeneralizePrecision;
 use PHPStan\Type\NeverType;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeCombinator;
 use PHPStan\Type\UnionType;
+use function in_array;
 
 class ArrayMergeFunctionDynamicReturnTypeExtension implements DynamicFunctionReturnTypeExtension
 {
@@ -26,15 +28,58 @@ class ArrayMergeFunctionDynamicReturnTypeExtension implements DynamicFunctionRet
 
 	public function getTypeFromFunctionCall(FunctionReflection $functionReflection, FuncCall $functionCall, Scope $scope): Type
 	{
-		if (!isset($functionCall->getArgs()[0])) {
+		$args = $functionCall->getArgs();
+
+		if (!isset($args[0])) {
 			return ParametersAcceptorSelector::selectSingle($functionReflection->getVariants())->getReturnType();
+		}
+
+		$argTypes = [];
+		$allConstant = true;
+		foreach ($args as $i => $arg) {
+			$argType = $scope->getType($arg->value);
+			$argTypes[$i] = $argType;
+
+			if (!$arg->unpack && $argType instanceof ConstantArrayType) {
+				continue;
+			}
+
+			$allConstant = false;
+		}
+
+		if ($allConstant) {
+			$newArrayBuilder = ConstantArrayTypeBuilder::createEmpty();
+			foreach ($args as $i => $arg) {
+				$argType = $argTypes[$i];
+
+				if (!$argType instanceof ConstantArrayType) {
+					throw new ShouldNotHappenException();
+				}
+
+				$keyTypes = $argType->getKeyTypes();
+				$valueTypes = $argType->getValueTypes();
+				$optionalKeys = $argType->getOptionalKeys();
+
+				foreach ($keyTypes as $k => $keyType) {
+					$isOptional = in_array($k, $optionalKeys, true);
+
+					$newArrayBuilder->setOffsetValueType(
+						$keyType,
+						$valueTypes[$k],
+						$isOptional,
+					);
+				}
+			}
+
+			return $newArrayBuilder->getArray();
 		}
 
 		$keyTypes = [];
 		$valueTypes = [];
 		$nonEmpty = false;
-		foreach ($functionCall->getArgs() as $arg) {
-			$argType = $scope->getType($arg->value);
+		foreach ($args as $i => $arg) {
+			$argType = $argTypes[$i];
+
 			if ($arg->unpack) {
 				$argType = $argType->getIterableValueType();
 				if ($argType instanceof UnionType) {
@@ -44,7 +89,7 @@ class ArrayMergeFunctionDynamicReturnTypeExtension implements DynamicFunctionRet
 				}
 			}
 
-			$keyTypes[] = $argType->getIterableKeyType()->generalize(GeneralizePrecision::moreSpecific());
+			$keyTypes[] = $argType->getIterableKeyType();
 			$valueTypes[] = $argType->getIterableValueType();
 
 			if (!$argType->isIterableAtLeastOnce()->yes()) {

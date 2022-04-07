35964eb8bcce4a5323b9184a6bb3a6e7c9014603
Richard van Velzen <richard@frank.nl>
2022-04-07 13:04:58 +0200

    Extract resolving dynamic constants into a helper
    See #1163 for discussion. `TypeNodeResolver` needs to be able to resolve constants the same way as `MutatingScope` does

---
+++ src/Analyser/ConstantResolver.php
@@ -0,0 +1,267 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Analyser;
+
+use PhpParser\Node\Name;
+use PHPStan\DependencyInjection\Container;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Type\Accessory\AccessoryNonEmptyStringType;
+use PHPStan\Type\Constant\ConstantIntegerType;
+use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\Type\ConstantType;
+use PHPStan\Type\GeneralizePrecision;
+use PHPStan\Type\IntegerRangeType;
+use PHPStan\Type\IntersectionType;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+use PHPStan\Type\UnionType;
+use function in_array;
+use const PHP_INT_SIZE;
+
+class ConstantResolver
+{
+
+	/** @var string[] */
+	private array $dynamicConstantNames;
+
+	public function __construct(
+		private ReflectionProvider $reflectionProvider,
+		Container $container,
+	)
+	{
+		$this->dynamicConstantNames = $container->getParameter('dynamicConstantNames');
+	}
+
+	public function resolveConstant(Name $name, ?Scope $scope): ?Type
+	{
+		if (!$this->reflectionProvider->hasConstant($name, $scope)) {
+			return null;
+		}
+
+		/** @var string $resolvedConstantName */
+		$resolvedConstantName = $this->reflectionProvider->resolveConstantName($name, $scope);
+		// core, https://www.php.net/manual/en/reserved.constants.php
+		if ($resolvedConstantName === 'PHP_VERSION') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_MAJOR_VERSION') {
+			return IntegerRangeType::fromInterval(5, null);
+		}
+		if ($resolvedConstantName === 'PHP_MINOR_VERSION') {
+			return IntegerRangeType::fromInterval(0, null);
+		}
+		if ($resolvedConstantName === 'PHP_RELEASE_VERSION') {
+			return IntegerRangeType::fromInterval(0, null);
+		}
+		if ($resolvedConstantName === 'PHP_VERSION_ID') {
+			return IntegerRangeType::fromInterval(50207, null);
+		}
+		if ($resolvedConstantName === 'PHP_ZTS') {
+			return new UnionType([
+				new ConstantIntegerType(0),
+				new ConstantIntegerType(1),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_DEBUG') {
+			return new UnionType([
+				new ConstantIntegerType(0),
+				new ConstantIntegerType(1),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_MAXPATHLEN') {
+			return IntegerRangeType::fromInterval(1, null);
+		}
+		if ($resolvedConstantName === 'PHP_OS') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_OS_FAMILY') {
+			return new UnionType([
+				new ConstantStringType('Windows'),
+				new ConstantStringType('BSD'),
+				new ConstantStringType('Darwin'),
+				new ConstantStringType('Solaris'),
+				new ConstantStringType('Linux'),
+				new ConstantStringType('Unknown'),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_SAPI') {
+			return new UnionType([
+				new ConstantStringType('apache'),
+				new ConstantStringType('apache2handler'),
+				new ConstantStringType('cgi'),
+				new ConstantStringType('cli'),
+				new ConstantStringType('cli-server'),
+				new ConstantStringType('embed'),
+				new ConstantStringType('fpm-fcgi'),
+				new ConstantStringType('litespeed'),
+				new ConstantStringType('phpdbg'),
+				new IntersectionType([
+					new StringType(),
+					new AccessoryNonEmptyStringType(),
+				]),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_EOL') {
+			return new UnionType([
+				new ConstantStringType("\n"),
+				new ConstantStringType("\r\n"),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_INT_MAX') {
+			return PHP_INT_SIZE === 8
+				? new UnionType([new ConstantIntegerType(2147483647), new ConstantIntegerType(9223372036854775807)])
+				: new ConstantIntegerType(2147483647);
+		}
+		if ($resolvedConstantName === 'PHP_INT_MIN') {
+			// Why the -1 you might wonder, the answer is to fit it into an int :/ see https://3v4l.org/4SHIQ
+			return PHP_INT_SIZE === 8
+				? new UnionType([new ConstantIntegerType(-9223372036854775807 - 1), new ConstantIntegerType(-2147483647 - 1)])
+				: new ConstantIntegerType(-2147483647 - 1);
+		}
+		if ($resolvedConstantName === 'PHP_INT_SIZE') {
+			return new UnionType([
+				new ConstantIntegerType(4),
+				new ConstantIntegerType(8),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_FLOAT_DIG') {
+			return IntegerRangeType::fromInterval(1, null);
+		}
+		if ($resolvedConstantName === 'PHP_EXTENSION_DIR') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_PREFIX') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_BINDIR') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_BINARY') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_MANDIR') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_LIBDIR') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_DATADIR') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_SYSCONFDIR') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_LOCALSTATEDIR') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_CONFIG_FILE_PATH') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_SHLIB_SUFFIX') {
+			return new UnionType([
+				new ConstantStringType('so'),
+				new ConstantStringType('dll'),
+			]);
+		}
+		if ($resolvedConstantName === 'PHP_FD_SETSIZE') {
+			return IntegerRangeType::fromInterval(1, null);
+		}
+		if ($resolvedConstantName === '__COMPILER_HALT_OFFSET__') {
+			return IntegerRangeType::fromInterval(0, null);
+		}
+		// core other, https://www.php.net/manual/en/info.constants.php
+		if ($resolvedConstantName === 'PHP_WINDOWS_VERSION_MAJOR') {
+			return IntegerRangeType::fromInterval(4, null);
+		}
+		if ($resolvedConstantName === 'PHP_WINDOWS_VERSION_MINOR') {
+			return IntegerRangeType::fromInterval(0, null);
+		}
+		if ($resolvedConstantName === 'PHP_WINDOWS_VERSION_BUILD') {
+			return IntegerRangeType::fromInterval(1, null);
+		}
+		// dir, https://www.php.net/manual/en/dir.constants.php
+		if ($resolvedConstantName === 'DIRECTORY_SEPARATOR') {
+			return new UnionType([
+				new ConstantStringType('/'),
+				new ConstantStringType('\\'),
+			]);
+		}
+		if ($resolvedConstantName === 'PATH_SEPARATOR') {
+			return new UnionType([
+				new ConstantStringType(':'),
+				new ConstantStringType(';'),
+			]);
+		}
+		// iconv, https://www.php.net/manual/en/iconv.constants.php
+		if ($resolvedConstantName === 'ICONV_IMPL') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		// libxml, https://www.php.net/manual/en/libxml.constants.php
+		if ($resolvedConstantName === 'LIBXML_VERSION') {
+			return IntegerRangeType::fromInterval(1, null);
+		}
+		if ($resolvedConstantName === 'LIBXML_DOTTED_VERSION') {
+			return new IntersectionType([
+				new StringType(),
+				new AccessoryNonEmptyStringType(),
+			]);
+		}
+		// openssl, https://www.php.net/manual/en/openssl.constants.php
+		if ($resolvedConstantName === 'OPENSSL_VERSION_NUMBER') {
+			return IntegerRangeType::fromInterval(1, null);
+		}
+
+		$constantType = $this->reflectionProvider->getConstant($name, $scope)->getValueType();
+
+		return $this->resolveConstantType($resolvedConstantName, $constantType);
+	}
+
+	public function resolveConstantType(string $constantName, Type $constantType): Type
+	{
+		if ($constantType instanceof ConstantType && in_array($constantName, $this->dynamicConstantNames, true)) {
+			return $constantType->generalize(GeneralizePrecision::lessSpecific());
+		}
+
+		return $constantType;
+	}
+
+}
--- src/Analyser/MutatingScope.php
+++ src/Analyser/MutatingScope.php
@@ -75,7 +75,6 @@ use PHPStan\Type\Constant\ConstantFloatType;
 use PHPStan\Type\Constant\ConstantIntegerType;
 use PHPStan\Type\Constant\ConstantStringType;
 use PHPStan\Type\ConstantScalarType;
-use PHPStan\Type\ConstantType;
 use PHPStan\Type\ConstantTypeHelper;
 use PHPStan\Type\DynamicReturnTypeExtensionRegistry;
 use PHPStan\Type\Enum\EnumCaseObjectType;
@@ -136,7 +135,6 @@ use function substr;
 use function usort;
 use const PHP_INT_MAX;
 use const PHP_INT_MIN;
-use const PHP_INT_SIZE;
 
 class MutatingScope implements Scope
 {
@@ -171,7 +169,6 @@ class MutatingScope implements Scope
 	 * @param array<string, bool> $currentlyAllowedUndefinedExpressions
 	 * @param array<string, Type> $nativeExpressionTypes
 	 * @param array<MethodReflection|FunctionReflection> $inFunctionCallsStack
-	 * @param string[] $dynamicConstantNames
 	 */
 	public function __construct(
 		private ScopeFactory $scopeFactory,
@@ -183,6 +180,7 @@ class MutatingScope implements Scope
 		private PropertyReflectionFinder $propertyReflectionFinder,
 		private Parser $parser,
 		private NodeScopeResolver $nodeScopeResolver,
+		private ConstantResolver $constantResolver,
 		private ScopeContext $context,
 		private PhpVersion $phpVersion,
 		private bool $declareStrictTypes = false,
@@ -199,7 +197,6 @@ class MutatingScope implements Scope
 		private array $currentlyAllowedUndefinedExpressions = [],
 		private array $nativeExpressionTypes = [],
 		private array $inFunctionCallsStack = [],
-		private array $dynamicConstantNames = [],
 		private bool $treatPhpDocTypesAsCertain = true,
 		private bool $afterExtractCall = false,
 		private ?Scope $parentScope = null,
@@ -2063,238 +2060,25 @@ class MutatingScope implements Scope
 
 			if ($node->name->isFullyQualified()) {
 				if (array_key_exists($node->name->toCodeString(), $this->constantTypes)) {
-					return $this->resolveConstantType($node->name->toString(), $this->constantTypes[$node->name->toCodeString()]);
+					return $this->constantResolver->resolveConstantType($node->name->toString(), $this->constantTypes[$node->name->toCodeString()]);
 				}
 			}
 
 			if ($this->getNamespace() !== null) {
 				$constantName = new FullyQualified([$this->getNamespace(), $constName]);
 				if (array_key_exists($constantName->toCodeString(), $this->constantTypes)) {
-					return $this->resolveConstantType($constantName->toString(), $this->constantTypes[$constantName->toCodeString()]);
+					return $this->constantResolver->resolveConstantType($constantName->toString(), $this->constantTypes[$constantName->toCodeString()]);
 				}
 			}
 
 			$constantName = new FullyQualified($constName);
 			if (array_key_exists($constantName->toCodeString(), $this->constantTypes)) {
-				return $this->resolveConstantType($constantName->toString(), $this->constantTypes[$constantName->toCodeString()]);
+				return $this->constantResolver->resolveConstantType($constantName->toString(), $this->constantTypes[$constantName->toCodeString()]);
 			}
 
-			if ($this->reflectionProvider->hasConstant($node->name, $this)) {
-				/** @var string $resolvedConstantName */
-				$resolvedConstantName = $this->reflectionProvider->resolveConstantName($node->name, $this);
-				// core, https://www.php.net/manual/en/reserved.constants.php
-				if ($resolvedConstantName === 'PHP_VERSION') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_MAJOR_VERSION') {
-					return IntegerRangeType::fromInterval(5, null);
-				}
-				if ($resolvedConstantName === 'PHP_MINOR_VERSION') {
-					return IntegerRangeType::fromInterval(0, null);
-				}
-				if ($resolvedConstantName === 'PHP_RELEASE_VERSION') {
-					return IntegerRangeType::fromInterval(0, null);
-				}
-				if ($resolvedConstantName === 'PHP_VERSION_ID') {
-					return IntegerRangeType::fromInterval(50207, null);
-				}
-				if ($resolvedConstantName === 'PHP_ZTS') {
-					return new UnionType([
-						new ConstantIntegerType(0),
-						new ConstantIntegerType(1),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_DEBUG') {
-					return new UnionType([
-						new ConstantIntegerType(0),
-						new ConstantIntegerType(1),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_MAXPATHLEN') {
-					return IntegerRangeType::fromInterval(1, null);
-				}
-				if ($resolvedConstantName === 'PHP_OS') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_OS_FAMILY') {
-					return new UnionType([
-						new ConstantStringType('Windows'),
-						new ConstantStringType('BSD'),
-						new ConstantStringType('Darwin'),
-						new ConstantStringType('Solaris'),
-						new ConstantStringType('Linux'),
-						new ConstantStringType('Unknown'),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_SAPI') {
-					return new UnionType([
-						new ConstantStringType('apache'),
-						new ConstantStringType('apache2handler'),
-						new ConstantStringType('cgi'),
-						new ConstantStringType('cli'),
-						new ConstantStringType('cli-server'),
-						new ConstantStringType('embed'),
-						new ConstantStringType('fpm-fcgi'),
-						new ConstantStringType('litespeed'),
-						new ConstantStringType('phpdbg'),
-						new IntersectionType([
-							new StringType(),
-							new AccessoryNonEmptyStringType(),
-						]),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_EOL') {
-					return new UnionType([
-						new ConstantStringType("\n"),
-						new ConstantStringType("\r\n"),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_INT_MAX') {
-					return PHP_INT_SIZE === 8
-						? new UnionType([new ConstantIntegerType(2147483647), new ConstantIntegerType(9223372036854775807)])
-						: new ConstantIntegerType(2147483647);
-				}
-				if ($resolvedConstantName === 'PHP_INT_MIN') {
-					// Why the -1 you might wonder, the answer is to fit it into an int :/ see https://3v4l.org/4SHIQ
-					return PHP_INT_SIZE === 8
-						? new UnionType([new ConstantIntegerType(-9223372036854775807 - 1), new ConstantIntegerType(-2147483647 - 1)])
-						: new ConstantIntegerType(-2147483647 - 1);
-				}
-				if ($resolvedConstantName === 'PHP_INT_SIZE') {
-					return new UnionType([
-						new ConstantIntegerType(4),
-						new ConstantIntegerType(8),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_FLOAT_DIG') {
-					return IntegerRangeType::fromInterval(1, null);
-				}
-				if ($resolvedConstantName === 'PHP_EXTENSION_DIR') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_PREFIX') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_BINDIR') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_BINARY') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_MANDIR') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_LIBDIR') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_DATADIR') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_SYSCONFDIR') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_LOCALSTATEDIR') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_CONFIG_FILE_PATH') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_SHLIB_SUFFIX') {
-					return new UnionType([
-						new ConstantStringType('so'),
-						new ConstantStringType('dll'),
-					]);
-				}
-				if ($resolvedConstantName === 'PHP_FD_SETSIZE') {
-					return IntegerRangeType::fromInterval(1, null);
-				}
-				if ($resolvedConstantName === '__COMPILER_HALT_OFFSET__') {
-					return IntegerRangeType::fromInterval(0, null);
-				}
-				// core other, https://www.php.net/manual/en/info.constants.php
-				if ($resolvedConstantName === 'PHP_WINDOWS_VERSION_MAJOR') {
-					return IntegerRangeType::fromInterval(4, null);
-				}
-				if ($resolvedConstantName === 'PHP_WINDOWS_VERSION_MINOR') {
-					return IntegerRangeType::fromInterval(0, null);
-				}
-				if ($resolvedConstantName === 'PHP_WINDOWS_VERSION_BUILD') {
-					return IntegerRangeType::fromInterval(1, null);
-				}
-				// dir, https://www.php.net/manual/en/dir.constants.php
-				if ($resolvedConstantName === 'DIRECTORY_SEPARATOR') {
-					return new UnionType([
-						new ConstantStringType('/'),
-						new ConstantStringType('\\'),
-					]);
-				}
-				if ($resolvedConstantName === 'PATH_SEPARATOR') {
-					return new UnionType([
-						new ConstantStringType(':'),
-						new ConstantStringType(';'),
-					]);
-				}
-				// iconv, https://www.php.net/manual/en/iconv.constants.php
-				if ($resolvedConstantName === 'ICONV_IMPL') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				// libxml, https://www.php.net/manual/en/libxml.constants.php
-				if ($resolvedConstantName === 'LIBXML_VERSION') {
-					return IntegerRangeType::fromInterval(1, null);
-				}
-				if ($resolvedConstantName === 'LIBXML_DOTTED_VERSION') {
-					return new IntersectionType([
-						new StringType(),
-						new AccessoryNonEmptyStringType(),
-					]);
-				}
-				// openssl, https://www.php.net/manual/en/openssl.constants.php
-				if ($resolvedConstantName === 'OPENSSL_VERSION_NUMBER') {
-					return IntegerRangeType::fromInterval(1, null);
-				}
-
-				$constantType = $this->reflectionProvider->getConstant($node->name, $this)->getValueType();
-
-				return $this->resolveConstantType($resolvedConstantName, $constantType);
+			$constantType = $this->constantResolver->resolveConstant($node->name, $this);
+			if ($constantType !== null) {
+				return $constantType;
 			}
 
 			return new ErrorType();
@@ -2389,12 +2173,10 @@ class MutatingScope implements Scope
 					$constantType = ConstantTypeHelper::getTypeFromValue($constantReflection->getValue());
 				}
 
-				if (
-					$constantType instanceof ConstantType
-					&& in_array(sprintf('%s::%s', $constantClassReflection->getName(), $constantName), $this->dynamicConstantNames, true)
-				) {
-					$constantType = $constantType->generalize(GeneralizePrecision::lessSpecific());
-				}
+				$constantType = $this->constantResolver->resolveConstantType(
+					sprintf('%s::%s', $constantClassReflection->getName(), $constantName),
+					$constantType,
+				);
 				$types[] = $constantType;
 			}
 
@@ -2941,15 +2723,6 @@ class MutatingScope implements Scope
 		return TypeCombinator::union(...$closureTypes);
 	}
 
-	private function resolveConstantType(string $constantName, Type $constantType): Type
-	{
-		if ($constantType instanceof ConstantType && in_array($constantName, $this->dynamicConstantNames, true)) {
-			return $constantType->generalize(GeneralizePrecision::lessSpecific());
-		}
-
-		return $constantType;
-	}
-
 	/** @api */
 	public function getNativeType(Expr $expr): Type
 	{
@@ -2990,6 +2763,7 @@ class MutatingScope implements Scope
 			$this->propertyReflectionFinder,
 			$this->parser,
 			$this->nodeScopeResolver,
+			$this->constantResolver,
 			$this->context,
 			$this->phpVersion,
 			$this->declareStrictTypes,
@@ -3006,7 +2780,6 @@ class MutatingScope implements Scope
 			$this->currentlyAllowedUndefinedExpressions,
 			$this->nativeExpressionTypes,
 			$this->inFunctionCallsStack,
-			$this->dynamicConstantNames,
 			false,
 			$this->afterExtractCall,
 			$this->parentScope,

af246e203b68a0b06244139efacd5f2009d255d4
Yohta Kimura <kitakita7617@gmail.com>
2022-04-11 09:36:03 +0900

    modify regression tests

--- tests/PHPStan/Rules/Arrays/data/bug-7000.php
+++ tests/PHPStan/Rules/Arrays/data/bug-7000.php
@@ -8,9 +8,12 @@ class Foo
 	{
 		/** @var array{require?: array<string, string>, require-dev?: array<string, string>} $composer */
 		$composer = array();
+		/** @var 'require'|'require-dev' $foo */
+		$foo = '';
 		foreach (array('require', 'require-dev') as $linkType) {
 			if (isset($composer[$linkType])) {
-				foreach ($composer[$linkType] as $x) {}
+				foreach ($composer[$linkType] as $x) {} // should not report error
+				foreach ($composer[$foo] as $x) {} // should report error. It can be $linkType = 'require', $foo = 'require-dev'
 			}
 		}
 	}

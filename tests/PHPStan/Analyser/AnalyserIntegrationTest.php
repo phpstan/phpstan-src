c5d0902d2b3fbf8f00913002f9ba72b6caa929a0
Ondrej Mirtes <ondrej@mirtes.cz>
2022-04-14 13:37:56 +0200

    Fix int overflow internal error

--- tests/PHPStan/Analyser/AnalyserIntegrationTest.php
+++ tests/PHPStan/Analyser/AnalyserIntegrationTest.php
@@ -641,6 +641,12 @@ class AnalyserIntegrationTest extends PHPStanTestCase
 		$this->assertNoErrors($errors);
 	}
 
+	public function testBug6979(): void
+	{
+		$errors = $this->runAnalyse(__DIR__ . '/data/bug-6979.php');
+		$this->assertNoErrors($errors);
+	}
+
 	/**
 	 * @param string[]|null $allAnalysedFiles
 	 * @return Error[]

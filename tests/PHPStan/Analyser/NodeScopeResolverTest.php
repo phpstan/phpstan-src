f091c5629a48620972c657993032522def8a840d
Ondrej Mirtes <ondrej@mirtes.cz>
2022-04-20 11:02:14 +0200

    Fix variadic parameters for generics

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -867,6 +867,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-6927.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/constant-array-optional-set.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-7068.php');
 	}
 
 	/**

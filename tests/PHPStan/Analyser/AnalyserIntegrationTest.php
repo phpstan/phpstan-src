25d07000ca183b35dfe089480b6bfed41e4c976a
Ondrej Mirtes <ondrej@mirtes.cz>
2022-04-22 10:12:08 +0200

    Fix

--- tests/PHPStan/Analyser/AnalyserIntegrationTest.php
+++ tests/PHPStan/Analyser/AnalyserIntegrationTest.php
@@ -743,6 +743,10 @@ class AnalyserIntegrationTest extends PHPStanTestCase
 
 	public function testBug7078(): void
 	{
+		if (PHP_VERSION_ID < 80000) {
+			$this->markTestSkipped('Test requires PHP 8.0.');
+		}
+
 		$errors = $this->runAnalyse(__DIR__ . '/data/bug-7078.php');
 		$this->assertNoErrors($errors);
 	}

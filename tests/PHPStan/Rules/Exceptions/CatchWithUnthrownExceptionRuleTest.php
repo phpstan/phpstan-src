1223fcadae709138a2b6e14a5f31e6b40b9c51fa
Yohta Kimura <kitakita7617@gmail.com>
2022-05-04 02:05:45 +0900

    division with float can also throw DivisionByZeroError

--- tests/PHPStan/Rules/Exceptions/CatchWithUnthrownExceptionRuleTest.php
+++ tests/PHPStan/Rules/Exceptions/CatchWithUnthrownExceptionRuleTest.php
@@ -355,14 +355,6 @@ class CatchWithUnthrownExceptionRuleTest extends RuleTestCase
 			],
 			[
 				'Dead catch - DivisionByZeroError is never thrown in the try block.',
-				74,
-			],
-			[
-				'Dead catch - DivisionByZeroError is never thrown in the try block.',
-				78,
-			],
-			[
-				'Dead catch - DivisionByZeroError is never thrown in the try block.',
 				121,
 			],
 			[

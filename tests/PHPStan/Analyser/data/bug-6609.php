7bd9fb7285ee8cab34dac2d29f84485ad3c40643
Ondrej Mirtes <ondrej@mirtes.cz>
2022-05-05 00:33:27 +0200

    Make DateTime::modify() behaviour less annoying

--- tests/PHPStan/Analyser/data/bug-6609.php
+++ tests/PHPStan/Analyser/data/bug-6609.php
@@ -23,4 +23,36 @@ class Foo
 		return $date;
 	}
 
+	/**
+	 * This method should return the same type as a parameter passed.
+	 *
+	 * @template T of \DateTime|\DateTimeImmutable
+	 *
+	 * @param T $date
+	 *
+	 * @return T
+	 */
+	function modify2(\DateTimeInterface $date) {
+		$date = $date->modify('invalidd');
+		assertType('false', $date);
+
+		return $date;
+	}
+
+	/**
+	 * This method should return the same type as a parameter passed.
+	 *
+	 * @template T of \DateTime|\DateTimeImmutable
+	 *
+	 * @param T $date
+	 *
+	 * @return T
+	 */
+	function modify3(\DateTimeInterface $date, string $s) {
+		$date = $date->modify($s);
+		assertType('(DateTime|DateTimeImmutable|false)', $date);
+
+		return $date;
+	}
+
 }

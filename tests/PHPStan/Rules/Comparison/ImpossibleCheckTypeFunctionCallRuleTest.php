49cb07b173b2917d6139f007e6e1829b53cd6242
Markus Staab <m.staab@complex-it.de>
2022-05-13 09:24:08 +0200

    fix

--- tests/PHPStan/Rules/Comparison/ImpossibleCheckTypeFunctionCallRuleTest.php
+++ tests/PHPStan/Rules/Comparison/ImpossibleCheckTypeFunctionCallRuleTest.php
@@ -545,9 +545,10 @@ class ImpossibleCheckTypeFunctionCallRuleTest extends RuleTestCase
 				12,
 			],
 			[
-				"Call to function is_a() with 'Bug4371\\\\Bar' and 'Bug4371\\\\Foo' will always evaluate to false.",
+				"Call to function is_a() with arguments 'Bug4371\\\\Bar', 'Bug4371\\\\Foo' and false will always evaluate to false.",
 				18,
 			],
 		]);
 	}
+
 }

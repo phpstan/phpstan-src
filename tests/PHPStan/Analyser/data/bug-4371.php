5340a7610dced807105253139b1731c82a2d85d8
Markus Staab <maggus.staab@googlemail.com>
2022-05-13 15:22:02 +0200

    drop "string" tests.. not a valid type to pass into is_a() anymore

--- tests/PHPStan/Analyser/data/bug-4371.php
+++ tests/PHPStan/Analyser/data/bug-4371.php
@@ -12,7 +12,7 @@ class HelloWorld
 	 * @param Exception $exception
 	 * @param class-string $classString
 	 */
-	public function sayHello(Exception $exception, $classString, string $s): void
+	public function sayHello(Exception $exception, $classString): void
 	{
 		assertType('bool', is_a($exception, $classString));
 		assertType('bool', is_a($exception, $classString, true));
@@ -22,9 +22,5 @@ class HelloWorld
 		assertType('false', is_a($exceptionClass, $classString));
 		assertType('bool', is_a($exceptionClass, $classString, true));
 		assertType('false', is_a($exceptionClass, $classString, false));
-
-		assertType('false', is_a($exceptionClass, $s));
-		assertType('bool', is_a($exceptionClass, $s, true));
-		assertType('false', is_a($exceptionClass, $s, false));
 	}
 }

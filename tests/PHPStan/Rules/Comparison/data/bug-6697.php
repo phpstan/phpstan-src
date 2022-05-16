20ff2f3e8c0f80d596cbc97c0bc3242b31c29ec0
Markus Staab <m.staab@complex-it.de>
2022-05-16 21:07:43 +0200

    more tests

--- tests/PHPStan/Rules/Comparison/data/bug-6697.php
+++ tests/PHPStan/Rules/Comparison/data/bug-6697.php
@@ -4,6 +4,10 @@ declare(strict_types=1);
 
 namespace Bug6697;
 
+function foo() {
+	$result = \is_subclass_of( '\\My\\Namespace\\MyClass', '\\My\\Namespace\\MyBaseClass', true);
+}
+
 trait H
 {
 	public static function isEnum(): bool

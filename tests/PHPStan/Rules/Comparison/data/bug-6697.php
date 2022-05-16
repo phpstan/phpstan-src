a1c0883d250ae11853be4cb989a6282a2b5ca0f9
Markus Staab <m.staab@complex-it.de>
2022-05-16 19:07:42 +0200

    reproduce bug 6697

---
+++ tests/PHPStan/Rules/Comparison/data/bug-6697.php
@@ -0,0 +1,33 @@
+<?php // lint >= 8.1
+
+declare(strict_types=1);
+
+namespace Bug6697;
+
+trait H
+{
+	public static function isEnum(): bool
+	{
+		return \is_subclass_of(static::class, \UnitEnum::class);
+	}
+
+	public static function isBacked(): bool
+	{
+		return \is_subclass_of(static::class, \BackedEnum::class);
+	}
+}
+
+enum BEnum: string
+{
+	use H;
+
+	case BAR = "bar";
+}
+
+enum UEnum
+{
+	use H;
+
+	case BAR;
+}
+

7b6d2b80bf2e5a3d4de2bf5a40678a6286a1abc8
Markus Staab <m.staab@complex-it.de>
2022-05-16 21:13:03 +0200

    fix test

--- tests/PHPStan/Rules/Comparison/data/bug-6697.php
+++ tests/PHPStan/Rules/Comparison/data/bug-6697.php
@@ -7,31 +7,3 @@ namespace Bug6697;
 function foo() {
 	$result = \is_subclass_of( '\\My\\Namespace\\MyClass', '\\My\\Namespace\\MyBaseClass', true);
 }
-
-trait H
-{
-	public static function isEnum(): bool
-	{
-		return \is_subclass_of(static::class, \UnitEnum::class);
-	}
-
-	public static function isBacked(): bool
-	{
-		return \is_subclass_of(static::class, \BackedEnum::class);
-	}
-}
-
-enum BEnum: string
-{
-	use H;
-
-	case BAR = "bar";
-}
-
-enum UEnum
-{
-	use H;
-
-	case BAR;
-}
-

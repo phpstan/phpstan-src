3fe16961b9a760bd5566de083d5944b502c013b1
Markus Staab <m.staab@complex-it.de>
2022-05-18 09:18:05 +0200

    fix test

--- tests/PHPStan/Rules/Functions/data/bug-6987.php
+++ tests/PHPStan/Rules/Functions/data/bug-6987.php
@@ -2,6 +2,22 @@
 
 namespace Bug6987;
 
+/**
+ * @param 'enabled'|'disabled'|'cancelled' $key
+ */
+function transformKey(string $key): int {
+	switch($key){
+		case 'enabled':
+			return 1;
+		case 'disabled':
+			return 2;
+		case 'cancelled':
+			return 3;
+		default:
+			return 0;
+	}
+}
+
 class Foo {
 	public function doFoo()
 	{
@@ -13,27 +29,10 @@ class Foo {
 
 		$map = [];
 		foreach($availableValues as $key => $value){
-			$map[$this->transformKey(strtolower($key))] = $value;
+			$map[transformKey(strtolower($key))] = $value;
 		}
 
 		return $map;
 
 	}
-
-	/**
-	 * @param 'enabled'|'disabled'|'cancelled' $key
-	 */
-	function transformKey(string $key): int {
-		switch($key){
-			case 'enabled':
-				return 1;
-			case 'disabled':
-				return 2;
-			case 'cancelled':
-				return 3;
-			default:
-				return 0;
-		}
-	}
-
 }

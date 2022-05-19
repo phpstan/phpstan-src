0511e363a590a83c8541f3c7c7486df1a2cc1784
Richard van Velzen <richard@frank.nl>
2022-05-19 17:14:06 +0200

    Add empty pseudo-type (used by PHPUnit)

---
+++ tests/PHPStan/Analyser/data/assert-empty.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace AssertEmpty;
+
+use function PHPStan\assertType;
+
+/**
+ * @phpstan-assert empty $var
+ */
+function assertEmpty(mixed $var): void
+{
+}
+
+/**
+ * @phpstan-assert !empty $var
+ */
+function assertNotEmpty(mixed $var): void
+{
+}
+
+function ($var) {
+	assertEmpty($var);
+	assertType("0|0.0|''|'0'|array{}|false|null", $var);
+};
+
+function ($var) {
+	assertNotEmpty($var);
+	assertType("mixed~0|0.0|''|'0'|array{}|false|null", $var);
+};

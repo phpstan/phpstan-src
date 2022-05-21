c909ae682bababd68151c243fcb5d7a101f5dd3b
Martin Herndl <martin@herndl.org>
2022-05-21 21:29:09 +0200

    Improve `array_reverse` with constant arrays

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -802,6 +802,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/array-search-type-specifying.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/array-push.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/array-replace.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/array-reverse.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-6889.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-6891.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/simplexml.php');
---
+++ tests/PHPStan/Analyser/data/array-reverse.php
@@ -0,0 +1,40 @@
+<?php declare(strict_types = 1);
+
+namespace ArrayReverse;
+
+use function PHPStan\Testing\assertType;
+
+class Foo
+{
+	/**
+	 * @param mixed[] $a
+	 * @param array<string, int> $b
+	 */
+	public function normalArrays(array $a, array $b): void
+	{
+		assertType('array', array_reverse($a));
+		assertType('array', array_reverse($a, true));
+
+		assertType('array<string, int>', array_reverse($b));
+		assertType('array<string, int>', array_reverse($b, true));
+	}
+
+	/** @param array{a: 'foo', b: 'bar', c?: 'baz'} $a */
+	public function constantArrays(array $a): void
+	{
+		assertType('array{1337, null, 42}', array_reverse([42, null, 1337]));
+		assertType('array{2: 1337, 1: null, 0: 42}', array_reverse([42, null, 1337], true));
+
+		assertType('array{test3: 1337, test2: null, test1: 42}', array_reverse(['test1' => 42, 'test2' => null, 'test3' => 1337]));
+		assertType('array{test3: 1337, test2: null, test1: 42}', array_reverse(['test1' => 42, 'test2' => null, 'test3' => 1337], true));
+
+		assertType('array{test3: 1337, test2: \'test 2\', 0: 42}', array_reverse([42, 'test2' => 'test 2', 'test3' => 1337]));
+		assertType('array{test3: 1337, test2: \'test 2\', 0: 42}', array_reverse([42, 'test2' => 'test 2', 'test3' => 1337], true));
+
+		assertType('array{bar: 17, 0: 1337, foo: null, 1: 42}', array_reverse([2 => 42, 'foo' => null, 3 => 1337, 'bar' => 17]));
+		assertType('array{bar: 17, 3: 1337, foo: null, 2: 42}', array_reverse([2 => 42, 'foo' => null, 3 => 1337, 'bar' => 17], true));
+
+		assertType('array{c?: \'baz\', b: \'bar\', a: \'foo\'}', array_reverse($a));
+		assertType('array{c?: \'baz\', b: \'bar\', a: \'foo\'}', array_reverse($a, true));
+	}
+}

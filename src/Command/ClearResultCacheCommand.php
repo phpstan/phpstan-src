5f5b96d432207395dc7371f62a9d52081f56a405
Ondrej Mirtes <ondrej@mirtes.cz>
2022-05-21 12:54:17 +0200

    ClearResultCacheCommand - allow --debug flag

--- src/Command/ClearResultCacheCommand.php
+++ src/Command/ClearResultCacheCommand.php
@@ -32,8 +32,21 @@ class ClearResultCacheCommand extends Command
 			->setDefinition([
 				new InputOption('configuration', 'c', InputOption::VALUE_REQUIRED, 'Path to project configuration file'),
 				new InputOption('autoload-file', 'a', InputOption::VALUE_REQUIRED, 'Project\'s additional autoload file path'),
+				new InputOption('debug', null, InputOption::VALUE_NONE, 'Show debug information - which file is analysed, do not catch internal errors'),
 				new InputOption('memory-limit', null, InputOption::VALUE_REQUIRED, 'Memory limit for clearing result cache'),
 			]);
+	}
+
+	protected function initialize(InputInterface $input, OutputInterface $output): void
+	{
+		if ((bool) $input->getOption('debug')) {
+			$application = $this->getApplication();
+			if ($application === null) {
+				throw new ShouldNotHappenException();
+			}
+			$application->setCatchExceptions(false);
+			return;
+		}
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
@@ -41,6 +54,7 @@ class ClearResultCacheCommand extends Command
 		$autoloadFile = $input->getOption('autoload-file');
 		$configuration = $input->getOption('configuration');
 		$memoryLimit = $input->getOption('memory-limit');
+		$debugEnabled = (bool) $input->getOption('debug');
 
 		if (
 			(!is_string($autoloadFile) && $autoloadFile !== null)
@@ -62,6 +76,7 @@ class ClearResultCacheCommand extends Command
 				null,
 				'0',
 				false,
+				$debugEnabled,
 			);
 		} catch (InceptionNotSuccessfulException) {
 			return 1;

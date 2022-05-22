ed6891395ffef4dcd8bb2be286b403ce483b699e
Bart Visscher <bartv@thisnet.nl>
2022-05-22 12:39:50 +0200

    Class attributes should also be included in class statements
    Fixes #7116

--- tests/PHPStan/Analyser/AnalyserIntegrationTest.php
+++ tests/PHPStan/Analyser/AnalyserIntegrationTest.php
@@ -728,6 +728,16 @@ class AnalyserIntegrationTest extends PHPStanTestCase
 		$this->assertNoErrors($errors);
 	}
 
+	public function testBug7116(): void
+	{
+		if (PHP_VERSION_ID < 80000) {
+			$this->markTestSkipped('Test requires PHP 8.0.');
+		}
+
+		$errors = $this->runAnalyse(__DIR__ . '/data/bug-7116.php');
+		$this->assertNoErrors($errors);
+	}
+
 	public function testBug3853(): void
 	{
 		$errors = $this->runAnalyse(__DIR__ . '/data/bug-3853.php');

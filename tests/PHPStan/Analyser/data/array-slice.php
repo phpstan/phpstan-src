0f718b15fda2cff88365b42af08f3b8205ffbe7d
Martin Herndl <martin@herndl.org>
2022-05-23 20:39:06 +0200

    Use reindex in `ConstantArrayType::slice`

--- tests/PHPStan/Analyser/data/array-slice.php
+++ tests/PHPStan/Analyser/data/array-slice.php
@@ -26,13 +26,16 @@ class Foo
 	/**
 	 * @param array<int, bool> $arr1
 	 * @param array<string, int> $arr2
+	 * @param array{17: 'foo', b: 'bar', 19: 'baz'} $arr3
 	 */
-	public function preserveTypes(array $arr1, array $arr2): void
+	public function preserveTypes(array $arr1, array $arr2, array $arr3): void
 	{
 		assertType('array<int, bool>', array_slice($arr1, 1, 2));
 		assertType('array<int, bool>', array_slice($arr1, 1, 2, true));
 		assertType('array<string, int>', array_slice($arr2, 1, 2));
 		assertType('array<string, int>', array_slice($arr2, 1, 2, true));
+		assertType('array{b: \'bar\', 0: \'baz\'}', array_slice($arr3, 1, 2));
+		assertType('array{b: \'bar\', 19: \'baz\'}', array_slice($arr3, 1, 2, true));
 	}
 
 }

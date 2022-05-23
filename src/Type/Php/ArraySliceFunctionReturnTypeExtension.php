58004a1e3ef9f97a911fdc978f2710d25f146dc9
Martin Herndl <martin@herndl.org>
2022-05-23 22:39:18 +0200

    Simplify and cleanup `ArraySliceFunctionReturnTypeExtension`

--- src/Type/Php/ArraySliceFunctionReturnTypeExtension.php
+++ src/Type/Php/ArraySliceFunctionReturnTypeExtension.php
@@ -5,18 +5,14 @@ namespace PHPStan\Type\Php;
 use PhpParser\Node\Expr\FuncCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\FunctionReflection;
-use PHPStan\Type\ArrayType;
 use PHPStan\Type\Constant\ConstantArrayType;
 use PHPStan\Type\Constant\ConstantBooleanType;
 use PHPStan\Type\Constant\ConstantIntegerType;
 use PHPStan\Type\DynamicFunctionReturnTypeExtension;
-use PHPStan\Type\IntegerType;
-use PHPStan\Type\MixedType;
-use PHPStan\Type\NullType;
+use PHPStan\Type\IntersectionType;
 use PHPStan\Type\Type;
-use PHPStan\Type\TypeCombinator;
-use PHPStan\Type\TypeUtils;
-use function array_map;
+use PHPStan\Type\TypeTraverser;
+use PHPStan\Type\UnionType;
 use function count;
 
 class ArraySliceFunctionReturnTypeExtension implements DynamicFunctionReturnTypeExtension
@@ -27,63 +23,38 @@ class ArraySliceFunctionReturnTypeExtension implements DynamicFunctionReturnType
 		return $functionReflection->getName() === 'array_slice';
 	}
 
-	public function getTypeFromFunctionCall(
-		FunctionReflection $functionReflection,
-		FuncCall $functionCall,
-		Scope $scope,
-	): Type
+	public function getTypeFromFunctionCall(FunctionReflection $functionReflection, FuncCall $functionCall, Scope $scope): ?Type
 	{
-		$arrayArg = $functionCall->getArgs()[0]->value ?? null;
-
-		if ($arrayArg === null) {
-			return new ArrayType(
-				new IntegerType(),
-				new MixedType(),
-			);
+		if (count($functionCall->getArgs()) < 1) {
+			return null;
 		}
 
-		$valueType = $scope->getType($arrayArg);
+		$valueType = $scope->getType($functionCall->getArgs()[0]->value);
+		if (!$valueType->isIterable()->yes()) {
+			return null;
+		}
 
-		if (isset($functionCall->getArgs()[1])) {
-			$offset = $scope->getType($functionCall->getArgs()[1]->value);
-			if (!$offset instanceof ConstantIntegerType) {
-				$offset = new ConstantIntegerType(0);
+		$offsetType = isset($functionCall->getArgs()[1]) ? $scope->getType($functionCall->getArgs()[1]->value) : null;
+		$offset = $offsetType instanceof ConstantIntegerType ? $offsetType->getValue() : 0;
+
+		$limitType = isset($functionCall->getArgs()[2]) ? $scope->getType($functionCall->getArgs()[2]->value) : null;
+		$limit = $limitType instanceof ConstantIntegerType ? $limitType->getValue() : null;
+
+		$preserveKeysType = isset($functionCall->getArgs()[3]) ? $scope->getType($functionCall->getArgs()[3]->value) : null;
+		$preserveKeys = $preserveKeysType instanceof ConstantBooleanType ? $preserveKeysType->getValue() : false;
+
+		return TypeTraverser::map($valueType, static function (Type $type, callable $traverse) use ($offset, $limit, $preserveKeys): Type {
+			if ($type instanceof UnionType || $type instanceof IntersectionType) {
+				return $traverse($type);
 			}
-		} else {
-			$offset = new ConstantIntegerType(0);
-		}
-
-		if (isset($functionCall->getArgs()[2])) {
-			$limit = $scope->getType($functionCall->getArgs()[2]->value);
-			if (!$limit instanceof ConstantIntegerType) {
-				$limit = new NullType();
+			if ($type instanceof ConstantArrayType) {
+				return $type->slice($offset, $limit, $preserveKeys);
 			}
-		} else {
-			$limit = new NullType();
-		}
-
-		$constantArrays = TypeUtils::getOldConstantArrays($valueType);
-		if (count($constantArrays) === 0) {
-			$arrays = TypeUtils::getArrays($valueType);
-			if (count($arrays) !== 0) {
-				return TypeCombinator::union(...$arrays);
+			if ($type->isIterableAtLeastOnce()->yes()) {
+				return $type->toArray();
 			}
-			return new ArrayType(
-				new MixedType(),
-				new MixedType(),
-			);
-		}
-
-		if (isset($functionCall->getArgs()[3])) {
-			$preserveKeys = $scope->getType($functionCall->getArgs()[3]->value);
-			$preserveKeys = (new ConstantBooleanType(true))->isSuperTypeOf($preserveKeys)->yes();
-		} else {
-			$preserveKeys = false;
-		}
-
-		$arrayTypes = array_map(static fn (ConstantArrayType $constantArray): ConstantArrayType => $constantArray->slice($offset->getValue(), $limit->getValue(), $preserveKeys), $constantArrays);
-
-		return TypeCombinator::union(...$arrayTypes);
+			return $type;
+		});
 	}
 
 }

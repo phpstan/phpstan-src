9b7ef1308f3d76ad8b406002518741a9a101f143
Martin Herndl <martin@herndl.org>
2022-05-23 22:39:18 +0200

    Simplify and cleanup `ArraySliceFunctionReturnTypeExtension`

--- tests/PHPStan/Analyser/data/array-slice.php
+++ tests/PHPStan/Analyser/data/array-slice.php
@@ -27,8 +27,9 @@ class Foo
 	 * @param array<int, bool> $arr1
 	 * @param array<string, int> $arr2
 	 * @param array{17: 'foo', b: 'bar', 19: 'baz'} $arr3
+	 * @param array{17: 'foo', 19: 'bar', 21: 'baz'}|array{foo: 17, bar: 19, baz: 21} $arr4
 	 */
-	public function preserveTypes(array $arr1, array $arr2, array $arr3): void
+	public function preserveTypes(array $arr1, array $arr2, array $arr3, array $arr4): void
 	{
 		assertType('array<int, bool>', array_slice($arr1, 1, 2));
 		assertType('array<int, bool>', array_slice($arr1, 1, 2, true));
@@ -36,6 +37,8 @@ class Foo
 		assertType('array<string, int>', array_slice($arr2, 1, 2, true));
 		assertType('array{b: \'bar\', 0: \'baz\'}', array_slice($arr3, 1, 2));
 		assertType('array{b: \'bar\', 19: \'baz\'}', array_slice($arr3, 1, 2, true));
+		assertType('array{\'bar\', \'baz\'}|array{bar: 19, baz: 21}', array_slice($arr4, 1, 2));
+		assertType('array{19: \'bar\', 21: \'baz\'}|array{bar: 19, baz: 21}', array_slice($arr4, 1, 2, true));
 	}
 
 }

076604d821ff49f42268984ac5ff917e23b7965c
Martin Herndl <martin@herndl.org>
2022-05-26 14:06:50 +0200

    Consider optional keys in ConstantArrayType removeFirst and removeLast

---
+++ tests/PHPStan/Analyser/data/array-shift.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types = 1);
+
+namespace ArrayShift;
+
+use function PHPStan\Testing\assertType;
+
+class Foo
+{
+
+	public function nonEmpty(array $arr): void
+	{
+		/** @var non-empty-array<string> $arr */
+		assertType('string', array_shift($arr));
+		assertType('array<string>', $arr);
+	}
+
+	public function normalArrays(array $arr): void
+	{
+		/** @var array<int, string> $arr */
+		assertType('string|null', array_shift($arr));
+		assertType('array<int, string>', $arr);
+	}
+
+	public function compoundTypes(array $arr): void
+	{
+		/** @var string[]|int[] $arr */
+		assertType('int|string|null', array_shift($arr));
+		assertType('array<int|string>', $arr);
+	}
+
+	public function constantArrays(array $arr): void
+	{
+		/** @var array{a: 0, b: 1, c: 2} $arr */
+		assertType('0', array_shift($arr));
+		assertType('array{b: 1, c: 2}', $arr);
+	}
+
+	public function constantArraysWithOptionalKeys(array $arr): void
+	{
+		/** @var array{a?: 0, b: 1, c: 2} $arr */
+		assertType('1', array_shift($arr)); // should be 0|1
+		assertType('array{b?: 1, c: 2}', $arr);
+
+		/** @var array{a: 0, b?: 1, c: 2} $arr */
+		assertType('0', array_shift($arr));
+		assertType('array{b?: 1, c: 2}', $arr);
+
+		/** @var array{a: 0, b: 1, c?: 2} $arr */
+		assertType('0', array_shift($arr));
+		assertType('array{b: 1, c?: 2}', $arr);
+	}
+
+}

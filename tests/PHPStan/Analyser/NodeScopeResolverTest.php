2ab6e6a5738deceb08eb8d32a0e7b7b71134e491
Markus Staab <m.staab@complex-it.de>
2022-06-01 17:43:14 +0200

    infer non-empty-string on str-casing functions comparison with constant string

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -902,6 +902,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-7341.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/non-empty-string-strstr-specifying.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/non-empty-string-strrchr-specifying.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/non-empty-string-strcasing-specifying.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/../Rules/Methods/data/conditional-complex-templates.php');
 	}
 
---
+++ tests/PHPStan/Analyser/data/non-empty-string-strcasing-specifying.php
@@ -0,0 +1,148 @@
+<?php
+
+namespace NonEmptyStringStrCasing;
+
+use function PHPStan\Testing\assertType;
+
+class Foo {
+	public function strtolower(string $s): void
+	{
+		if (strtolower($s) === 'hallo') {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+		if ('hallo' === strtolower($s)) {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+
+		$x = (strtolower($s) === 'hallo');
+		assertType('string', $s);
+		var_dump($x);
+
+		$x = (strtolower($s) !== 'hallo');
+		assertType('string', $s);
+		var_dump($x);
+	}
+
+	public function strtoupper(string $s): void
+	{
+		if (strtoupper($s) === 'HA') {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+		if ('hallo' === strtoupper($s)) {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+
+		$x = (strtoupper($s) === 'HA');
+		assertType('string', $s);
+		var_dump($x);
+
+		$x = (strtoupper($s) !== 'HA');
+		assertType('string', $s);
+		var_dump($x);
+	}
+
+	public function mb_strtoupper(string $s): void
+	{
+		if (mb_strtoupper($s) === 'HA') {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+		if ('hallo' === mb_strtoupper($s)) {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+
+		$x = (mb_strtoupper($s) === 'HA');
+		assertType('string', $s);
+		var_dump($x);
+
+		$x = (mb_strtoupper($s) !== 'HA');
+		assertType('string', $s);
+		var_dump($x);
+	}
+
+	public function mb_strtolower(string $s): void
+	{
+		if (mb_strtolower($s) === 'hallo') {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+		if ('hallo' === mb_strtolower($s)) {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+
+		$x = (mb_strtolower($s) === 'hallo');
+		assertType('string', $s);
+		var_dump($x);
+
+		$x = (mb_strtolower($s) !== 'hallo');
+		assertType('string', $s);
+		var_dump($x);
+	}
+
+	public function ucfirst(string $s): void
+	{
+		if (ucfirst($s) === 'Hallo') {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+		if ('hallo' === ucfirst($s)) {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+
+		$x = (ucfirst($s) === 'Hallo');
+		assertType('string', $s);
+		var_dump($x);
+
+		$x = (ucfirst($s) !== 'Hallo');
+		assertType('string', $s);
+		var_dump($x);
+	}
+
+	public function lcfirst(string $s): void
+	{
+		if (lcfirst($s) === 'hallo') {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+		if ('hallo' === lcfirst($s)) {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+
+		$x = (lcfirst($s) === 'hallo');
+		assertType('string', $s);
+		var_dump($x);
+
+		$x = (lcfirst($s) !== 'hallo');
+		assertType('string', $s);
+		var_dump($x);
+	}
+
+	public function ucwords(string $s): void
+	{
+		if (ucwords($s) === 'Hallo') {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+		if ('hallo' === ucwords($s)) {
+			assertType('non-empty-string', $s);
+		}
+		assertType('string', $s);
+
+		$x = (ucwords($s) === 'Hallo');
+		assertType('string', $s);
+		var_dump($x);
+
+		$x = (ucwords($s) !== 'Hallo');
+		assertType('string', $s);
+		var_dump($x);
+	}
+
+}

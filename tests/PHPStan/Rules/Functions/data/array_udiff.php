98a3b07916bfba5ac0cbf79a1e7ea6417b8a656b
Alex Kalineskou <alexkalineskou.dev@gmail.com>
2022-06-09 14:46:32 +0300

    Add array_udiff to arrayFunctions.stub

---
+++ tests/PHPStan/Rules/Functions/data/array_udiff.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types = 1);
+
+array_udiff(
+	[1,2,3],
+	[4,5,6],
+	function(string $a, string $b): string {
+		return $a . $b;
+	},
+);
+
+array_udiff(
+	[1,2,3],
+	[4,5,6],
+	function(int $a, int $b): string {
+		return $a . $b;
+	},
+);
+
+array_udiff(
+	null,
+	null,
+	function(string $a, int $b): string {
+		return $a . $b;
+	},
+);
+
+array_udiff(
+	[25,26],
+	[26,27],
+	static function(int $a, int $b): int {
+		return $a <=> $b;
+	},
+);
+

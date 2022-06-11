5cbc0b7ed0a4a1668b180dbdd83099e232c1a215
Vincent Langlet <vincentlanglet@hotmail.fr>
2022-06-11 13:35:00 +0200

    Add dynamic return type extension for date_create

--- tests/PHPStan/Analyser/data/DateTimeDynamicReturnTypes.php
+++ tests/PHPStan/Analyser/data/DateTimeDynamicReturnTypes.php
@@ -2,40 +2,31 @@
 
 namespace DateTimeDynamicReturnTypes;
 
-use DateTime;
-use DateTimeImmutable;
-use function date_create_from_format;
 use function PHPStan\Testing\assertType;
 
 class Foo
 {
 
-	public function createDynamic(string $format, string $datetime): void {
-		assertType('DateTime|false', date_create_from_format($format, $datetime));
-		assertType('DateTimeImmutable|false', date_create_immutable_from_format($format, $datetime));
-	}
-
-	public function staticInvalidFormat(): void {
-		assertType('false', date_create_from_format('Foobar', '2022-02-20'));
-		assertType('false', date_create_immutable_from_format('Foobar', '2022-02-20'));
+	public function createDynamic(string $datetime): void {
+		assertType('DateTime|false', date_create($datetime));
+		assertType('DateTimeImmutable|false', date_create_immutable($datetime));
 	}
 
 	public function staticInvalidDatetime(): void {
-		assertType('false', date_create_from_format('Y-m-d', '2022/02/20'));
-		assertType('false', date_create_immutable_from_format('Y-m-d', '2022/02/20'));
+		assertType('false', date_create('202/20'));
+		assertType('false', date_create_immutable('202/20'));
 	}
 
 	public function staticValidStrings(): void {
-		assertType('DateTime', date_create_from_format('Y-m-d', '2020-10-12'));
-		assertType('DateTimeImmutable', date_create_immutable_from_format('Y-m-d', '2020-10-12'));
+		assertType('DateTime', date_create('2020-10-12'));
+		assertType('DateTimeImmutable', date_create_immutable('2020-10-12'));
 	}
 
 	public function localVariables(): void {
-		$format = 'Y-m-d';
 		$datetime = '2020-10-12';
 
-		assertType('DateTime', date_create_from_format($format, $datetime));
-		assertType('DateTimeImmutable', date_create_immutable_from_format($format, $datetime));
+		assertType('DateTime', date_create($datetime));
+		assertType('DateTimeImmutable', date_create_immutable($datetime));
 	}
 
 }

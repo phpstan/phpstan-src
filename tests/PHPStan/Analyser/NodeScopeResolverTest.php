4f810ffc19dc20af932e5a1e9caff3b7d26a3c3c
Richard van Velzen <richard@frank.nl>
2022-06-14 14:58:31 +0200

    Fix return type for arrow function generators

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -911,6 +911,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/finally-scope.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-7387.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-7353.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-7031.php');
 	}
 
 	/**

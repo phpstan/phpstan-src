e4dd0786763fd7544fb97de7d647f0a420ab8026
Martin Herndl <martin@herndl.org>
2022-06-17 20:59:40 +0200

    Reindex constant arrays via `shuffle`

---
+++ tests/PHPStan/Analyser/data/bug-6138.php
@@ -0,0 +1,29 @@
+<?php declare(strict_types = 1);
+
+namespace Bug6138;
+
+use function PHPStan\Testing\assertType;
+
+$indexed = [
+	1,
+	2,
+	3,
+];
+$associative = [
+	'a' => 1,
+	'b' => 2,
+	'c' => 3,
+];
+$unordered = [
+	0 => 1,
+	3 => 2,
+	42 => 3,
+];
+
+shuffle( $indexed );
+shuffle( $associative );
+shuffle( $unordered );
+
+assertType( 'non-empty-array<int, 0|1|2>', array_keys( $indexed ) );
+assertType( 'non-empty-array<int, 0|1|2>', array_keys( $associative ) );
+assertType( 'non-empty-array<int, 0|1|2>', array_keys( $unordered ) );

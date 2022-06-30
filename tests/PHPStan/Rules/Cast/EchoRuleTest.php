c30898689b6c0300d3cbc867d253e8a1bc9d1143
Vincent Langlet <vincentlanglet@hotmail.fr>
2022-06-30 21:26:10 +0200

    Another debug

--- tests/PHPStan/Rules/Cast/EchoRuleTest.php
+++ tests/PHPStan/Rules/Cast/EchoRuleTest.php
@@ -20,35 +20,35 @@ class EchoRuleTest extends RuleTestCase
 		);
 	}
 
-	public function testEchoRule(): void
-	{
-		$this->analyse([__DIR__ . '/data/echo.php'], [
-			[
-				'Parameter #1 (array{}) of echo cannot be converted to string.',
-				7,
-			],
-			[
-				'Parameter #1 (stdClass) of echo cannot be converted to string.',
-				9,
-			],
-			[
-				'Parameter #1 (array{}) of echo cannot be converted to string.',
-				11,
-			],
-			[
-				'Parameter #2 (stdClass) of echo cannot be converted to string.',
-				11,
-			],
-			[
-				'Parameter #1 (Closure(): void) of echo cannot be converted to string.',
-				13,
-			],
-			[
-				'Parameter #1 (\'string\'|array{\'string\'}) of echo cannot be converted to string.',
-				17,
-			],
-		]);
-	}
+//	public function testEchoRule(): void
+//	{
+//		$this->analyse([__DIR__ . '/data/echo.php'], [
+//			[
+//				'Parameter #1 (array{}) of echo cannot be converted to string.',
+//				7,
+//			],
+//			[
+//				'Parameter #1 (stdClass) of echo cannot be converted to string.',
+//				9,
+//			],
+//			[
+//				'Parameter #1 (array{}) of echo cannot be converted to string.',
+//				11,
+//			],
+//			[
+//				'Parameter #2 (stdClass) of echo cannot be converted to string.',
+//				11,
+//			],
+//			[
+//				'Parameter #1 (Closure(): void) of echo cannot be converted to string.',
+//				13,
+//			],
+//			[
+//				'Parameter #1 (\'string\'|array{\'string\'}) of echo cannot be converted to string.',
+//				17,
+//			],
+//		]);
+//	}
 
 	public function testRuleWithNullsafeVariant(): void
 	{

e0cd35a343448775c73a1ac09064b51a30d063ca
Pucci, Murilo henrique [GTSBR NON-J&J] <mpucci@its.jnj.com>
2022-07-07 14:21:50 -0300

    Add non-whitespace html test

--- tests/PHPStan/Rules/DeadCode/data/bug-7531.php
+++ tests/PHPStan/Rules/DeadCode/data/bug-7531.php
@@ -12,3 +12,7 @@ namespace Bug7531;
         <?php throw new \Exception("Unexpected behavior") ?>
     <?php endif ?>
 <?php endforeach; ?>
+
+<?php foreach ([1,2,3] as $number) : ?>
+	<?php if ($number > $compareTo) : ?><xs:sequence>some xml data</xs:sequence><?php else : ?><?php throw new \Exception("Unexpected behavior") ?><?php endif ?>
+<?php endforeach; ?>

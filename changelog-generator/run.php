2b850c592d54ee8e9e769e25c9ae38bdbeac1ea3
Ondrej Mirtes <ondrej@mirtes.cz>
2022-07-13 13:39:05 +0200

    Next-gen changelog generator

--- changelog-generator/run.php
+++ changelog-generator/run.php
@@ -1,16 +1,33 @@
 #!/usr/bin/env php
 <?php declare(strict_types = 1);
 
-use Httpful\Request;
+namespace PHPStan\ChangelogGenerator;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Github\Api\Search;
+use Github\AuthMethod;
+use Github\Client;
+use Github\HttpClient\Builder;
+use InvalidArgumentException;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use function array_map;
+use function array_merge;
+use function count;
+use function escapeshellarg;
+use function exec;
+use function explode;
+use function implode;
+use function sprintf;
 
 (function (): void {
 	require_once __DIR__ . '/../vendor/autoload.php';
 
-	$command = new class() extends Symfony\Component\Console\Command\Command {
+	$command = new class() extends Command {
 
 		protected function configure(): void
 		{
@@ -21,6 +38,19 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 		protected function execute(InputInterface $input, OutputInterface $output)
 		{
+			$token = $_SERVER['GITHUB_TOKEN'];
+
+			$rateLimitPlugin = new RateLimitPlugin();
+			$httpBuilder = new Builder();
+			$httpBuilder->addPlugin($rateLimitPlugin);
+
+			$gitHubClient = new Client($httpBuilder);
+			$gitHubClient->authenticate($token, AuthMethod::ACCESS_TOKEN);
+			$rateLimitPlugin->setClient($gitHubClient);
+
+			/** @var Search $searchApi */
+			$searchApi = $gitHubClient->api('search');
+
 			$commitLines = $this->exec(['git', 'log', sprintf('%s..%s', $input->getArgument('fromCommit'), $input->getArgument('toCommit')), '--reverse', '--pretty=%H %s']);
 			$commits = array_map(static function (string $line): array {
 				[$hash, $message] = explode(' ', $line, 2);
@@ -31,48 +61,23 @@ use Symfony\Component\Console\Output\OutputInterface;
 				];
 			}, explode("\n", $commitLines));
 
-			$i = 0;
-
 			foreach ($commits as $commit) {
-				$searchPullRequestsResponse = Request::get(sprintf('https://api.github.com/search/issues?q=repo:phpstan/phpstan-src+%s', $commit['hash']))
-					->sendsAndExpectsType('application/json')
-					->basicAuth('ondrejmirtes', getenv('GITHUB_TOKEN'))
-					->send();
-				if ($searchPullRequestsResponse->code !== 200) {
-					$output->writeln(var_export($searchPullRequestsResponse->body, true));
-					throw new InvalidArgumentException((string) $searchPullRequestsResponse->code);
-				}
-				$searchPullRequestsResponse = $searchPullRequestsResponse->body;
-
-				$searchIssuesResponse = Request::get(sprintf('https://api.github.com/search/issues?q=repo:phpstan/phpstan+%s&sort=created&order=desc', $commit['hash']))
-					->sendsAndExpectsType('application/json')
-					->basicAuth('ondrejmirtes', getenv('GITHUB_TOKEN'))
-					->send();
-				if ($searchIssuesResponse->code !== 200) {
-					$output->writeln(var_export($searchIssuesResponse->body, true));
-					throw new InvalidArgumentException((string) $searchIssuesResponse->code);
-				}
-				$searchIssuesResponse = $searchIssuesResponse->body;
-				$items = array_merge($searchPullRequestsResponse->items, $searchIssuesResponse->items);
+				$pullRequests = $searchApi->issues(sprintf('repo:phpstan/phpstan-src %s', $commit['hash']));
+				$issues = $searchApi->issues(sprintf('repo:phpstan/phpstan %s', $commit['hash']), 'created');
+				$items = array_merge($pullRequests['items'], $issues['items']);
 				$parenthesis = 'https://github.com/phpstan/phpstan-src/commit/' . $commit['hash'];
 				$thanks = null;
 				$issuesToReference = [];
 				foreach ($items as $responseItem) {
-					if (isset($responseItem->pull_request)) {
-						$parenthesis = sprintf('[#%d](%s)', $responseItem->number, 'https://github.com/phpstan/phpstan-src/pull/' . $responseItem->number);
-						$thanks = $responseItem->user->login;
+					if (isset($responseItem['pull_request'])) {
+						$parenthesis = sprintf('[#%d](%s)', $responseItem['number'], 'https://github.com/phpstan/phpstan-src/pull/' . $responseItem['number']);
+						$thanks = $responseItem['user']['login'];
 					} else {
-						$issuesToReference[] = sprintf('#%d', $responseItem->number);
+						$issuesToReference[] = sprintf('#%d', $responseItem['number']);
 					}
 				}
 
 				$output->writeln(sprintf('* %s (%s)%s%s', $commit['message'], $parenthesis, count($issuesToReference) > 0 ? ', ' . implode(', ', $issuesToReference) : '', $thanks !== null ? sprintf(', thanks @%s!', $thanks) : ''));
-
-				if ($i > 0 && $i % 8 === 0) {
-					sleep(60);
-				}
-
-				$i++;
 			}
 
 			return 0;
@@ -99,5 +104,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 	$application = new Application();
 	$application->add($command);
 	$application->setDefaultCommand('run', true);
+	$application->setCatchExceptions(false);
 	$application->run();
 })();

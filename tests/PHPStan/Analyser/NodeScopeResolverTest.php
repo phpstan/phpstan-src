60561a80dbecd83168d580786b41c176b9a44036
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-07-18 17:58:50 +0200

    Add test.

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -927,6 +927,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-7580.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/this-subtractable.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/match-expression-inference.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-1519.php');
 	}
 
 	/**

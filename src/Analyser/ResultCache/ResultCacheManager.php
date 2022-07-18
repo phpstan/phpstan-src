1f90cc21ed8ebbca1ceff6e1f4ee1bd47e4dc62b
Markus Staab <maggus.staab@googlemail.com>
2022-07-18 20:42:16 +0200

    fixed method case

--- src/Analyser/ResultCache/ResultCacheManager.php
+++ src/Analyser/ResultCache/ResultCacheManager.php
@@ -616,7 +616,7 @@ php;
 			(!array_key_exists('tmpDir', $projectConfig['parameters']) || $projectConfig['parameters']['tmpDir'] === null)
 		) {
 
-			return dirname($this->cacheFilePath) . '/resultCache-' . md5(Neon::encode([$this->analysedPaths, $this->getPhpstanVersion()])) . '.php';
+			return dirname($this->cacheFilePath) . '/resultCache-' . md5(Neon::encode([$this->analysedPaths, $this->getPhpStanVersion()])) . '.php';
 		}
 
 		return $this->cacheFilePath;

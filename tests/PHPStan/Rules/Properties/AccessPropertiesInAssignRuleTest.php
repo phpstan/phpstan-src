22561e7f12e29bbcaec1b4c3fb9da3946d8ee9de
Ondrej Mirtes <ondrej@mirtes.cz>
2022-07-19 13:46:28 +0200

    PHP 8.2 - behave as if checkDynamicProperties was set to true

--- tests/PHPStan/Rules/Properties/AccessPropertiesInAssignRuleTest.php
+++ tests/PHPStan/Rules/Properties/AccessPropertiesInAssignRuleTest.php
@@ -2,6 +2,7 @@
 
 namespace PHPStan\Rules\Properties;
 
+use PHPStan\Php\PhpVersion;
 use PHPStan\Rules\Rule;
 use PHPStan\Rules\RuleLevelHelper;
 use PHPStan\Testing\RuleTestCase;
@@ -17,7 +18,7 @@ class AccessPropertiesInAssignRuleTest extends RuleTestCase
 	{
 		$reflectionProvider = $this->createReflectionProvider();
 		return new AccessPropertiesInAssignRule(
-			new AccessPropertiesRule($reflectionProvider, new RuleLevelHelper($reflectionProvider, true, false, true, false), true, true),
+			new AccessPropertiesRule($reflectionProvider, new RuleLevelHelper($reflectionProvider, true, false, true, false), new PhpVersion(PHP_VERSION_ID), true, true),
 		);
 	}
 

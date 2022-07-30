9b634cf5b682d30275f6a1d64160cf7ee22e5b8c
Brad <28307684+mad-briller@users.noreply.github.com>
2022-07-30 14:28:15 +0100

    Added stub files and tests for:
    array_diff_uassoc
    array_diff_ukey
    array_intersect_uassoc
    array_intersect_ukey
    array_udiff_assoc
    array_udiff_uassoc
    array_uintersect_assoc
    array_uintersect_uassoc
    array_uintersect

---
+++ tests/PHPStan/Rules/Functions/data/array_uintersect_uassoc.php
@@ -0,0 +1,45 @@
+<?php
+
+array_uintersect_uassoc(
+	['a' => 'a', 'b' => 'b'],
+	['c' => 'c', 'd' => 'd'],
+	static function (string $a, string $b): int {
+		return $a <=> $b;
+	},
+	static function (string $a, string $b): int {
+		return $a <=> $b;
+	}
+);
+
+array_uintersect_uassoc(
+	[1, 2, 3],
+	[1, 2, 4, 5],
+	static function (int $a, int $b): int {
+		return $a <=> $b;
+	},
+	static function (int $a, int $b): int {
+		return $a <=> $b;
+	},
+);
+
+array_uintersect_uassoc(
+	['a' => 'a', 'b' => 'b'],
+	['c' => 'c', 'd' => 'd'],
+	static function (int $a, int $b): int {
+		return $a <=> $b;
+	},
+	static function (int $a, int $b): int {
+		return $a <=> $b;
+	}
+);
+
+array_uintersect_uassoc(
+	[1, 2, 3],
+	[1, 2, 4, 5],
+	static function (string $a, string $b): int {
+		return $a <=> $b;
+	},
+	static function (string $a, string $b): int {
+		return $a <=> $b;
+	}
+);

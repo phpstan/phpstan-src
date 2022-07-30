c7069b61e6784f8fbbd27598f4c06bf50500f53c
Brad <28307684+mad-briller@users.noreply.github.com>
2022-07-30 14:28:15 +0100

    Added stub files and tests for:
    array_diff_uassoc
    array_diff_ukey
    array_intersect_uassoc
    array_intersect_ukey
    array_udiff_assoc
    array_udiff_uassoc
    array_uintersect_assoc
    array_uintersect_uassoc
    array_uintersect

---
+++ tests/PHPStan/Rules/Functions/data/array_intersect_ukey.php
@@ -0,0 +1,33 @@
+<?php
+
+array_intersect_ukey(
+	['a' => 1, 'b' => 2],
+	['c' => 1, 'd' => 2],
+	static function (string $a, string $b): int {
+		return $a <=> $b;
+	}
+);
+
+array_intersect_ukey(
+	[1, 2, 3],
+	[1, 2, 4, 5],
+	static function (int $a, int $b): int {
+		return $a <=> $b;
+	}
+);
+
+array_intersect_ukey(
+	['a' => 1, 'b' => 2],
+	['c' => 1, 'd' => 2],
+	static function (int $a, int $b): int {
+		return $a <=> $b;
+	}
+);
+
+array_intersect_ukey(
+	[1, 2, 3],
+	[1, 2, 4, 5],
+	static function (string $a, string $b): int {
+		return $a <=> $b;
+	}
+);
---
+++ tests/PHPStan/Rules/Functions/data/array_uintersect_assoc.php
@@ -0,0 +1,33 @@
+<?php
+
+array_uintersect_assoc(
+	['a' => 'a', 'b' => 'b'],
+	['c' => 'c', 'd' => 'd'],
+	static function (string $a, string $b): int {
+		return $a <=> $b;
+	}
+);
+
+array_uintersect_assoc(
+	[1, 2, 3],
+	[1, 2, 3, 4],
+	static function (int $a, int $b): int {
+		return $a <=> $b;
+	}
+);
+
+array_uintersect_assoc(
+	['a' => 'a', 'b' => 'b'],
+	['c' => 'c', 'd' => 'd'],
+	static function (int $a, int $b): int {
+		return $a <=> $b;
+	}
+);
+
+array_uintersect_assoc(
+	[1, 2, 3],
+	[1, 2, 3, 4],
+	static function (string $a, string $b): int {
+		return $a <=> $b;
+	}
+);

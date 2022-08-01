b9280060c72199d9f7480d4a16343b1b9771f71c
Dgame <rswhite4@googlemail.com>
2022-08-01 20:40:28 +0200

    Fixed backward compatibility

--- tests/PHPStan/Rules/Comparison/MatchExpressionRuleTest.php
+++ tests/PHPStan/Rules/Comparison/MatchExpressionRuleTest.php
@@ -226,6 +226,10 @@ class MatchExpressionRuleTest extends RuleTestCase
 
 	public function testBug7698(): void
 	{
+		if (PHP_VERSION_ID < 80000) {
+			$this->markTestSkipped('Test requires PHP 8.0.');
+		}
+
 		$this->analyse([__DIR__.'/data/bug-7698.php'], [
 			[
 				'Match expression does not handle remaining value: class-string<C>',

5c22edfbff4d80865579be91580ea396d1fcea0a
Markus Staab <maggus.staab@googlemail.com>
2022-08-04 23:00:39 +0200

    added failling match test

---
+++ tests/PHPStan/Rules/Comparison/data/bug-7698.php
@@ -0,0 +1,29 @@
+<?php // lint >= 8.1
+
+namespace Bug7698Match;
+
+final class A
+{
+}
+
+final class B
+{
+}
+
+final class Test
+{
+	public function __construct(public readonly A|B $value)
+	{
+	}
+}
+
+function matchIt()
+{
+	$t = new Test(new A());
+	$class = $t->value::class;
+	echo match ($t->value::class) {
+		A::class => 'A',
+		B::class => 'B'
+	};
+}
+

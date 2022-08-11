694d622d9e69db9ecaf2dd6052e7c94f39aff079
Vincent Langlet <vincentlanglet@hotmail.fr>
2022-08-12 00:06:39 +0200

    Add more tests

--- tests/PHPStan/Analyser/data/phpstan-type-stub-files.php
+++ tests/PHPStan/Analyser/data/phpstan-type-stub-files.php
@@ -5,10 +5,22 @@ namespace PhpstanTypeStubFiles;
 use function PHPStan\Testing\assertType;
 
 /**
+ * @phpstan-template T of object
+ */
+class Bar
+{
+	/** @var T|null */
+	public $object;
+}
+
+/**
  * @phpstan-type AnotherType = array{foo: int}
  * @phpstan-type CustomType = array{foo: string}
+ *
+ * @phpstan-template T of object
+ * @phpstan-extends Bar<object>
  */
-class Foo
+class Foo extends Bar
 {
 	/** @var CustomType */
 	public $array;
@@ -16,11 +28,18 @@ class Foo
 	/** @var string */
 	public $string;
 
+	/** @var object */
+	public $template;
+
 	/** @param int $test1 */
 	public function test($test1, $test2): void {
+		/** @var Foo<\DateTime> $foo */
 		$foo = new Foo();
+
 		assertType('array{foo: string}', $foo->array);
 		assertType('string', $foo->string);
+		assertType('DateTime|null', $foo->object);
+		assertType('DateTime', $foo->template);
 		assertType('int', $test1);
 		assertType('array<string>', $test2);
 	}

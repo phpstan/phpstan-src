519059e2287d843ca11a585d96ab103c2cf80500
Ondrej Mirtes <ondrej@mirtes.cz>
2022-09-03 11:24:52 +0200

    Regression performance test
    Closes https://github.com/phpstan/phpstan/issues/7140

--- tests/PHPStan/Analyser/AnalyserIntegrationTest.php
+++ tests/PHPStan/Analyser/AnalyserIntegrationTest.php
@@ -946,6 +946,12 @@ class AnalyserIntegrationTest extends PHPStanTestCase
 		$this->assertNoErrors($errors);
 	}
 
+	public function testBug7140(): void
+	{
+		$errors = $this->runAnalyse(__DIR__ . '/data/bug-7140.php');
+		$this->assertNoErrors($errors);
+	}
+
 	/**
 	 * @param string[]|null $allAnalysedFiles
 	 * @return Error[]
---
+++ tests/PHPStan/Analyser/data/bug-7140.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Bug7140;
+
+/**
+ * @param mixed[] $arr
+ */
+function foo(array $arr): void
+{
+	for ($i = 1; $i <= 2; $i++) {
+		if (!isset($arr['a_' . $i])) {
+			continue;
+		}
+
+		if (isset($arr['b_' . $i])) {
+		}
+
+		if (isset($arr['c_' . $i])) {
+		}
+
+		if (isset($arr['d_' . $i])) {
+		}
+
+		if (isset($arr['e_' . $i])) {
+		}
+
+		if (isset($arr['f_' . $i])) {
+		}
+
+		if (isset($arr['g_' . $i])) {
+		}
+
+		if (isset($arr['h_' . $i])) {
+		}
+
+		if (isset($arr['i_' . $i])) {
+		}
+
+		if (isset($arr['j_' . $i])) {
+		}
+
+		if (isset($arr['k_' . $i])) {
+		}
+	}
+}

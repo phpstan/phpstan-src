d535f166f70238325fd373a624cf9908493b9b8b
Richard van Velzen <richard@frank.nl>
2022-09-06 10:11:12 +0200

    Rename to dump-parameters and add more options

--- src/Command/DumpParametersCommand.php
+++ src/Command/DumpParametersCommand.php
@@ -12,10 +12,10 @@ use Symfony\Component\Console\Output\OutputInterface;
 use function is_array;
 use function is_string;
 
-class DumpConfigCommand extends Command
+class DumpParametersCommand extends Command
 {
 
-	private const NAME = 'dump-config';
+	private const NAME = 'dump-parameters';
 
 	/**
 	 * @param string[] $composerAutoloaderProjectPaths
@@ -30,25 +30,41 @@ class DumpConfigCommand extends Command
 	protected function configure(): void
 	{
 		$this->setName(self::NAME)
-			->setDescription('Dumps config')
+			->setDescription('Dumps all parameters')
 			->setDefinition([
 				new InputArgument('paths', InputArgument::OPTIONAL | InputArgument::IS_ARRAY, 'Paths with source code to run analysis on'),
 				new InputOption('configuration', 'c', InputOption::VALUE_REQUIRED, 'Path to project configuration file'),
 				new InputOption(AnalyseCommand::OPTION_LEVEL, 'l', InputOption::VALUE_REQUIRED, 'Level of rule options - the higher the stricter'),
 				new InputOption('autoload-file', 'a', InputOption::VALUE_REQUIRED, 'Project\'s additional autoload file path'),
+				new InputOption('debug', null, InputOption::VALUE_NONE, 'Show debug information - which file is analysed, do not catch internal errors'),
+				new InputOption('memory-limit', null, InputOption::VALUE_REQUIRED, 'Memory limit for clearing result cache'),
 			])
 			->setHidden(true);
+	}
+
+	protected function initialize(InputInterface $input, OutputInterface $output): void
+	{
+		if ((bool) $input->getOption('debug')) {
+			$application = $this->getApplication();
+			if ($application === null) {
+				throw new ShouldNotHappenException();
+			}
+			$application->setCatchExceptions(false);
+			return;
+		}
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
 		$paths = $input->getArgument('paths');
+		$memoryLimit = $input->getOption('memory-limit');
 		$autoloadFile = $input->getOption('autoload-file');
 		$configuration = $input->getOption('configuration');
 		$level = $input->getOption(AnalyseCommand::OPTION_LEVEL);
 
 		if (
 			!is_array($paths)
+			|| (!is_string($memoryLimit) && $memoryLimit !== null)
 			|| (!is_string($autoloadFile) && $autoloadFile !== null)
 			|| (!is_string($configuration) && $configuration !== null)
 			|| (!is_string($level) && $level !== null)
@@ -61,13 +77,13 @@ class DumpConfigCommand extends Command
 				$input,
 				$output,
 				$paths,
-				null,
+				$memoryLimit,
 				$autoloadFile,
 				$this->composerAutoloaderProjectPaths,
 				$configuration,
 				null,
 				$level,
-				true,
+				false,
 			);
 		} catch (InceptionNotSuccessfulException) {
 			return 1;

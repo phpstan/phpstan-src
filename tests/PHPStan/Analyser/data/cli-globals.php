ba9d2f68d34e29ff4a472e3cfd5aca2986db2e8e
Markus Staab <maggus.staab@googlemail.com>
2022-09-10 08:21:29 +0200

    more precise $argc, $argv global var type

---
+++ tests/PHPStan/Analyser/data/cli-globals.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace CliGlobals;
+
+use function PHPStan\Testing\assertType;
+
+assertType('int<1, max>', $argc);
+assertType('non-empty-array<int, string>', $argv);
+
+function f() {
+	assertType('*ERROR*', $argc);
+	assertType('*ERROR*', $argv);
+}
+
+function g($argc, $argv) {
+	assertType('mixed', $argc);
+	assertType('mixed', $argv);
+}
+
+function h() {
+	global $argc, $argv;
+	assertType('mixed', $argc); // should be int<1, max>
+	assertType('mixed', $argv); // should be non-empty-array<int, string>
+}
+
+function i() {
+	// user created local variable
+	$argc = 'hallo';
+	$argv = 'welt';
+
+	assertType("'hallo'", $argc);
+	assertType("'welt'", $argv);
+}

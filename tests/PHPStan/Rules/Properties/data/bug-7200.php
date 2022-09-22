d4b720dd7d847a7abaf3761ae0f1cfeffc69a5e9
Richard van Velzen <richard@frank.nl>
2022-09-22 13:52:11 +0200

    Regression test

---
+++ tests/PHPStan/Rules/Properties/data/bug-7200.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types = 1);
+
+namespace Bug7200;
+
+class HelloWorld
+{
+	/**
+	* @param class-string<Model&One&Two&Three>|null $class
+	*/
+	public function __construct(public ?Model $model = null, public ?string $class = null)
+	{
+		if ($model instanceof One && $model instanceof Two && $model instanceof Three) {
+			$this->class ??= $model::class;
+		}
+	}
+}
+
+class Model {}
+interface One {}
+interface Two {}
+interface Three {}

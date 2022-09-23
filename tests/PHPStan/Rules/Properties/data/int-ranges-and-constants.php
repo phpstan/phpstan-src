0483ffd453219dd94ca53f3853272565b2e96357
Markus Staab <m.staab@complex-it.de>
2022-09-23 15:39:52 +0200

    added more integer range vs. int constants tests

---
+++ tests/PHPStan/Rules/Properties/data/int-ranges-and-constants.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace IntegerRangesAndConstants;
+
+class HelloWorld
+{
+	/** @var 0|1|3 */
+	public $i = 0;
+	/** @var 0|1|2|3|string */
+	public $j = 0;
+	/** @var 0|1|bool|2|3 */
+	public $k = 0;
+	/** @var 0|1|2|3|4|5 */
+	public $l;
+
+	public function test(): void {
+		$this->i = random_int(0, 3);
+		$this->j = random_int(0, 3);
+		$this->k = random_int(0, 3);
+		$this->l = random_int(0, 3);
+	}
+
+	/**
+	 * @var int<0,3>
+	 */
+	public $x = 0;
+
+	/**
+	 * @param 0| $a
+	 * @param 0|1 $b
+	 * @param 0|1|3 $c
+	 * @param 0|1|2|3|string $j
+	 * @param 0|1|bool|2|3 $k
+	 * @param 0|1|bool|3 $l
+	 * @param 0|1|3|4 $m
+	 */
+	public function test2($a, $b, $c, $j, $k, $l, $m): void {
+		$this->x = $a;
+		$this->x = $b;
+		$this->x = $c;
+
+		$this->x = $j;
+		$this->x = $k;
+		$this->x = $l;
+		$this->x = $m;
+	}
+}

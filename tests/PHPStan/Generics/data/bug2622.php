c64e7337234b509eb5fa901311dc831c78412b7e
Ondrej Mirtes <ondrej@mirtes.cz>
2022-09-27 18:31:25 +0200

    Fix integration tests

--- tests/PHPStan/Generics/data/bug2622.php
+++ tests/PHPStan/Generics/data/bug2622.php
@@ -14,6 +14,7 @@ class MyArray implements \IteratorAggregate {
 		$this->values = [];
 	}
 
+	#[\ReturnTypeWillChange]
 	public function getIterator() {
 		return new \ArrayObject($this->values);
 	}

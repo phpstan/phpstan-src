05a9304d157dd93ec9f6f9a47ba84da62d390870
Martin Jonas <martin.jonas@growjob.com>
2022-09-28 09:29:16 +0200

    ignoreErrors fix

---
+++ tests/PHPStan/DependencyInjection/IgnoreErrorsTest.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\DependencyInjection;
+
+use PHPStan\Testing\PHPStanTestCase;
+
+class IgnoreErrorsTest extends PHPStanTestCase
+{
+
+	public function testIgnoreErrors(): void
+	{
+		$this->assertCount(10, self::getContainer()->getParameter('ignoreErrors'));
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public static function getAdditionalConfigFiles(): array
+	{
+		return [
+			__DIR__ . '/ignoreErrors.neon',
+		];
+	}
+
+}

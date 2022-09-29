9b5f42514009be2d019442685b1fdeb9a56f7c64
Richard van Velzen <richard@frank.nl>
2022-09-29 16:17:11 +0200

    Fix PHP<8 tests

--- tests/PHPStan/Analyser/data/bug-3961.php
+++ tests/PHPStan/Analyser/data/bug-3961.php
@@ -9,13 +9,13 @@ class Foo
 
 	public function doFoo(string $v, string $d, $m): void
 	{
-		assertType('non-empty-array<int, string>', explode('.', $v));
+		assertType('non-empty-list<string>', explode('.', $v));
 		assertType('false', explode('', $v));
-		assertType('array<int, string>', explode('.', $v, -2));
-		assertType('non-empty-array<int, string>', explode('.', $v, 0));
-		assertType('non-empty-array<int, string>', explode('.', $v, 1));
-		assertType('non-empty-array<int, string>|false', explode($d, $v));
-		assertType('(non-empty-array<int, string>|false)', explode($m, $v));
+		assertType('list<string>', explode('.', $v, -2));
+		assertType('non-empty-list<string>', explode('.', $v, 0));
+		assertType('non-empty-list<string>', explode('.', $v, 1));
+		assertType('array<int, string>|false', explode($d, $v));
+		assertType('(array<int, string>|false)', explode($m, $v));
 	}
 
 }

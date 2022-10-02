2e4cdabcb5fa04986f4537fad356b354a5a7099a
Markus Staab <maggus.staab@googlemail.com>
2022-10-02 18:12:27 +0200

    fix bad merge

--- tests/PHPStan/Rules/Classes/InstantiationRuleTest.php
+++ tests/PHPStan/Rules/Classes/InstantiationRuleTest.php
@@ -424,7 +424,11 @@ class InstantiationRuleTest extends RuleTestCase
 
 	public function testBug7594(): void
 	{
-		$this->checkExplicitMixed = true;
+		if (PHP_VERSION_ID < 80000) {
+			$this->markTestSkipped('Test requires PHP 8.0.');
+		}
+
 		$this->analyse([__DIR__ . '/data/bug-7594.php'], []);
 	}
+
 }

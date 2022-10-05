926058491578747d2f99ec6b02ad28c988f3c28d
Ondrej Mirtes <ondrej@mirtes.cz>
2022-10-05 11:06:16 +0200

    Fix list checking on level 7 vs. level 8

--- tests/PHPStan/Rules/Functions/ReturnTypeRuleTest.php
+++ tests/PHPStan/Rules/Functions/ReturnTypeRuleTest.php
@@ -13,16 +13,19 @@ use PHPStan\Testing\RuleTestCase;
 class ReturnTypeRuleTest extends RuleTestCase
 {
 
-	private bool $checkExplicitMixed = false;
+	private bool $checkNullables;
+
+	private bool $checkExplicitMixed;
 
 	protected function getRule(): Rule
 	{
-		return new ReturnTypeRule(new FunctionReturnTypeCheck(new RuleLevelHelper($this->createReflectionProvider(), true, false, true, $this->checkExplicitMixed, false)));
+		return new ReturnTypeRule(new FunctionReturnTypeCheck(new RuleLevelHelper($this->createReflectionProvider(), $this->checkNullables, false, true, $this->checkExplicitMixed, false)));
 	}
 
 	public function testReturnTypeRule(): void
 	{
 		require_once __DIR__ . '/data/returnTypes.php';
+		$this->checkNullables = true;
 		$this->checkExplicitMixed = false;
 		$this->analyse([__DIR__ . '/data/returnTypes.php'], [
 			[
@@ -71,6 +74,7 @@ class ReturnTypeRuleTest extends RuleTestCase
 	public function testReturnTypeRulePhp70(): void
 	{
 		$this->checkExplicitMixed = false;
+		$this->checkNullables = true;
 		$this->analyse([__DIR__ . '/data/returnTypes-7.0.php'], [
 			[
 				'Function ReturnTypes\Php70\returnInteger() should return int but empty return statement found.',
@@ -82,6 +86,7 @@ class ReturnTypeRuleTest extends RuleTestCase
 	public function testIsGenerator(): void
 	{
 		$this->checkExplicitMixed = false;
+		$this->checkNullables = true;
 		$this->analyse([__DIR__ . '/data/is-generator.php'], []);
 	}
 
@@ -89,6 +94,7 @@ class ReturnTypeRuleTest extends RuleTestCase
 	{
 		require_once __DIR__ . '/data/bug-2568.php';
 		$this->checkExplicitMixed = false;
+		$this->checkNullables = true;
 		$this->analyse([__DIR__ . '/data/bug-2568.php'], []);
 	}
 
@@ -96,6 +102,7 @@ class ReturnTypeRuleTest extends RuleTestCase
 	{
 		require_once __DIR__ . '/data/bug-2723.php';
 		$this->checkExplicitMixed = false;
+		$this->checkNullables = true;
 		$this->analyse([__DIR__ . '/data/bug-2723.php'], [
 			[
 				'Function Bug2723\baz() should return Bug2723\Bar<Bug2723\Foo<T4>> but returns Bug2723\BarOfFoo<string>.',
@@ -107,36 +114,42 @@ class ReturnTypeRuleTest extends RuleTestCase
 	public function testBug5706(): void
 	{
 		$this->checkExplicitMixed = false;
+		$this->checkNullables = true;
 		$this->analyse([__DIR__ . '/data/bug-5706.php'], []);
 	}
 
 	public function testBug5844(): void
 	{
 		$this->checkExplicitMixed = false;
+		$this->checkNullables = true;
 		$this->analyse([__DIR__ . '/data/bug-5844.php'], []);
 	}
 
 	public function testBug7218(): void
 	{
 		$this->checkExplicitMixed = true;
+		$this->checkNullables = true;
 		$this->analyse([__DIR__ . '/data/bug-7218.php'], []);
 	}
 
 	public function testBug5751(): void
 	{
 		$this->checkExplicitMixed = true;
+		$this->checkNullables = true;
 		$this->analyse([__DIR__ . '/data/bug-5751.php'], []);
 	}
 
 	public function testBug3931(): void
 	{
 		$this->checkExplicitMixed = true;
+		$this->checkNullables = true;
 		$this->analyse([__DIR__ . '/data/bug-3931.php'], []);
 	}
 
 	public function testBug3801(): void
 	{
 		$this->checkExplicitMixed = true;
+		$this->checkNullables = true;
 		$this->analyse([__DIR__ . '/data/bug-3801.php'], [
 			[
 				'Function Bug3801\do_foo() should return array{bool, null}|array{null, bool} but returns array{false, true}.',
@@ -147,6 +160,25 @@ class ReturnTypeRuleTest extends RuleTestCase
 				21,
 			],
 		]);
+	}
+
+	public function testListWithNullablesChecked(): void
+	{
+		$this->checkExplicitMixed = false;
+		$this->checkNullables = true;
+		$this->analyse([__DIR__ . '/data/return-list-nullables.php'], [
+			[
+				'Function ReturnListNullables\doFoo() should return array<string>|null but returns array<int, string|null>.',
+				16,
+			],
+		]);
+	}
+
+	public function testListWithNullablesUnchecked(): void
+	{
+		$this->checkExplicitMixed = false;
+		$this->checkNullables = false;
+		$this->analyse([__DIR__ . '/data/return-list-nullables.php'], []);
 	}
 
 }

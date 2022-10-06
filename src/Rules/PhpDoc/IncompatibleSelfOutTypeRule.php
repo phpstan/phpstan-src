d00b45caa1c9fbaa9053b6d2750bed2bd58a2e09
Richard van Velzen <richard@frank.nl>
2022-10-06 10:29:25 +0200

    Add compatibility rule

---
+++ src/Rules/PhpDoc/IncompatibleSelfOutTypeRule.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\PhpDoc;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\Node\InClassMethodNode;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+use PHPStan\ShouldNotHappenException;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\VerbosityLevel;
+
+/**
+ * @implements Rule<InClassMethodNode>
+ */
+class IncompatibleSelfOutTypeRule implements Rule
+{
+
+	public function getNodeType(): string
+	{
+		return InClassMethodNode::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		$method = $scope->getFunction();
+		if ($method === null) {
+			throw new ShouldNotHappenException();
+		}
+
+		$selfOutType = $method->getSelfOutType();
+		if ($selfOutType === null) {
+			return [];
+		}
+
+		$classReflection = $method->getDeclaringClass();
+		$classType = new ObjectType($classReflection->getName(), null, $classReflection);
+
+		if (!$classType->isSuperTypeOf($selfOutType)->no()) {
+			return [];
+		}
+
+		return [
+			RuleErrorBuilder::message(sprintf(
+				'Out type %s is not compatible with %s.',
+				$selfOutType->describe(VerbosityLevel::precise()),
+				$classType->describe(VerbosityLevel::precise()),
+			))->build(),
+		];
+	}
+
+}

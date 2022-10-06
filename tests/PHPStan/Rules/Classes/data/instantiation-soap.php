3c214fa6ee095a56d513315c98b96ddf7d2b5c98
Ahmed Ashraf <ahmed29329@gmail.com>
2022-10-06 23:15:39 +0200

    add tests
    - more tests to ensure mixed
    - support older versions

--- tests/PHPStan/Rules/Classes/data/instantiation-soap.php
+++ tests/PHPStan/Rules/Classes/data/instantiation-soap.php
@@ -4,5 +4,21 @@ namespace TestInstantiation;
 
 function () {
 	throw new \SoapFault('Server', 123);
+};
+
+function () {
 	throw new \SoapFault('Server', 'Some error message');
+};
+
+function () {
+	throw new \SoapFault('Server', 'Some error message', 'actor', [], 'name', []);
+};
+
+function () {
+	throw new \SoapFault('Server', 'Some error message', 'actor', 'test', 'name', 'test');
+};
+
+
+function () {
+	throw new \SoapFault('Server', 'Some error message', 'actor', 1, 'name', 2);
 };

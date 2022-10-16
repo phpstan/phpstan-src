644de2dff5075d8d2c2af08a13cf536fc844d9a7
Markus Staab <maggus.staab@googlemail.com>
2022-10-16 10:03:16 +0200

    more testcoverage

--- tests/PHPStan/Analyser/data/array_values.php
+++ tests/PHPStan/Analyser/data/array_values.php
@@ -6,13 +6,26 @@ use function PHPStan\Testing\assertType;
 
 class HelloWorld
 {
-	public function sayHello($mixed): void
+	public function foo1($mixed): void
 	{
 		if(is_array($mixed)) {
 			assertType('list<mixed>', array_values($mixed));
 		} else {
 			assertType('mixed~array', $mixed);
 			assertType('*NEVER*', array_values($mixed));
+		}
+	}
+
+	/**
+	 * @param list<string> $list
+	 */
+	public function foo2($list): void
+	{
+		if(is_array($list)) {
+			assertType('list<string>', array_values($list));
+		} else {
+			assertType('*NEVER*', $list);
+			assertType('*ERROR*', array_values($list));
 		}
 	}
 }

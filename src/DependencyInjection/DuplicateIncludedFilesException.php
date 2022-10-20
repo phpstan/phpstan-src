f15cd6deea2dda4b41272aa50fb3b61304ad3515
Ondrej Mirtes <ondrej@mirtes.cz>
2022-10-20 11:06:47 +0200

    Detect duplicate included files in ContainerFactory

---
+++ src/DependencyInjection/DuplicateIncludedFilesException.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\DependencyInjection;
+
+use Exception;
+use function implode;
+use function sprintf;
+
+class DuplicateIncludedFilesException extends Exception
+{
+
+	/**
+	 * @param string[] $files
+	 */
+	public function __construct(private array $files)
+	{
+		parent::__construct(sprintf('These files are included multiple times: %s', implode(', ', $this->files)));
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getFiles(): array
+	{
+		return $this->files;
+	}
+
+}

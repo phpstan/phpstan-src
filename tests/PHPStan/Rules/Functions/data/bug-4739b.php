c26999a41e66fd134fc7423e6c910d0d955f942d
Markus Staab <markus.staab@redaxo.de>
2022-10-26 10:08:02 +0200

    move global code into function

--- tests/PHPStan/Rules/Functions/data/bug-4739b.php
+++ tests/PHPStan/Rules/Functions/data/bug-4739b.php
@@ -22,53 +22,55 @@ class Record {
 	public $name;
 }
 
-$emails = [];
-$records = [];
-filter(
-	function (Record $domain) use (&$emails): bool {
-		if (!isset($emails[$domain->name])) {
-			$emails[$domain->name] = TRUE;
-			return TRUE;
-		}
-		return !$domain->isInactive;
-	},
-	$records
-);
-$test = (bool) mt_rand(0, 1);
-filter(
-	function (bool $arg) use (&$emails): bool {
-		if (empty($emails)) {
-			return TRUE;
-		}
-		return $arg;
-	},
-	$records
-);
-filter(
-	function (bool $arg) use ($emails): bool {
-		if (empty($emails)) {
-			return TRUE;
-		}
-		return $arg;
-	},
-	$records
-);
-$test = (bool) mt_rand(0, 1);
-filter(
-	function (bool $arg) use (&$test): bool {
-		if ($test) {
-			return TRUE;
-		}
-		return $arg;
-	},
-	$records
-);
-filter(
-	function (bool $arg) use ($test): bool {
-		if ($test) {
-			return TRUE;
-		}
-		return $arg;
-	},
-	$records
-);
+function doFoo() {
+	$emails = [];
+	$records = [];
+	filter(
+		function (Record $domain) use (&$emails): bool {
+			if (!isset($emails[$domain->name])) {
+				$emails[$domain->name] = TRUE;
+				return TRUE;
+			}
+			return !$domain->isInactive;
+		},
+		$records
+	);
+	$test = (bool) mt_rand(0, 1);
+	filter(
+		function (bool $arg) use (&$emails): bool {
+			if (empty($emails)) {
+				return TRUE;
+			}
+			return $arg;
+		},
+		$records
+	);
+	filter(
+		function (bool $arg) use ($emails): bool {
+			if (empty($emails)) {
+				return TRUE;
+			}
+			return $arg;
+		},
+		$records
+	);
+	$test = (bool) mt_rand(0, 1);
+	filter(
+		function (bool $arg) use (&$test): bool {
+			if ($test) {
+				return TRUE;
+			}
+			return $arg;
+		},
+		$records
+	);
+	filter(
+		function (bool $arg) use ($test): bool {
+			if ($test) {
+				return TRUE;
+			}
+			return $arg;
+		},
+		$records
+	);
+}

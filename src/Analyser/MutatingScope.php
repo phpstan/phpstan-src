bf45605280b6d81470566fff99e2813162b4f74b
Ondrej Mirtes <ondrej@mirtes.cz>
2022-10-31 14:05:05 +0100

    Call TypeSpecifier for native types too

--- src/Analyser/MutatingScope.php
+++ src/Analyser/MutatingScope.php
@@ -3535,6 +3535,11 @@ class MutatingScope implements Scope
 
 		$specifiedTypes = $this->typeSpecifier->specifyTypesInCondition($this, $expr, TypeSpecifierContext::createTruthy());
 		$scope = $this->filterBySpecifiedTypes($specifiedTypes);
+
+		$nativeScope = $this->promoteNativeTypes();
+		$nativeSpecifiedTypes = $this->typeSpecifier->specifyTypesInCondition($nativeScope, $expr, TypeSpecifierContext::createTruthy());
+		$scope->nativeExpressionTypes = $nativeScope->filterBySpecifiedTypes($nativeSpecifiedTypes)->expressionTypes;
+
 		$this->truthyScopes[$exprString] = $scope;
 
 		return $scope;
@@ -3553,6 +3558,10 @@ class MutatingScope implements Scope
 
 		$specifiedTypes = $this->typeSpecifier->specifyTypesInCondition($this, $expr, TypeSpecifierContext::createFalsey());
 		$scope = $this->filterBySpecifiedTypes($specifiedTypes);
+		$nativeScope = $this->promoteNativeTypes();
+		$nativeSpecifiedTypes = $this->typeSpecifier->specifyTypesInCondition($nativeScope, $expr, TypeSpecifierContext::createFalsey());
+		$scope->nativeExpressionTypes = $nativeScope->filterBySpecifiedTypes($nativeSpecifiedTypes)->expressionTypes;
+
 		$this->falseyScopes[$exprString] = $scope;
 
 		return $scope;

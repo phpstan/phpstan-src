4093ea74e0827ee5be32d127dd761878f04c34fd
Markus Staab <maggus.staab@googlemail.com>
2022-11-01 09:00:47 +0100

    also assert on variables

--- tests/PHPStan/Analyser/data/bug-7913.php
+++ tests/PHPStan/Analyser/data/bug-7913.php
@@ -15,8 +15,11 @@ if (!empty(X)) {
 assertType('array{}', X);
 
 $x = [];
+assertType('array{}', $x);
 if (!empty($x)) {
+	assertType('*NEVER*', $x);
 	foreach ($x as $y) {
 		print($y);
 	}
 }
+assertType('array{}', $x);

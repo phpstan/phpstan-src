bc4bedbf85cc8f2e4357cfcedd9c9081624a19d4
Yohta Kimura <kitakita7617@gmail.com>
2022-11-04 17:09:58 +0900

    fix test for literal string

--- tests/PHPStan/Rules/Methods/data/array-cast-list-types.php
+++ tests/PHPStan/Rules/Methods/data/array-cast-list-types.php
@@ -9,14 +9,15 @@ class Foo {
 	public function foo($var): void {}
 
 	/**
+	 * @param literal-string $literalString
 	 * @param non-empty-string $nonEmptyString
 	 * @param non-falsy-string $nonFalsyString
 	 * @param numeric-string $numericString
 	 * @param resource $resource
 	 */
-	public function bar(string $nonEmptyString, string $nonFalsyString, string $numericString, $resource) {
+	public function bar(string $literalString, string $nonEmptyString, string $nonFalsyString, string $numericString, $resource) {
 		$this->foo((array) true);
-		$this->foo((array) 'literal');
+		$this->foo((array) $literalString);
 		$this->foo((array) 1.0);
 		$this->foo((array) 1);
 		$this->foo((array) $resource);

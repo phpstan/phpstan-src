980e931ab302f94a2bafe2d229345af1adc7a3f7
Takuya Aramaki <takaram71@gmail.com>
2022-11-05 21:57:52 +0900

    Fix return type of array_search() with constant array type haystack

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -1141,6 +1141,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-7805.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-82.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-4565.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-3789.php');
 	}
 
 	/**

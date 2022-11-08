2e77b93d0809764abdd8bd9844fd615cba2aecdb
Niels Keurentjes <niels.keurentjes@omines.com>
2022-11-08 09:32:37 +0100

    Extend Xdebug note
    If a problem is emitted proper DX says you should also suggest a solution.

--- src/Command/CommandHelper.php
+++ src/Command/CommandHelper.php
@@ -96,7 +96,7 @@ class CommandHelper
 		if ($allowXdebug && !XdebugHandler::isXdebugActive()) {
 			$errorOutput->getStyle()->note('You are running with "--xdebug" enabled, but the Xdebug PHP extension is not active. The process will not halt at breakpoints.');
 		} elseif (!$allowXdebug && XdebugHandler::isXdebugActive()) {
-			$errorOutput->getStyle()->note('The Xdebug PHP extension is active, but "--xdebug" is not used. This may slow down performance and the process will not halt at breakpoints.');
+			$errorOutput->getStyle()->note('The Xdebug PHP extension is active, but "--xdebug" is not used. This may slow down performance and the process will not halt at breakpoints. Run with XDEBUG_MODE=off to disable Xdebug and remove this message.');
 		}
 
 		if (!$allowXdebug) {

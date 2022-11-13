fa5dc287c825709a151ae74c6221b5e5eed8f34f
Yohta Kimura <kitakita7617@gmail.com>
2022-11-13 12:08:50 +0900

    Revert "preserve the original test by adding @phpstan-impure"
    This reverts commit ee0bbfac7d75ee79de32e19b2dbcb8e8714b1960.

--- tests/PHPStan/Analyser/data/bug-7918.php
+++ tests/PHPStan/Analyser/data/bug-7918.php
@@ -18,14 +18,6 @@ class TestController
 		return random_int(0, 1) > 0;
 	}
 
-	/**
-	 * @phpstan-impure
-	 */
-	private function randImpure(): bool
-	{
-		return random_int(0, 1) > 0;
-	}
-
 	/** @return list<array<string, bool>> */
 	public function run(): array
 	{
@@ -74,55 +66,6 @@ class TestController
 			// $result['val5'] = $this->rand();
 			// $result['val6'] = $this->rand();
 
-
-			$arr3[] = $result;
-			assertType('non-empty-array<int, array{val1: bool, val2: bool, val3: bool, val4: bool, val5: bool, val6: bool, val7: false}>', $arr3);
-		}
-
-		assertType('array<int, array{val1: bool, val2: bool, val3: bool, val4: bool, val5: bool, val6: bool, val7: false}>', $arr3);
-
-		return $arr3;
-	}
-
-	/** @return list<array<string, bool>> */
-	public function runImpure(): array
-	{
-		$arr3 = [];
-		foreach ($this->someFunc() as $id => $arr2) {
-			// Solution 1 - Specify $result type
-			// /** @var array<string,bool> $result */
-			$result = [
-				'val1' => false,
-				'val2' => false,
-				'val3' => false,
-				'val4' => false,
-				'val5' => false,
-				'val6' => false,
-				'val7' => false,
-			];
-
-			if ($this->randImpure()) {
-				$result['val1'] = true;
-			}
-			if ($this->randImpure()) {
-				$result['val2'] = true;
-			}
-
-			if ($this->randImpure()) {
-				$result['val3'] = true;
-			}
-
-			if ($this->randImpure()) {
-				$result['val4'] = true;
-			}
-
-			if ($this->randImpure()) {
-				$result['val5'] = true;
-			}
-
-			if ($this->randImpure()) {
-				$result['val6'] = true;
-			}
 
 			$arr3[] = $result;
 			assertType('non-empty-array<int, array{val1: bool, val2: bool, val3: bool, val4: bool, val5: bool, val6: bool, val7: false}>', $arr3);

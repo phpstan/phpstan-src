323451a8e94be9e20453d66b85585fcb456f4405
Théo FIDRY <theo.fidry@gmail.com>
2022-12-08 12:10:53 +0100

    Improve the CPU core count detection
    See https://github.com/theofidry/cpu-core-counter/issues/11 for the motivations.
    
    The code should be sensibly the same, the notable differences are:
    
    - fixed the deprecated usage of `hw.ncpu`
    - /proc/cpuinfo is check _last_
    - nproc is checked first (before was not checked at all, it's considered to be more accurate and less convoluted than cpuinfo though)
    - not sure about the `return 2`, but this was not too clear [here](https://github.com/phpstan/phpstan-src/pull/514#pullrequestreview-660926363) neither. I could otherwise change the fallback value to return `1` if `proc_open` doesn't exist and 2 otherwise
    - add more ways to find the CPU cores count

--- src/Process/CpuCoreCounter.php
+++ src/Process/CpuCoreCounter.php
@@ -2,16 +2,8 @@
 
 namespace PHPStan\Process;
 
-use function count;
-use function fgets;
-use function file_get_contents;
-use function function_exists;
-use function is_file;
-use function is_resource;
-use function pclose;
-use function popen;
-use function preg_match_all;
-use const DIRECTORY_SEPARATOR;
+use Fidry\CpuCoreCounter\CpuCoreCounter as FidryCpuCoreCounter;
+use Fidry\CpuCoreCounter\NumberOfCpuCoreNotFound;
 
 class CpuCoreCounter
 {
@@ -24,42 +16,13 @@ class CpuCoreCounter
 			return $this->count;
 		}
 
-		if (!function_exists('proc_open')) {
-			return $this->count = 1;
+		try {
+			$this->count = (new FidryCpuCoreCounter())->getCount();
+		} catch (NumberOfCpuCoreNotFound) {
+			$this->count = 1;
 		}
 
-		// from brianium/paratest
-		if (@is_file('/proc/cpuinfo')) {
-			// Linux (and potentially Windows with linux sub systems)
-			$cpuinfo = @file_get_contents('/proc/cpuinfo');
-			if ($cpuinfo !== false) {
-				preg_match_all('/^processor/m', $cpuinfo, $matches);
-				return $this->count = count($matches[0]);
-			}
-		}
-
-		if (DIRECTORY_SEPARATOR === '\\') {
-			// Windows
-			$process = @popen('wmic cpu get NumberOfLogicalProcessors', 'rb');
-			if (is_resource($process)) {
-				fgets($process);
-				$cores = (int) fgets($process);
-				pclose($process);
-
-				return $this->count = $cores;
-			}
-		}
-
-		$process = @popen('sysctl -n hw.ncpu', 'rb');
-		if (is_resource($process)) {
-			// *nix (Linux, BSD and Mac)
-			$cores = (int) fgets($process);
-			pclose($process);
-
-			return $this->count = $cores;
-		}
-
-		return $this->count = 2;
+		return $this->count;
 	}
 
 }

5fc2bfd3522909ca40af7e6bcbf9c95e11120683
Markus Staab <maggus.staab@googlemail.com>
2022-12-13 09:28:09 +0100

    Implement identical/equal comparisons on EnumCaseObjectType

--- tests/PHPStan/Rules/Comparison/IfConstantConditionRuleTest.php
+++ tests/PHPStan/Rules/Comparison/IfConstantConditionRuleTest.php
@@ -4,6 +4,7 @@ namespace PHPStan\Rules\Comparison;
 
 use PHPStan\Rules\Rule;
 use PHPStan\Testing\RuleTestCase;
+use const PHP_VERSION_ID;
 
 /**
  * @extends RuleTestCase<IfConstantConditionRule>
@@ -127,6 +128,29 @@ class IfConstantConditionRuleTest extends RuleTestCase
 	{
 		$this->treatPhpDocTypesAsCertain = true;
 		$this->analyse([__DIR__ . '/data/bug-6902.php'], []);
+	}
+
+	public function testBug8485(): void
+	{
+		if (PHP_VERSION_ID < 80100) {
+			$this->markTestSkipped('Test requires PHP 8.1.');
+		}
+
+		$this->treatPhpDocTypesAsCertain = true;
+		$this->analyse([__DIR__ . '/data/bug-8485.php'], [
+			[
+				'If condition is always true.',
+				19,
+			],
+			[
+				'If condition is always false.',
+				24,
+			],
+			[
+				'If condition is always false.',
+				29,
+			],
+		]);
 	}
 
 }

b76cbab2ac64d43e34ff7cb4dbdbdec4c2be88c6
Yohta Kimura <kitakita7617@gmail.com>
2022-12-14 22:14:59 +0900

    add regression test for https://github.com/phpstan/phpstan/issues/7805

---
+++ tests/PHPStan/Analyser/data/bug-7805.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types = 1);
+
+use function PHPStan\Testing\assertNativeType;
+use function PHPStan\Testing\assertType;
+
+/**
+ * @phpstan-param array{help?: null} $params
+ */
+function foo(array $params)
+{
+	assertType('array{help?: null}', $params);
+	assertNativeType('array', $params);
+	if (array_key_exists('help', $params)) {
+		assertType('array{help: null}', $params);
+		assertNativeType("array&hasOffset('help')", $params);
+		unset($params['help']);
+
+		assertType('array{}', $params);
+		assertNativeType("array<mixed~'help', mixed>", $params);
+		$params = $params === [] ? ['list'] : $params;
+		assertType("array{'list'}", $params);
+		assertNativeType("array{'list'}", $params);
+		// assertNativeType("array<mixed~'help', mixed>", $params); // not working yet
+		array_unshift($params, 'help');
+		assertType("array{'help', 'list'}", $params);
+		assertNativeType("array{'list'}", $params);
+		// assertNativeType("array<mixed~'help', mixed>", $params); // not working yet
+	}
+	assertType("array{}|array{'help', 'list'}", $params);
+	assertNativeType('array', $params);
+
+	return $params;
+}

cb9761e88489051b2636508621c72610d0cfc06c
Yohta Kimura <38206553+rajyan@users.noreply.github.com>
2022-12-15 21:50:28 +0900

    Add regression test

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -1138,6 +1138,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-8467b.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/PDOStatement.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/discussion-8447.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-7805.php');
 	}
 
 	/**

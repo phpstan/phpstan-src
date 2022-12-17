b445bdb7fc80e9a0ec67a4e6fe7d38372aec4f65
ryium <ryium114@gmail.com>
2022-12-17 23:31:33 +0900

    Added test for function(method)s with pass by reference params

--- tests/PHPStan/Rules/Variables/data/bug-8084.php
+++ tests/PHPStan/Rules/Variables/data/bug-8084.php
@@ -9,12 +9,11 @@ use function PHPStan\Testing\assertType;
 class Bug8084
 {
 	/**
-	 * @param array{a: 0, b?: 1} $arr
+	 * @param array{a?: 0} $arr
 	 * @throws Exception
 	 */
 	public function run(array $arr): void
 	{
-		assertType('0', array_shift($arr) ?? throw new Exception());
-		assertType('1|null', array_shift($arr));
+		assertType('0|null', array_shift($arr) ?? throw new Exception());
 	}
 }

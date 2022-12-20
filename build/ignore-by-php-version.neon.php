25dbb8f7b61ba7110a5ddc03e1132fd9c8dd2a92
Ondrej Mirtes <ondrej@mirtes.cz>
2022-12-20 21:43:21 +0100

    Fix build

--- build/ignore-by-php-version.neon.php
+++ build/ignore-by-php-version.neon.php
@@ -24,7 +24,7 @@ if (PHP_VERSION_ID < 70400) {
 	$includes[] = __DIR__ . '/enum-adapter-errors.neon';
 }
 
-if (PHP_VERSION_ID >= 70300 && PHP_VERSION_ID < 80000) {
+if (PHP_VERSION_ID < 80000) {
 	$includes[] = __DIR__ . '/more-enum-adapter-errors.neon';
 }
 

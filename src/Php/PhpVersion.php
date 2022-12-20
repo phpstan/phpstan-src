9a31c4baa29f752fd9467bd1395a95f3ed5317d9
Jan Nedbal <janedbal@gmail.com>
2022-12-20 14:03:28 +0100

    PhpVersion: supportsDisjunctiveNormalForm

--- src/Php/PhpVersion.php
+++ src/Php/PhpVersion.php
@@ -201,6 +201,11 @@ class PhpVersion
 		return $this->versionId >= 80200;
 	}
 
+	public function supportsDisjunctiveNormalForm(): bool
+	{
+		return $this->versionId >= 80200;
+	}
+	
 	public function serializableRequiresMagicMethods(): bool
 	{
 		return $this->versionId >= 80100;

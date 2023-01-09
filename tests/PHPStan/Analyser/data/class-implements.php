f36ad4081476d071ac01de233f3d0a639da377ef
Vincent Langlet <vincentlanglet@hotmail.fr>
2023-01-09 22:24:55 +0100

    More tests

--- tests/PHPStan/Analyser/data/class-implements.php
+++ tests/PHPStan/Analyser/data/class-implements.php
@@ -15,7 +15,9 @@ class ClassImplements
 		object|string $objectOrClassString,
 		object|string $objectOrString,
 		string $classString,
-		string $className
+		string $className,
+		bool $bool,
+		mixed $mixed,
 	): void {
 		assertType('array<string, class-string>', class_implements($object));
 		assertType('array<string, class-string>', class_implements($objectOrClassString));
@@ -23,11 +25,29 @@ class ClassImplements
 		assertType('array<string, class-string>', class_implements($classString));
 		assertType('array<string, class-string>|false', class_implements($className));
 
+		assertType('array<string, class-string>', class_implements($object, true));
+		assertType('array<string, class-string>', class_implements($objectOrClassString, true));
+		assertType('array<string, class-string>|false', class_implements($objectOrString, true));
+		assertType('array<string, class-string>', class_implements($classString, true));
+		assertType('array<string, class-string>|false', class_implements($className, true));
+
 		assertType('array<string, class-string>', class_implements($object, false));
 		assertType('array<string, class-string>|false', class_implements($objectOrClassString, false));
 		assertType('array<string, class-string>|false', class_implements($objectOrString, false));
 		assertType('array<string, class-string>|false', class_implements($classString, false));
 		assertType('array<string, class-string>|false', class_implements($className, false));
+
+		assertType('array<string, class-string>', class_implements($object, $bool));
+		assertType('array<string, class-string>|false', class_implements($objectOrClassString, $bool));
+		assertType('array<string, class-string>|false', class_implements($objectOrString, $bool));
+		assertType('array<string, class-string>|false', class_implements($classString, $bool));
+		assertType('array<string, class-string>|false', class_implements($className, $bool));
+
+		assertType('array<string, class-string>', class_implements($object, $mixed));
+		assertType('array<string, class-string>|false', class_implements($objectOrClassString, $mixed));
+		assertType('array<string, class-string>|false', class_implements($objectOrString, $mixed));
+		assertType('array<string, class-string>|false', class_implements($classString, $mixed));
+		assertType('array<string, class-string>|false', class_implements($className, $mixed));
 
 		assertType('array<string, string>', class_uses($object));
 		assertType('array<string, string>', class_uses($objectOrClassString));
@@ -35,11 +55,29 @@ class ClassImplements
 		assertType('array<string, string>', class_uses($classString));
 		assertType('array<string, string>|false', class_uses($className));
 
+		assertType('array<string, string>', class_uses($object, true));
+		assertType('array<string, string>', class_uses($objectOrClassString, true));
+		assertType('array<string, string>|false', class_uses($objectOrString, true));
+		assertType('array<string, string>', class_uses($classString, true));
+		assertType('array<string, string>|false', class_uses($className, true));
+
 		assertType('array<string, string>', class_uses($object, false));
 		assertType('array<string, string>|false', class_uses($objectOrClassString, false));
 		assertType('array<string, string>|false', class_uses($objectOrString, false));
 		assertType('array<string, string>|false', class_uses($classString, false));
 		assertType('array<string, string>|false', class_uses($className, false));
+
+		assertType('array<string, string>', class_uses($object, $bool));
+		assertType('array<string, string>|false', class_uses($objectOrClassString, $bool));
+		assertType('array<string, string>|false', class_uses($objectOrString, $bool));
+		assertType('array<string, string>|false', class_uses($classString, $bool));
+		assertType('array<string, string>|false', class_uses($className, $bool));
+
+		assertType('array<string, string>', class_uses($object, $mixed));
+		assertType('array<string, string>|false', class_uses($objectOrClassString, $mixed));
+		assertType('array<string, string>|false', class_uses($objectOrString, $mixed));
+		assertType('array<string, string>|false', class_uses($classString, $mixed));
+		assertType('array<string, string>|false', class_uses($className, $mixed));
 
 		assertType('array<string, class-string>', class_parents($object));
 		assertType('array<string, class-string>', class_parents($objectOrClassString));
@@ -47,11 +85,29 @@ class ClassImplements
 		assertType('array<string, class-string>', class_parents($classString));
 		assertType('array<string, class-string>|false', class_parents($className));
 
+		assertType('array<string, class-string>', class_parents($object, true));
+		assertType('array<string, class-string>', class_parents($objectOrClassString, true));
+		assertType('array<string, class-string>|false', class_parents($objectOrString, true));
+		assertType('array<string, class-string>', class_parents($classString, true));
+		assertType('array<string, class-string>|false', class_parents($className, true));
+
 		assertType('array<string, class-string>', class_parents($object, false));
 		assertType('array<string, class-string>|false', class_parents($objectOrClassString, false));
 		assertType('array<string, class-string>|false', class_parents($objectOrString, false));
 		assertType('array<string, class-string>|false', class_parents($classString, false));
 		assertType('array<string, class-string>|false', class_parents($className, false));
+
+		assertType('array<string, class-string>', class_parents($object, $bool));
+		assertType('array<string, class-string>|false', class_parents($objectOrClassString, $bool));
+		assertType('array<string, class-string>|false', class_parents($objectOrString, $bool));
+		assertType('array<string, class-string>|false', class_parents($classString, $bool));
+		assertType('array<string, class-string>|false', class_parents($className, $bool));
+
+		assertType('array<string, class-string>', class_parents($object, $mixed));
+		assertType('array<string, class-string>|false', class_parents($objectOrClassString, $mixed));
+		assertType('array<string, class-string>|false', class_parents($objectOrString, $mixed));
+		assertType('array<string, class-string>|false', class_parents($classString, $mixed));
+		assertType('array<string, class-string>|false', class_parents($className, $mixed));
 	}
 
 }

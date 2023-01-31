8cc1a93dd885a91d0aa48d176f20f1c40dbbcc4e
Vincent Langlet <vincentlanglet@hotmail.fr>
2023-01-31 21:01:16 +0100

    Add numeric-string test

--- tests/PHPStan/Analyser/data/bug-8803.php
+++ tests/PHPStan/Analyser/data/bug-8803.php
@@ -12,11 +12,15 @@ class HelloWorld
 		for ($offset = 1; $offset <= 14; $offset++) {
 			$value = $from->format('N') + $offset;
 			$value2 = $offset + $from->format('N');
+			$value3 = '1e3' + $offset;
+			$value4 = $offset + '1e3';
 
 			assertType("'1'|'2'|'3'|'4'|'5'|'6'|'7'", $from->format('N'));
 			assertType('int<1, 14>', $offset);
 			assertType('int<2, 21>', $value);
 			assertType('int<2, 21>', $value2);
+			assertType('float', $value3);
+			assertType('float', $value4);
 		}
 	}
 

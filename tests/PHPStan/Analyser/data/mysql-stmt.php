9dee2969e3ebb925f8f232322afe6255f9315ff1
Javier Spagnoletti <phansys@gmail.com>
2023-02-06 07:17:59 -0300

    Use "numeric-string" type for some MySQLi functions

---
+++ tests/PHPStan/Analyser/data/mysql-stmt.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace MySQLiStmt;
+
+use mysqli;
+use function PHPStan\Testing\assertType;
+
+final class Foo {
+	public function bar(): void
+	{
+		$mysqli = new mysqli();
+		$stmt = $mysqli->prepare('SELECT x FROM z;');
+		$stmt->execute();
+		assertType('int<0, max>|numeric-string', $stmt->num_rows);
+
+		$stmt = $mysqli->prepare('DELETE FROM z;');
+		$stmt->execute();
+		assertType('int<-1, max>|numeric-string', $stmt->affected_rows);
+	}
+}

a593a423cf8f33dd4d622dcbff43281ac63526eb
Ondrej Mirtes <ondrej@mirtes.cz>
2023-02-20 20:54:44 +0100

    BooleanAndConstantConditionRule - respect reportAlwaysTrueInLastCondition

---
+++ tests/PHPStan/Rules/Comparison/data/boolean-and-report-always-true-last-condition.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace BooleanAndReportAlwaysTrueLastCondition;
+
+class Foo
+{
+
+	public function doFoo(): void
+	{
+		$a = 1;
+		if (rand(0, 1)) {
+
+		} elseif ($a && rand(0, 1)) {
+
+		}
+	}
+
+	public function doBar(): void
+	{
+		$a = 1;
+		if (rand(0, 1)) {
+
+		} elseif ($a && rand(0, 1)) {
+
+		} else {
+
+		}
+	}
+
+}
+
+class Foo2
+{
+
+	public function doFoo(): void
+	{
+		$a = 1;
+		if (rand(0, 1)) {
+
+		} elseif (rand(0, 1) && $a) {
+
+		}
+	}
+
+	public function doBar(): void
+	{
+		$a = 1;
+		if (rand(0, 1)) {
+
+		} elseif (rand(0, 1) && $a) {
+
+		} else {
+
+		}
+	}
+
+}
+
+class Foo3
+{
+
+	/**
+	 * @param \Throwable&\Traversable $o
+	 */
+	public function doFoo($o)
+	{
+		if (rand(0, 1)) {
+
+		} elseif ($o instanceof \Throwable && $o instanceof \Traversable) {
+
+		}
+	}
+
+	/**
+	 * @param \Throwable&\Traversable $o
+	 */
+	public function doBar($o)
+	{
+		if (rand(0, 1)) {
+
+		} elseif ($o instanceof \Throwable && $o instanceof \Traversable) {
+
+		} else {
+
+		}
+	}
+
+}

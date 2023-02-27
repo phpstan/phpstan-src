f8c51c838d52bd3fbd240938cc03aa3f150ecee3
Ruud Kamphuis <ruudk@users.noreply.github.com>
2023-02-27 11:22:26 +0100

    Keep ignored errors on a single line

--- src/Command/ErrorFormatter/BaselinePhpErrorFormatter.php
+++ src/Command/ErrorFormatter/BaselinePhpErrorFormatter.php
@@ -63,7 +63,7 @@ class BaselinePhpErrorFormatter
 
 			foreach ($fileErrorsCounts as $message => $count) {
 				$php .= sprintf(
-					"\$ignoreErrors[] = [\n\t'message' => %s,\n\t'count' => %d,\n\t'path' => __DIR__ . %s,\n];\n",
+					"\$ignoreErrors[] = ['message' => %s, 'count' => %d, 'path' => __DIR__ . %s];\n",
 					self::exportDoubleQuoted(Helpers::escape('#^' . preg_quote($message, '#') . '$#')),
 					var_export($count, true),
 					var_export(Helpers::escape($file), true),

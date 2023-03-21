3a878f19354eb02787765f651cf7ed3a558ca6b7
Simon Podlipsky <simon@podlipsky.net>
2023-03-21 10:48:08 +0100

    feat: improve `Map::get()` @throws inference

---
+++ src/Type/Php/DsMapDynamicMethodThrowTypeExtension.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Type\Php;
+
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\DynamicMethodThrowTypeExtension;
+use PHPStan\Type\Type;
+use PHPStan\Type\VoidType;
+use function count;
+
+final class DsMapDynamicMethodThrowTypeExtension implements DynamicMethodThrowTypeExtension
+{
+
+	public function isMethodSupported(MethodReflection $methodReflection): bool
+	{
+		return $methodReflection->getDeclaringClass()->getName() === 'Ds\Map'
+			&& $methodReflection->getName() === 'get';
+	}
+
+	public function getThrowTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): Type|null
+	{
+		if (count($methodCall->args) < 2) {
+			return $methodReflection->getThrowType();
+		}
+
+		return new VoidType();
+	}
+
+}

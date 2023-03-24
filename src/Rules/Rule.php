23d29aba97762c39f5d6df58461a2545bb270d9b
Ondrej Mirtes <ondrej@mirtes.cz>
2023-03-24 09:59:12 +0100

    Rich PHPDoc for the Rule interface

--- src/Rules/Rule.php
+++ src/Rules/Rule.php
@@ -6,6 +6,19 @@ use PhpParser\Node;
 use PHPStan\Analyser\Scope;
 
 /**
+ * This is the interface custom rules implement. To register it in the configuration file
+ * use the `phpstan.rules.rule` service tag:
+ *
+ * ```
+ * services:
+ * 	-
+ *		class: App\MyRule
+ *		tags:
+ *			- phpstan.rules.rule
+ * ```
+ *
+ * Learn more: https://phpstan.org/developing-extensions/rules
+ *
  * @api
  * @phpstan-template TNodeType of Node
  */

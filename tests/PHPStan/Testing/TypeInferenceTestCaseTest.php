7c6cfc04a70605faacf79583e0a6e60869a14f5a
Markus Staab <markus.staab@redaxo.de>
2023-04-06 09:35:24 +0200

    use dataProvider

--- tests/PHPStan/Testing/TypeInferenceTestCaseTest.php
+++ tests/PHPStan/Testing/TypeInferenceTestCaseTest.php
@@ -7,76 +7,55 @@ use PHPUnit\Framework\AssertionFailedError;
 final class TypeInferenceTestCaseTest extends TypeInferenceTestCase
 {
 
-	public function testMissingAssertCertaintyNamespace(): void
+	public static function dataFileAssertionFailedErrors(): iterable
 	{
-		$this->expectException(AssertionFailedError::class);
-		$this->expectExceptionMessage('Missing use statement for assertVariableCertainty() on line 8.');
-
-		$this->gatherAssertTypes(__DIR__ . '/data/assert-certainty-missing-namespace.php');
+		yield [
+			__DIR__ . '/data/assert-certainty-missing-namespace.php',
+			'Missing use statement for assertVariableCertainty() on line 8.',
+		];
+		yield [
+			__DIR__ . '/data/assert-native-type-missing-namespace.php',
+			'Missing use statement for assertNativeType() on line 6.',
+		];
+		yield [
+			__DIR__ . '/data/assert-type-missing-namespace.php',
+			'Missing use statement for assertType() on line 6.',
+		];
+		yield [
+			__DIR__ . '/data/assert-certainty-wrong-namespace.php',
+			'Function PHPStan\Testing\assertVariableCertainty imported with wrong namespace SomeWrong\Namespace\assertVariableCertainty called on line 9.',
+		];
+		yield [
+			__DIR__ . '/data/assert-native-type-wrong-namespace.php',
+			'Function PHPStan\Testing\assertNativeType imported with wrong namespace SomeWrong\Namespace\assertNativeType called on line 8.',
+		];
+		yield [
+			__DIR__ . '/data/assert-type-wrong-namespace.php',
+			'Function PHPStan\Testing\assertType imported with wrong namespace SomeWrong\Namespace\assertType called on line 8.',
+		];
+		yield [
+			__DIR__ . '/data/assert-certainty-case-insensitive.php',
+			'Missing use statement for assertvariablecertainty() on line 8.',
+		];
+		yield [
+			__DIR__ . '/data/assert-native-type-case-insensitive.php',
+			'Missing use statement for assertNATIVEType() on line 6.',
+		];
+		yield [
+			__DIR__ . '/data/assert-type-case-insensitive.php',
+			'Missing use statement for assertTYPe() on line 6.',
+		];
 	}
 
-	public function testMissingAssertNativeTypeNamespace(): void
+	/**
+	 * @dataProvider dataFileAssertionFailedErrors
+	 */
+	public function testFileAssertionFailedErrors(string $filePath, string $errorMessage): void
 	{
 		$this->expectException(AssertionFailedError::class);
-		$this->expectExceptionMessage('Missing use statement for assertNativeType() on line 6.');
+		$this->expectExceptionMessage($errorMessage);
 
-		$this->gatherAssertTypes(__DIR__ . '/data/assert-native-type-missing-namespace.php');
-	}
-
-	public function testMissingAssertTypeNamespace(): void
-	{
-		$this->expectException(AssertionFailedError::class);
-		$this->expectExceptionMessage('Missing use statement for assertType() on line 6.');
-
-		$this->gatherAssertTypes(__DIR__ . '/data/assert-type-missing-namespace.php');
-	}
-
-	public function testWrongAssertCertaintyNamespace(): void
-	{
-		$this->expectException(AssertionFailedError::class);
-		$this->expectExceptionMessage('Function PHPStan\Testing\assertVariableCertainty imported with wrong namespace SomeWrong\Namespace\assertVariableCertainty called on line 9.');
-
-		$this->gatherAssertTypes(__DIR__ . '/data/assert-certainty-wrong-namespace.php');
-	}
-
-	public function testWrongAssertNativeTypeNamespace(): void
-	{
-		$this->expectException(AssertionFailedError::class);
-		$this->expectExceptionMessage('Function PHPStan\Testing\assertNativeType imported with wrong namespace SomeWrong\Namespace\assertNativeType called on line 8.');
-
-		$this->gatherAssertTypes(__DIR__ . '/data/assert-native-type-wrong-namespace.php');
-	}
-
-	public function testWrongAssertTypeNamespace(): void
-	{
-		$this->expectException(AssertionFailedError::class);
-		$this->expectExceptionMessage('Function PHPStan\Testing\assertType imported with wrong namespace SomeWrong\Namespace\assertType called on line 8.');
-
-		$this->gatherAssertTypes(__DIR__ . '/data/assert-type-wrong-namespace.php');
-	}
-
-	public function testMissingAssertCertaintyCaseSensitivity(): void
-	{
-		$this->expectException(AssertionFailedError::class);
-		$this->expectExceptionMessage('Missing use statement for assertvariablecertainty() on line 8.');
-
-		$this->gatherAssertTypes(__DIR__ . '/data/assert-certainty-case-insensitive.php');
-	}
-
-	public function testMissingAssertNativeTypeCaseSensitivity(): void
-	{
-		$this->expectException(AssertionFailedError::class);
-		$this->expectExceptionMessage('Missing use statement for assertNATIVEType() on line 6.');
-
-		$this->gatherAssertTypes(__DIR__ . '/data/assert-native-type-case-insensitive.php');
-	}
-
-	public function testMissingAssertTypeCaseSensitivity(): void
-	{
-		$this->expectException(AssertionFailedError::class);
-		$this->expectExceptionMessage('Missing use statement for assertTYPe() on line 6.');
-
-		$this->gatherAssertTypes(__DIR__ . '/data/assert-type-case-insensitive.php');
+		$this->gatherAssertTypes($filePath);
 	}
 
 }

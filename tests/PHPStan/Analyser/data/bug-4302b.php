403f213a504fdb2fdeb27ec691f2ec0135b58c85
Markus Staab <maggus.staab@googlemail.com>
2023-04-10 08:54:31 +0200

    test more cases involving interfaces

--- tests/PHPStan/Analyser/data/bug-4302b.php
+++ tests/PHPStan/Analyser/data/bug-4302b.php
@@ -6,9 +6,15 @@ use function PHPStan\Testing\assertType;
 
 class HelloWorld
 {
-	public function theMethod(MyInterface $object) {
-		assertType('class-string|false', get_parent_class($object));
+	public function theMethod(MyInterface $interface, MyClass $class, MySubClass $subclass) {
+		assertType('class-string|false', get_parent_class($interface));
+		assertType('false', get_parent_class($class));
+		assertType("'Bug4302b\\\\MyClass'", get_parent_class($subclass));
 	}
 }
 
 interface MyInterface {}
+
+class MyClass implements MyInterface {}
+
+class MySubClass extends MyClass {}

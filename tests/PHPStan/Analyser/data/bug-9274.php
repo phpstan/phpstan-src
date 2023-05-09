b86479246a9f562e099c5e94e61ad1971fde2261
Takuya Aramaki <takaram71@gmail.com>
2023-05-10 00:58:50 +0900

    Put test codes into functions

--- tests/PHPStan/Analyser/data/bug-9274.php
+++ tests/PHPStan/Analyser/data/bug-9274.php
@@ -16,14 +16,20 @@ class A extends SplDoublyLinkedList {}
 /** @extends SplQueue<Point> */
 class B extends SplQueue {}
 
-$dll = new A();
-$p1 = $dll[0];
+function testSplDoublyLinkedList(): void
+{
+	$dll = new A();
+	$p1 = $dll[0];
 
-assertType('Bug9274\Point', $p1);
-assertType('int', $p1->x);
+	assertType('Bug9274\Point', $p1);
+	assertType('int', $p1->x);
+}
 
-$queue = new B();
-$p2 = $queue[0];
+function testSplQueue(): void
+{
+	$queue = new B();
+	$p2 = $queue[0];
 
-assertType('Bug9274\Point', $p2);
-assertType('int', $p2->x);
+	assertType('Bug9274\Point', $p2);
+	assertType('int', $p2->x);
+}

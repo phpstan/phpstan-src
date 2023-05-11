7bb15fac5144f025d4052220f782a434ba1d34c9
Jan Tvrdík <git@jantvrdik.com>
2023-05-12 00:05:28 +0200

    Report dead types even in multi-exception catch
    [closes phpstan/phpstan#7312]
    
    
    Co-authored-by: Jan Nedbal <janedbal@gmail.com>

---
+++ tests/PHPStan/Rules/Exceptions/CatchWithUnthrownExceptionRuleWithDisabledMultiCatchTest.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Exceptions;
+
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use function array_merge;
+
+/**
+ * @extends RuleTestCase<CatchWithUnthrownExceptionRule>
+ */
+class CatchWithUnthrownExceptionRuleWithDisabledMultiCatchTest extends RuleTestCase
+{
+
+	protected function getRule(): Rule
+	{
+		return new CatchWithUnthrownExceptionRule();
+	}
+
+	public static function getAdditionalConfigFiles(): array
+	{
+		return array_merge(
+			parent::getAdditionalConfigFiles(),
+			[__DIR__ . '/disable-detect-multi-catch.neon'],
+		);
+	}
+
+	public function testMultiCatchBackwardCompatible(): void
+	{
+		$this->analyse([__DIR__ . '/data/unthrown-exception-multi.php'], [
+			[
+				'Dead catch - InvalidArgumentException is already caught above.',
+				145,
+			],
+			[
+				'Dead catch - InvalidArgumentException is already caught above.',
+				156,
+			],
+		]);
+	}
+
+}

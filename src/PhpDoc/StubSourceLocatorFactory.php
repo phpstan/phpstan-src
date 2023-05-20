d004c239677fb7abe41aa16e8e1b45bfac63f773
Ondrej Mirtes <ondrej@mirtes.cz>
2023-05-20 18:55:02 +0200

    Stub validator - PHPStan classes are known without extra stubs

--- src/PhpDoc/StubSourceLocatorFactory.php
+++ src/PhpDoc/StubSourceLocatorFactory.php
@@ -6,10 +6,13 @@ use PhpParser\Parser;
 use PHPStan\BetterReflection\SourceLocator\Ast\Locator;
 use PHPStan\BetterReflection\SourceLocator\SourceStubber\PhpStormStubsSourceStubber;
 use PHPStan\BetterReflection\SourceLocator\Type\AggregateSourceLocator;
+use PHPStan\BetterReflection\SourceLocator\Type\Composer\Psr\Psr4Mapping;
 use PHPStan\BetterReflection\SourceLocator\Type\MemoizingSourceLocator;
 use PHPStan\BetterReflection\SourceLocator\Type\PhpInternalSourceLocator;
 use PHPStan\BetterReflection\SourceLocator\Type\SourceLocator;
+use PHPStan\Reflection\BetterReflection\SourceLocator\OptimizedPsrAutoloaderLocatorFactory;
 use PHPStan\Reflection\BetterReflection\SourceLocator\OptimizedSingleFileSourceLocatorRepository;
+use function dirname;
 
 class StubSourceLocatorFactory
 {
@@ -18,6 +21,7 @@ class StubSourceLocatorFactory
 		private Parser $php8Parser,
 		private PhpStormStubsSourceStubber $phpStormStubsSourceStubber,
 		private OptimizedSingleFileSourceLocatorRepository $optimizedSingleFileSourceLocatorRepository,
+		private OptimizedPsrAutoloaderLocatorFactory $optimizedPsrAutoloaderLocatorFactory,
 		private StubFilesProvider $stubFilesProvider,
 	)
 	{
@@ -30,6 +34,12 @@ class StubSourceLocatorFactory
 		foreach ($this->stubFilesProvider->getStubFiles() as $stubFile) {
 			$locators[] = $this->optimizedSingleFileSourceLocatorRepository->getOrCreate($stubFile);
 		}
+
+		$locators[] = $this->optimizedPsrAutoloaderLocatorFactory->create(
+			Psr4Mapping::fromArrayMappings([
+				'PHPStan\\' => [dirname(__DIR__) . '/'],
+			]),
+		);
 
 		$locators[] = new PhpInternalSourceLocator($astPhp8Locator, $this->phpStormStubsSourceStubber);
 

1de604ad11fdf4495a37d5454e99f2c5fed2d9c4
Markus Staab <markus.staab@redaxo.de>
2023-06-05 13:35:35 +0200

    Cover __sleep() and __serialize() in tests. No fix needed though

--- tests/PHPStan/Rules/Methods/data/bug-9391.php
+++ tests/PHPStan/Rules/Methods/data/bug-9391.php
@@ -6,6 +6,8 @@ namespace Bug9391;
 
 class A
 {
+	private $a;
+
 	/**
 	 * @return array<string, mixed>
 	 */
@@ -15,14 +17,34 @@ class A
 			'a' => 1,
 		];
 	}
+
+	/**
+	 * @return array<int, string>
+	 */
+	public function __serialize(): array
+	{
+		return [
+			'a' => $this->a
+		];
+	}
 }
 
 class B extends A
 {
+	private $b;
+
 	public function __debugInfo(): array
 	{
 		return [
 			'b' => 2,
 		];
 	}
+
+	public function __serialize(): array
+	{
+		return [
+			'b' => $this->b
+		];
+	}
+
 }

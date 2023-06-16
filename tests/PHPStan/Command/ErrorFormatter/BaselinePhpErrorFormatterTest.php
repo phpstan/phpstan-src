4b32cac2d1b98c6af65c07041fdbc64e5d3f3baf
Ondrej Mirtes <ondrej@mirtes.cz>
2023-06-16 08:56:20 +0200

    BaselinePhpErrorFormatter - error identifiers in comments

--- tests/PHPStan/Command/ErrorFormatter/BaselinePhpErrorFormatterTest.php
+++ tests/PHPStan/Command/ErrorFormatter/BaselinePhpErrorFormatterTest.php
@@ -49,6 +49,105 @@ return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
 
 PHP,
 		];
+
+		yield [
+			[
+				new Error(
+					'Foo',
+					__DIR__ . '/Foo.php',
+					5,
+				),
+				new Error(
+					'Foo',
+					__DIR__ . '/Foo.php',
+					5,
+				),
+				(new Error(
+					'Foo with identifier',
+					__DIR__ . '/Foo.php',
+					5,
+				))->withIdentifier('argument.type'),
+				(new Error(
+					'Foo with identifier',
+					__DIR__ . '/Foo.php',
+					6,
+				))->withIdentifier('argument.type'),
+			],
+			<<<'PHP'
+<?php declare(strict_types = 1);
+
+$ignoreErrors = [];
+$ignoreErrors[] = [
+	'message' => '#^Foo$#',
+	'count' => 2,
+	'path' => __DIR__ . '/Foo.php',
+];
+$ignoreErrors[] = [
+	// identifier: argument.type
+	'message' => '#^Foo with identifier$#',
+	'count' => 2,
+	'path' => __DIR__ . '/Foo.php',
+];
+
+return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
+
+PHP,
+		];
+
+		yield [
+			[
+				new Error(
+					'Foo',
+					__DIR__ . '/Foo.php',
+					5,
+				),
+				new Error(
+					'Foo',
+					__DIR__ . '/Foo.php',
+					5,
+				),
+				(new Error(
+					'Foo with same message, different identifier',
+					__DIR__ . '/Foo.php',
+					5,
+				))->withIdentifier('argument.type'),
+				(new Error(
+					'Foo with same message, different identifier',
+					__DIR__ . '/Foo.php',
+					6,
+				))->withIdentifier('argument.byRef'),
+				(new Error(
+					'Foo with another message',
+					__DIR__ . '/Foo.php',
+					5,
+				))->withIdentifier('argument.type'),
+			],
+			<<<'PHP'
+<?php declare(strict_types = 1);
+
+$ignoreErrors = [];
+$ignoreErrors[] = [
+	'message' => '#^Foo$#',
+	'count' => 2,
+	'path' => __DIR__ . '/Foo.php',
+];
+$ignoreErrors[] = [
+	// identifier: argument.type
+	'message' => '#^Foo with another message$#',
+	'count' => 1,
+	'path' => __DIR__ . '/Foo.php',
+];
+$ignoreErrors[] = [
+	// identifiers: argument.byRef, argument.type
+	'message' => '#^Foo with same message, different identifier$#',
+	'count' => 2,
+	'path' => __DIR__ . '/Foo.php',
+];
+
+return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
+
+PHP,
+		];
 	}
 
 	/**

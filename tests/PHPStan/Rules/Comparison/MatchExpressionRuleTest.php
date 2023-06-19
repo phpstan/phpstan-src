b4a6eb46659b14f0e5a28510afd013f8839eb6e8
Ondrej Mirtes <ondrej@mirtes.cz>
2023-06-19 17:03:55 +0200

    Regression tests
    Closes https://github.com/phpstan/phpstan/issues/6064
    Closes https://github.com/phpstan/phpstan/issues/9457

--- tests/PHPStan/Rules/Comparison/MatchExpressionRuleTest.php
+++ tests/PHPStan/Rules/Comparison/MatchExpressionRuleTest.php
@@ -489,4 +489,13 @@ class MatchExpressionRuleTest extends RuleTestCase
 		$this->analyse([__DIR__ . '/data/bug-9007.php'], []);
 	}
 
+	public function testBug9457(): void
+	{
+		if (PHP_VERSION_ID < 80100) {
+			$this->markTestSkipped('Test requires PHP 8.1.');
+		}
+
+		$this->analyse([__DIR__ . '/data/bug-9457.php'], []);
+	}
+
 }

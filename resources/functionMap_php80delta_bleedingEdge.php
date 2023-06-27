0624165e5ed4e242630a4d48b7e77fde1c300ae4
Markus Staab <maggus.staab@googlemail.com>
2023-06-27 21:49:19 +0200

    simplify

--- resources/functionMap_php80delta_bleedingEdge.php
+++ resources/functionMap_php80delta_bleedingEdge.php
@@ -6,7 +6,7 @@ return [
 		'filter_input' => ['mixed', 'type'=>'INPUT_GET|INPUT_POST|INPUT_COOKIE|INPUT_SERVER|INPUT_ENV', 'variable_name'=>'string', 'filter='=>'int', 'options='=>'array|int'],
 		'filter_input_array' => ['array|false|null', 'type'=>'INPUT_GET|INPUT_POST|INPUT_COOKIE|INPUT_SERVER|INPUT_ENV', 'definition='=>'int|array', 'add_empty='=>'bool'],
 		// 4194304 is JSON_THROW_ON_ERROR
-		'json_decode' => ['mixed', 'json'=>'string', 'assoc='=>'bool|null', 'depth='=>'positive-int', 'options='=>'int-mask<0|JSON_BIGINT_AS_STRING|JSON_INVALID_UTF8_IGNORE|JSON_INVALID_UTF8_SUBSTITUTE|JSON_OBJECT_AS_ARRAY|4194304>'],
+		'json_decode' => ['mixed', 'json'=>'string', 'assoc='=>'bool|null', 'depth='=>'positive-int', 'options='=>'int-mask<JSON_BIGINT_AS_STRING|JSON_INVALID_UTF8_IGNORE|JSON_INVALID_UTF8_SUBSTITUTE|JSON_OBJECT_AS_ARRAY|4194304>'],
 	],
 	'old' => [
 

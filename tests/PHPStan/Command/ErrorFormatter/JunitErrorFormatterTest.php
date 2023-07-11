67658a629c92b57aeeeab9f581a342d519c95940
Sebastian Hädrich <11225821+shaedrich@users.noreply.github.com>
2023-07-11 09:30:41 +0200

    Update schema version in tests accordingly
    from 5.5.1 to 5.9.3

--- tests/PHPStan/Command/ErrorFormatter/JunitErrorFormatterTest.php
+++ tests/PHPStan/Command/ErrorFormatter/JunitErrorFormatterTest.php
@@ -29,7 +29,7 @@ class JunitErrorFormatterTest extends ErrorFormatterTestCase
 			0,
 			0,
 			'<?xml version="1.0" encoding="UTF-8"?>
-<testsuite failures="0" name="phpstan" tests="1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.5.1/platform-tests/src/test/resources/jenkins-junit.xsd">
+<testsuite failures="0" name="phpstan" tests="1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.9.3/platform-tests/src/test/resources/jenkins-junit.xsd">
   <testcase name="phpstan"/>
 </testsuite>
 ',
@@ -40,7 +40,7 @@ class JunitErrorFormatterTest extends ErrorFormatterTestCase
 			1,
 			0,
 			'<?xml version="1.0" encoding="UTF-8"?>
-<testsuite failures="1" name="phpstan" tests="1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.5.1/platform-tests/src/test/resources/jenkins-junit.xsd">
+<testsuite failures="1" name="phpstan" tests="1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.9.3/platform-tests/src/test/resources/jenkins-junit.xsd">
   <testcase name="folder with unicode &#x1F603;/file name with &quot;spaces&quot; and unicode &#x1F603;.php:4">
     <failure type="ERROR" message="Foo" />
   </testcase>
@@ -53,7 +53,7 @@ class JunitErrorFormatterTest extends ErrorFormatterTestCase
 			0,
 			1,
 			'<?xml version="1.0" encoding="UTF-8"?>
-<testsuite failures="1" name="phpstan" tests="1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.5.1/platform-tests/src/test/resources/jenkins-junit.xsd">
+<testsuite failures="1" name="phpstan" tests="1" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.9.3/platform-tests/src/test/resources/jenkins-junit.xsd">
   <testcase name="General error">
     <failure type="ERROR" message="first generic error" />
   </testcase>
@@ -66,7 +66,7 @@ class JunitErrorFormatterTest extends ErrorFormatterTestCase
 			4,
 			0,
 			'<?xml version="1.0" encoding="UTF-8"?>
-<testsuite failures="4" name="phpstan" tests="4" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.5.1/platform-tests/src/test/resources/jenkins-junit.xsd">
+<testsuite failures="4" name="phpstan" tests="4" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.9.3/platform-tests/src/test/resources/jenkins-junit.xsd">
   <testcase name="folder with unicode &#x1F603;/file name with &quot;spaces&quot; and unicode &#x1F603;.php:2">
     <failure type="ERROR" message="Bar Bar2" />
   </testcase>
@@ -88,7 +88,7 @@ class JunitErrorFormatterTest extends ErrorFormatterTestCase
 			0,
 			2,
 			'<?xml version="1.0" encoding="UTF-8"?>
-<testsuite failures="2" name="phpstan" tests="2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.5.1/platform-tests/src/test/resources/jenkins-junit.xsd">
+<testsuite failures="2" name="phpstan" tests="2" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.9.3/platform-tests/src/test/resources/jenkins-junit.xsd">
   <testcase name="General error">
     <failure type="ERROR" message="first generic error" />
   </testcase>
@@ -104,7 +104,7 @@ class JunitErrorFormatterTest extends ErrorFormatterTestCase
 			4,
 			2,
 			'<?xml version="1.0" encoding="UTF-8"?>
-<testsuite failures="6" name="phpstan" tests="6" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.5.1/platform-tests/src/test/resources/jenkins-junit.xsd">
+<testsuite failures="6" name="phpstan" tests="6" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:noNamespaceSchemaLocation="https://raw.githubusercontent.com/junit-team/junit5/r5.9.3/platform-tests/src/test/resources/jenkins-junit.xsd">
   <testcase name="folder with unicode &#x1F603;/file name with &quot;spaces&quot; and unicode &#x1F603;.php:2">
     <failure type="ERROR" message="Bar Bar2" />
   </testcase>

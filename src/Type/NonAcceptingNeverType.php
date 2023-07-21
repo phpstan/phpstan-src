5a0ff0acf9a7054cdf83b4e8c4096602bbe1ebbc
Jiří Pudil <me@jiripudil.cz>
2023-07-21 17:49:33 +0200

    add NonAcceptingNeverType

---
+++ src/Type/NonAcceptingNeverType.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Type;
+
+/** @api */
+class NonAcceptingNeverType extends NeverType
+{
+
+	/** @api */
+	public function __construct()
+	{
+		parent::__construct(true);
+	}
+
+	public function acceptsWithReason(Type $type, bool $strictTypes): AcceptsResult
+	{
+		return AcceptsResult::createNo();
+	}
+
+	public function describe(VerbosityLevel $level): string
+	{
+		return 'never';
+	}
+
+}

c016c538a2867738012cfb34dbcc7f43d449a66d
Jan Nedbal <janedbal@gmail.com>
2023-08-18 14:21:06 +0200

    Ability to disable `implicitThrows` in tests

---
+++ tests/PHPStan/Rules/Exceptions/AbilityToDisableImplicitThrowsTest.php
@@ -0,0 +1,38 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Exceptions;
+
+use PHPStan\Rules\Rule;
+use PHPStan\Testing\RuleTestCase;
+use function array_merge;
+
+/**
+ * @extends RuleTestCase<CatchWithUnthrownExceptionRule>
+ */
+class AbilityToDisableImplicitThrowsTest extends RuleTestCase
+{
+
+	protected function getRule(): Rule
+	{
+		return new CatchWithUnthrownExceptionRule();
+	}
+
+	public function testRule(): void
+	{
+		$this->analyse([__DIR__ . '/data/ability-to-disable-implicit-throws.php'], [
+			[
+				'Dead catch - Throwable is never thrown in the try block.',
+				17,
+			],
+		]);
+	}
+
+	public static function getAdditionalConfigFiles(): array
+	{
+		return array_merge(
+			parent::getAdditionalConfigFiles(),
+			[__DIR__ . '/data/ability-to-disable-implicit-throws.neon'],
+		);
+	}
+
+}

2bef891930282fa1bb347590f9ab2a0404fb1cd6
zer0-star <zer0star.65535@gmail.com>
2023-08-28 12:13:47 +0900

    fix type inference of return value of array_sum

--- src/Type/Php/ArraySumFunctionDynamicReturnTypeExtension.php
+++ src/Type/Php/ArraySumFunctionDynamicReturnTypeExtension.php
@@ -6,13 +6,23 @@ use PhpParser\Node\Expr\FuncCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Reflection\ParametersAcceptorSelector;
+use PHPStan\Type\Accessory\NonEmptyArrayType;
+use PHPStan\Type\Constant\ConstantFloatType;
 use PHPStan\Type\Constant\ConstantIntegerType;
 use PHPStan\Type\DynamicFunctionReturnTypeExtension;
 use PHPStan\Type\FloatType;
+use PHPStan\Type\GeneralizePrecision;
+use PHPStan\Type\IntegerRangeType;
 use PHPStan\Type\IntegerType;
+use PHPStan\Type\NeverType;
 use PHPStan\Type\Type;
 use PHPStan\Type\TypeCombinator;
 use PHPStan\Type\UnionType;
+use function array_filter;
+use function array_sum;
+use function count;
+use function is_float;
+use function is_int;
 
 final class ArraySumFunctionDynamicReturnTypeExtension implements DynamicFunctionReturnTypeExtension
 {
@@ -35,21 +45,85 @@ final class ArraySumFunctionDynamicReturnTypeExtension implements DynamicFunctio
 			return new ConstantIntegerType(0);
 		}
 
-		$intUnionFloat = new UnionType([new IntegerType(), new FloatType()]);
+		$constantArray = $arrayType->getConstantArrays()[0] ?? null;
 
-		if ($arrayType->isIterableAtLeastOnce()->yes()) {
-			if ($intUnionFloat->isSuperTypeOf($itemType)->yes()) {
-				return $itemType;
+		if ($constantArray !== null) {
+			$nonScalarTypes = [];
+			$scalarValues = [];
+
+			$types = $constantArray->getValueTypes();
+
+			foreach ($types as $type) {
+				$scalar = $type->getConstantScalarValues();
+				if (count($scalar) === 1) {
+					$scalarValues[] = $scalar[0];
+				} else {
+					$nonScalarTypes[] = $type;
+				}
 			}
 
-			return $intUnionFloat;
+			if (count($nonScalarTypes) === 0) {
+				$sum = array_sum($scalarValues);
+				if (is_int($sum)) {
+					$newItemType = new ConstantIntegerType($sum);
+				} else {
+					$newItemType = new ConstantFloatType($sum);
+				}
+			} else {
+				$newItemType = count(array_filter($types, static fn ($type) => $type instanceof FloatType)) !== 0
+					? new FloatType()
+					: $itemType->generalize(GeneralizePrecision::lessSpecific());
+			}
+		} else {
+			$newItemType = new NeverType();
+
+			if ($itemType instanceof UnionType) {
+				$types = $itemType->getTypes();
+			} else {
+				$types = [$itemType];
+			}
+
+			$positive = false;
+			$negative = false;
+
+			foreach ($types as $type) {
+				$constant = $type->getConstantScalarValues()[0] ?? null;
+				if ($constant !== null) {
+					if (is_float($constant)) {
+						$nextType = new FloatType();
+					} elseif (is_int($constant)) {
+						if ($constant > 0) {
+							$nextType = IntegerRangeType::fromInterval($negative ? 0 : 1, null);
+							$positive = true;
+						} elseif ($constant < 0) {
+							$nextType = IntegerRangeType::fromInterval(null, $positive ? 0 : -1);
+							$negative = true;
+						} else {
+							$nextType = new ConstantIntegerType(0);
+						}
+					} else {
+						$nextType = $type;
+					}
+				} else {
+					$nextType = $type;
+				}
+
+				$newItemType = TypeCombinator::union($newItemType, $nextType);
+			}
+
+			$nonEmptyArrayType = new NonEmptyArrayType();
+
+			if (!$nonEmptyArrayType->isSuperTypeOf($arrayType)->yes()) {
+				$newItemType = TypeCombinator::union($newItemType, new ConstantIntegerType(0));
+			}
 		}
 
-		if ($intUnionFloat->isSuperTypeOf($itemType)->yes()) {
-			return TypeCombinator::union(new ConstantIntegerType(0), $itemType);
-		}
+		$intUnionFloat = new UnionType([new IntegerType(), new FloatType()]);
 
-		return TypeCombinator::union(new ConstantIntegerType(0), $intUnionFloat);
+		if ($intUnionFloat->isSuperTypeOf($newItemType)->yes()) {
+			return $newItemType;
+		}
+		return $intUnionFloat;
 	}
 
 }
--- tests/PHPStan/Analyser/data/array-sum.php
+++ tests/PHPStan/Analyser/data/array-sum.php
@@ -48,3 +48,162 @@ function foo5($list)
 	$sum = array_sum($list);
 	assertType('float|int', $sum);
 }
+
+/**
+ * @param list<0> $list
+ */
+function foo6($list)
+{
+	assertType('0', array_sum($list));
+}
+/**
+ * @param list<1> $list
+ */
+function foo7($list)
+{
+	assertType('int<0, max>', array_sum($list));
+}
+
+/**
+ * @param non-empty-list<1> $list
+ */
+function foo8($list)
+{
+	assertType('int<1, max>', array_sum($list));
+}
+
+/**
+ * @param list<-1> $list
+ */
+function foo9($list)
+{
+	assertType('int<min, 0>', array_sum($list));
+}
+
+/**
+ * @param list<1|2|3> $list
+ */
+function foo10($list)
+{
+	assertType('int<0, max>', array_sum($list));
+}
+
+/**
+ * @param non-empty-list<1|2|3> $list
+ */
+function foo11($list)
+{
+	assertType('int<1, max>', array_sum($list));
+}
+
+/**
+ * @param list<1|-1> $list
+ */
+function foo12($list)
+{
+	assertType('int', array_sum($list));
+}
+/**
+ * @param non-empty-list<1|-1> $list
+ */
+function foo13($list)
+{
+	assertType('int', array_sum($list));
+}
+
+/**
+ * @param array{0} $list
+ */
+function foo14($list)
+{
+	assertType('0', array_sum($list));
+}
+/**
+ * @param array{1} $list
+ */
+function foo15($list)
+{
+	assertType('1', array_sum($list));
+}
+
+/**
+ * @param array{1, 2, 3} $list
+ */
+function foo16($list)
+{
+	assertType('6', array_sum($list));
+}
+
+/**
+ * @param array{1, int} $list
+ */
+function foo17($list)
+{
+	assertType('int', array_sum($list));
+}
+
+/**
+ * @param array{1, float} $list
+ */
+function foo18($list)
+{
+	assertType('float', array_sum($list));
+}
+
+/**
+ * @param array{} $list
+ */
+function foo19($list)
+{
+	assertType('0', array_sum($list));
+}
+
+
+/**
+ * @param list<1|float> $list
+ */
+function foo20($list)
+{
+	assertType('float|int<0, max>', array_sum($list));
+}
+
+/**
+ * @param array{1, int|float} $list
+ */
+function foo21($list)
+{
+	assertType('float|int', array_sum($list));
+}
+
+/**
+ * @param array{1, string} $list
+ */
+function foo22($list)
+{
+	assertType('float|int', array_sum($list));
+}
+
+
+/**
+ * @param array{1, 3.2} $list
+ */
+function foo23($list)
+{
+	assertType('4.2', array_sum($list));
+}
+
+/**
+ * @param array{1, float|4} $list
+ */
+function foo24($list)
+{
+	assertType('float|int', array_sum($list));
+}
+
+/**
+ * @param array{1, 2|3.4} $list
+ */
+function foo25($list)
+{
+	assertType('float|int', array_sum($list));
+}

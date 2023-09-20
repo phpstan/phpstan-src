d3c9b7a5653115290e59d0925fd1da907c44e1d8
Markus Staab <markus.staab@redaxo.de>
2023-09-20 16:46:10 +0200

    sprinkle assertType()

--- tests/PHPStan/Rules/Functions/data/bug-9803.php
+++ tests/PHPStan/Rules/Functions/data/bug-9803.php
@@ -4,17 +4,24 @@ declare(strict_types=1);
 
 namespace Bug9803;
 
+use function PHPStan\Testing\assertType;
+
 function doFoo() {
 	$random = rand(1, 5);
 	$array = array("one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten");
 
 	$keys = array();
-	if ($random == 1)
+	if ($random == 1) {
 		$keys = array(array_rand($array));
-	else
+		assertType('array{int}', $keys);
+	}
+	else {
 		$keys = array_rand($array, $random);
+		assertType('array<int, int>', $keys);
+	}
 
 	$theKeys = array_keys($keys);
+	assertType('list<int>', $theKeys);
 }
 
 

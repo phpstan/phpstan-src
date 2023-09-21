a1a98d3e4b6c6e7e7a98164ee1dab8a7e110eb6c
Markus Staab <markus.staab@redaxo.de>
2023-09-21 10:07:51 +0200

    separate php8 only tests

--- tests/PHPStan/Analyser/data/array-chunk.php
+++ tests/PHPStan/Analyser/data/array-chunk.php
@@ -45,15 +45,9 @@ class Foo
 
 	/**
 	 * @param int<2, 3> $positiveRange
-	 * @param int<-5, -10> $negativeRange
-	 * @param int<-5, 0> $negativeWithZero
-	 *
 	 * @param 2|3 $positiveUnion
 	 */
-	public function chunkUnionTypeLength(array $arr, $positiveRange, $negativeRange, $negativeWithZero, $positiveUnion) {
-		assertType('*NEVER*', array_chunk($arr, $negativeRange));
-		assertType('*NEVER*', array_chunk($arr, $negativeWithZero));
-
+	public function chunkUnionTypeLength(array $arr, $positiveRange, $positiveUnion) {
 		/** @var array{a: 0, b?: 1, c: 2} $arr */
 		assertType('array{0: array{0: 0, 1?: 1|2, 2?: 2}, 1?: array{0?: 2}}', array_chunk($arr, $positiveRange));
 		assertType('array{0: array{a: 0, b?: 1, c?: 2}, 1?: array{c?: 2}}', array_chunk($arr, $positiveRange, true));

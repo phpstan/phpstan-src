9c1c44ef88f4190bfc794fedb672b13e5c3957c7
Markus Staab <maggus.staab@googlemail.com>
2023-09-30 14:55:24 +0200

    Added regression test

---
+++ tests/PHPStan/Analyser/data/bug-9908.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Bug9908;
+
+use function PHPStan\Testing\assertType;
+
+class HelloWorld
+{
+	public function test(): void
+	{
+		$a = [];
+		if (rand() % 2) {
+			$a = ['bar' => 'string'];
+		}
+
+		assertType("array{}|array{bar: 'string'}", $a);
+		if (isset($a['bar'])) {
+			assertType("array{bar: 'string'}", $a);
+			$a['bar'] = 1;
+		} else {
+			assertType('array{}', $a);
+		}
+
+		assertType('array{}|array{bar: 1}', $a);
+	}
+}

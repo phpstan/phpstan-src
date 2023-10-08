16148da9e6e2dd3c7417e35d534af05a5c3859a6
Markus Staab <maggus.staab@googlemail.com>
2023-10-08 19:39:13 +0200

    fix tests

--- tests/PHPStan/Analyser/data/falsey-isset-certainty.php
+++ tests/PHPStan/Analyser/data/falsey-isset-certainty.php
@@ -5,45 +5,26 @@ namespace FalseyIssetCertainty;
 use function PHPStan\Testing\assertVariableCertainty;
 use PHPStan\TrinaryLogic;
 
-function falseyEmptyArrayDimFetch(): void
-{
-	if (rand() % 2) {
-		$a = ['bar' => null];
-		if (rand() % 3) {
-			$a = ['bar' => 'hello'];
-		}
-	}
 
-	assertVariableCertainty(TrinaryLogic::createMaybe(), $a);
-	if (empty($a['bar'])) {
-		assertVariableCertainty(TrinaryLogic::createYes(), $a);
-	} else {
-		assertVariableCertainty(TrinaryLogic::createMaybe(), $a);
-	}
-
-	assertVariableCertainty(TrinaryLogic::createMaybe(), $a);
-}
-
-function falseyEmptyVariable(): void
-{
-	if (rand() % 2) {
-		$a = 'bar';
-		if (rand() % 3) {
-			$a = null;
-		}
-	}
-
-	assertVariableCertainty(TrinaryLogic::createMaybe(), $a);
-	if (empty($a)) {
-		assertVariableCertainty(TrinaryLogic::createYes(), $a);
-	} else {
-		assertVariableCertainty(TrinaryLogic::createMaybe(), $a);
-	}
-
-	assertVariableCertainty(TrinaryLogic::createMaybe(), $a);
-}
 
 function falseyIssetArrayDimFetch(): void
+{
+	$a = ['bar' => null];
+	if (rand() % 3) {
+		$a = ['bar' => 'hello'];
+	}
+
+	assertVariableCertainty(TrinaryLogic::createYes(), $a);
+	if (isset($a['bar'])) {
+		assertVariableCertainty(TrinaryLogic::createYes(), $a);
+	} else {
+		assertVariableCertainty(TrinaryLogic::createYes(), $a);
+	}
+
+	assertVariableCertainty(TrinaryLogic::createYes(), $a);
+}
+
+function falseyIssetUncertainArrayDimFetch(): void
 {
 	if (rand() % 2) {
 		$a = ['bar' => null];

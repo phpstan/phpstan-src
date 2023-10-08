e46fa8ff424838c04855c4bebae9751231cefc43
Markus Staab <maggus.staab@googlemail.com>
2023-10-08 11:33:04 +0200

    Added falsey-isset variable certainty test

---
+++ tests/PHPStan/Analyser/data/falsey-isset-certainty.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace FalseyIssetCertainty;
+
+use function PHPStan\Testing\assertVariableCertainty;
+use PHPStan\TrinaryLogic;
+
+
+function falseyIssetArrayDimFetch(): void
+{
+	if (rand() % 2) {
+		$a = ['bar' => null];
+		if (rand() % 3) {
+			$a = ['bar' => 'hello'];
+		}
+	}
+
+	assertVariableCertainty(TrinaryLogic::createMaybe(), $a);
+	if (isset($a['bar'])) {
+		assertVariableCertainty(TrinaryLogic::createYes(), $a);
+	} else {
+		assertVariableCertainty(TrinaryLogic::createMaybe(), $a);
+	}
+
+	assertVariableCertainty(TrinaryLogic::createMaybe(), $a);
+}
+
+function falseyIssetVariable(): void
+{
+	if (rand() % 2) {
+		$a = 'bar';
+	}
+
+	assertVariableCertainty(TrinaryLogic::createMaybe(), $a);
+	if (isset($a)) {
+		assertVariableCertainty(TrinaryLogic::createYes(), $a);
+	} else {
+		assertVariableCertainty(TrinaryLogic::createMaybe(), $a);
+	}
+
+	assertVariableCertainty(TrinaryLogic::createMaybe(), $a);
+}

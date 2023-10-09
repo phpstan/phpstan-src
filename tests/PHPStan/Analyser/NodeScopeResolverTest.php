d0297b4ed8da5cd9437f7dc4403865b64a1cef53
Markus Staab <markus.staab@redaxo.de>
2023-10-09 13:59:12 +0200

    added test for one snippet from bug 7291

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -1358,6 +1358,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-9995.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/falsey-isset-certainty.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/falsey-empty-certainty.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-7291.php');
 	}
 
 	/**

35a0c769fb119cbc7b33a985897d92a87d5c496c
Markus Staab <markus.staab@redaxo.de>
2023-10-12 10:52:46 +0200

    Infer object type mysqli_fetch_object

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -1459,6 +1459,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-10037.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/../Rules/PhpDoc/data/bug-10594.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/set-type-type-specifying.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/mysqli_fetch_object.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-10468.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-6613.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-10187.php');

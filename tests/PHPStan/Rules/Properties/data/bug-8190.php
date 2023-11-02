01a0de2513609feff039df6a3b8d83d188a6c1bd
Markus Staab <maggus.staab@googlemail.com>
2023-11-02 15:21:40 +0100

    More precise falsey `isset()`

---
+++ tests/PHPStan/Rules/Properties/data/bug-8190.php
@@ -0,0 +1,57 @@
+<?php // lint >= 7.4
+
+namespace Bug8190;
+
+/**
+ * @phpstan-type OwnerBackup array{name: string, isTest?: bool}
+ */
+class ClassA
+{
+	/** @var OwnerBackup */
+	public array $ownerBackup;
+
+	/**
+	 * @param OwnerBackup|null $ownerBackup
+	 */
+	public function __construct(?array $ownerBackup)
+	{
+		$this->ownerBackup = $ownerBackup ?? [
+			'name' => 'Deleted',
+		];
+	}
+
+
+	/**
+	 * @param OwnerBackup|null $ownerBackup
+	 */
+	public function setOwnerBackup(?array $ownerBackup): void
+	{
+		$this->ownerBackup = $ownerBackup ?: [
+			'name' => 'Deleted',
+		];
+	}
+
+	/**
+	 * @param OwnerBackup|null $ownerBackup
+	 */
+	public function setOwnerBackupWorksForSomeReason(?array $ownerBackup): void
+	{
+		$this->ownerBackup = $ownerBackup !== null ? $ownerBackup : [
+			'name' => 'Deleted',
+		];
+	}
+
+	/**
+	 * @param OwnerBackup|null $ownerBackup
+	 */
+	public function setOwnerBackupAlsoWorksForSomeReason(?array $ownerBackup): void
+	{
+		if ($ownerBackup) {
+			$this->ownerBackup = $ownerBackup;
+		} else {
+			$this->ownerBackup = [
+				'name' => 'Deleted',
+			];
+		}
+	}
+}

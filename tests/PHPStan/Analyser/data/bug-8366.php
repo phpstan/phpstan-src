affd1097010073a593e6c45a075c19d729910255
Markus Staab <maggus.staab@googlemail.com>
2023-11-02 15:58:18 +0100

    sprinkle asserts

--- tests/PHPStan/Analyser/data/bug-8366.php
+++ tests/PHPStan/Analyser/data/bug-8366.php
@@ -11,16 +11,26 @@ function validateParams(
 ): void {
 
 	if (isset($untilDate, $count)) {
+		assertType('DateTimeImmutable', $untilDate);
+		assertType('int', $count);
+
 		throw new \InvalidArgumentException('Too much params, choose between until and count.');
 	}
 	assertType('DateTimeImmutable|null', $untilDate);
 	assertType('int|null', $count);
 
 	if ($untilDate !== null && $datePeriod > $untilDate) {
+		assertType('DateTimeImmutable', $untilDate);
+		assertType('int|null', $count);
 		throw new \InvalidArgumentException('End date must not be greater than until date.');
 	}
 
 	if ($count !== null && $count < 1) {
+		assertType('DateTimeImmutable|null', $untilDate);
+		assertType('int<min, 0>', $count);
 		throw new \InvalidArgumentException('Count must be positive.');
 	}
+
+	assertType('DateTimeImmutable|null', $untilDate);
+	assertType('int<1, max>|null', $count);
 }

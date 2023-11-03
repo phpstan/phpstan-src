40c9d7e53a93e7bdd04d027f431b1ffa219ee23f
Caleb White <cdwhite3@pm.me>
2023-11-03 16:16:41 -0500

    Update exception message

--- src/Internal/DirectoryCreatorException.php
+++ src/Internal/DirectoryCreatorException.php
@@ -3,6 +3,8 @@
 namespace PHPStan\Internal;
 
 use Exception;
+use function error_get_last;
+use function is_null;
 use function sprintf;
 
 final class DirectoryCreatorException extends Exception
@@ -10,7 +12,13 @@ final class DirectoryCreatorException extends Exception
 
 	public function __construct(public readonly string $directory)
 	{
-		parent::__construct(sprintf('Failed to create directory "%s".', $directory));
+		$error = error_get_last();
+
+		parent::__construct(sprintf(
+			'Failed to create directory "%s" (%s).',
+			$directory,
+			is_null($error) ? 'unknown cause' : $error['message'],
+		));
 	}
 
 }

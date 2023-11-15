d89bf0e688a05b3fb62df32d6b4118970a0cf32d
Jan Nedbal <janedbal@gmail.com>
2023-11-15 14:55:29 +0100

    VarTagTypeRuleHelper: fix widening array shapes

---
+++ tests/PHPStan/Rules/PhpDoc/data/bug-10130.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types = 1);
+
+class Ipsum
+{
+
+	/**
+	 * @param array<int> $map
+	 * @param list<int> $list
+	 * @param array{id: int} $shape
+	 */
+	public function doFoo($map, $list, $shape): void
+	{
+		/** @var mixed[] $map */
+		if ($map) {}
+
+		/** @var mixed[] $list */
+		if ($list) {}
+
+		/** @var mixed[] $shape */
+		if ($shape) {}
+	}
+
+}

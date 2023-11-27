8ae98dec7ec95ed79f1f745d3bec3771ef868e38
Ondrej Mirtes <ondrej@mirtes.cz>
2023-11-27 13:26:48 +0100

    Regression test
    Closes https://github.com/phpstan/phpstan/issues/10201

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -41,6 +41,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		if (PHP_VERSION_ID >= 80100) {
 			yield from $this->gatherAssertTypes(__DIR__ . '/data/generic-enum-class-string.php');
 			yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-7162.php');
+			yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-10201.php');
 		}
 
 		require_once __DIR__ . '/data/generic-generalization.php';

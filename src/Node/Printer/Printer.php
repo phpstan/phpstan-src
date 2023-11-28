0860ca3366f1db5e6824fafd43c16f797241cdbe
Markus Staab <maggus.staab@googlemail.com>
2023-11-28 15:24:47 +0100

    Discard changes to src/Node/Printer/Printer.php

--- src/Node/Printer/Printer.php
+++ src/Node/Printer/Printer.php
@@ -11,7 +11,6 @@ use PHPStan\Node\Expr\OriginalPropertyTypeExpr;
 use PHPStan\Node\Expr\PropertyInitializationExpr;
 use PHPStan\Node\Expr\SetOffsetValueTypeExpr;
 use PHPStan\Node\Expr\TypeExpr;
-use PHPStan\Node\IssetExpr;
 use PHPStan\Type\VerbosityLevel;
 use function sprintf;
 
@@ -56,11 +55,6 @@ class Printer extends Standard
 	protected function pPHPStan_Node_PropertyInitializationExpr(PropertyInitializationExpr $expr): string // phpcs:ignore
 	{
 		return sprintf('__phpstanPropertyInitialization(%s)', $expr->getPropertyName());
-	}
-
-	protected function pPHPStan_Node_IssetExpr(IssetExpr $expr): string // phpcs:ignore
-	{
-		return sprintf('__phpstanIssetExpr(%s)', $this->p($expr->getExpr()));
 	}
 
 }

003d7daa5d0fe4d0832d61bc8c579b02c0c8ca41
Markus Staab <maggus.staab@googlemail.com>
2023-12-13 20:58:19 +0100

    Added regression test

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -1401,6 +1401,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-7291.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-10264.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/conditional-vars.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-5961.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-10189.php');
 	}
 

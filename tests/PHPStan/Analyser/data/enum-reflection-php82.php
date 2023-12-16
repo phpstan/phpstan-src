1f9a0540f86cef0ea5e0236cc8efe0d8b5cb4e47
Markus Staab <maggus.staab@googlemail.com>
2023-12-16 17:01:52 +0100

    Narrow ReflectionEnum::getBackingType() after ReflectionEnum::isBacked()

---
+++ tests/PHPStan/Analyser/data/enum-reflection-php82.php
@@ -0,0 +1,23 @@
+<?php // lint >= 8.1
+
+namespace EnumReflection82;
+
+use ReflectionEnum;
+use ReflectionEnumBackedCase;
+use ReflectionEnumUnitCase;
+use function PHPStan\Testing\assertType;
+
+enum Foo: int
+{
+
+	case FOO = 1;
+	case BAR = 2;
+}
+
+function testNarrowGetBackingTypeAfterIsBacked() {
+	$r = new ReflectionEnum(Foo::class);
+	assertType('ReflectionNamedType|null', $r->getBackingType());
+	if ($r->isBacked()) {
+		assertType('ReflectionNamedType', $r->getBackingType());
+	}
+}

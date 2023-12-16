88249440034829a04ce4a2c536115cbce6bded45
Ondrej Mirtes <ondrej@mirtes.cz>
2023-12-16 12:39:19 +0100

    Switch from Rector to simple-downgrader

--- build/rector-cache-files-hash.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types = 1);
-
-use PHPStan\Build\RectorCache;
-
-require_once __DIR__ . '/../vendor/autoload.php';
-
-$cache = new RectorCache();
-echo $cache->getOriginalFilesHash();

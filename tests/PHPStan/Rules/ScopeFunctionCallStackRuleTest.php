9be137675ebc41be5c9c7c230f96cab4dbf1f08e
Ondrej Mirtes <ondrej@mirtes.cz>
2024-01-07 13:47:30 +0100

    `Scope::getFunctionCallStack()`

---
+++ tests/PHPStan/Rules/ScopeFunctionCallStackRuleTest.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules;
+
+use PHPStan\Testing\RuleTestCase;
+use const PHP_VERSION_ID;
+
+/**
+ * @extends RuleTestCase<ScopeFunctionCallStackRule>
+ */
+class ScopeFunctionCallStackRuleTest extends RuleTestCase
+{
+
+	protected function getRule(): Rule
+	{
+		return new ScopeFunctionCallStackRule();
+	}
+
+	public function testRule(): void
+	{
+		if (PHP_VERSION_ID < 80000) {
+			$this->markTestSkipped('Test requires PHP 8.0.');
+		}
+
+		$this->analyse([__DIR__ . '/data/scope-function-call-stack.php'], [
+			[
+				"var_dump\nprint_r\nsleep",
+				7,
+			],
+		]);
+	}
+
+}

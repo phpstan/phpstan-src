dcedfede39e1201eba40a01765b9e6e036845ebb
Markus Staab <maggus.staab@googlemail.com>
2024-01-14 12:09:19 +0100

    fix

--- e2e/result-cache-7/src/Bar.php
+++ e2e/result-cache-7/src/Bar.php
@@ -2,12 +2,6 @@
 
 namespace TestResultCache7;
 
-/**
- * @phpstan-require-extends Baz
- */
 interface Bar
 {
-	public function doFoo():string {
-		return 'foo';
-	}
 }
--- e2e/result-cache-7/src/Foo.php
+++ e2e/result-cache-7/src/Foo.php
@@ -5,7 +5,4 @@ namespace TestResultCache7;
 /** @phpstan-require-implements Bar */
 trait Foo
 {
-	public function doBar() {
-		echo $this->doFoo();
-	}
 }

dba3b997cd3d6342230ef4e1e4ec2db783d16597
Markus Staab <markus.staab@redaxo.de>
2024-01-31 08:59:58 +0100

    Added regression test

---
+++ tests/PHPStan/Rules/Comparison/data/bug-10502.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+
+namespace Bug10502;
+
+use ArrayObject;
+
+/** @param ?ArrayObject<int, int> $x */
+function doFoo(?ArrayObject $x):void {
+	$callable1 = [$x, 'count'];
+	$callable2 = array_reverse($callable1, true);
+
+	var_dump(
+		is_callable($callable1),
+		is_callable($callable2),
+	);
+}
+
+function doBar():void {
+	$callable1 = [new ArrayObject([0]), 'count'];
+	$callable2 = array_reverse($callable1, true);
+
+	var_dump(
+		is_callable($callable1),
+		is_callable($callable2),
+	);
+}

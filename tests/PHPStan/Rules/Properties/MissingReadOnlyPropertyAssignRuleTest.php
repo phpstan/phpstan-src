af8d4379d5a1800b7e716e0d119e143ef99bba36
Markus Staab <markus.staab@redaxo.de>
2024-02-01 18:22:33 +0100

    fix test

--- tests/PHPStan/Rules/Properties/MissingReadOnlyPropertyAssignRuleTest.php
+++ tests/PHPStan/Rules/Properties/MissingReadOnlyPropertyAssignRuleTest.php
@@ -264,6 +264,10 @@ class MissingReadOnlyPropertyAssignRuleTest extends RuleTestCase
 
 	public function testBug10523(): void
 	{
+		if (PHP_VERSION_ID < 80100) {
+			$this->markTestSkipped('Test requires PHP 8.1.');
+		}
+
 		$this->analyse([__DIR__ . '/data/bug-10523.php'], []);
 	}
 

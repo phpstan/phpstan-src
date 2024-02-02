435f26d0b918a2979f25bdb776dee38d567f2c10
Brad Miller <28307684+mad-briller@users.noreply.github.com>
2024-02-02 18:20:26 +0000

    Fix ArrayAccess destructuring assignment producing mixed.

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -1433,6 +1433,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-9123.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-10037.php');
 		yield from $this->gatherAssertTypes(__DIR__ . '/data/set-type-type-specifying.php');
+		yield from $this->gatherAssertTypes(__DIR__ . '/data/bug-9456.php');
 	}
 
 	/**

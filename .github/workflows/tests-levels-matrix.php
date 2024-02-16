030b8c0db2c5c9d01160375f93129d7eb6176403
Markus Staab <markus.staab@redaxo.de>
2024-02-16 14:34:02 +0100

    Implement parallelized test levels via github actions matrix

---
+++ .github/workflows/tests-levels-matrix.php
@@ -0,0 +1,31 @@
+<?php
+
+$testList = shell_exec('php vendor/bin/phpunit --list-tests');
+
+if (!is_string($testList)) {
+	throw new RuntimeException('Error while listing tests');
+}
+
+$testFilters = [];
+foreach(explode("\n", $testList) as $line) {
+	$cleanedLine = trim($line, ' -');
+
+	if ($cleanedLine === '') {
+		continue;
+	}
+
+	if (
+		!str_contains($cleanedLine, 'PHPStan\Generics\GenericsIntegrationTest') &&
+		!str_contains($cleanedLine, 'PHPStan\Levels\LevelsIntegrationTest')
+	) {
+		continue;
+	}
+
+	$testFilters[] = $cleanedLine;
+}
+
+if ($testFilters === []) {
+	throw new RuntimeException('No tests found');
+}
+
+echo json_encode($testFilters, JSON_PRETTY_PRINT);

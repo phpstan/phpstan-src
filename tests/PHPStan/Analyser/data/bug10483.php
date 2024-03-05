a314e58c03da2178d4c940aa19b6093e064532d6
Markus Staab <markus.staab@redaxo.de>
2024-03-05 16:08:50 +0100

    Revert "added type inference test"
    This reverts commit 84de37845f8d4e4e907518a078786202c5f2763c.

--- tests/PHPStan/Analyser/data/bug10483.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Bug10483;
-
-use function PHPStan\Testing\assertType;
-
-function doFoo(): void {
-	assertType('non-falsy-string|false', filter_var("no", FILTER_VALIDATE_REGEXP));
-}

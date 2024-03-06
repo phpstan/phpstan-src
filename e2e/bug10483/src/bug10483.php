a6bf8727bb93240b3afe38d5f7e327d16d14e9e6
Markus Staab <markus.staab@redaxo.de>
2024-03-06 16:13:59 +0100

    Revert "assert a invalid constant turns into mixed"
    This reverts commit 42307ceab4c12bf1b0c83f8b13be26ea7951b565.

--- e2e/bug10483/src/bug10483.php
+++ e2e/bug10483/src/bug10483.php
@@ -1,11 +1,5 @@
 <?php
 
-use function PHPStan\Testing\assertType;
-
 function doFoo(mixed $filter): void {
-	assertType('non-falsy-string|false', filter_var("no", FILTER_VALIDATE_REGEXP));
-
-	// if FILTER_SANITIZE_MAGIC_QUOTES would contain a valid value, the following line would yield "string|false".
-	// but since in phpstan bootstrap file the constant was miss-defined with a invalid value, PHPStan does not know the constant and returns "mixed", like for regular unknown constants.
-	assertType('mixed', filter_var($filter, FILTER_SANITIZE_MAGIC_QUOTES));
+    \PHPStan\Testing\assertType('non-falsy-string|false', filter_var("no", FILTER_VALIDATE_REGEXP));
 }

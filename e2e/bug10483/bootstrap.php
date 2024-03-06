f93972056ed473c0bf12ccfbe49045c7e93dd225
Markus Staab <markus.staab@redaxo.de>
2024-03-06 16:03:01 +0100

    assert a invalid constant turns into mixed

--- e2e/bug10483/bootstrap.php
+++ e2e/bug10483/bootstrap.php
@@ -3,3 +3,4 @@
 // constant that's used in the Filter extension that was introduced in a later version of PHP.
 // on earlier php version introduce the same constant via a bootstrap file but with a wrong type
 if(!defined("FILTER_SANITIZE_ADD_SLASHES"))define("FILTER_SANITIZE_ADD_SLASHES",false);
+if(!defined("FILTER_SANITIZE_MAGIC_QUOTES"))define("FILTER_SANITIZE_MAGIC_QUOTES",false);
--- e2e/bug10483/src/bug10483.php
+++ e2e/bug10483/src/bug10483.php
@@ -1,5 +1,11 @@
 <?php
 
+use function PHPStan\Testing\assertType;
+
 function doFoo(mixed $filter): void {
-    \PHPStan\Testing\assertType('non-falsy-string|false', filter_var("no", FILTER_VALIDATE_REGEXP));
+	assertType('non-falsy-string|false', filter_var("no", FILTER_VALIDATE_REGEXP));
+
+	// if FILTER_SANITIZE_MAGIC_QUOTES would contain a valid value, the following line would yield "string|false".
+	// but since in phpstan bootstrap file the constant was miss-defined with a invalid value, PHPStan does not know the constant and returns "mixed", like for regular unknown constants.
+	assertType('mixed', filter_var($filter, FILTER_SANITIZE_MAGIC_QUOTES));
 }

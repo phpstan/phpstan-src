19497ba0317be7e37f2c969118c93ce3be1e6fd2
Ondrej Mirtes <ondrej@mirtes.cz>
2024-03-13 12:39:35 +0100

    ParameterOutAssignedTypeRule - fix for conditional types

--- tests/PHPStan/Rules/Variables/ParameterOutAssignedTypeRuleTest.php
+++ tests/PHPStan/Rules/Variables/ParameterOutAssignedTypeRuleTest.php
@@ -54,4 +54,9 @@ class ParameterOutAssignedTypeRuleTest extends RuleTestCase
 		]);
 	}
 
+	public function testBug10699(): void
+	{
+		$this->analyse([__DIR__ . '/../../Analyser/data/bug-10699.php'], []);
+	}
+
 }

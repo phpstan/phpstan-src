c549f96095bcc85b43397388ea422f8a3bcad4aa
Markus Staab <markus.staab@redaxo.de>
2024-03-22 17:27:43 +0100

    Update AnalyserIntegrationTest.php

--- tests/PHPStan/Analyser/AnalyserIntegrationTest.php
+++ tests/PHPStan/Analyser/AnalyserIntegrationTest.php
@@ -1301,6 +1301,10 @@ class AnalyserIntegrationTest extends PHPStanTestCase
 
 	public function testBug10772(): void
 	{
+		if (PHP_VERSION_ID < 80000) {
+			$this->markTestSkipped('Test requires PHP 8.0.');
+		}
+
 		$errors = $this->runAnalyse(__DIR__ . '/data/bug-10772.php');
 		$this->assertNoErrors($errors);
 	}

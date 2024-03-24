7bc59d1a1f9348d1e949a5c3eed7d5ff64eab61a
Markus Staab <maggus.staab@googlemail.com>
2024-03-24 22:03:40 +0100

    another test

--- e2e/composer-max-version/test.php
+++ e2e/composer-max-version/test.php
@@ -4,3 +4,7 @@
 \PHPStan\Testing\assertType('int<5, 8>', PHP_MAJOR_VERSION);
 \PHPStan\Testing\assertType('int<0, max>', PHP_MINOR_VERSION);
 \PHPStan\Testing\assertType('int<0, max>', PHP_RELEASE_VERSION);
+
+\PHPStan\Testing\assertType('-1|0|1', version_compare(PHP_VERSION, '7.0.0'));
+\PHPStan\Testing\assertType('bool', version_compare(PHP_VERSION, '7.0.0', '<'));
+\PHPStan\Testing\assertType('bool', version_compare(PHP_VERSION, '7.0.0', '>'));

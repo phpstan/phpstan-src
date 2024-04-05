59c3eaaa66015b9835c3e3c930a4f99aacdc9498
Markus Staab <maggus.staab@googlemail.com>
2024-04-05 10:07:38 +0200

    Incremented numeric-string should change to int/float

---
+++ tests/PHPStan/Analyser/data/bug-10122.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Bug10122;
+
+use function PHPStan\Testing\assertType;
+
+function doFoo():void
+{
+	function(string $s) {
+		assertType('(float|int|string)', ++$s);
+	};
+	function(string $s) {
+		assertType('(float|int|string)', --$s);
+	};
+	function(string $s) {
+		assertType('string', $s++);
+		assertType('(float|int|string)', $s);
+	};
+	function(string $s) {
+		assertType('string', $s--);
+		assertType('(float|int|string)', $s);
+	};
+
+	function(float $f) {
+		assertType('float', ++$f);
+	};
+	function(float $f) {
+		assertType('float', --$f);
+	};
+	function(float $f) {
+		assertType('float', $f++);
+	};
+	function(float $f) {
+		assertType('float', $f--);
+	};
+}
+
+/** @param numeric-string $ns */
+function doNumericString(string $ns) {
+	function() use ($ns) {
+		assertType('(float|int)', ++$ns);
+	};
+	function() use ($ns) {
+		assertType('(float|int)', --$ns);
+	};
+	function() use ($ns) {
+		assertType('numeric-string', $ns++);
+		assertType('(float|int)', $ns);
+	};
+	function() use ($ns) {
+		assertType('numeric-string', $ns--);
+		assertType('(float|int)', $ns);
+	};
+}

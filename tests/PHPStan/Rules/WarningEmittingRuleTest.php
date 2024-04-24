2b56fc864d91406e14293a60692c27548665afe0
Ondrej Mirtes <ondrej@mirtes.cz>
2024-04-24 15:17:18 +0200

    Fix

--- tests/PHPStan/Rules/WarningEmittingRuleTest.php
+++ tests/PHPStan/Rules/WarningEmittingRuleTest.php
@@ -6,6 +6,7 @@ use PhpParser\Node;
 use PHPStan\Analyser\Scope;
 use PHPStan\Testing\RuleTestCase;
 use PHPUnit\Framework\AssertionFailedError;
+use const PHP_VERSION_ID;
 
 /**
  * @extends RuleTestCase<Rule>
@@ -36,6 +37,10 @@ class WarningEmittingRuleTest extends RuleTestCase
 
 	public function testRule(): void
 	{
+		if (PHP_VERSION_ID < 70300) {
+			self::markTestSkipped('For some reason this test does not work on PHP 7.2 with old PHPUnit');
+		}
+
 		try {
 			$this->analyse([__DIR__ . '/data/empty-file.php'], []);
 			self::fail('Should throw an exception');

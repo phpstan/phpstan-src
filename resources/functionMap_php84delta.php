1f160db09b66d7b9bef094d472adc97e7aba2e3f
Jordi Boggiano <j.boggiano@seld.be>
2024-05-24 15:13:07 +0200

    Add new header functions for PHP 8.4
    See https://wiki.php.net/rfc/http-last-response-headers

---
+++ resources/functionMap_php84delta.php
@@ -0,0 +1,24 @@
+<?php // phpcs:ignoreFile
+
+/**
+ * This contains the information needed to convert the function signatures for php 8.4 to php 8.3 (and vice versa)
+ *
+ * This has two sections.
+ * The 'new' section contains function/method names from FunctionSignatureMap (And alternates, if applicable) that do not exist in php8.3 or have different signatures in php 8.4.
+ *   If they were just updated, the function/method will be present in the 'added' signatures.
+ * The 'old' signatures contains the signatures that are different in php 8.3.
+ *   Functions are expected to be removed only in major releases of php.
+ *
+ * @see FunctionSignatureMap.php
+ *
+ * @phan-file-suppress PhanPluginMixedKeyNoKey (read by Phan when analyzing this file)
+ */
+return [
+	'new' => [
+		'http_get_last_response_header ' => ['list<string>'],
+		'http_clear_last_response_header' => ['void'],
+	],
+	'old' => [
+
+	]
+];

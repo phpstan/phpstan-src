2a29c80dd8581f0f2055b596d671b96cf30f350c
Markus Staab <maggus.staab@googlemail.com>
2024-06-01 17:36:13 +0200

    validate php-version earlier

--- src/DependencyInjection/ContainerFactory.php
+++ src/DependencyInjection/ContainerFactory.php
@@ -7,6 +7,7 @@ use Nette\DI\Config\Adapters\PhpAdapter;
 use Nette\DI\Definitions\Statement;
 use Nette\DI\Extensions\ExtensionsExtension;
 use Nette\DI\Helpers;
+use Nette\DI\InvalidConfigurationException;
 use Nette\Schema\Context as SchemaContext;
 use Nette\Schema\Elements\AnyOf;
 use Nette\Schema\Elements\Structure;
@@ -352,6 +353,17 @@ class ContainerFactory
 			$context->path = ['parameters'];
 		};
 		$processor->process($schema, $parameters);
+
+		if (
+			array_key_exists('phpVersion', $parameters)
+			&& is_array($parameters['phpVersion']))
+		{
+			$phpVersion = $parameters['phpVersion'];
+
+			if ($phpVersion['max'] < $phpVersion['min']) {
+				throw new InvalidPhpVersionException('Invalid PHP version range: phpVersion.max should be greater or equal to phpVersion.min.');
+			}
+		}
 	}
 
 	/**

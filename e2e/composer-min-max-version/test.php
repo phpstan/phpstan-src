4f90ac07f48a35ec378b90b936979355da0bc25c
Markus Staab <maggus.staab@googlemail.com>
2024-06-01 17:42:27 +0200

    fix expectations

--- e2e/composer-min-max-version/test.php
+++ e2e/composer-min-max-version/test.php
@@ -1,8 +1,8 @@
 <?php
 
-\PHPStan\Testing\assertType('int<80099, 80299>', PHP_VERSION_ID);
+\PHPStan\Testing\assertType('int<80100, 80299>', PHP_VERSION_ID);
 \PHPStan\Testing\assertType('8', PHP_MAJOR_VERSION);
-\PHPStan\Testing\assertType('int<0, 2>', PHP_MINOR_VERSION);
+\PHPStan\Testing\assertType('int<1, 2>', PHP_MINOR_VERSION);
 \PHPStan\Testing\assertType('int<0, max>', PHP_RELEASE_VERSION);
 
 \PHPStan\Testing\assertType('1', version_compare(PHP_VERSION, '7.0.0'));
--- e2e/composer-min-open-end-version/test.php
+++ e2e/composer-min-open-end-version/test.php
@@ -1,6 +1,6 @@
 <?php
 
-\PHPStan\Testing\assertType('int<80099, max>', PHP_VERSION_ID);
+\PHPStan\Testing\assertType('int<80100, max>', PHP_VERSION_ID);
 \PHPStan\Testing\assertType('int<8, max>', PHP_MAJOR_VERSION);
 \PHPStan\Testing\assertType('int<0, max>', PHP_MINOR_VERSION);
 \PHPStan\Testing\assertType('int<0, max>', PHP_RELEASE_VERSION);

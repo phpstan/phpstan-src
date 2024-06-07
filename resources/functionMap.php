683502e73dbf37ee3039d7904858684cf36408ec
Wolfgang Zwiauer <wolfgang.zwiauer@docfinder.at>
2024-06-07 09:30:39 +0200

    Update outdated `RedisCluster::__construct` signature

--- resources/functionMap.php
+++ resources/functionMap.php
@@ -9746,7 +9746,7 @@ return [
 'RedisArray::_hosts' => ['array'],
 'RedisArray::_rehash' => ['', 'callable='=>'callable'],
 'RedisArray::_target' => ['string', 'key'=>'string'],
-'RedisCluster::__construct' => ['void', 'name'=>'string|null', 'seeds'=>'array', 'timeout='=>'float', 'readTimeout='=>'float', 'persistent='=>'bool|false', 'auth='=>'string|array|null'],
+'RedisCluster::__construct' => ['void', 'name'=>'string|null', 'seeds='=>'string[]|null', 'timeout='=>'int|float', 'read_timeout='=>'int|float', 'persistent='=>'bool', 'auth='=>'mixed', 'context'=>'array|null'],
 'RedisCluster::_masters' => ['array'],
 'RedisCluster::_prefix' => ['string', 'value'=>'mixed'],
 'RedisCluster::_serialize' => ['mixed', 'value'=>'mixed'],

27cbdd43f23b97ba74b77242bafe274382b2592f
Giovanni Giacobbi <giovanni@giacobbi.net>
2024-06-09 18:41:23 +0200

    Improve image related functions signature
    - The parameters involving red, green, blue, and alpha are now required to be
      in the respective allowed range (0-255 for colors and 0-127 for alpha) in
      bleeding edge mode.
    - Return values are specified in their possible ranges (0-max for color
      indexes, and 0-256 for imagecolorstotal()).
    - imagecreate() and imagecreatetruecolor() now returns a benevolent union,
      it can only return false if width x height exceeds INT_MAX, and the size
      parameters are required to be in the range 1-max in bleeding edge.
    - imagesx() and imagesy() return value is in the range 1-max and can no longer
      return false.
    - Return type of imagecolorsforindex() now contains the array signature.

--- resources/functionMap_php80delta_bleedingEdge.php
+++ resources/functionMap_php80delta_bleedingEdge.php
@@ -7,6 +7,8 @@ return [
 		'filter_input_array' => ['array|false|null', 'type'=>'INPUT_GET|INPUT_POST|INPUT_COOKIE|INPUT_SERVER|INPUT_ENV', 'definition='=>'int|array', 'add_empty='=>'bool'],
 		'hash_hkdf' => ['non-empty-string', 'algo'=>'non-falsy-string', 'key'=>'string', 'length='=>'0|positive-int', 'info='=>'string', 'salt='=>'string'],
 		'hash_pbkdf2' => ['non-empty-string', 'algo'=>'non-falsy-string', 'password'=>'string', 'salt'=>'string', 'iterations'=>'positive-int', 'length='=>'0|positive-int', 'raw_output='=>'bool'],
+		'imagecreate' => ['__benevolent<GdImage|false>', 'width'=>'int<1, max>', 'height'=>'int<1, max>'],
+		'imagecreatetruecolor' => ['__benevolent<GdImage|false>', 'width'=>'int<1, max>', 'height'=>'int<1, max>'],
 		'mb_detect_order' => ['bool|list<non-falsy-string>', 'encoding_list='=>'non-empty-list<non-falsy-string>|non-falsy-string|null'],
 	],
 	'old' => [

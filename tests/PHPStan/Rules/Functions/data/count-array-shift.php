9cf4eb89a060dec1a064950442b203ea5a8211e5
Markus Staab <maggus.staab@googlemail.com>
2024-06-10 10:18:01 +0200

    Regression test

---
+++ tests/PHPStan/Rules/Functions/data/count-array-shift.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CountArrayShift;
+
+/** @param array<mixed>|false $a */
+function foo($a): void
+{
+	while (count($a) > 0) {
+		array_shift($a);
+	}
+}
+
+/** @param non-empty-array<mixed>|false $a */
+function bar($a): void
+{
+	while (count($a) > 0) {
+		array_shift($a);
+	}
+}

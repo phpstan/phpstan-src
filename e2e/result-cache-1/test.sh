1328bc94416b97671a6a92de6f518524b9db5afc
Markus Staab <maggus.staab@googlemail.com>
2024-06-15 10:46:11 +0200

    Update test.sh

--- e2e/result-cache-1/test.sh
+++ e2e/result-cache-1/test.sh
@@ -3,10 +3,15 @@
 function test_result_cache1() {
     echo -n > phpstan-baseline.neon
     ../../bin/phpstan -vvv
+    assert_successful_code
+
     patch -b src/Bar.php < patch-1.patch
     cat baseline-1.neon > phpstan-baseline.neon
     ../../bin/phpstan -vvv
+    assert_successful_code
+
     mv src/Bar.php.orig src/Bar.php
     echo -n > phpstan-baseline.neon
     ../../bin/phpstan -vvv
+    assert_successful_code
 } 2>&1

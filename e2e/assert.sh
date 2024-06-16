fe5ccbe443bd3a7facccd6f0a49647b8f3cd5910
Markus Staab <maggus.staab@googlemail.com>
2024-06-16 09:52:41 +0200

    fix

--- e2e/assert.sh
+++ e2e/assert.sh
@@ -10,18 +10,22 @@
 
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
+
+# import all bashunit functions into the current process
 shopt -s expand_aliases  # enable alias expansion (off by default in noninteractive shells)
 alias exit=return        # ...and alias 'exit' to 'return'
 source ${__dir}/bashunit --version > /dev/null 2>&1;
 unalias exit             # disable the alias...
 
-# the next line calls the function passed as the first parameter to the script.
-# the remaining script arguments can be passed to this function.
 
+# return non-zero exit code if unknown assertion is called
 set -e
 
+# the next line calls the function passed as the first parameter to the script.
+# the remaining script arguments can be passed to this function.
 "assert_$1" $2 $3 $4 $5 $6 $7 $8 $9
 
+# return non-zero exit code when assertion fails
 if [[ "$(state::get_tests_failed)" -gt 0 ]] || [[ "$(state::get_assertions_failed)" -gt 0 ]]; then
     exit 1
 fi

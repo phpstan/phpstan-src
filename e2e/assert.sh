67d6f64e2ccde1063a6c1e654f033b69edb16a6d
Markus Staab <maggus.staab@googlemail.com>
2024-06-16 09:18:22 +0200

    Update assert.sh

--- e2e/assert.sh
+++ e2e/assert.sh
@@ -10,8 +10,10 @@ unalias exit             # disable the alias...
 
 "assert_$1" $2 $3 $4 $5 $6 $7 $8 $9
 
+AsserExit=$?
+
 if [[ "$(state::get_tests_failed)" -gt 0 ]] || [[ "$(state::get_assertions_failed)" -gt 0 ]]; then
     exit 1
 fi
 
-exit 0
+exit $AsserExit

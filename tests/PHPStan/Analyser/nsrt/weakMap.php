3be40377c47e336797bca822fcc12cbddc4a164f
Mikhailov Nikolai <mikhaylov.n@promail366.com>
2024-06-18 23:12:52 +0700

    Fix type of return value for WeakMap::offsetGet()

---
+++ tests/PHPStan/Analyser/nsrt/weakMap.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types = 1);
+
+namespace weakMap;
+
+use WeakMap;
+use function PHPStan\Testing\assertType;
+
+interface Foo {}
+interface Bar {}
+
+/**
+ * @param WeakMap<Foo, Bar> $weakMap
+ */
+function weakMapOffsetGetNotNullable(WeakMap $weakMap, Foo $foo): void
+{
+	$bar = $weakMap[$foo];
+
+	assertType(Bar::class, $bar);
+}
+
+
+/**
+ * @param WeakMap<Foo, Bar|null> $weakMap
+ */
+function weakMapOffsetGetNullable(WeakMap $weakMap, Foo $foo): void
+{
+	$bar = $weakMap[$foo];
+
+	assertType( 'weakMap\\Bar|null', $bar);
+}

51bad3df60b25270ca86106ba2e760d570c26656
Markus Staab <markus.staab@redaxo.de>
2024-06-24 16:23:10 +0200

    document room for improvement

--- tests/PHPStan/Analyser/nsrt/preg_match_shapes_php82.php
+++ tests/PHPStan/Analyser/nsrt/preg_match_shapes_php82.php
@@ -8,13 +8,12 @@ use function PHPStan\Testing\assertType;
 // https://php.watch/versions/8.2/preg-n-no-capture-modifier
 function doNonAutoCapturingFlag(string $s): void {
 	if (preg_match('/(\d+)/n', $s, $matches)) {
-		assertType('array{string, string}', $matches);
+		assertType('array{string, string}', $matches); // should be 'array{string}'
 	}
 	assertType('array{}|array{string, string}', $matches);
 
 	if (preg_match('/(\d+)(?P<num>\d+)/n', $s, $matches)) {
-		// could be assertType('array{0: string, num: string, 1: string}', $matches);
-		assertType('array<string>', $matches);
+		assertType('array<string>', $matches); // could be 'array{0: string, num: string, 1: string}'
 	}
 	assertType('array<string>', $matches);
 }

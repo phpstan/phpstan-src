f86b1528cce87929f311cc0d60b9bd1a0e887af2
Markus Staab <markus.staab@redaxo.de>
2024-06-24 16:01:52 +0200

    cleanup version specific expectations

--- tests/PHPStan/Analyser/LegacyNodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/LegacyNodeScopeResolverTest.php
@@ -302,7 +302,7 @@ class LegacyNodeScopeResolverTest extends TypeInferenceTestCase
 				$testScope,
 				'matches',
 				TrinaryLogic::createYes(),
-				PHP_VERSION_ID <= 80000 ? 'array<string>' : 'array{0?: string}',
+				'array{0?: string}',
 			],
 			[
 				$testScope,
@@ -343,7 +343,7 @@ class LegacyNodeScopeResolverTest extends TypeInferenceTestCase
 				$testScope,
 				'matches2',
 				TrinaryLogic::createMaybe(),
-				PHP_VERSION_ID <= 80000 ? 'array<string>' : 'array{0?: string}',
+				'array{0?: string}',
 			],
 			[
 				$testScope,
@@ -355,7 +355,7 @@ class LegacyNodeScopeResolverTest extends TypeInferenceTestCase
 				$testScope,
 				'matches3',
 				TrinaryLogic::createYes(),
-				PHP_VERSION_ID <= 80000 ? 'array<string>' : 'array{0?: string}',
+				'array{0?: string}',
 			],
 			[
 				$testScope,
@@ -415,7 +415,7 @@ class LegacyNodeScopeResolverTest extends TypeInferenceTestCase
 				$testScope,
 				'ternaryMatches',
 				TrinaryLogic::createYes(),
-				PHP_VERSION_ID <= 80000 ? 'array<string>' : 'array{0?: string}',
+				'array{0?: string}',
 			],
 			[
 				$testScope,
--- tests/PHPStan/Analyser/data/param-out.php
+++ tests/PHPStan/Analyser/data/param-out.php
@@ -485,3 +485,20 @@ function ($s, $t): void {
 	assertType('string', $s);
 	assertType('string', $t);
 };
+
+function fooMatch(string $input): void {
+	preg_match_all('/@[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}(?!\w)/', $input, $matches, PREG_PATTERN_ORDER);
+	assertType('array<list<string>>', $matches);
+
+	preg_match_all('/@[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}(?!\w)/', $input, $matches, PREG_SET_ORDER);
+	assertType('list<array<string>>', $matches);
+
+	preg_match('/@[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}(?!\w)/', $input, $matches, PREG_UNMATCHED_AS_NULL);
+	assertType("array{0?: string}", $matches);
+}
+
+function testMatch() {
+	preg_match('#.*#', 'foo', $matches);
+	assertType('array{0?: string}', $matches);
+}
+

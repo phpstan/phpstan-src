78dcc5a7cc8dca11b3b940cf5c5b05fcb96c2f64
Markus Staab <maggus.staab@googlemail.com>
2024-07-06 11:49:40 +0200

    Update bug-11292.php

--- tests/PHPStan/Analyser/data/bug-11292.php
+++ tests/PHPStan/Analyser/data/bug-11292.php
@@ -2,10 +2,12 @@
 
 namespace Bug11292;
 
+use function PHPStan\Testing\assertType;
+
 function (string $s): void {
 	$nonUrl = "[^-_#$+.!*%'(),;/?:@~=&a-zA-Z0-9\x7f-\xff]";
 	$pattern = '{\\b(https?://)?(?:([^]\\\\\\x00-\\x20\\"(),:-<>[\\x7f-\\xff]{1,64})(:[^]\\\\\\x00-\\x20\\"(),:-<>[\\x7f-\\xff]{1,64})?@)?((?:[-a-zA-Z0-9\\x7f-\\xff]{1,63}\\.)+[a-zA-Z\\x7f-\\xff][-a-zA-Z0-9\\x7f-\\xff]{1,62})((:[0-9]{1,5})?(/[!$-/0-9:;=@_~\':;!a-zA-Z\\x7f-\\xff]*?)?(\\?[!$-/0-9:;=@_\':;!a-zA-Z\\x7f-\\xff]+?)?(#[!$-/0-9?:;=@_\':;!a-zA-Z\\x7f-\\xff]+?)?)(?=[)\'?.!,;:]*(' . $nonUrl . '|$))}';
 	if (preg_match($pattern, $s, $matches, PREG_OFFSET_CAPTURE, 0)) {
-
+		assertType('array<array{string, int<-1, max>}>', $matches);
 	}
 };

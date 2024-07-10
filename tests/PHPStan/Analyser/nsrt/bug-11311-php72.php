fd3c3ec6e86456ac69d412b7008f3efae61ad69b
Markus Staab <markus.staab@redaxo.de>
2024-07-10 11:50:00 +0200

    fix test

--- tests/PHPStan/Analyser/nsrt/bug-11311-php72.php
+++ tests/PHPStan/Analyser/nsrt/bug-11311-php72.php
@@ -4,8 +4,8 @@ namespace Bug11311;
 
 use function PHPStan\Testing\assertType;
 
-function doFoo() {
-	if (1 === preg_match('/(?<major>\d+)\.(?<minor>\d+)(?:\.(?<patch>\d+))?/', '12.5', $matches, PREG_UNMATCHED_AS_NULL)) {
+function doFoo(string $s) {
+	if (1 === preg_match('/(?<major>\d+)\.(?<minor>\d+)(?:\.(?<patch>\d+))?/', $s, $matches, PREG_UNMATCHED_AS_NULL)) {
         // on PHP < 7.4, unmatched-as-null does not return null values; see https://3v4l.org/v3HE4
 		assertType('array{0: string, major: string, 1: string, minor: string, 2: string, patch?: string, 3?: string}', $matches);
 	}
--- tests/PHPStan/Analyser/nsrt/bug-11311.php
+++ tests/PHPStan/Analyser/nsrt/bug-11311.php
@@ -4,8 +4,8 @@ namespace Bug11311;
 
 use function PHPStan\Testing\assertType;
 
-function doFoo() {
-	if (1 === preg_match('/(?<major>\d+)\.(?<minor>\d+)(?:\.(?<patch>\d+))?/', '12.5', $matches, PREG_UNMATCHED_AS_NULL)) {
+function doFoo(string $s) {
+	if (1 === preg_match('/(?<major>\d+)\.(?<minor>\d+)(?:\.(?<patch>\d+))?/', $s, $matches, PREG_UNMATCHED_AS_NULL)) {
 
 		assertType('array{0: string, major: string|null, 1: string|null, minor: string|null, 2: string|null, patch: string|null, 3: string|null}', $matches);
 	}

c7c9689cd86a9811eeaa2bb365ab5bc302109802
Ondrej Mirtes <ondrej@mirtes.cz>
2024-07-15 13:51:58 +0200

    Parallel scheduler becomes DiagnoseExtension

--- src/Parallel/Scheduler.php
+++ src/Parallel/Scheduler.php
@@ -2,14 +2,20 @@
 
 namespace PHPStan\Parallel;
 
+use PHPStan\Command\Output;
+use PHPStan\Diagnose\DiagnoseExtension;
 use function array_chunk;
 use function count;
 use function floor;
 use function max;
 use function min;
+use function sprintf;
 
-class Scheduler
+class Scheduler implements DiagnoseExtension
 {
+
+	/** @var array{int, int, int, int}|null */
+	private ?array $storedData = null;
 
 	/**
 	 * @param positive-int $jobSize
@@ -38,7 +44,31 @@ class Scheduler
 			$cpuCores,
 		);
 
-		return new Schedule(min($numberOfProcesses, $this->maximumNumberOfProcesses), $jobs);
+		$usedNumberOfProcesses = min($numberOfProcesses, $this->maximumNumberOfProcesses);
+		$this->storedData = [$cpuCores, count($files), count($jobs), $usedNumberOfProcesses];
+
+		return new Schedule($usedNumberOfProcesses, $jobs);
+	}
+
+	public function print(Output $output): void
+	{
+		if ($this->storedData === null) {
+			return;
+		}
+
+		[$cpuCores, $filesCount, $jobsCount, $usedNumberOfProcesses] = $this->storedData;
+
+		$output->writeLineFormatted('<info>Parallel processing scheduler:</info>');
+		$output->writeLineFormatted(sprintf(
+			'# of detected CPU %s:  %s%d',
+			$cpuCores === 1 ? 'core' : 'cores',
+			$cpuCores === 1 ? '' : ' ',
+			$cpuCores,
+		));
+		$output->writeLineFormatted(sprintf('# of analysed files:       %d', $filesCount));
+		$output->writeLineFormatted(sprintf('# of jobs:                 %d', $jobsCount));
+		$output->writeLineFormatted(sprintf('# of spawned processes:    %d', $usedNumberOfProcesses));
+		$output->writeLineFormatted('');
 	}
 
 }

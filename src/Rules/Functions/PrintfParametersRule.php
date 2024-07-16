14d0951febe640418286b8cd364c0dbba44fb93e
Markus Staab <markus.staab@redaxo.de>
2024-07-16 08:55:45 +0200

    Refactor PrintfParametersRule

--- src/Rules/Functions/PrintfParametersRule.php
+++ src/Rules/Functions/PrintfParametersRule.php
@@ -2,22 +2,16 @@
 
 namespace PHPStan\Rules\Functions;
 
-use Nette\Utils\Strings;
 use PhpParser\Node;
 use PhpParser\Node\Expr\FuncCall;
 use PHPStan\Analyser\Scope;
-use PHPStan\Php\PhpVersion;
+use PHPStan\Reflection\ReflectionProvider;
 use PHPStan\Rules\Rule;
 use PHPStan\Rules\RuleErrorBuilder;
-use function array_filter;
 use function array_key_exists;
 use function count;
 use function in_array;
-use function max;
 use function sprintf;
-use function strlen;
-use function strtolower;
-use const PREG_SET_ORDER;
 
 /**
  * @implements Rule<Node\Expr\FuncCall>
@@ -25,7 +19,23 @@ use const PREG_SET_ORDER;
 class PrintfParametersRule implements Rule
 {
 
-	public function __construct(private PhpVersion $phpVersion)
+	private const FORMAT_ARGUMENT_POSITIONS = [
+		'printf' => 0,
+		'sprintf' => 0,
+		'sscanf' => 1,
+		'fscanf' => 1,
+	];
+	private const MINIMUM_NUMBER_OF_ARGUMENTS = [
+		'printf' => 1,
+		'sprintf' => 1,
+		'sscanf' => 3,
+		'fscanf' => 3,
+	];
+
+	public function __construct(
+		private PrintfHelper $printfHelper,
+		private ReflectionProvider $reflectionProvider,
+	)
 	{
 	}
 
@@ -40,25 +50,17 @@ class PrintfParametersRule implements Rule
 			return [];
 		}
 
-		$functionsArgumentPositions = [
-			'printf' => 0,
-			'sprintf' => 0,
-			'sscanf' => 1,
-			'fscanf' => 1,
-		];
-		$minimumNumberOfArguments = [
-			'printf' => 1,
-			'sprintf' => 1,
-			'sscanf' => 3,
-			'fscanf' => 3,
-		];
-
-		$name = strtolower((string) $node->name);
-		if (!array_key_exists($name, $functionsArgumentPositions)) {
+		if (!$this->reflectionProvider->hasFunction($node->name, $scope)) {
 			return [];
 		}
 
-		$formatArgumentPosition = $functionsArgumentPositions[$name];
+		$functionReflection = $this->reflectionProvider->getFunction($node->name, $scope);
+		$name = $functionReflection->getName();
+		if (!array_key_exists($name, self::FORMAT_ARGUMENT_POSITIONS)) {
+			return [];
+		}
+
+		$formatArgumentPosition = self::FORMAT_ARGUMENT_POSITIONS[$name];
 
 		$args = $node->getArgs();
 		foreach ($args as $arg) {
@@ -67,89 +69,50 @@ class PrintfParametersRule implements Rule
 			}
 		}
 		$argsCount = count($args);
-		if ($argsCount < $minimumNumberOfArguments[$name]) {
+		if ($argsCount < self::MINIMUM_NUMBER_OF_ARGUMENTS[$name]) {
 			return []; // caught by CallToFunctionParametersRule
 		}
 
 		$formatArgType = $scope->getType($args[$formatArgumentPosition]->value);
-		$placeHoldersCount = null;
+		$maxPlaceHoldersCount = null;
 		foreach ($formatArgType->getConstantStrings() as $formatString) {
 			$format = $formatString->getValue();
-			$tempPlaceHoldersCount = $this->getPlaceholdersCount($name, $format);
-			if ($placeHoldersCount === null) {
-				$placeHoldersCount = $tempPlaceHoldersCount;
-			} elseif ($tempPlaceHoldersCount > $placeHoldersCount) {
-				$placeHoldersCount = $tempPlaceHoldersCount;
+
+			if (in_array($name, ['sprintf', 'printf'], true)) {
+				$tempPlaceHoldersCount = $this->printfHelper->getPrintfPlaceholdersCount($format);
+			} else {
+				$tempPlaceHoldersCount = $this->printfHelper->getScanfPlaceholdersCount($format);
+			}
+
+			if ($maxPlaceHoldersCount === null) {
+				$maxPlaceHoldersCount = $tempPlaceHoldersCount;
+			} elseif ($tempPlaceHoldersCount > $maxPlaceHoldersCount) {
+				$maxPlaceHoldersCount = $tempPlaceHoldersCount;
 			}
 		}
 
-		if ($placeHoldersCount === null) {
+		if ($maxPlaceHoldersCount === null) {
 			return [];
 		}
 
 		$argsCount -= $formatArgumentPosition;
 
-		if ($argsCount !== $placeHoldersCount + 1) {
+		if ($argsCount !== $maxPlaceHoldersCount + 1) {
 			return [
 				RuleErrorBuilder::message(sprintf(
 					sprintf(
 						'%s, %s.',
-						$placeHoldersCount === 1 ? 'Call to %s contains %d placeholder' : 'Call to %s contains %d placeholders',
+						$maxPlaceHoldersCount === 1 ? 'Call to %s contains %d placeholder' : 'Call to %s contains %d placeholders',
 						$argsCount - 1 === 1 ? '%d value given' : '%d values given',
 					),
 					$name,
-					$placeHoldersCount,
+					$maxPlaceHoldersCount,
 					$argsCount - 1,
 				))->identifier(sprintf('argument.%s', $name))->build(),
 			];
 		}
 
 		return [];
-	}
-
-	private function getPlaceholdersCount(string $functionName, string $format): int
-	{
-		$specifiers = in_array($functionName, ['sprintf', 'printf'], true) ? '(?:[bs%s]|l?[cdeEgfFGouxX])' : '(?:[cdDeEfinosuxX%s]|\[[^\]]+\])';
-		$addSpecifier = '';
-		if ($this->phpVersion->supportsHhPrintfSpecifier()) {
-			$addSpecifier .= 'hH';
-		}
-
-		$specifiers = sprintf($specifiers, $addSpecifier);
-
-		$pattern = '~(?<before>%*)%(?:(?<position>\d+)\$)?[-+]?(?:[ 0]|(?:\'[^%]))?(?<width>\*)?-?\d*(?:\.(?:\d+|(?<precision>\*))?)?' . $specifiers . '~';
-
-		$matches = Strings::matchAll($format, $pattern, PREG_SET_ORDER);
-
-		if (count($matches) === 0) {
-			return 0;
-		}
-
-		$placeholders = array_filter($matches, static fn (array $match): bool => strlen($match['before']) % 2 === 0);
-
-		if (count($placeholders) === 0) {
-			return 0;
-		}
-
-		$maxPositionedNumber = 0;
-		$maxOrdinaryNumber = 0;
-		foreach ($placeholders as $placeholder) {
-			if (isset($placeholder['width']) && $placeholder['width'] !== '') {
-				$maxOrdinaryNumber++;
-			}
-
-			if (isset($placeholder['precision']) && $placeholder['precision'] !== '') {
-				$maxOrdinaryNumber++;
-			}
-
-			if (isset($placeholder['position']) && $placeholder['position'] !== '') {
-				$maxPositionedNumber = max((int) $placeholder['position'], $maxPositionedNumber);
-			} else {
-				$maxOrdinaryNumber++;
-			}
-		}
-
-		return max($maxPositionedNumber, $maxOrdinaryNumber);
 	}
 
 }

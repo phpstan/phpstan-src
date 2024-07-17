40a8751c9058518ccece7c3309516fdcd5df9002
Markus Staab <markus.staab@redaxo.de>
2024-07-17 10:05:42 +0200

    fix php 7.3

--- tests/PHPStan/Analyser/nsrt/bug-11311-php72.php
+++ tests/PHPStan/Analyser/nsrt/bug-11311-php72.php
@@ -8,23 +8,23 @@ use function PHPStan\Testing\assertType;
 
 function doFoo(string $s) {
 	if (1 === preg_match('/(?<major>\d+)\.(?<minor>\d+)(?:\.(?<patch>\d+))?/', $s, $matches, PREG_UNMATCHED_AS_NULL)) {
-		assertType('array{0: string, major: string, 1: string, minor: string, 2: string, patch?: string, 3?: string}', $matches);
+		assertType('array{0: string, major: numeric-string, 1: numeric-string, minor: numeric-string, 2: numeric-string, patch?: numeric-string, 3?: numeric-string}', $matches);
 	}
 }
 
 function doUnmatchedAsNull(string $s): void {
 	if (preg_match('/(foo)?(bar)?(baz)?/', $s, $matches, PREG_UNMATCHED_AS_NULL)) {
-		assertType('array{0: string, 1?: string, 2?: string, 3?: string}', $matches);
+		assertType('array{0: string, 1?: non-empty-string, 2?: non-empty-string, 3?: non-empty-string}', $matches);
 	}
-	assertType('array{}|array{0: string, 1?: string, 2?: string, 3?: string}', $matches);
+	assertType('array{}|array{0: string, 1?: non-empty-string, 2?: non-empty-string, 3?: non-empty-string}', $matches);
 }
 
 // see https://3v4l.org/VeDob#veol
 function unmatchedAsNullWithOptionalGroup(string $s): void {
 	if (preg_match('/Price: (£|€)?\d+/', $s, $matches, PREG_UNMATCHED_AS_NULL)) {
-		assertType('array{0: string, 1?: string}', $matches);
+		assertType('array{0: string, 1?: non-empty-string}', $matches);
 	} else {
 		assertType('array{}', $matches);
 	}
-	assertType('array{}|array{0: string, 1?: string}', $matches);
+	assertType('array{}|array{0: string, 1?: non-empty-string}', $matches);
 }

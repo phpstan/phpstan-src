831f4cd05e06ab917ea26713588ac31a324dab60
Ondrej Mirtes <ondrej@mirtes.cz>
2024-07-19 13:59:14 +0200

    Fix path

--- src/Type/Php/RegexArrayShapeMatcher.php
+++ src/Type/Php/RegexArrayShapeMatcher.php
@@ -372,7 +372,7 @@ final class RegexArrayShapeMatcher
 	{
 		if (self::$parser === null) {
 			/** @throws void */
-			self::$parser = Llk::load(new Read(__DIR__ . '/../../resources/RegexGrammar.pp'));
+			self::$parser = Llk::load(new Read(__DIR__ . '/../../../resources/RegexGrammar.pp'));
 		}
 
 		try {

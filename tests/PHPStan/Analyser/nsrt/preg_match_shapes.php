2223dd9623522c223357c556189e7ec5280dba59
Markus Staab <maggus.staab@googlemail.com>
2024-07-21 11:32:15 +0200

    RegexArrayShapeMatcher - `(\d*)` can be empty-string

--- tests/PHPStan/Analyser/nsrt/preg_match_shapes.php
+++ tests/PHPStan/Analyser/nsrt/preg_match_shapes.php
@@ -3,7 +3,7 @@
 namespace PregMatchShapes;
 
 use function PHPStan\Testing\assertType;
-
+use InvalidArgumentException;
 
 function doMatch(string $s): void {
 	if (preg_match('/Price: /i', $s, $matches)) {
@@ -519,3 +519,9 @@ function bug11323(string $s): void {
 		assertType('array{string, non-empty-string}', $matches);
 	}
 }
+
+function (string $s): void {
+	preg_match('/%a(\d*)/', $s, $matches);
+	assertType("array{0?: string, 1?: ''|numeric-string}", $matches);
+};
+

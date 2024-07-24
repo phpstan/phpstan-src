40eaa6eedb81d8d90bb61d5650239b2db75c817f
Markus Staab <maggus.staab@googlemail.com>
2024-07-24 20:32:32 +0200

    add more tests

--- tests/PHPStan/Analyser/nsrt/preg_match_all_shapes.php
+++ tests/PHPStan/Analyser/nsrt/preg_match_all_shapes.php
@@ -29,5 +29,37 @@ function (string $size): void {
 		assertType("array{0: list<string>, num: list<''|numeric-string>, 1: list<''|numeric-string>}", $matches);
 		return;
 	}
+	assertType("array{}", $matches);
+};
+
+function (string $size): void {
+	if (preg_match_all('/ab(?P<num>\d+)?/', $size, $matches) > 0) {
+		assertType("array{0: list<string>, num: list<''|numeric-string>, 1: list<''|numeric-string>}", $matches);
+		return;
+	}
+	assertType("array{0: list<string>, num: list<''|numeric-string>, 1: list<''|numeric-string>}", $matches);
+};
+
+function (string $size): void {
+	if (preg_match_all('/ab(?P<num>\d+)?/', $size, $matches) != false) {
+		assertType("array{0: list<string>, num: list<''|numeric-string>, 1: list<''|numeric-string>}", $matches);
+		return;
+	}
+	assertType("array{0: list<string>, num: list<''|numeric-string>, 1: list<''|numeric-string>}", $matches);
+};
+
+function (string $size): void {
+	if (preg_match_all('/ab(?P<num>\d+)?/', $size, $matches) == true) {
+		assertType("array{0: list<string>, num: list<''|numeric-string>, 1: list<''|numeric-string>}", $matches);
+		return;
+	}
+	assertType("array{}", $matches);
+};
+
+function (string $size): void {
+	if (preg_match_all('/ab(?P<num>\d+)?/', $size, $matches) === 1) {
+		assertType("array{0: list<string>, num: list<''|numeric-string>, 1: list<''|numeric-string>}", $matches);
+		return;
+	}
 	assertType("array{0: list<string>, num: list<''|numeric-string>, 1: list<''|numeric-string>}", $matches);
 };

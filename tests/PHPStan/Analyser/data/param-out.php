2229debcdc300987d0949a4ce9845c6e5561cd47
Markus Staab <markus.staab@redaxo.de>
2024-08-01 17:54:13 +0200

    Bleeding edge - Precise array shape for preg_match_all() $matches

--- tests/PHPStan/Analyser/data/param-out.php
+++ tests/PHPStan/Analyser/data/param-out.php
@@ -488,10 +488,10 @@ function ($s, $t): void {
 
 function fooMatch(string $input): void {
 	preg_match_all('/@[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}(?!\w)/', $input, $matches, PREG_PATTERN_ORDER);
-	assertType('array<list<string>>', $matches);
+	assertType('array{list<string>}', $matches);
 
 	preg_match_all('/@[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}(?!\w)/', $input, $matches, PREG_SET_ORDER);
-	assertType('list<array<string>>', $matches);
+	assertType('list<array{string}>', $matches);
 
 	preg_match('/@[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}(?!\w)/', $input, $matches, PREG_UNMATCHED_AS_NULL);
 	assertType("array{0?: string}", $matches);

4889f9e727a7aa66d27546f2247f94ee6a26ed5a
Bellangelo <aggelosbellos7@gmail.com>
2024-08-06 01:49:41 +0300

    Const types are not released until PHP 8.3

--- tests/PHPStan/Rules/Keywords/ClassThatContainsConst.php
+++ tests/PHPStan/Rules/Keywords/ClassThatContainsConst.php
@@ -6,6 +6,6 @@ namespace PHPStan\Rules\Keywords;
 
 class ClassThatContainsConst
 {
-    public const string FILE_EXISTS = 'include-me-to-prove-you-work.txt';
-    public const string FILE_DOES_NOT_EXIST = 'a-file-that-does-not-exist.php';
+    public const FILE_EXISTS = 'include-me-to-prove-you-work.txt';
+    public const FILE_DOES_NOT_EXIST = 'a-file-that-does-not-exist.php';
 }

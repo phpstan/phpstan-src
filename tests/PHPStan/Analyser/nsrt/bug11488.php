1ffc94ab4b652d5e36229ab102602b89392316eb
Markus Staab <maggus.staab@googlemail.com>
2024-08-09 10:29:41 +0200

    more assertions

--- tests/PHPStan/Analyser/nsrt/bug11488.php
+++ tests/PHPStan/Analyser/nsrt/bug11488.php
@@ -13,19 +13,28 @@ class Foo
 	{
 		if (count($row) === 1) {
 			assertType('array{0: mixed, 1?: string|null}', $row);
+		} else {
+			assertType('array{0: mixed, 1?: string|null}', $row);
 		}
 
 		if (count($row) !== 1) {
+			assertType('array{0: mixed, 1?: string|null}', $row);
+		} else {
 			assertType('array{0: mixed, 1?: string|null}', $row);
 		}
 
 		if (count($row) !== 2) {
 			assertType('array{0: mixed, 1?: string|null}', $row);
+		} else {
+			// should be array{0: mixed, 1: string|null}
+			assertType('array{mixed, mixed}', $row);
 		}
 
 		if (count($row) !== 3) {
 			// should be array{0: mixed, 1?: string|null}
 			assertType('array{0: mixed, 1?: mixed}', $row);
+		} else {
+			assertType('*NEVER*', $row);
 		}
 	}
 
@@ -36,14 +45,20 @@ class Foo
 	{
 		if (count($row) !== 1) {
 			assertType('array{mixed, string|null, mixed}', $row);
+		} else {
+			assertType('array{0: mixed, 1?: string|null}', $row);
 		}
 
 		if (count($row) !== 2) {
 			assertType('array{mixed, string|null, mixed}|array{mixed}', $row);
+		} else {
+			assertType('*NEVER*', $row);
 		}
 
 		if (count($row) !== 3) {
 			assertType('array{mixed}', $row);
+		} else {
+			assertType('array{mixed, string|null, mixed}', $row);
 		}
 	}
 }

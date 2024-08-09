499d9efefa62db06f6abcf5c3923ce079ee98618
Markus Staab <maggus.staab@googlemail.com>
2024-08-09 10:01:23 +0200

    more tests

--- tests/PHPStan/Analyser/nsrt/bug11488.php
+++ tests/PHPStan/Analyser/nsrt/bug11488.php
@@ -7,6 +7,28 @@ use function PHPStan\Testing\assertType;
 class Foo
 {
 	/**
+	 * @param array{mixed}|array{0: mixed, 1?: string|null} $row
+	 */
+	protected function testOptionalKeys(array $row): void
+	{
+		if (count($row) === 1) {
+			assertType('array{0: mixed, 1?: string|null}', $row);
+		}
+
+		if (count($row) !== 1) {
+			assertType('array{0: mixed, 1?: string|null}', $row);
+		}
+
+		if (count($row) !== 2) {
+			assertType('array{0: mixed, 1?: string|null}', $row);
+		}
+
+		if (count($row) !== 3) {
+			assertType('array{0: mixed, 1?: string|null}', $row);
+		}
+	}
+
+	/**
 	 * @param array{mixed}|array{mixed, string|null, mixed} $row
 	 */
 	protected function test(array $row): void

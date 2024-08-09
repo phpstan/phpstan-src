f894e7b42b92df346065613ac56cdbe0c3238ddf
Markus Staab <maggus.staab@googlemail.com>
2024-08-09 10:19:43 +0200

    fix

--- src/Type/Constant/ConstantArrayType.php
+++ src/Type/Constant/ConstantArrayType.php
@@ -366,15 +366,6 @@ class ConstantArrayType extends ArrayType implements ConstantType
 				return $type->isIterableAtLeastOnce()->negate();
 			}
 
-			if (
-				$this->isList->yes()
-				&& $type->isList->yes()
-				&& count($this->keyTypes) !== count($type->keyTypes)
-				&& count($type->optionalKeys) === 0
-			) {
-				return TrinaryLogic::createNo();
-			}
-
 			$results = [];
 			foreach ($this->keyTypes as $i => $keyType) {
 				$hasOffset = $type->hasOffsetValueType($keyType);
@@ -394,6 +385,25 @@ class ConstantArrayType extends ArrayType implements ConstantType
 					return TrinaryLogic::createNo();
 				}
 				$results[] = $isValueSuperType;
+			}
+
+			if (
+				$this->isList->yes()
+				&& $type->isList->yes()
+				&& count($this->keyTypes) !== count($type->keyTypes)
+				&& count($type->optionalKeys) === 0
+			) {
+				$keepSeparate = true;
+				foreach ($this->valueTypes as $valueType) {
+					if ($valueType->isConstantValue()->yes()) {
+						$keepSeparate = false;
+						break;
+					}
+				}
+
+				if ($keepSeparate) {
+					return TrinaryLogic::createNo();
+				}
 			}
 
 			return TrinaryLogic::createYes()->and(...$results);

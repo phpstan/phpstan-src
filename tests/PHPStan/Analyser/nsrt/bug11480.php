e9c60a28ed13838d643ef64f1b1aa24820bbeb8e
Markus Staab <maggus.staab@googlemail.com>
2024-08-13 21:38:26 +0200

    Narrow arrays in union based on count() with smaller/greater operator

---
+++ tests/PHPStan/Analyser/nsrt/bug11480.php
@@ -0,0 +1,113 @@
+<?php declare(strict_types=1);
+
+namespace Bug11480;
+
+use function PHPStan\Testing\assertType;
+
+class HelloWorld
+{
+	public function arrayGreatherThan(): void
+	{
+		$x = [];
+		if (rand(0, 1)) {
+			$x[] = 'ab';
+		}
+		if (rand(0, 1)) {
+			$x[] = 'xy';
+		}
+
+		if (count($x) > 0) {
+			assertType("array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+		} else {
+			assertType("array{}", $x);
+		}
+		assertType("array{}|array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+
+		if (count($x) > 1) {
+			assertType("array{0: 'ab', 1?: 'xy'}", $x);
+		} else {
+			assertType("array{}|array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+		}
+		assertType("array{}|array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+
+		if (count($x) >= 1) {
+			assertType("array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+		} else {
+			assertType("array{}", $x);
+		}
+		assertType("array{}|array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+	}
+
+	public function arraySmallerThan(): void
+	{
+		$x = [];
+		if (rand(0, 1)) {
+			$x[] = 'ab';
+		}
+		if (rand(0, 1)) {
+			$x[] = 'xy';
+		}
+
+		if (count($x) < 1) {
+			assertType("array{}", $x);
+		} else {
+			assertType("array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+		}
+		assertType("array{}|array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+
+		if (count($x) <= 1) {
+			assertType("array{}|array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+		} else {
+			assertType("array{0: 'ab', 1?: 'xy'}", $x);
+		}
+		assertType("array{}|array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+	}
+
+	public function intUnionCount(): void
+	{
+		$count = 1;
+		if (rand(0, 1)) {
+			$count++;
+		}
+
+		$x = [];
+		if (rand(0, 1)) {
+			$x[] = 'ab';
+		}
+		if (rand(0, 1)) {
+			$x[] = 'xy';
+		}
+
+		assertType('1|2', $count);
+
+		assertType("array{}|array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+		if (count($x) >= $count) {
+			assertType("array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+		} else {
+			assertType("array{}|array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+		}
+		assertType("array{}|array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+	}
+
+	/**
+	 * @param int<1,2> $count
+	 */
+	public function intRangeCount($count): void
+	{
+		$x = [];
+		if (rand(0, 1)) {
+			$x[] = 'ab';
+		}
+		if (rand(0, 1)) {
+			$x[] = 'xy';
+		}
+
+		assertType("array{}|array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+		if (count($x) >= $count) {
+			assertType("array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+		} else {
+			assertType("array{}|array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+		}
+		assertType("array{}|array{'xy'}|array{0: 'ab', 1?: 'xy'}", $x);
+	}
+}

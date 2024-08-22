b06a7eba7cd8c92af37676c0f9b06c82eaaec7eb
Markus Staab <maggus.staab@googlemail.com>
2024-08-22 22:57:57 +0200

    Narrow string on strlen() == and === comparison with integer-range
    simplify
    
    Update TypeSpecifier.php
    
    Update TypeSpecifier.php
    
    remove seemingly dead code
    
    Update strlen-int-range.php
    
    more tests
    
    more tests
    
    test count on int-range
    
    more tests
    
    more assertions
    
    fix negative context
    
    cs
    
    simplify

--- tests/PHPStan/Analyser/nsrt/count-type.php
+++ tests/PHPStan/Analyser/nsrt/count-type.php
@@ -18,4 +18,50 @@ class Foo
 		assertType('int<1, max>', sizeof($nonEmpty));
 	}
 
+	/**
+	 * @param int<3,5> $range
+	 * @param int<0,5> $maybeZero
+	 * @param int<-10,-5> $negative
+	 */
+	public function doFooBar(
+		array $arr,
+		int $range,
+		int $maybeZero,
+		int $negative
+	)
+	{
+		if (count($arr) == $range) {
+			assertType('non-empty-array', $arr);
+		} else {
+			assertType('array', $arr);
+		}
+		if (count($arr) === $range) {
+			assertType('non-empty-array', $arr);
+		} else {
+			assertType('array', $arr);
+		}
+
+		if (count($arr) == $maybeZero) {
+			assertType('array', $arr);
+		} else {
+			assertType('non-empty-array', $arr);
+		}
+		if (count($arr) === $maybeZero) {
+			assertType('array', $arr);
+		} else {
+			assertType('non-empty-array', $arr);
+		}
+
+		if (count($arr) == $negative) {
+			assertType('*NEVER*', $arr);
+		} else {
+			assertType('array', $arr);
+		}
+		if (count($arr) === $negative) {
+			assertType('*NEVER*', $arr);
+		} else {
+			assertType('array', $arr);
+		}
+	}
+
 }

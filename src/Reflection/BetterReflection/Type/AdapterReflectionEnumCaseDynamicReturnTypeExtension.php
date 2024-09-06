ff910cec6a377c25bc99a79c63dabd761ec26dc4
Ondrej Mirtes <ondrej@mirtes.cz>
2024-09-06 13:55:11 +0200

    Fix Adapter ReflectionEnum tests on PHP 7.4

---
+++ src/Reflection/BetterReflection/Type/AdapterReflectionEnumCaseDynamicReturnTypeExtension.php
@@ -0,0 +1,66 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Reflection\BetterReflection\Type;
+
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\BetterReflection\Reflection\Adapter\ReflectionIntersectionType;
+use PHPStan\BetterReflection\Reflection\Adapter\ReflectionNamedType;
+use PHPStan\BetterReflection\Reflection\Adapter\ReflectionUnionType;
+use PHPStan\Php\PhpVersion;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\Constant\ConstantBooleanType;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\NullType;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+use PHPStan\Type\UnionType;
+use function in_array;
+
+class AdapterReflectionEnumCaseDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
+{
+
+	public function __construct(private PhpVersion $phpVersion, private string $class)
+	{
+	}
+
+	public function getClass(): string
+	{
+		return $this->class;
+	}
+
+	public function isMethodSupported(MethodReflection $methodReflection): bool
+	{
+		return in_array($methodReflection->getName(), [
+			'getDocComment',
+			'getType',
+		], true);
+	}
+
+	public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): ?Type
+	{
+		if ($this->phpVersion->getVersionId() >= 80000) {
+			return null;
+		}
+
+		if ($methodReflection->getName() === 'getDocComment') {
+			return new UnionType([
+				new StringType(),
+				new ConstantBooleanType(false),
+			]);
+		}
+
+		if ($methodReflection->getName() === 'getType') {
+			return new UnionType([
+				new ObjectType(ReflectionIntersectionType::class),
+				new ObjectType(ReflectionNamedType::class),
+				new ObjectType(ReflectionUnionType::class),
+				new NullType(),
+			]);
+		}
+
+		return null;
+	}
+
+}
---
+++ src/Reflection/BetterReflection/Type/AdapterReflectionEnumDynamicReturnTypeExtension.php
@@ -0,0 +1,105 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Reflection\BetterReflection\Type;
+
+use PhpParser\Node\Expr\MethodCall;
+use PHPStan\Analyser\Scope;
+use PHPStan\BetterReflection\Reflection\Adapter\ReflectionClass;
+use PHPStan\BetterReflection\Reflection\Adapter\ReflectionClassConstant;
+use PHPStan\BetterReflection\Reflection\Adapter\ReflectionEnum;
+use PHPStan\BetterReflection\Reflection\Adapter\ReflectionNamedType;
+use PHPStan\Php\PhpVersion;
+use PHPStan\Reflection\MethodReflection;
+use PHPStan\Type\Accessory\AccessoryNonEmptyStringType;
+use PHPStan\Type\Constant\ConstantBooleanType;
+use PHPStan\Type\DynamicMethodReturnTypeExtension;
+use PHPStan\Type\IntegerType;
+use PHPStan\Type\IntersectionType;
+use PHPStan\Type\NullType;
+use PHPStan\Type\ObjectType;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+use PHPStan\Type\UnionType;
+use function in_array;
+
+class AdapterReflectionEnumDynamicReturnTypeExtension implements DynamicMethodReturnTypeExtension
+{
+
+	public function __construct(private PhpVersion $phpVersion)
+	{
+	}
+
+	public function getClass(): string
+	{
+		return ReflectionEnum::class;
+	}
+
+	public function isMethodSupported(MethodReflection $methodReflection): bool
+	{
+		return in_array($methodReflection->getName(), [
+			'getFileName',
+			'getStartLine',
+			'getEndLine',
+			'getDocComment',
+			'getReflectionConstant',
+			'getParentClass',
+			'getExtensionName',
+			'getBackingType',
+		], true);
+	}
+
+	public function getTypeFromMethodCall(MethodReflection $methodReflection, MethodCall $methodCall, Scope $scope): ?Type
+	{
+		if ($this->phpVersion->getVersionId() >= 80000) {
+			return null;
+		}
+
+		if (in_array($methodReflection->getName(), ['getFileName', 'getExtensionName'], true)) {
+			return new UnionType([
+				new IntersectionType([
+					new StringType(),
+					new AccessoryNonEmptyStringType(),
+				]),
+				new ConstantBooleanType(false),
+			]);
+		}
+
+		if ($methodReflection->getName() === 'getDocComment') {
+			return new UnionType([
+				new StringType(),
+				new ConstantBooleanType(false),
+			]);
+		}
+
+		if (in_array($methodReflection->getName(), ['getStartLine', 'getEndLine'], true)) {
+			return new UnionType([
+				new IntegerType(),
+				new ConstantBooleanType(false),
+			]);
+		}
+
+		if ($methodReflection->getName() === 'getReflectionConstant') {
+			return new UnionType([
+				new ObjectType(ReflectionClassConstant::class),
+				new ConstantBooleanType(false),
+			]);
+		}
+
+		if ($methodReflection->getName() === 'getParentClass') {
+			return new UnionType([
+				new ObjectType(ReflectionClass::class),
+				new ConstantBooleanType(false),
+			]);
+		}
+
+		if ($methodReflection->getName() === 'getBackingType') {
+			return new UnionType([
+				new ObjectType(ReflectionNamedType::class),
+				new NullType(),
+			]);
+		}
+
+		return null;
+	}
+
+}

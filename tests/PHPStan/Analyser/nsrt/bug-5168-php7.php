e3cebb9ff44c76d65a6aedeb45ffe0f24531337d
Ondrej Mirtes <ondrej@mirtes.cz>
2024-09-10 12:36:56 +0200

    Regression test

---
+++ tests/PHPStan/Analyser/nsrt/bug-5168-php7.php
@@ -0,0 +1,12 @@
+<?php // lint < 8.0
+
+namespace Bug5168Php8;
+
+use function PHPStan\Testing\assertType;
+
+function (float $f): void {
+	define('LARAVEL_START', microtime(true));
+
+	$comment = 'Calculated in ' . microtime(true) - $f;
+	assertType('float', $comment);
+};

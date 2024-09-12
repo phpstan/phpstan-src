7427d1ac8fcf917d6456142eb64bb22f15c17a0e
Michael Voříšek <mvorisek@mvorisek.cz>
2024-09-12 19:41:15 +0200

    Fix late static binding calls for first class callable

--- tests/PHPStan/Analyser/nsrt/static-late-binding.php
+++ tests/PHPStan/Analyser/nsrt/static-late-binding.php
@@ -112,6 +112,15 @@ class B extends A
         assertType('bool', $v);
         $clUnioned->outStaticConst($v);
         assertType('bool', $v); // should be bool|int
+
+        assertType('StaticLateBinding\A', A::retStatic(...)());
+        assertType('StaticLateBinding\B', B::retStatic(...)());
+        assertType('static(StaticLateBinding\B)', self::retStatic()(...));
+        assertType('static(StaticLateBinding\B)', static::retStatic(...)());
+        assertType('static(StaticLateBinding\B)', parent::retStatic(...)());
+        assertType('static(StaticLateBinding\B)', $this->retStatic(...)());
+        assertType('bool', X::retStatic(...)());
+        assertType('bool|StaticLateBinding\A|StaticLateBinding\X', $clUnioned::retStatic(...)()); // should be bool|StaticLateBinding\A
     }
 }
 

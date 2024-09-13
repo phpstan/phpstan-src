51ce513308fb5d50459f0896dd7e6421f4d3be85
Ondrej Mirtes <ondrej@mirtes.cz>
2024-09-13 14:46:46 +0200

    Fix

--- src/Analyser/TypeSpecifier.php
+++ src/Analyser/TypeSpecifier.php
@@ -1405,7 +1405,7 @@ final class TypeSpecifier
 					$assertedType,
 					$assert->isNegated() ? TypeSpecifierContext::createFalse() : TypeSpecifierContext::createTrue(),
 					$scope,
-				)->setRootExpr($containsUnresolvedTemplate || $assert->isEquality() ? $call : null);
+				)->setRootExpr($containsUnresolvedTemplate || $assert->isEquality() ? $call : $assertExpr);
 				$types = $types !== null ? $types->unionWith($newTypes) : $newTypes;
 
 				if (!$context->null() || !$assertedType instanceof ConstantBooleanType) {

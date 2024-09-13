31a526034b1142864480056474f72d7112cd7815
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-09-13 15:55:07 +0200

    Fix tests

--- tests/PHPStan/Analyser/nsrt/lowercase-string-implode.php
+++ tests/PHPStan/Analyser/nsrt/lowercase-string-implode.php
@@ -2,21 +2,7 @@
 
 namespace LowercaseStringImplode;
 
-use function htmlspecialchars;
-use function lcfirst;
 use function PHPStan\Testing\assertType;
-use function strtolower;
-use function strtoupper;
-use function ucfirst;
-
-class ExplodingStrings
-{
-	public function doFoo(string $s): void
-	{
-		assertType('non-empty-list<lowercase-string>', explode($s, 'foo'));
-		assertType('non-empty-list<string>', explode($s, 'FOO'));
-	}
-}
 
 class ImplodingStrings
 {

f2bb4ca9fb645246482db193f2a4138f2b422a93
Ondrej Mirtes <ondrej@mirtes.cz>
2024-09-16 09:20:51 +0200

    Fix prefixing in PHP-Parser

--- compiler/build/scoper.inc.php
+++ compiler/build/scoper.inc.php
@@ -217,6 +217,16 @@ return [
 			return str_replace(sprintf('use %s\\PhpParser;', $prefix), 'use PhpParser;', $content);
 		},
 		function (string $filePath, string $prefix, string $content): string {
+			if (!str_starts_with($filePath, 'vendor/nikic/php-parser/lib')) {
+				return $content;
+			}
+
+			return str_replace([
+				sprintf('\\%s', $prefix),
+				sprintf('\\\\%s', $prefix),
+			], '', $content);
+		},
+		function (string $filePath, string $prefix, string $content): string {
 			if (
 				$filePath !== 'vendor/nette/utils/src/Utils/Strings.php'
 				&& $filePath !== 'vendor/nette/utils/src/Utils/Arrays.php'

9437056907cedf1916a4298d2bbdd2c5ad28d27e
Markus Staab <markus.staab@redaxo.de>
2024-09-16 12:15:36 +0200

    RegexArrayShapeMatcher - Fix matching literal dot character

---
+++ tests/PHPStan/Analyser/nsrt/bug-11699.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types = 1);
+
+namespace Bug11699;
+
+use function PHPStan\Testing\assertType;
+
+function doFoo():void {
+	$string = 'Foo.bar';
+	$match = [];
+	$result = preg_match('~(?<AB>[\~,\?\.])~', $string, $match);
+	if ($result === 1) {
+		assertType("','|'.'|'?'|'~'", $match['AB']);
+	}
+}
+
+function doFoo1():void {
+	$string = 'Foo.bar';
+	$match = [];
+	$result = preg_match('~(?<AB>[\~,\?.])~', $string, $match); // dot in character class does not need to be escaped
+	if ($result === 1) {
+		assertType("','|'.'|'?'|'~'", $match['AB']);
+	}
+}
+
+function doFoo2():void {
+	$string = 'Foo.bar';
+	$match = [];
+	$result = preg_match('~(?<AB>.)~', $string, $match);
+	if ($result === 1) {
+		assertType("non-empty-string", $match['AB']);
+	}
+}
+
+
+function doFoo3():void {
+	$string = 'Foo.bar';
+	$match = [];
+	$result = preg_match('~(?<AB>\.)~', $string, $match);
+	if ($result === 1) {
+		assertType("'.'", $match['AB']);
+	}
+}

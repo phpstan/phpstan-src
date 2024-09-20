971db708d19c28f6542a26c7bfe899e09c1566a8
Jiří Pudil <me@jiripudil.cz>
2024-09-20 15:11:38 +0200

    deduplicate test fixtures

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -207,7 +207,7 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield __DIR__ . '/../Rules/Classes/data/bug-11591-property-tag.php';
 		yield __DIR__ . '/../Rules/Classes/data/mixin-trait-use.php';
 
-		yield __DIR__ . '/data/bug-4801.php';
+		yield __DIR__ . '/../Rules/Methods/data/bug-4801.php';
 		yield __DIR__ . '/data/template-default.php';
 	}
 
--- tests/PHPStan/Rules/Methods/data/bug-4801.php
+++ tests/PHPStan/Rules/Methods/data/bug-4801.php
@@ -2,6 +2,8 @@
 
 namespace Bug4801;
 
+use function PHPStan\Testing\assertType;
+
 /**
  * @template T
  */
@@ -18,5 +20,6 @@ interface I {
  * @param I<string> $i
  */
 function x(I $i) {
-	$i->work(null);
+	assertType('Bug4801\\I<string>', $i->work(null));
+	assertType('Bug4801\\I<int>', $i->work(fn(string $a) => (int) $a));
 }

ee802d682e2a8a0b03cc6bcce25ecf7b25749933
Manuel Huber <info@manuelhu.de>
2024-09-26 13:07:35 +0200

    Reflection `getAttributes` returns `list`

--- tests/PHPStan/Analyser/nsrt/reflectionclass-issue-5511-php8.php
+++ tests/PHPStan/Analyser/nsrt/reflectionclass-issue-5511-php8.php
@@ -36,38 +36,38 @@ function testGetAttributes(
 	$classStr = $reflectionClass->getAttributes($str);
 	$classNonsense = $reflectionClass->getAttributes("some random string");
 
-	assertType('array<ReflectionAttribute<object>>', $classAll);
-	assertType('array<ReflectionAttribute<Issue5511\Abc>>', $classAbc1);
-	assertType('array<ReflectionAttribute<Issue5511\Abc>>', $classAbc2);
-	assertType('array<ReflectionAttribute<Issue5511\Abc>>', $classGCN);
-	assertType('array<ReflectionAttribute<object>>', $classCN);
-	assertType('array<ReflectionAttribute<object>>', $classStr);
-	assertType('array<ReflectionAttribute<*ERROR*>>', $classNonsense);
+	assertType('list<ReflectionAttribute<object>>', $classAll);
+	assertType('list<ReflectionAttribute<Issue5511\Abc>>', $classAbc1);
+	assertType('list<ReflectionAttribute<Issue5511\Abc>>', $classAbc2);
+	assertType('list<ReflectionAttribute<Issue5511\Abc>>', $classGCN);
+	assertType('list<ReflectionAttribute<object>>', $classCN);
+	assertType('list<ReflectionAttribute<object>>', $classStr);
+	assertType('list<ReflectionAttribute<*ERROR*>>', $classNonsense);
 
 	$methodAll = $reflectionMethod->getAttributes();
 	$methodAbc = $reflectionMethod->getAttributes(Abc::class);
-	assertType('array<ReflectionAttribute<object>>', $methodAll);
-	assertType('array<ReflectionAttribute<Issue5511\Abc>>', $methodAbc);
+	assertType('list<ReflectionAttribute<object>>', $methodAll);
+	assertType('list<ReflectionAttribute<Issue5511\Abc>>', $methodAbc);
 
 	$paramAll = $reflectionParameter->getAttributes();
 	$paramAbc = $reflectionParameter->getAttributes(Abc::class);
-	assertType('array<ReflectionAttribute<object>>', $paramAll);
-	assertType('array<ReflectionAttribute<Issue5511\Abc>>', $paramAbc);
+	assertType('list<ReflectionAttribute<object>>', $paramAll);
+	assertType('list<ReflectionAttribute<Issue5511\Abc>>', $paramAbc);
 
 	$propAll = $reflectionProperty->getAttributes();
 	$propAbc = $reflectionProperty->getAttributes(Abc::class);
-	assertType('array<ReflectionAttribute<object>>', $propAll);
-	assertType('array<ReflectionAttribute<Issue5511\Abc>>', $propAbc);
+	assertType('list<ReflectionAttribute<object>>', $propAll);
+	assertType('list<ReflectionAttribute<Issue5511\Abc>>', $propAbc);
 
 	$constAll = $reflectionClassConstant->getAttributes();
 	$constAbc = $reflectionClassConstant->getAttributes(Abc::class);
-	assertType('array<ReflectionAttribute<object>>', $constAll);
-	assertType('array<ReflectionAttribute<Issue5511\Abc>>', $constAbc);
+	assertType('list<ReflectionAttribute<object>>', $constAll);
+	assertType('list<ReflectionAttribute<Issue5511\Abc>>', $constAbc);
 
 	$funcAll = $reflectionFunction->getAttributes();
 	$funcAbc = $reflectionFunction->getAttributes(Abc::class);
-	assertType('array<ReflectionAttribute<object>>', $funcAll);
-	assertType('array<ReflectionAttribute<Issue5511\Abc>>', $funcAbc);
+	assertType('list<ReflectionAttribute<object>>', $funcAll);
+	assertType('list<ReflectionAttribute<Issue5511\Abc>>', $funcAbc);
 }
 
 /**

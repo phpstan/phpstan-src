69c44db876b764ac1d7b88e68bb711b82d27c042
Michael Voříšek <mvorisek@mvorisek.cz>
2024-09-27 13:09:39 +0200

    fix PHP 7.2 lint

--- tests/PHPStan/Analyser/AnalyserIntegrationTest.php
+++ tests/PHPStan/Analyser/AnalyserIntegrationTest.php
@@ -1410,11 +1410,9 @@ class AnalyserIntegrationTest extends PHPStanTestCase
 	{
 		$errors = $this->runAnalyse(__DIR__ . '/data/bug-11292.php');
 		$this->assertCount(1, $errors);
-		$this->assertSame(str_replace('\x7f-\xff|$', "\x7f-\xff]|$", <<<'EOD'
-			Regex pattern cannot be parsed: Unrecognized token "\" at line 1 and column 1:
-			\b(https?://)?(?:([^]\\\x00-\x20\"(),:-<>[\x7f-\xff]{1,64})(:[^]\\\x00-\x20\"(),:-<>[\x7f-\xff]{1,64})?@)?((?:[-a-zA-Z0-9\x7f-\xff]{1,63}\.)+[a-zA-Z\x7f-\xff][-a-zA-Z0-9\x7f-\xff]{1,62})((:[0-9]{1,5})?(/[!$-/0-9:;=@_~':;!a-zA-Z\x7f-\xff]*?)?(\?[!$-/0-9:;=@_':;!a-zA-Z\x7f-\xff]+?)?(#[!$-/0-9?:;=@_':;!a-zA-Z\x7f-\xff]+?)?)(?=[)'?.!,;:]*([^-_#$+.!*%'(),;/?:@~=&a-zA-Z0-9\x7f-\xff|$))
-			↑
-			EOD), $errors[0]->getMessage());
+		$this->assertSame(str_replace('\x7f-\xff|$', "\x7f-\xff]|$", 'Regex pattern cannot be parsed: Unrecognized token "\\" at line 1 and column 1:
+\\b(https?://)?(?:([^]\\\\\\x00-\\x20\\"(),:-<>[\\x7f-\\xff]{1,64})(:[^]\\\\\\x00-\\x20\\"(),:-<>[\\x7f-\\xff]{1,64})?@)?((?:[-a-zA-Z0-9\\x7f-\\xff]{1,63}\\.)+[a-zA-Z\\x7f-\\xff][-a-zA-Z0-9\\x7f-\\xff]{1,62})((:[0-9]{1,5})?(/[!$-/0-9:;=@_~\':;!a-zA-Z\\x7f-\\xff]*?)?(\\?[!$-/0-9:;=@_\':;!a-zA-Z\\x7f-\\xff]+?)?(#[!$-/0-9?:;=@_\':;!a-zA-Z\\x7f-\\xff]+?)?)(?=[)\'?.!,;:]*([^-_#$+.!*%\'(),;/?:@~=&a-zA-Z0-9\\x7f-\\xff|$))
+↑'), $errors[0]->getMessage());
 	}
 
 	public function testBug11297(): void

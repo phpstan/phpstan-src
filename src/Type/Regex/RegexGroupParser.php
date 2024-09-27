103ea0cd313f082320903249ec1e9eb6d39c9afb
Michael Voříšek <mvorisek@mvorisek.cz>
2024-09-28 00:39:19 +0200

    Revert "skip alternation (`|`)  tokens"

--- src/Type/Regex/RegexGroupParser.php
+++ src/Type/Regex/RegexGroupParser.php
@@ -302,10 +302,6 @@ final class RegexGroupParser
 		if ($rootAlternation !== null) {
 			$types = [];
 			foreach ($rootAlternation->getChildren() as $alternative) {
-				if ($alternative->getValueToken() === 'alternation') {
-					continue;
-				}
-
 				$types[] = $this->createGroupType($alternative, $maybeConstant, $patternModifiers);
 			}
 

f0a629685de2202687b9f92bd0e1a516daf2443e
Ondrej Mirtes <ondrej@mirtes.cz>
2024-09-30 12:42:48 +0200

    ReadWritePropertiesExtension - use ExtendedPropertyReflection in parameter type

--- src/Rules/Properties/ReadWritePropertiesExtension.php
+++ src/Rules/Properties/ReadWritePropertiesExtension.php
@@ -2,7 +2,7 @@
 
 namespace PHPStan\Rules\Properties;
 
-use PHPStan\Reflection\PropertyReflection;
+use PHPStan\Reflection\ExtendedPropertyReflection;
 
 /**
  * This is the extension interface to implement if you want to describe
@@ -25,10 +25,10 @@ use PHPStan\Reflection\PropertyReflection;
 interface ReadWritePropertiesExtension
 {
 
-	public function isAlwaysRead(PropertyReflection $property, string $propertyName): bool;
+	public function isAlwaysRead(ExtendedPropertyReflection $property, string $propertyName): bool;
 
-	public function isAlwaysWritten(PropertyReflection $property, string $propertyName): bool;
+	public function isAlwaysWritten(ExtendedPropertyReflection $property, string $propertyName): bool;
 
-	public function isInitialized(PropertyReflection $property, string $propertyName): bool;
+	public function isInitialized(ExtendedPropertyReflection $property, string $propertyName): bool;
 
 }

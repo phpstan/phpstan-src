e336826001f6eafd8b717372c11d2eb0575b85ee
USAMI Kenta <tadsan@zonu.me>
2024-10-01 01:35:32 +0900

    Add ArrayFindFunctionReturnTypeExtension and ArrayFindKeyFunctionReturnTypeExtension

---
+++ tests/PHPStan/Analyser/nsrt/array-find.php
@@ -0,0 +1,43 @@
+<?php // lint >= 8.4
+
+namespace {
+
+	if (!function_exists('array_find')) {
+		function (array $array, callable $callback)
+		{
+			foreach ($array as $key => $value) {
+				if ($callback($value, $key)) {
+					return $value;
+				}
+			}
+
+			return null;
+		}
+	}
+
+}
+
+namespace ArrayFind
+{
+
+	use function PHPStan\Testing\assertType;
+
+	/**
+	 * @param array<mixed> $array
+	 */
+	function testMixed(array $array, callable $callback): void
+	{
+		assertType('mixed', array_find($array, $callback));
+		assertType('mixed', array_find($array, 'is_int'));
+	}
+
+	/**
+	 * @param array{1, 'foo', \DateTime} $array
+	 */
+	function testConstant(array $array, callable $callback): void
+	{
+		assertType("1|'foo'|DateTime|null", array_find($array, $callback));
+		assertType("1|'foo'|DateTime|null", array_find($array, 'is_int'));
+	}
+
+}

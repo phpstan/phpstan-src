933b48743ef363f3a7e8f39ec525e015d43b99ec
Ondrej Mirtes <ondrej@mirtes.cz>
2024-09-30 12:47:27 +0200

    [BCB] Virtual node `PHPStan\Node\ClassMethod` is no longer a node

--- src/Node/ClassMethod.php
+++ src/Node/ClassMethod.php
@@ -2,32 +2,22 @@
 
 namespace PHPStan\Node;
 
-use PhpParser\Node\Stmt\ClassMethod as PhpParserClassMethod;
-
 /**
  * @api
  */
-final class ClassMethod extends PhpParserClassMethod
+final class ClassMethod
 {
 
 	public function __construct(
-		\PhpParser\Node\Stmt\ClassMethod $node,
+		private \PhpParser\Node\Stmt\ClassMethod $node,
 		private bool $isDeclaredInTrait,
 	)
 	{
-		parent::__construct($node->name, [
-			'flags' => $node->flags,
-			'byRef' => $node->byRef,
-			'params' => $node->params,
-			'returnType' => $node->returnType,
-			'stmts' => $node->stmts,
-			'attrGroups' => $node->attrGroups,
-		], $node->attributes);
 	}
 
-	public function getNode(): PhpParserClassMethod
+	public function getNode(): \PhpParser\Node\Stmt\ClassMethod
 	{
-		return $this;
+		return $this->node;
 	}
 
 	public function isDeclaredInTrait(): bool

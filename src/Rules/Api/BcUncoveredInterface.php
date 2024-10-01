cb6ab5544a016c52f931fc390bcdf9c627819d8f
Ondrej Mirtes <ondrej@mirtes.cz>
2024-10-01 10:39:28 +0200

    Even more interfaces that are not supposed to be implemented

--- src/Rules/Api/BcUncoveredInterface.php
+++ src/Rules/Api/BcUncoveredInterface.php
@@ -4,13 +4,20 @@ namespace PHPStan\Rules\Api;
 
 use PHPStan\Analyser\Scope;
 use PHPStan\Command\Output;
+use PHPStan\Command\OutputStyle;
+use PHPStan\DependencyInjection\Container;
+use PHPStan\Node\ReturnStatementsNode;
+use PHPStan\Node\VirtualNode;
+use PHPStan\PhpDoc\Tag\TypedTag;
 use PHPStan\Reflection\Callables\CallableParametersAcceptor;
 use PHPStan\Reflection\ClassConstantReflection;
+use PHPStan\Reflection\ClassMemberAccessAnswerer;
 use PHPStan\Reflection\ClassMemberReflection;
 use PHPStan\Reflection\ConstantReflection;
 use PHPStan\Reflection\ExtendedMethodReflection;
 use PHPStan\Reflection\ExtendedPropertyReflection;
 use PHPStan\Reflection\FunctionReflection;
+use PHPStan\Reflection\NamespaceAnswerer;
 use PHPStan\Reflection\ParameterReflectionWithPhpDocs;
 use PHPStan\Reflection\ParametersAcceptorWithPhpDocs;
 use PHPStan\Reflection\ReflectionProvider;
@@ -21,13 +28,21 @@ use PHPStan\Rules\MetadataRuleError;
 use PHPStan\Rules\NonIgnorableRuleError;
 use PHPStan\Rules\RuleError;
 use PHPStan\Rules\TipRuleError;
+use PHPStan\Type\CompoundType;
+use PHPStan\Type\Generic\TemplateType;
 use PHPStan\Type\Type;
+use PHPStan\Type\TypeWithClassName;
 
 final class BcUncoveredInterface
 {
 
 	public const CLASSES = [
 		Type::class,
+		CompoundType::class,
+		TemplateType::class,
+		TypedTag::class,
+		TypeWithClassName::class,
+		VirtualNode::class,
 		ReflectionProvider::class,
 		Scope::class,
 		FunctionReflection::class,
@@ -47,6 +62,11 @@ final class BcUncoveredInterface
 		ClassMemberReflection::class,
 		ConstantReflection::class,
 		ClassConstantReflection::class,
+		ClassMemberAccessAnswerer::class,
+		NamespaceAnswerer::class,
+		Container::class,
+		OutputStyle::class,
+		ReturnStatementsNode::class,
 	];
 
 }

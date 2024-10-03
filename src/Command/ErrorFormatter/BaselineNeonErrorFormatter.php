c8b7ea9e8f51c8bbc38dfa6b04f9a0172f5cfea0
Ondrej Mirtes <ondrej@mirtes.cz>
2024-10-03 17:11:05 +0200

    Neon baseline - add identifier key

--- src/Command/ErrorFormatter/BaselineNeonErrorFormatter.php
+++ src/Command/ErrorFormatter/BaselineNeonErrorFormatter.php
@@ -9,6 +9,7 @@ use PHPStan\Command\AnalysisResult;
 use PHPStan\Command\Output;
 use PHPStan\File\RelativePathHelper;
 use PHPStan\ShouldNotHappenException;
+use function count;
 use function ksort;
 use function preg_quote;
 use function substr;
@@ -37,29 +38,57 @@ final class BaselineNeonErrorFormatter
 			if (!$fileSpecificError->canBeIgnored()) {
 				continue;
 			}
-			$fileErrors[$this->relativePathHelper->getRelativePath($fileSpecificError->getFilePath())][] = $fileSpecificError->getMessage();
+			$fileErrors[$this->relativePathHelper->getRelativePath($fileSpecificError->getFilePath())][] = $fileSpecificError;
 		}
 		ksort($fileErrors, SORT_STRING);
 
 		$errorsToOutput = [];
-		foreach ($fileErrors as $file => $errorMessages) {
-			$fileErrorsCounts = [];
-			foreach ($errorMessages as $errorMessage) {
-				if (!isset($fileErrorsCounts[$errorMessage])) {
-					$fileErrorsCounts[$errorMessage] = 1;
+		foreach ($fileErrors as $file => $errors) {
+			$fileErrorsByMessage = [];
+			foreach ($errors as $error) {
+				$errorMessage = $error->getMessage();
+				$identifier = $error->getIdentifier();
+				if (!isset($fileErrorsByMessage[$errorMessage])) {
+					$fileErrorsByMessage[$errorMessage] = [
+						1,
+						$identifier !== null ? [$identifier => 1] : [],
+					];
 					continue;
 				}
 
-				$fileErrorsCounts[$errorMessage]++;
-			}
-			ksort($fileErrorsCounts, SORT_STRING);
+				$fileErrorsByMessage[$errorMessage][0]++;
 
-			foreach ($fileErrorsCounts as $message => $count) {
-				$errorsToOutput[] = [
-					'message' => Helpers::escape('#^' . preg_quote($message, '#') . '$#'),
-					'count' => $count,
-					'path' => Helpers::escape($file),
-				];
+				if ($identifier === null) {
+					continue;
+				}
+
+				if (!isset($fileErrorsByMessage[$errorMessage][1][$identifier])) {
+					$fileErrorsByMessage[$errorMessage][1][$identifier] = 1;
+					continue;
+				}
+
+				$fileErrorsByMessage[$errorMessage][1][$identifier]++;
+			}
+			ksort($fileErrorsByMessage, SORT_STRING);
+
+			foreach ($fileErrorsByMessage as $message => [$totalCount, $identifiers]) {
+				ksort($identifiers, SORT_STRING);
+				if (count($identifiers) > 0) {
+					foreach ($identifiers as $identifier => $identifierCount) {
+						$errorsToOutput[] = [
+							'message' => Helpers::escape('#^' . preg_quote($message, '#') . '$#'),
+							'identifier' => $identifier,
+							'count' => $identifierCount,
+							'path' => Helpers::escape($file),
+						];
+					}
+				} else {
+					$errorsToOutput[] = [
+						'message' => Helpers::escape('#^' . preg_quote($message, '#') . '$#'),
+						'count' => $totalCount,
+						'path' => Helpers::escape($file),
+					];
+				}
 			}
 		}
 
--- tests/PHPStan/Command/ErrorFormatter/BaselineNeonErrorFormatterTest.php
+++ tests/PHPStan/Command/ErrorFormatter/BaselineNeonErrorFormatterTest.php
@@ -448,4 +448,137 @@ class BaselineNeonErrorFormatterTest extends ErrorFormatterTestCase
 		Assert::assertNotSame("\n", substr($content, -($expectedNewlinesCount + 1), 1));
 	}
 
+	public function dataFormatErrorsWithIdentifiers(): iterable
+	{
+		yield [
+			[
+				new Error(
+					'Foo',
+					__DIR__ . '/Foo.php',
+					5,
+				),
+				new Error(
+					'Foo',
+					__DIR__ . '/Foo.php',
+					5,
+				),
+				(new Error(
+					'Foo with identifier',
+					__DIR__ . '/Foo.php',
+					5,
+				))->withIdentifier('argument.type'),
+				(new Error(
+					'Foo with identifier',
+					__DIR__ . '/Foo.php',
+					6,
+				))->withIdentifier('argument.type'),
+			],
+			[
+				'parameters' => [
+					'ignoreErrors' => [
+						[
+							'message' => '#^Foo$#',
+							'count' => 2,
+							'path' => 'Foo.php',
+						],
+						[
+							'message' => '#^Foo with identifier$#',
+							'identifier' => 'argument.type',
+							'count' => 2,
+							'path' => 'Foo.php',
+						],
+					],
+				],
+			],
+		];
+
+		yield [
+			[
+				new Error(
+					'Foo',
+					__DIR__ . '/Foo.php',
+					5,
+				),
+				new Error(
+					'Foo',
+					__DIR__ . '/Foo.php',
+					5,
+				),
+				(new Error(
+					'Foo with same message, different identifier',
+					__DIR__ . '/Foo.php',
+					5,
+				))->withIdentifier('argument.type'),
+				(new Error(
+					'Foo with same message, different identifier',
+					__DIR__ . '/Foo.php',
+					6,
+				))->withIdentifier('argument.byRef'),
+				(new Error(
+					'Foo with another message',
+					__DIR__ . '/Foo.php',
+					5,
+				))->withIdentifier('argument.type'),
+			],
+			[
+				'parameters' => [
+					'ignoreErrors' => [
+						[
+							'message' => '#^Foo$#',
+							'count' => 2,
+							'path' => 'Foo.php',
+						],
+						[
+							'message' => '#^Foo with another message$#',
+							'identifier' => 'argument.type',
+							'count' => 1,
+							'path' => 'Foo.php',
+						],
+						[
+							'message' => '#^Foo with same message, different identifier$#',
+							'identifier' => 'argument.byRef',
+							'count' => 1,
+							'path' => 'Foo.php',
+						],
+						[
+							'message' => '#^Foo with same message, different identifier$#',
+							'identifier' => 'argument.type',
+							'count' => 1,
+							'path' => 'Foo.php',
+						],
+					],
+				],
+			],
+		];
+	}
+
+	/**
+	 * @dataProvider dataFormatErrorsWithIdentifiers
+	 * @param list<Error> $errors
+	 * @param mixed[] $expectedOutput
+	 */
+	public function testFormatErrorsWithIdentifiers(array $errors, array $expectedOutput): void
+	{
+		$formatter = new BaselineNeonErrorFormatter(new SimpleRelativePathHelper(__DIR__));
+		$formatter->formatErrors(
+			new AnalysisResult(
+				$errors,
+				[],
+				[],
+				[],
+				[],
+				false,
+				null,
+				true,
+				0,
+				true,
+				[],
+			),
+			$this->getOutput(),
+			'',
+		);
+
+		$this->assertSame($expectedOutput, Neon::decode($this->getOutputContent()));
+	}
+
 }

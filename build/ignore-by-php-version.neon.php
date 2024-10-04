2b19dcfbc9b97ebdbfa3493fc032ceaf187e6a16
Ondrej Mirtes <ondrej@mirtes.cz>
2024-10-04 13:28:08 +0200

    Cleanup

--- build/ignore-by-php-version.neon.php
+++ build/ignore-by-php-version.neon.php
@@ -1,11 +1,6 @@
 <?php declare(strict_types = 1);
 
 $includes = [];
-if (PHP_VERSION_ID < 70300) {
-	$includes[] = __DIR__ . '/baseline-lt-7.3.neon';
-} else {
-	$includes[] = __DIR__ . '/baseline-7.3.neon';
-}
 if (PHP_VERSION_ID >= 80000) {
 	$includes[] = __DIR__ . '/baseline-8.0.neon';
 }
@@ -18,10 +13,6 @@ if (PHP_VERSION_ID >= 80100) {
 
 if (PHP_VERSION_ID >= 70400) {
 	$includes[] = __DIR__ . '/ignore-gte-php7.4-errors.neon';
-}
-
-if (PHP_VERSION_ID < 70400) {
-	$includes[] = __DIR__ . '/enum-adapter-errors.neon';
 }
 
 if (PHP_VERSION_ID < 80000) {

98e2b6e72a22bb2c287aa0b84a011839697c36c5
Markus Staab <markus.staab@redaxo.de>
2024-10-08 17:46:14 +0200

    More precise md5/sha1 return type

--- tests/PHPStan/Rules/Comparison/StrictComparisonOfDifferentTypesRuleTest.php
+++ tests/PHPStan/Rules/Comparison/StrictComparisonOfDifferentTypesRuleTest.php
@@ -1121,4 +1121,24 @@ class StrictComparisonOfDifferentTypesRuleTest extends RuleTestCase
 		$this->analyse([__DIR__ . '/data/bug-10493.php'], []);
 	}
 
+	public function testHashing(): void
+	{
+		$this->checkAlwaysTrueStrictComparison = true;
+		$this->analyse([__DIR__ . '/data/hashing.php'], [
+			[
+				"Strict comparison using === between lowercase-string&non-falsy-string and 'ABC' will always evaluate to false.",
+				9,
+			],
+			[
+				"Strict comparison using === between (lowercase-string&non-falsy-string)|false and 'ABC' will always evaluate to false.",
+				12,
+			],
+			[
+				"Strict comparison using === between (lowercase-string&non-falsy-string)|(non-falsy-string&numeric-string) and 'A' will always evaluate to false.",
+				31,
+				'Because the type is coming from a PHPDoc, you can turn off this check by setting <fg=cyan>treatPhpDocTypesAsCertain: false</> in your <fg=cyan>%configurationFile%</>.',
+			],
+		]);
+	}
+
 }

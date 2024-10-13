70f319a25cc75cbb22288674108cf3212b6fe735
Ondrej Mirtes <ondrej@mirtes.cz>
2024-10-13 17:01:59 +0200

    StubParser

---
+++ src/Parser/StubParser.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Parser;
+
+use PhpParser\ErrorHandler\Collecting;
+use PhpParser\Node;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitor\NameResolver;
+use PHPStan\File\FileReader;
+use PHPStan\ShouldNotHappenException;
+
+final class StubParser implements Parser
+{
+
+	public function __construct(
+		private \PhpParser\Parser $parser,
+		private NameResolver $nameResolver,
+	)
+	{
+	}
+
+	/**
+	 * @param string $file path to a file to parse
+	 * @return Node\Stmt[]
+	 */
+	public function parseFile(string $file): array
+	{
+		try {
+			return $this->parseString(FileReader::read($file));
+		} catch (ParserErrorsException $e) {
+			throw new ParserErrorsException($e->getErrors(), $file);
+		}
+	}
+
+	/**
+	 * @return Node\Stmt[]
+	 */
+	public function parseString(string $sourceCode): array
+	{
+		$errorHandler = new Collecting();
+		$nodes = $this->parser->parse($sourceCode, $errorHandler);
+		if ($errorHandler->hasErrors()) {
+			throw new ParserErrorsException($errorHandler->getErrors(), null);
+		}
+		if ($nodes === null) {
+			throw new ShouldNotHappenException();
+		}
+
+		$nodeTraverser = new NodeTraverser();
+		$nodeTraverser->addVisitor($this->nameResolver);
+
+		/** @var array<Node\Stmt> */
+		return $nodeTraverser->traverse($nodes);
+	}
+
+}

c7672652d9783746a1417f57b5463ffb8bb0d56d
Markus Staab <markus.staab@redaxo.de>
2024-11-04 16:45:12 +0100

    add another failling test

--- tests/PHPStan/Analyser/nsrt/bug-11561.php
+++ tests/PHPStan/Analyser/nsrt/bug-11561.php
@@ -57,3 +57,24 @@ function main3(mixed $c): void{
 
 	assertType("array{date: DateTime, id: 1, name: 'ruud'|'staabm'}", $c);
 }
+
+/** @param array{date: DateTime} $c */
+function main4(mixed $c): void{
+	assertType('array{date: DateTime}', $c);
+	$c['id']=1;
+	$c['name'] = 'staabm';
+	assertType("array{date: DateTime, id: 1, name: 'staabm'}", $c);
+
+	$x = (function() use (&$c) {
+		assertType("array{date: DateTime, id: 1, name: 'staabm'}", $c);
+		if (rand(0,1)) {
+			$c['name'] = 'ruud';
+			assertType("array{date: DateTime, id: 1, name: 'ruud'}", $c);
+			return 'y';
+		}
+		assertType("array{date: DateTime, id: 1, name: 'staabm'}", $c);
+		return 'x';
+	})();
+
+	assertType("array{date: DateTime, id: 1, name: 'ruud'|'staabm'}", $c);
+}

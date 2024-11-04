2cc91b2f6ab328801d5eaad81b2ba06996a9f356
Ruud Kamphuis <ruudk@users.noreply.github.com>
2024-11-04 09:39:15 +0100

    Add `@api` to TypeExpr
    See https://github.com/phpstan/phpstan/discussions/11960

--- src/Node/Expr/TypeExpr.php
+++ src/Node/Expr/TypeExpr.php
@@ -6,9 +6,13 @@ use PhpParser\Node\Expr;
 use PHPStan\Node\VirtualNode;
 use PHPStan\Type\Type;
 
+/**
+ * @api
+ */
 final class TypeExpr extends Expr implements VirtualNode
 {
 
+	/** @api */
 	public function __construct(private Type $exprType)
 	{
 		parent::__construct();

104d3fb9560997c490475e9762d70c3725101684
USAMI Kenta <tadsan@zonu.me>
2024-11-06 04:58:40 +0900

    Add tests for array_find() and array_find_key() to CallToFunctionParametersRuleTest

--- tests/PHPStan/Rules/Functions/CallToFunctionParametersRuleTest.php
+++ tests/PHPStan/Rules/Functions/CallToFunctionParametersRuleTest.php
@@ -886,6 +886,58 @@ class CallToFunctionParametersRuleTest extends RuleTestCase
 		$this->analyse([__DIR__ . '/data/array_filter_callback.php'], $errors);
 	}
 
+	public function testArrayFindCallback(): void
+	{
+		$this->analyse([__DIR__ . '/data/array_find.php'], [
+			[
+				'Parameter #2 $callback of function array_find expects callable(1|2, \'bar\'|\'foo\'): bool, Closure(string, int): bool given.',
+				22,
+			],
+			[
+				'Parameter #2 $callback of function array_find expects callable(1|2, \'bar\'|\'foo\'): bool, Closure(string, int): bool given.',
+				30,
+			],
+			[
+				'Parameter #2 $callback of function array_find expects callable(1|2, \'bar\'|\'foo\'): bool, Closure(int, string): (\'bar\'|\'foo\') given.',
+				36,
+			],
+			[
+				'Parameter #2 $callback of function array_find expects callable(mixed, int|string): bool, Closure(string, array): false given.',
+				49,
+			],
+			[
+				'Parameter #2 $callback of function array_find expects callable(mixed, int|string): bool, Closure(string, int): array{} given.',
+				52,
+			],
+		]);
+	}
+
+	public function testArrayFindKeyCallback(): void
+	{
+		$this->analyse([__DIR__ . '/data/array_find_key.php'], [
+			[
+				'Parameter #2 $callback of function array_find_key expects callable(1|2, \'bar\'|\'foo\'): bool, Closure(string, int): bool given.',
+				22,
+			],
+			[
+				'Parameter #2 $callback of function array_find_key expects callable(1|2, \'bar\'|\'foo\'): bool, Closure(string, int): bool given.',
+				30,
+			],
+			[
+				'Parameter #2 $callback of function array_find_key expects callable(1|2, \'bar\'|\'foo\'): bool, Closure(int, string): (\'bar\'|\'foo\') given.',
+				36,
+			],
+			[
+				'Parameter #2 $callback of function array_find_key expects callable(mixed, int|string): bool, Closure(string, array): false given.',
+				49,
+			],
+			[
+				'Parameter #2 $callback of function array_find_key expects callable(mixed, int|string): bool, Closure(string, int): array{} given.',
+				52,
+			],
+		]);
+	}
+
 	public function testBug5356(): void
 	{
 		$this->analyse([__DIR__ . '/data/bug-5356.php'], [
---
+++ tests/PHPStan/Rules/Functions/data/array_find_key.php
@@ -0,0 +1,53 @@
+<?php declare(strict_types = 1); // lint >= 8.4
+
+// ok
+array_find_key(
+	['foo' => 1, 'bar' => 2],
+	function($value, $key) {
+		return $key === 0;
+	}
+);
+
+// ok
+array_find_key(
+	['foo' => 1, 'bar' => 2],
+	function(int $value, string $key) {
+		return $key === 0;
+	}
+);
+
+// bad parameters
+array_find_key(
+	['foo' => 1, 'bar' => 2],
+	function(string $value, int $key): bool {
+		return $key === 0;
+	}
+);
+
+// bad parameters
+array_find_key(
+	['foo' => 1, 'bar' => 2],
+	fn (string $item, int $key) => $key === 0,
+);
+
+// bad return type
+array_find_key(
+	['foo' => 1, 'bar' => 2],
+	function(int $value, string $key) {
+		return $key;
+	},
+);
+
+if (is_array($array)) {
+	// ok
+	array_find_key($array, fn ($value, $key) => $key === 0);
+
+	// ok
+	array_find_key($array, fn (string $value, int $key) => $key === 0);
+
+	// bad parameters
+	array_find_key($array, fn (string $item, array $key) => $key === 0);
+
+	// bad return type
+	array_find_key($array, fn (string $value, int $key): array => []);
+}

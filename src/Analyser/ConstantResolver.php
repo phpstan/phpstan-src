45e3dd5b3f9407fea5d073098555fc59425fc8b9
Stephen Sigwart <ssigwart@gmail.com>
2024-11-07 22:13:30 -0500

    PHP 7.4 fix

--- src/Analyser/ConstantResolver.php
+++ src/Analyser/ConstantResolver.php
@@ -412,8 +412,8 @@ final class ConstantResolver
 		if ($constantType->isConstantValue()->yes()) {
 			if (array_key_exists($constantName, $this->dynamicConstantNames)) {
 				$phpdocTypes = $this->dynamicConstantNames[$constantName];
-				$typeStringResolver = $this->container?->getByType(TypeStringResolver::class);
-				if ($typeStringResolver !== null) {
+				if ($this->container !== null) {
+					$typeStringResolver = $this->container->getByType(TypeStringResolver::class);
 					return $typeStringResolver->resolve($phpdocTypes, new NameScope(null, [], null));
 				}
 			}
@@ -435,8 +435,8 @@ final class ConstantResolver
 
 			if ($constantType->isConstantValue()->yes()) {
 				$phpdocTypes = $this->dynamicConstantNames[$lookupConstantName];
-				$typeStringResolver = $this->container?->getByType(TypeStringResolver::class);
-				if ($typeStringResolver !== null) {
+				if ($this->container !== null) {
+					$typeStringResolver = $this->container->getByType(TypeStringResolver::class);
 					return $typeStringResolver->resolve($phpdocTypes, new NameScope(null, [], $className));
 				}
 			}

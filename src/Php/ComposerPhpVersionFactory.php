934227923d5a9bb425f0692d2adef9cc5e0c6340
Markus Staab <markus.staab@redaxo.de>
2024-11-12 16:42:52 +0100

    Refactor ComposerPhpVersionFactory

--- src/Php/ComposerPhpVersionFactory.php
+++ src/Php/ComposerPhpVersionFactory.php
@@ -3,16 +3,12 @@
 namespace PHPStan\Php;
 
 use Composer\Semver\VersionParser;
-use Nette\Utils\Json;
-use Nette\Utils\JsonException;
 use Nette\Utils\Strings;
-use PHPStan\File\CouldNotReadFileException;
-use PHPStan\File\FileReader;
+use PHPStan\Internal\ComposerHelper;
 use PHPStan\ShouldNotHappenException;
 use function count;
 use function end;
 use function is_array;
-use function is_file;
 use function is_int;
 use function is_string;
 use function min;
@@ -114,21 +110,18 @@ final class ComposerPhpVersionFactory
 	private function getComposerRequireVersion(): ?string
 	{
 		$composerPhpVersion = null;
+
 		if (count($this->composerAutoloaderProjectPaths) > 0) {
-			$composerJsonPath = end($this->composerAutoloaderProjectPaths) . '/composer.json';
-			if (is_file($composerJsonPath)) {
-				try {
-					$composerJsonContents = FileReader::read($composerJsonPath);
-					$composer = Json::decode($composerJsonContents, Json::FORCE_ARRAY);
-					$requiredVersion = $composer['require']['php'] ?? null;
-					if (is_string($requiredVersion)) {
-						$composerPhpVersion = $requiredVersion;
-					}
-				} catch (CouldNotReadFileException | JsonException) {
-					// pass
+			$composer = ComposerHelper::getComposerConfig(end($this->composerAutoloaderProjectPaths));
+			if ($composer !== null) {
+				$requiredVersion = $composer['require']['php'] ?? null;
+
+				if (is_string($requiredVersion)) {
+					$composerPhpVersion = $requiredVersion;
 				}
 			}
 		}
+
 		return $composerPhpVersion;
 	}
 

0a4c9d26b9737ee8081321adf82b53db6e5ab07d
Vincent Langlet <vincentlanglet@hotmail.fr>
2024-12-23 14:54:27 +0100

    Add tests

--- tests/PHPStan/Analyser/nsrt/bug-12312.php
+++ tests/PHPStan/Analyser/nsrt/bug-12312.php
@@ -18,6 +18,39 @@ class HelloWorld
 	}
 
 	/**
+	 * @param lowercase-string $s
+	 */
+	public function sayLowercase2(string $s): void
+	{
+		if ('' != $s) {
+			assertType('lowercase-string&non-empty-string', $s);
+		}
+		assertType('lowercase-string', $s);
+	}
+
+	/**
+	 * @param lowercase-string&non-empty-string $s
+	 */
+	public function sayLowercase3(string $s): void
+	{
+		if ($s != '0') {
+			assertType('lowercase-string&non-falsy-string', $s);
+		}
+		assertType('lowercase-string&non-empty-string', $s);
+	}
+
+	/**
+	 * @param lowercase-string&non-empty-string $s
+	 */
+	public function sayLowercase4(string $s): void
+	{
+		if ('0' != $s) {
+			assertType('lowercase-string&non-falsy-string', $s);
+		}
+		assertType('lowercase-string&non-empty-string', $s);
+	}
+
+	/**
 	 * @param uppercase-string $s
 	 */
 	public function sayUppercase(string $s): void
@@ -29,6 +62,39 @@ class HelloWorld
 	}
 
 	/**
+	 * @param uppercase-string $s
+	 */
+	public function sayUppercase2(string $s): void
+	{
+		if ('' != $s) {
+			assertType('non-empty-string&uppercase-string', $s);
+		}
+		assertType('uppercase-string', $s);
+	}
+
+	/**
+	 * @param uppercase-string&non-empty-string $s
+	 */
+	public function sayUppercase3(string $s): void
+	{
+		if ($s != '0') {
+			assertType('non-falsy-string&uppercase-string', $s);
+		}
+		assertType('non-empty-string&uppercase-string', $s);
+	}
+
+	/**
+	 * @param uppercase-string&non-empty-string $s
+	 */
+	public function sayUppercase4(string $s): void
+	{
+		if ('0' != $s) {
+			assertType('non-falsy-string&uppercase-string', $s);
+		}
+		assertType('non-empty-string&uppercase-string', $s);
+	}
+
+	/**
 	 * @param lowercase-string&uppercase-string $s
 	 */
 	public function sayBoth(string $s): void
@@ -37,5 +103,38 @@ class HelloWorld
 			assertType('lowercase-string&non-empty-string&uppercase-string', $s);
 		}
 		assertType('lowercase-string&uppercase-string', $s);
+	}
+
+	/**
+	 * @param lowercase-string&uppercase-string $s
+	 */
+	public function sayBoth2(string $s): void
+	{
+		if ('' != $s) {
+			assertType('lowercase-string&non-empty-string&uppercase-string', $s);
+		}
+		assertType('lowercase-string&uppercase-string', $s);
+	}
+
+	/**
+	 * @param lowercase-string&uppercase-string&non-empty-string $s
+	 */
+	public function sayBoth3(string $s): void
+	{
+		if ($s != '0') {
+			assertType('lowercase-string&non-falsy-string&uppercase-string', $s);
+		}
+		assertType('lowercase-string&non-empty-string&uppercase-string', $s);
+	}
+
+	/**
+	 * @param lowercase-string&uppercase-string&non-empty-string $s
+	 */
+	public function sayBoth4(string $s): void
+	{
+		if ('0' != $s) {
+			assertType('lowercase-string&non-falsy-string&uppercase-string', $s);
+		}
+		assertType('lowercase-string&non-empty-string&uppercase-string', $s);
 	}
 }

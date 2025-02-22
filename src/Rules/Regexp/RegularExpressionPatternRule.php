5cd62c8b79f944f47aad29dbdb1745ab5a3c9bff
Markus Staab <maggus.staab@googlemail.com>
2025-02-22 14:43:31 +0100

    cs

--- src/Rules/Regexp/RegularExpressionPatternRule.php
+++ src/Rules/Regexp/RegularExpressionPatternRule.php
@@ -15,7 +15,7 @@ use function in_array;
 use function sprintf;
 use function str_contains;
 use function str_starts_with;
-use function strrpos;
+use function strpos;
 use function strtolower;
 use function substr;
 

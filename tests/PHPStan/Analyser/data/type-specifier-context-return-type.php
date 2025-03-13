877cbf67884a01eb763f80df908eb36e2a42808f
Markus Staab <markus.staab@redaxo.de>
2025-03-13 15:02:04 +0100

    Update type-specifier-context-return-type.php

--- tests/PHPStan/Analyser/data/type-specifier-context-return-type.php
+++ tests/PHPStan/Analyser/data/type-specifier-context-return-type.php
@@ -7,9 +7,19 @@ use function PHPStan\Testing\assertType;
 class ContextReturnType {
 	public function returnsInt(int $specifiedContextReturnType): int {}
 
-	public function doFoo(int $i) {
+	public function doFooLeft(int $i) {
 		assertType('int', $i);
 		if ($this->returnsInt($i) > 0) {
+			assertType('int<1, max>', $i);
+		} else {
+			assertType('int<min, 0>', $i);
+		}
+		assertType('int', $i);
+	}
+
+	public function doFooRight(int $i) {
+		assertType('int', $i);
+		if (0 < $this->returnsInt($i)) {
 			assertType('int<1, max>', $i);
 		} else {
 			assertType('int<min, 0>', $i);

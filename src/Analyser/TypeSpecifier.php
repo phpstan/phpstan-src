c7174a80ef6baaad01030e1414e885720ff0b3d4
Markus Staab <markus.staab@redaxo.de>
2025-03-20 11:46:08 +0100

    zero or more

--- src/Analyser/TypeSpecifier.php
+++ src/Analyser/TypeSpecifier.php
@@ -1063,12 +1063,12 @@ final class TypeSpecifier
 
 		$isConstantArray = $type->isConstantArray();
 		$isList = $type->isList();
-		$positiveInt = IntegerRangeType::fromInterval(1, null);
+		$zeroOrMore = IntegerRangeType::fromInterval(0, null);
 		if (
 			!$isNormalCount->yes()
 			|| (!$isConstantArray->yes() && !$isList->yes())
 			|| $type->isIterableAtLeastOnce()->no() // array{} cannot be used for further narrowing
-			|| !$positiveInt->isSuperTypeOf($sizeType)->yes()
+			|| !$zeroOrMore->isSuperTypeOf($sizeType)->yes()
 		) {
 			return null;
 		}

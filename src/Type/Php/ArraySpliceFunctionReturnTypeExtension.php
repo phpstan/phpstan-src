0405a2c534afa074ef9afeeabb96548fbcbdb250
Martin Herndl <martin@herndl.org>
2025-04-18 16:46:40 +0200

    Improve return type of `array_splice()`

--- src/Type/Php/ArraySpliceFunctionReturnTypeExtension.php
+++ src/Type/Php/ArraySpliceFunctionReturnTypeExtension.php
@@ -4,13 +4,21 @@ namespace PHPStan\Type\Php;
 
 use PhpParser\Node\Expr\FuncCall;
 use PHPStan\Analyser\Scope;
+use PHPStan\Php\PhpVersion;
 use PHPStan\Reflection\FunctionReflection;
-use PHPStan\Type\ArrayType;
+use PHPStan\TrinaryLogic;
 use PHPStan\Type\DynamicFunctionReturnTypeExtension;
+use PHPStan\Type\NeverType;
+use PHPStan\Type\NullType;
 use PHPStan\Type\Type;
+use function count;
 
 final class ArraySpliceFunctionReturnTypeExtension implements DynamicFunctionReturnTypeExtension
 {
+
+	public function __construct(private PhpVersion $phpVersion)
+	{
+	}
 
 	public function isFunctionSupported(FunctionReflection $functionReflection): bool
 	{
@@ -23,13 +31,20 @@ final class ArraySpliceFunctionReturnTypeExtension implements DynamicFunctionRet
 		Scope $scope,
 	): ?Type
 	{
-		if (!isset($functionCall->getArgs()[0])) {
+		$args = $functionCall->getArgs();
+		if (count($args) < 2) {
 			return null;
 		}
 
-		$arrayArg = $scope->getType($functionCall->getArgs()[0]->value);
+		$arrayType = $scope->getType($args[0]->value);
+		if ($arrayType->isArray()->no()) {
+			return $this->phpVersion->arrayFunctionsReturnNullWithNonArray() ? new NullType() : new NeverType();
+		}
 
-		return new ArrayType($arrayArg->getIterableKeyType(), $arrayArg->getIterableValueType());
+		$offsetType = $scope->getType($args[1]->value);
+		$lengthType = isset($args[2]) ? $scope->getType($args[2]->value) : new NullType();
+
+		return $arrayType->sliceArray($offsetType, $lengthType, TrinaryLogic::createNo());
 	}
 
 }
--- tests/PHPStan/Analyser/nsrt/bug-5017.php
+++ tests/PHPStan/Analyser/nsrt/bug-5017.php
@@ -15,7 +15,7 @@ class Foo
 			assertType('non-empty-array<0|1|2|3|4, 0|1|2|3|4>', $items);
 			$batch = array_splice($items, 0, 2);
 			assertType('array<0|1|2|3|4, 0|1|2|3|4>', $items);
-			assertType('array<0|1|2|3|4, 0|1|2|3|4>', $batch);
+			assertType('non-empty-array<0|1|2|3|4, 0|1|2|3|4>', $batch);
 		}
 	}
 
@@ -28,7 +28,7 @@ class Foo
 			assertType('non-empty-array<int>', $items);
 			$batch = array_splice($items, 0, 2);
 			assertType('array<int>', $items);
-			assertType('array<int>', $batch);
+			assertType('non-empty-array<int>', $batch);
 		}
 	}
 
@@ -38,7 +38,7 @@ class Foo
 		assertType('array{0, 1, 2, 3, 4}', $items);
 		$batch = array_splice($items, 0, 2);
 		assertType('array<0|1|2|3|4, 0|1|2|3|4>', $items);
-		assertType('array<0|1|2|3|4, 0|1|2|3|4>', $batch);
+		assertType('array{0, 1}', $batch);
 	}
 
 	/**

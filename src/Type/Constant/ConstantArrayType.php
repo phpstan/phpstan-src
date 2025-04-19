bec0bba846d2d4a4703b6972dfc507b7b7f563ff
Martin Herndl <martin@herndl.org>
2025-04-19 20:59:05 +0200

    Make constant array degradation to general array more DRY

--- src/Type/Constant/ConstantArrayType.php
+++ src/Type/Constant/ConstantArrayType.php
@@ -913,10 +913,7 @@ class ConstantArrayType implements Type
 
 	public function shuffleArray(): Type
 	{
-		$builder = ConstantArrayTypeBuilder::createFromConstantArray($this->getValuesArray());
-		$builder->degradeToGeneralArray();
-
-		return $builder->getArray();
+		return $this->getValuesArray()->degradeToGeneralArray();
 	}
 
 	public function sliceArray(Type $offsetType, Type $lengthType, TrinaryLogic $preserveKeys): Type
@@ -937,10 +934,7 @@ class ConstantArrayType implements Type
 		}
 
 		if ($offset === null || $length === null) {
-			$builder = ConstantArrayTypeBuilder::createFromConstantArray($this);
-			$builder->degradeToGeneralArray();
-
-			return $builder->getArray()
+			return $this->degradeToGeneralArray()
 				->sliceArray($offsetType, $lengthType, $preserveKeys);
 		}
 
@@ -1250,6 +1244,14 @@ class ConstantArrayType implements Type
 		}
 
 		return new self($this->keyTypes, $valueTypes, $this->nextAutoIndexes, $this->optionalKeys, $this->isList);
+	}
+
+	private function degradeToGeneralArray(): Type
+	{
+		$builder = ConstantArrayTypeBuilder::createFromConstantArray($this);
+		$builder->degradeToGeneralArray();
+
+		return $builder->getArray();
 	}
 
 	public function getKeysArray(): self

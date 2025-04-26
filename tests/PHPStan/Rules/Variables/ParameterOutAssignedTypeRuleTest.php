dda3a817586e722b66008bc09ba6b47fd1f72c5e
Markus Staab <maggus.staab@googlemail.com>
2025-04-26 10:18:11 +0200

    cs

--- tests/PHPStan/Rules/Variables/ParameterOutAssignedTypeRuleTest.php
+++ tests/PHPStan/Rules/Variables/ParameterOutAssignedTypeRuleTest.php
@@ -5,6 +5,7 @@ namespace PHPStan\Rules\Variables;
 use PHPStan\Rules\Rule as TRule;
 use PHPStan\Rules\RuleLevelHelper;
 use PHPStan\Testing\RuleTestCase;
+use const PHP_VERSION_ID;
 
 /**
  * @extends RuleTestCase<ParameterOutAssignedTypeRule>

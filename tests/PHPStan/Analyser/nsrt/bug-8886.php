ac4cf733fb6e7909db2b1b7bfcb06427ac0fbcd1
Michael Bolli <michael@bolli.us>
2025-04-30 12:14:07 +0200

    PHP7.4 doesn't have `PDOStatement::getIterator()`

--- tests/PHPStan/Analyser/nsrt/bug-8886.php
+++ tests/PHPStan/Analyser/nsrt/bug-8886.php
@@ -8,5 +8,12 @@ use function PHPStan\Testing\assertType;
 function testPDOStatementGetIterator(): void {
     $pdo = new PDO('sqlite::memory:');
     $stmt = $pdo->query('SELECT 1');
-    assertType('Iterator', $stmt->getIterator());
+
+    if (PHP_VERSION_ID >= 80000) {
+        // PHP 8 and above
+        assertType('Iterator', $stmt->getIterator());
+    } else {
+        // Fallback for PHP 7.4
+        assertType('Traversable', $stmt);
+    }
 }

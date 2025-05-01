016d44ae65083cb93dd200de9b7e4942ed8c5d69
Markus Staab <maggus.staab@googlemail.com>
2025-05-01 14:03:58 +0200

    Update bug-12393b.php

--- tests/PHPStan/Analyser/nsrt/bug-12393b.php
+++ tests/PHPStan/Analyser/nsrt/bug-12393b.php
@@ -686,6 +686,7 @@ class StringableFoo {
         assertType('*NEVER*', $this->foo);
     }
 
+    // https://3v4l.org/nelJF#v8.4.6
     public function doFoo3(\BcMath\Number $foo): void {
         $this->foo = $foo;
         assertType('non-empty-string&numeric-string', $this->foo);

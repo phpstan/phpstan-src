95186fec15566f005f8d57e570e70d5de787a485
Markus Staab <maggus.staab@googlemail.com>
2025-05-01 14:06:25 +0200

    test stringable in strict-types

--- tests/PHPStan/Analyser/nsrt/bug-12393.php
+++ tests/PHPStan/Analyser/nsrt/bug-12393.php
@@ -163,3 +163,28 @@ class CallableArray {
 		assertType('array', $this->foo); // could be non-empty-array
 	}
 }
+
+class StringableFoo {
+	private string $foo;
+
+	// https://3v4l.org/DQSgA#v8.4.6
+	public function doFoo(StringableFoo $foo): void {
+		$this->foo = $foo;
+		assertType('*NEVER*', $this->foo);
+	}
+
+	public function doFoo2(NotStringable $foo): void {
+		$this->foo = $foo;
+		assertType('*NEVER*', $this->foo);
+	}
+
+	// https://3v4l.org/2SPPj#v8.4.6
+	public function doFoo3(\BcMath\Number $foo): void {
+		$this->foo = $foo;
+		assertType('*NEVER*', $this->foo);
+	}
+
+	public function __toString(): string {
+		return 'Foo';
+	}
+}

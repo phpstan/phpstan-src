61e0eea85605f71095efb997b34dc54bbf6ad638
Markus Staab <maggus.staab@googlemail.com>
2025-05-01 09:21:48 +0200

    added strict-type test-variants for callable array/string

--- tests/PHPStan/Analyser/nsrt/bug-12393.php
+++ tests/PHPStan/Analyser/nsrt/bug-12393.php
@@ -143,3 +143,23 @@ class EntryPointLookup
 	}
 
 }
+
+// https://3v4l.org/LK6Rh
+class CallableString {
+	private string $foo;
+
+	public function doFoo(callable $foo): void {
+		$this->foo = $foo; // PHPStorm wrongly reports an error on this line
+		assertType('callable-string|non-empty-string', $this->foo);
+	}
+}
+
+// https://3v4l.org/WJ8NW
+class CallableArray {
+	private array $foo;
+
+	public function doFoo(callable $foo): void {
+		$this->foo = $foo;
+		assertType('array', $this->foo); // could be non-empty-array
+	}
+}

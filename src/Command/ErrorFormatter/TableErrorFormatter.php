59a003a4bf84b319d94fd2a4ff43a5870c316923
Felix Bernhard <mail@felix-bernhard.com>
2025-05-05 10:44:45 +0200

    use one instead of two spaces

--- src/Command/ErrorFormatter/TableErrorFormatter.php
+++ src/Command/ErrorFormatter/TableErrorFormatter.php
@@ -83,7 +83,7 @@ final class TableErrorFormatter implements ErrorFormatter
 				$filePath = $error->getTraitFilePath() ?? $error->getFilePath();
 				if ($error->getIdentifier() !== null && $error->canBeIgnored()) {
 					$message .= "\n";
-					$message .= '🪪  ' . $error->getIdentifier();
+					$message .= '🪪 ' . $error->getIdentifier();
 				}
 				if ($error->getTip() !== null) {
 					$tip = $error->getTip();
@@ -93,11 +93,11 @@ final class TableErrorFormatter implements ErrorFormatter
 					if (str_contains($tip, "\n")) {
 						$lines = explode("\n", $tip);
 						foreach ($lines as $line) {
-							$message .= '💡  ' . ltrim($line, ' •') . "\n";
+							$message .= '💡 ' . ltrim($line, ' •') . "\n";
 						}
 						$message = rtrim($message, "\n");
 					} else {
-						$message .= '💡  ' . $tip;
+						$message .= '💡 ' . $tip;
 					}
 				}
 				if (is_string($this->editorUrl)) {
@@ -117,7 +117,7 @@ final class TableErrorFormatter implements ErrorFormatter
 						$title = $this->relativePathHelper->getRelativePath($filePath);
 					}
 
-					$message .= "\n✏️  <href=" . OutputFormatter::escape($url) . '>' . $title . '</>';
+					$message .= "\n✏️ <href=" . OutputFormatter::escape($url) . '>' . $title . '</>';
 				}
 				$rows[] = [
 					$this->formatLineNumber($error->getLine()),

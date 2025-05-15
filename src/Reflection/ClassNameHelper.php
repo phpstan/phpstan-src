746944525d0f9e34f1cf548f4119eba7da1a39e5
Markus Staab <markus.staab@redaxo.de>
2025-05-15 09:12:27 +0200

    Faster ClassNameHelper

--- src/Reflection/ClassNameHelper.php
+++ src/Reflection/ClassNameHelper.php
@@ -3,15 +3,23 @@
 namespace PHPStan\Reflection;
 
 use Nette\Utils\Strings;
+use function array_key_exists;
 use function ltrim;
 
 final class ClassNameHelper
 {
 
+	/** @var array<string, bool> */
+	private static array $checked = [];
+
 	public static function isValidClassName(string $name): bool
 	{
-		// from https://stackoverflow.com/questions/3195614/validate-class-method-names-with-regex#comment104531582_12011255
-		return Strings::match(ltrim($name, '\\'), '/^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*(\\\\[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*)*$/') !== null;
+		if (!array_key_exists($name, self::$checked)) {
+			// from https://stackoverflow.com/questions/3195614/validate-class-method-names-with-regex#comment104531582_12011255
+			self::$checked[$name] = Strings::match(ltrim($name, '\\'), '/^[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*(\\\\[a-zA-Z_\x80-\xff][a-zA-Z0-9_\x80-\xff]*)*$/') !== null;
+
+		}
+		return self::$checked[$name];
 	}
 
 }

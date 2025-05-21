5369a992b1a4c04296dd78b1127878cdf27e7877
Markus Staab <markus.staab@redaxo.de>
2025-05-21 10:06:28 +0200

    Reproduce autoloader bug

---
+++ e2e/bug-12972b/real-world.php
@@ -0,0 +1,5 @@
+<?php
+
+require 'autoloader.php';
+
+new \Foo12972\MyRoot();
---
+++ e2e/bug-12972b/src/folder/file2.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Foo12972;
+
+use other12972\MyClass;
+
+class MyRoot {
+	function doBar(MyClass $myClass):void {}
+}

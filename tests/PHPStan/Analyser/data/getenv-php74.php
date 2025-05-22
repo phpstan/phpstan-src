647fc095fbf82b88e3ea5e1188128a0f56fd9a20
Vincent Langlet <vincentlanglet@hotmail.fr>
2025-05-22 16:07:47 +0200

    Add test

--- tests/PHPStan/Analyser/data/getenv-php74.php
+++ tests/PHPStan/Analyser/data/getenv-php74.php
@@ -7,11 +7,14 @@ use function PHPStan\Testing\assertType;
 class Foo
 {
 
-	public function test()
+	public function test(string|null $stringOrNull, mixed $mixed)
 	{
 		assertType('string|false', getenv(null));
 		assertType('array<string, string>', getenv());
 		assertType('string|false', getenv('foo'));
+
+		assertType('string|false', getenv($stringOrNull));
+		assertType('string|false', getenv($mixed));
 	}
 
 }
--- tests/PHPStan/Analyser/data/getenv-php80.php
+++ tests/PHPStan/Analyser/data/getenv-php80.php
@@ -7,11 +7,14 @@ use function PHPStan\Testing\assertType;
 class Foo
 {
 
-	public function test()
+	public function test(string|null $stringOrNull, mixed $mixed)
 	{
 		assertType('array<string, string>', getenv(null));
 		assertType('array<string, string>', getenv());
 		assertType('string|false', getenv('foo'));
+
+		assertType('array<string, string>|string|false', getenv($stringOrNull));
+		assertType('array<string, string>|string|false', getenv($mixed));
 	}
 
 }

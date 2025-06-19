913f05c9a00f2ebce7a8d0ebfe65941ea75d7705
Felix Bernhard <mail@felix-bernhard.com>
2025-06-19 21:05:14 +0200

    static dataProvider

--- tests/PHPStan/Command/ErrorFormatter/GroupedErrorFormatterTest.php
+++ tests/PHPStan/Command/ErrorFormatter/GroupedErrorFormatterTest.php
@@ -10,7 +10,7 @@ use function sprintf;
 class GroupedErrorFormatterTest extends ErrorFormatterTestCase
 {
 
-	public function dataFormatterOutputProvider(): iterable
+	public static function dataFormatterOutputProvider(): iterable
 	{
 		yield [
 			'message' => 'No errors',

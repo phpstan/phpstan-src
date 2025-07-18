c4d44e79dc889423d4209af8eb2a0715e3f45286
Vincent Langlet <vincentlanglet@hotmail.fr>
2025-07-18 10:43:06 +0200

    Introduce dumpNativeType

---
+++ src/Rules/Debug/DumpNativeTypeRule.php
@@ -0,0 +1,61 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Rules\Debug;
+
+use PhpParser\Node;
+use PHPStan\Analyser\Scope;
+use PHPStan\DependencyInjection\AutowiredService;
+use PHPStan\Reflection\ReflectionProvider;
+use PHPStan\Rules\Rule;
+use PHPStan\Rules\RuleErrorBuilder;
+use PHPStan\Type\VerbosityLevel;
+use function count;
+use function sprintf;
+use function strtolower;
+
+/**
+ * @implements Rule<Node\Expr\FuncCall>
+ */
+#[AutowiredService]
+final class DumpNativeTypeRule implements Rule
+{
+
+	public function __construct(private ReflectionProvider $reflectionProvider)
+	{
+	}
+
+	public function getNodeType(): string
+	{
+		return Node\Expr\FuncCall::class;
+	}
+
+	public function processNode(Node $node, Scope $scope): array
+	{
+		if (!$node->name instanceof Node\Name) {
+			return [];
+		}
+
+		$functionName = $this->reflectionProvider->resolveFunctionName($node->name, $scope);
+		if ($functionName === null) {
+			return [];
+		}
+
+		if (strtolower($functionName) !== 'phpstan\dumpnativetype') {
+			return [];
+		}
+
+		if (count($node->getArgs()) === 0) {
+			return [];
+		}
+
+		return [
+			RuleErrorBuilder::message(
+				sprintf(
+					'Dumped type: %s',
+					$scope->getNativeType($node->getArgs()[0]->value)->describe(VerbosityLevel::precise()),
+				),
+			)->nonIgnorable()->identifier('phpstan.dumpNativeType')->build(),
+		];
+	}
+
+}
--- src/dumpType.php
+++ src/dumpType.php
@@ -21,6 +21,18 @@ function dumpType($value) // phpcs:ignore Squiz.Functions.GlobalFunction.Found
  *
  * @throws void
  */
+function dumpNativeType($value) // phpcs:ignore Squiz.Functions.GlobalFunction.Found
+{
+	return null;
+}
+
+/**
+ * @phpstan-pure
+ * @param mixed $value
+ * @return mixed
+ *
+ * @throws void
+ */
 function dumpPhpDocType($value) // phpcs:ignore Squiz.Functions.GlobalFunction.Found
 {
 	return null;

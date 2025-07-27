a641c000f0d89d9593b46fb25c4ee911d86f5222
Markus Staab <maggus.staab@googlemail.com>
2025-07-27 14:32:43 +0200

    fix php7.4 build

--- tests/PHPStan/Rules/Arrays/IterableInForeachRuleTest.php
+++ tests/PHPStan/Rules/Arrays/IterableInForeachRuleTest.php
@@ -139,6 +139,7 @@ class IterableInForeachRuleTest extends RuleTestCase
 		$this->analyse([__DIR__ . '/data/foreach-mixed.php'], $errors);
 	}
 
+	#[RequiresPhp('>= 8.0')]
 	public function testBug13312(): void
 	{
 		$this->analyse([__DIR__ . '/../../Analyser/nsrt/bug-13312.php'], []);

f40c14a62ca65acb669ba38b0e52820a883a13b7
Markus Staab <maggus.staab@googlemail.com>
2025-07-27 10:54:13 +0200

    Added regression test

--- tests/PHPStan/Rules/Functions/ReturnTypeRuleTest.php
+++ tests/PHPStan/Rules/Functions/ReturnTypeRuleTest.php
@@ -370,4 +370,12 @@ class ReturnTypeRuleTest extends RuleTestCase
 		]);
 	}
 
+	public function testBug7076(): void
+	{
+		$this->checkExplicitMixed = true;
+		$this->checkNullables = true;
+
+		$this->analyse([__DIR__ . '/data/bug-7076.php'], []);
+	}
+
 }

c2d9636985e53a1a1fc457c1f3da4e9e78b03ecf
Vincent Langlet <vincentlanglet@hotmail.fr>
2025-07-27 21:28:26 +0200

    Add test

--- tests/PHPStan/Rules/Functions/data/bug-3616.php
+++ tests/PHPStan/Rules/Functions/data/bug-3616.php
@@ -29,3 +29,25 @@ class HelloWorld
 		(self::FACTORIES[$id])();
 	}
 }
+
+class HelloWorld2
+{
+	const FACTORIES = [
+		'a' => [Factory::class, 'a'],
+		'b' => [Factory::class, 'b']
+	];
+
+	public function withLiteral(): void
+	{
+		(self::FACTORIES['a'])();
+	}
+
+	public function withVariable(string $id): void
+	{
+		if (!isset(self::FACTORIES[$id])) {
+			return;
+		}
+
+		(self::FACTORIES[$id])();
+	}
+}

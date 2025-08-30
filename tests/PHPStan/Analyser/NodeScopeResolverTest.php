9ee745713377a488e2e0a321615dbe5bee6b8155
Ondřej Mirtes <ondrej@mirtes.cz>
2025-08-30 18:01:31 +0200

    Update tests/PHPStan/Analyser/NodeScopeResolverTest.php

--- tests/PHPStan/Analyser/NodeScopeResolverTest.php
+++ tests/PHPStan/Analyser/NodeScopeResolverTest.php
@@ -229,8 +229,6 @@ class NodeScopeResolverTest extends TypeInferenceTestCase
 		yield __DIR__ . '/../Rules/Methods/data/bug-4801.php';
 		yield __DIR__ . '/../Rules/Arrays/data/narrow-superglobal.php';
 		yield __DIR__ . '/../Rules/Methods/data/bug-12927.php';
-		
-		// note: files in ./nsrt will be automatically picked up
 	}
 
 	/**

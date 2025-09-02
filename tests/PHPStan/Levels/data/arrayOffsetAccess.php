6f99f04c397e57edd81fcfc811a43aced9eac317
Vincent Langlet <vincentlanglet@hotmail.fr>
2025-09-02 17:15:22 +0200

    Add more cases

--- tests/PHPStan/Levels/data/arrayOffsetAccess.php
+++ tests/PHPStan/Levels/data/arrayOffsetAccess.php
@@ -4,13 +4,13 @@ namespace Levels\ArrayOffsetAccess;
 
 class Foo {
 	/**
+	 * @param array|\ArrayAccess $arrayOrObject
 	 * @param int|null $intOrNull
 	 * @param object|int $objectOrInt
 	 * @param object|null $objectOrNull
 	 * @param mixed $explicitlyMixed
-	 * @return void
 	 */
-	public function test(array $a, $intOrNull, $objectOrInt, $objectOrNull, $explicitlyMixed, $implicitlyMixed)
+	public function test(array $a, $arrayOrObject, $intOrNull, $objectOrInt, $objectOrNull, $explicitlyMixed, $implicitlyMixed): void
 	{
 		$a[42];
 		$a[null];
@@ -20,5 +20,32 @@ class Foo {
 		$a[$objectOrNull];
 		$a[$explicitlyMixed];
 		$a[$implicitlyMixed];
+
+		$arrayOrObject[42];
+		$arrayOrObject[null];
+		$arrayOrObject[new \DateTimeImmutable()];
+		$arrayOrObject[$intOrNull];
+		$arrayOrObject[$objectOrInt];
+		$arrayOrObject[$objectOrNull];
+		$arrayOrObject[$explicitlyMixed];
+		$arrayOrObject[$implicitlyMixed];
+
+		$explicitlyMixed[42];
+		$explicitlyMixed[null];
+		$explicitlyMixed[new \DateTimeImmutable()];
+		$explicitlyMixed[$intOrNull];
+		$explicitlyMixed[$objectOrInt];
+		$explicitlyMixed[$objectOrNull];
+		$explicitlyMixed[$explicitlyMixed];
+		$explicitlyMixed[$implicitlyMixed];
+
+		$implicitlyMixed[42];
+		$implicitlyMixed[null];
+		$implicitlyMixed[new \DateTimeImmutable()];
+		$implicitlyMixed[$intOrNull];
+		$implicitlyMixed[$objectOrInt];
+		$implicitlyMixed[$objectOrNull];
+		$implicitlyMixed[$explicitlyMixed];
+		$implicitlyMixed[$implicitlyMixed];
 	}
 }

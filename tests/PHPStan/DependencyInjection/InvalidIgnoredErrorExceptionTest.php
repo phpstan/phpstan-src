f92b6480967d1826c3eaeb923a909f2f4befec33
Gregor Harlan <mail@gh01.de>
2025-09-09 23:31:39 +0200

    Add more tests

--- tests/PHPStan/DependencyInjection/InvalidIgnoredErrorExceptionTest.php
+++ tests/PHPStan/DependencyInjection/InvalidIgnoredErrorExceptionTest.php
@@ -13,12 +13,24 @@ class InvalidIgnoredErrorExceptionTest extends PHPStanTestCase
 	public static function dateValidateIgnoreErrors(): iterable
 	{
 		yield [
-			__DIR__ . '/invalidIgnoreErrors/one.neon',
+			__DIR__ . '/invalidIgnoreErrors/message-and-messages.neon',
 			'An ignoreErrors entry cannot contain both message and messages fields.',
 		];
 		yield [
-			__DIR__ . '/invalidIgnoreErrors/two.neon',
+			__DIR__ . '/invalidIgnoreErrors/identifier-and-identifiers.neon',
+			'An ignoreErrors entry cannot contain both identifier and identifiers fields.',
+		];
+		yield [
+			__DIR__ . '/invalidIgnoreErrors/path-and-paths.neon',
 			'An ignoreErrors entry cannot contain both path and paths fields.',
+		];
+		yield [
+			__DIR__ . '/invalidIgnoreErrors/missing-main-key.neon',
+			'An ignoreErrors entry must contain at least one of the following fields: message, messages, identifier, identifiers, path, paths.',
+		];
+		yield [
+			__DIR__ . '/invalidIgnoreErrors/count-without-path.neon',
+			'An ignoreErrors entry with count field must also contain path field.',
 		];
 	}
 

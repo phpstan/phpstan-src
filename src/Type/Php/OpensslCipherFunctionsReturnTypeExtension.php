0fa224281f431478fe1195697d852ec11b9f9caf
Christophe Coevoet <stof@notk.org>
2025-09-12 13:39:22 +0200

    Add missing use statement for the AutowireService attribute

--- src/Type/Php/OpensslCipherFunctionsReturnTypeExtension.php
+++ src/Type/Php/OpensslCipherFunctionsReturnTypeExtension.php
@@ -4,6 +4,7 @@ namespace PHPStan\Type\Php;
 
 use PhpParser\Node\Expr\FuncCall;
 use PHPStan\Analyser\Scope;
+use PHPStan\DependencyInjection\AutowiredService;
 use PHPStan\Php\PhpVersion;
 use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Reflection\ParametersAcceptorSelector;

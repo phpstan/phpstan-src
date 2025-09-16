160ac7327af2720f74408369cfe03f55c1a4dbed
Ondrej Mirtes <ondrej@mirtes.cz>
2025-09-16 17:30:51 +0200

    Revert "Temporarily remove InvalidIgnoredErrorExceptionTest because it causes whole test suite instability"
    This reverts commit 9ddddab804609ff34cc3917ff6839f6f2f370f55.

---
+++ tests/PHPStan/DependencyInjection/InvalidIgnoredErrorExceptionTest.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\DependencyInjection;
+
+use PHPStan\Testing\PHPStanTestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+class InvalidIgnoredErrorExceptionTest extends PHPStanTestCase
+{
+
+	private static string $configFile;
+
+	/**
+	 * @return iterable<array{string, string}>
+	 */
+	public static function dataValidateIgnoreErrors(): iterable
+	{
+		yield [
+			__DIR__ . '/invalidIgnoreErrors/message-and-messages.neon',
+			'An ignoreErrors entry cannot contain both message and messages fields.',
+		];
+		yield [
+			__DIR__ . '/invalidIgnoreErrors/rawMessage-and-message.neon',
+			'An ignoreErrors entry cannot contain both rawMessage and message fields.',
+		];
+		yield [
+			__DIR__ . '/invalidIgnoreErrors/rawMessage-and-messages.neon',
+			'An ignoreErrors entry cannot contain both rawMessage and messages fields.',
+		];
+		yield [
+			__DIR__ . '/invalidIgnoreErrors/identifier-and-identifiers.neon',
+			'An ignoreErrors entry cannot contain both identifier and identifiers fields.',
+		];
+		yield [
+			__DIR__ . '/invalidIgnoreErrors/path-and-paths.neon',
+			'An ignoreErrors entry cannot contain both path and paths fields.',
+		];
+		yield [
+			__DIR__ . '/invalidIgnoreErrors/missing-main-key.neon',
+			'An ignoreErrors entry must contain at least one of the following fields: message, messages, rawMessage, identifier, identifiers, path, paths.',
+		];
+		yield [
+			__DIR__ . '/invalidIgnoreErrors/count-without-path.neon',
+			'An ignoreErrors entry with count field must also contain path field.',
+		];
+	}
+
+	#[DataProvider('dataValidateIgnoreErrors')]
+	public function testValidateIgnoreErrors(string $file, string $expectedMessage): void
+	{
+		self::$configFile = $file;
+		$this->expectExceptionMessage($expectedMessage);
+		self::getContainer();
+	}
+
+	public static function getAdditionalConfigFiles(): array
+	{
+		return [
+			__DIR__ . '/../../../conf/bleedingEdge.neon',
+			self::$configFile,
+		];
+	}
+
+}

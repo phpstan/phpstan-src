0f1d4da8cb934c15b90de8cae83d67087cb925b9
Markus Staab <maggus.staab@googlemail.com>
2025-09-18 11:26:34 +0200

    fix

--- src/Testing/PHPUnit/PHPStanPHPUnitExtension.php
+++ src/Testing/PHPUnit/PHPStanPHPUnitExtension.php
@@ -8,7 +8,7 @@ use PHPUnit\Runner\Extension\Facade;
 use PHPUnit\Runner\Extension\ParameterCollection;
 use PHPUnit\TextUI\Configuration\Configuration;
 
-final class PHPUnitExtension implements Extension
+final class PHPStanPHPUnitExtension implements Extension
 {
 
 	#[Override]

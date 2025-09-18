b8d5e75ba37420d1fdca1ddb1dca03bc98f93f3c
Markus Staab <maggus.staab@googlemail.com>
2025-09-18 10:36:57 +0200

    fix

--- build/ignore-by-php-version.neon.php
+++ build/ignore-by-php-version.neon.php
@@ -35,7 +35,7 @@ if (PHP_VERSION_ID >= 80400) {
 	$includes[] = __DIR__ . '/deprecated-8.4.neon';
 }
 
-if (PHP_VERSION_ID < 80200) {
+if (PHP_VERSION_ID < 80100) {
 	$includes[] = __DIR__ . '/old-phpunit.neon';
 } else {
 	$includes[] = __DIR__ . '/new-phpunit.neon';

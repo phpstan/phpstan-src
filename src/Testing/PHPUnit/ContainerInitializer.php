f27a4354a1673b1e5e2d626790790f09a17757bd
Markus Staab <maggus.staab@googlemail.com>
2025-09-18 22:17:06 +0200

    cs

--- src/Testing/PHPUnit/ContainerInitializer.php
+++ src/Testing/PHPUnit/ContainerInitializer.php
@@ -3,10 +3,10 @@
 namespace PHPStan\Testing\PHPUnit;
 
 use PHPStan\DependencyInjection\InvalidIgnoredErrorExceptionTest;
-use function array_key_exists;
 
 final class ContainerInitializer
 {
+
 	public static function initialize(string $testClassName): void
 	{
 		// This test expects an exception during container initialization

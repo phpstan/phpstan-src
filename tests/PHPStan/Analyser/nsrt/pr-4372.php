44f7c6a1100b0a7d7d83e51d74096ef14386cf41
Markus Staab <markus.staab@redaxo.de>
2025-09-29 12:17:02 +0200

    expectations

---
+++ tests/PHPStan/Analyser/nsrt/pr-4372.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace PR4372;
+
+use function PHPStan\debugScope;
+use function PHPStan\Testing\assertType;
+
+function (string $s): void {
+	$locations = [];
+	for ($i = 0; $i < 10; $i++) {
+		$locations[$i] = [];
+		for ($j = 0; $j < 10; $j++) {
+			$locations[$i][$j] = $s;
+		}
+	}
+
+	assertType('non-empty-array<int<0, 9>, non-empty-array<int<0, 9>, string>>', $locations);
+	assertType('non-empty-array<int<0, 9>, string>>', $locations[0]);
+};

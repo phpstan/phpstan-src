db2675f66ba60de1904c2c26e5283927c2dc7791
Markus Staab <markus.staab@redaxo.de>
2025-10-02 09:50:51 +0200

    requires non-empty num

--- tests/PHPStan/Rules/Functions/data/array-rand.php
+++ tests/PHPStan/Rules/Functions/data/array-rand.php
@@ -7,3 +7,11 @@ function doFoo(int $i) {
 	$x = array_rand($arr);
 	$y = array_rand($arr, $i);
 }
+
+/** @param non-empty-array $arr */
+function doBar(array $arr) {
+	$y = array_rand($arr, -5);
+	$y = array_rand($arr, 0);
+	$y = array_rand($arr, 1);
+	$y = array_rand($arr);
+}

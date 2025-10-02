5856e00de349d61b3bfe54e094a33888bdbde73a
Markus Staab <markus.staab@redaxo.de>
2025-10-02 09:52:21 +0200

    Update functionMap_php80delta_bleedingEdge.php

--- resources/functionMap_php80delta_bleedingEdge.php
+++ resources/functionMap_php80delta_bleedingEdge.php
@@ -6,7 +6,7 @@ return [
 		'array_rand\'1' => ['int|string', 'input'=>'non-empty-array'],
 	],
 	'old' => [
-		'array_rand' => ['int|string|array<int,int>|array<int,string>', 'input'=>'array', 'num_req'=>'positive-int'],
+		'array_rand' => ['int|string|array<int,int>|array<int,string>', 'input'=>'array', 'num_req'=>'int'],
 		'array_rand\'1' => ['int|string', 'input'=>'array'],
 	],
 ];

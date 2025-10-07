0e599f813e3828608ffd3a712e0d96a1265b6849
Markus Staab <markus.staab@redaxo.de>
2025-10-07 15:30:33 +0200

    fix php7 build

--- tests/PHPStan/Analyser/nsrt/bug-2001-php8.php
+++ tests/PHPStan/Analyser/nsrt/bug-2001-php8.php
@@ -1,6 +1,6 @@
-<?php // lint < 8.0
+<?php // lint >= 8.0
 
-namespace Bug2001;
+namespace Bug2001Php8;
 
 use function PHPStan\Testing\assertType;
 
@@ -16,21 +16,21 @@ class HelloWorld
 			throw new \RuntimeException('Absolute URLs are prohibited for the redirectTo parameter.');
 		}
 
-		assertType('array{scheme?: string, port?: int<0, 65535>, user?: string, pass?: string, path?: string, query?: string, fragment?: string}|false', $parsedUrl);
+		assertType('array{scheme?: string, host?: string, port?: int<0, 65535>, user?: string, pass?: string, path?: string, query?: string, fragment?: string}', $parsedUrl);
 
 		$redirectUrl = $parsedUrl['path'];
 
 		if (array_key_exists('query', $parsedUrl)) {
-			assertType('array{scheme?: string, port?: int<0, 65535>, user?: string, pass?: string, path?: string, query: string, fragment?: string}', $parsedUrl);
+			assertType('array{scheme?: string, host?: string, port?: int<0, 65535>, user?: string, pass?: string, path?: string, query: string, fragment?: string}', $parsedUrl);
 			$redirectUrl .= '?' . $parsedUrl['query'];
 		}
 
 		if (array_key_exists('fragment', $parsedUrl)) {
-			assertType('array{scheme?: string, port?: int<0, 65535>, user?: string, pass?: string, path?: string, query?: string, fragment: string}', $parsedUrl);
+			assertType('array{scheme?: string, host?: string, port?: int<0, 65535>, user?: string, pass?: string, path?: string, query?: string, fragment: string}', $parsedUrl);
 			$redirectUrl .= '#' . $parsedUrl['query'];
 		}
 
-		assertType('array{scheme?: string, port?: int<0, 65535>, user?: string, pass?: string, path?: string, query?: string, fragment?: string}|false', $parsedUrl);
+		assertType('array{scheme?: string, host?: string, port?: int<0, 65535>, user?: string, pass?: string, path?: string, query?: string, fragment?: string}', $parsedUrl);
 
 		return $redirectUrl;
 	}

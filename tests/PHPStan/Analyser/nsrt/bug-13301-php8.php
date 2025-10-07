676931712076b87030eec39dfa4384a28fd6cc36
Markus Staab <markus.staab@redaxo.de>
2025-10-07 09:59:41 +0200

    Update bug-13301-php8.php

--- tests/PHPStan/Analyser/nsrt/bug-13301-php8.php
+++ tests/PHPStan/Analyser/nsrt/bug-13301-php8.php
@@ -12,4 +12,16 @@ function doFoo($mixed) {
 		assertType('array', $mixed); // could be array~hasOffset('a') after arrays got subtractable
 		echo "NO-a";
 	}
+	assertType('array', $mixed);
+}
+
+function doArray(array $arr) {
+	if (array_key_exists('a', $arr)) {
+		assertType("non-empty-array&hasOffset('a')", $arr);
+		echo "has-a";
+	} else {
+		assertType('array', $arr);
+		echo "NO-a";
+	}
+	assertType('array', $arr);
 }

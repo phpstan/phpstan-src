e38ca48968c018e283c7de123c7292768e6fbd9b
Markus Staab <markus.staab@redaxo.de>
2025-10-14 14:54:12 +0200

    Implement CompositeRule
    Allows testing of rules which delegate work to NodeCallbackInvoker

---
+++ src/Testing/CompositeRule.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Testing;
+
+use PhpParser\Node;
+use PHPStan\Analyser\NodeCallbackInvoker;
+use PHPStan\Analyser\Scope;
+use PHPStan\Rules\DirectRegistry;
+use PHPStan\Rules\Rule;
+use function get_class;
+
+/**
+ * Allows testing of rules which delegate work to NodeCallbackInvoker.
+ *
+ * @implements Rule<Node>
+ *
+ * @api
+ */
+final class CompositeRule implements Rule
+{
+
+	private DirectRegistry $registry;
+
+	public function __construct(DirectRegistry $ruleRegistry)
+	{
+		$this->registry = $ruleRegistry;
+	}
+
+	public function getNodeType(): string
+	{
+		return Node::class;
+	}
+
+	public function processNode(Node $node, Scope&NodeCallbackInvoker $scope): array
+	{
+		$errors = [];
+
+		$nodeType = get_class($node);
+		foreach ($this->registry->getRules($nodeType) as $rule) {
+			foreach ($rule->processNode($node, $scope) as $error) {
+				$errors[] = $error;
+			}
+		}
+
+		return $errors;
+	}
+
+}

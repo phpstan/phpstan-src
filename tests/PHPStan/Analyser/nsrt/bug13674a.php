4966dab0131c1df085b335a198bb8b38fbdfda82
Markus Staab <maggus.staab@googlemail.com>
2025-10-15 07:56:09 +0200

    Create bug13674a.php

---
+++ tests/PHPStan/Analyser/nsrt/bug13674a.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Bug13674a;
+
+use function PHPStan\Testing\assertType;
+
+class HelloWorld
+{
+	/**
+	 * @param array<int> $arrayA
+	 * @param list<int> $listA
+	 */
+	public function sayHello($arrayA, $listA, int $i): void
+	{
+		if (array_key_exists($i, $arrayA)) {
+			assertType('non-empty-array<int>', $arrayA);
+		}
+
+		if (array_key_exists($i, $listA)) {
+			assertType('non-empty-list<int>', $listA);
+		}
+	}
+}

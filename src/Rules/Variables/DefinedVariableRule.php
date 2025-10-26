4863a3cf5682fa61e2e00edac9bf7367df55fe3a
Markus Staab <maggus.staab@googlemail.com>
2025-10-26 19:13:42 +0100

    Discard changes to src/Rules/Variables/DefinedVariableRule.php

--- src/Rules/Variables/DefinedVariableRule.php
+++ src/Rules/Variables/DefinedVariableRule.php
@@ -12,7 +12,6 @@ use PHPStan\DependencyInjection\RegisteredRule;
 use PHPStan\Rules\IdentifierRuleError;
 use PHPStan\Rules\Rule;
 use PHPStan\Rules\RuleErrorBuilder;
-use PHPStan\Type\ObjectType;
 use function array_merge;
 use function in_array;
 use function is_string;
@@ -46,15 +45,6 @@ final class DefinedVariableRule implements Rule
 			$variableNameScopes = [$node->name => $scope];
 		} else {
 			$nameType = $scope->getType($node->name);
-
-			// testing stuff
-			$obj = (new ObjectType('SomeClass'))->isSuperTypeOf($nameType);
-			if ($obj->yes()) {
-				$x = 1;
-			} else {
-				$x = 2;
-			}
-
 			$variableNameScopes = [];
 			foreach ($nameType->getConstantStrings() as $constantString) {
 				$name = $constantString->getValue();

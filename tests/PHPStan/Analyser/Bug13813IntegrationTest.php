c9beede9ef7f2aa927f46f44f0eb62f47626251e
Markus Staab <markus.staab@redaxo.de>
2025-11-17 11:04:33 +0100

    fix php 7.4

--- tests/PHPStan/Analyser/Bug13813IntegrationTest.php
+++ tests/PHPStan/Analyser/Bug13813IntegrationTest.php
@@ -24,14 +24,25 @@ class Bug13813IntegrationTest extends PHPStanTestCase
 		$this->assertCount(2, $analyzerResult->getAllPhpErrors());
 		$this->assertCount(2, $analyzerResult->getFilteredPhpErrors());
 
-		$this->assertSame(
-			'Warning: Undefined variable $x',
-			$analyzerResult->getAllPhpErrors()[0]->getMessage(),
-		);
-		$this->assertSame(
-			'Warning: Undefined variable $x',
-			$analyzerResult->getAllPhpErrors()[1]->getMessage(),
-		);
+		if (PHP_VERSION_ID >= 80000) {
+			$this->assertSame(
+				'Warning: Undefined variable $x',
+				$analyzerResult->getAllPhpErrors()[0]->getMessage(),
+			);
+			$this->assertSame(
+				'Warning: Undefined variable $x',
+				$analyzerResult->getAllPhpErrors()[1]->getMessage(),
+			);
+		} else {
+			$this->assertSame(
+				'Notice: Undefined variable $x',
+				$analyzerResult->getAllPhpErrors()[0]->getMessage(),
+			);
+			$this->assertSame(
+				'Notice: Undefined variable $x',
+				$analyzerResult->getAllPhpErrors()[1]->getMessage(),
+			);
+		}
 	}
 
 	/**

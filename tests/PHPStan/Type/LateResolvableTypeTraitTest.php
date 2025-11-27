8c950a24bb5d1f184844fbd8a6612264920909c9
tajimayuki <softte2su77@gmail.com>
2025-11-28 00:27:45 +0900

    Add data-provider tests for late-resolvable isSuperTypeOf/isSubTypeOf

--- tests/PHPStan/Type/LateResolvableTypeTraitTest.php
+++ tests/PHPStan/Type/LateResolvableTypeTraitTest.php
@@ -3,32 +3,76 @@
 namespace PHPStan\Type;
 
 use PHPStan\Type\Constant\ConstantStringType;
+use PHPStan\TrinaryLogic;
 use PHPStan\Type\IntegerType;
 use PHPStan\Type\NeverType;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+use PHPStan\Type\ConditionalTypeForParameter;
 use PHPStan\Type\UnionType;
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\DataProvider;
 
 class LateResolvableTypeTraitTest extends TestCase
 {
-
-	public function testIsSuperTypeOfForConditional(): void
+	public static function dataIsSuperTypeOf(): array
 	{
-		$conditional = new ConditionalTypeForParameter(
-			'$operator',
-			new ConstantStringType('in'),
-			new IntegerType(),
-			new NeverType(),
-			false,
+		return self::provideCases();
+	}
+
+	public static function dataIsSubTypeOf(): array
+	{
+		return self::provideCases();
+	}
+
+	private static function createConditional(
+		string $parameterName = '$operator',
+		string $targetLiteral = 'in',
+		?Type $ifType = null,
+		?Type $elseType = null,
+		bool $negated = false,
+	): ConditionalTypeForParameter
+	{
+		return new ConditionalTypeForParameter(
+			$parameterName,
+			new ConstantStringType($targetLiteral),
+			$ifType ?? new IntegerType(),
+			$elseType ?? new NeverType(),
+			$negated,
 		);
+	}
 
-		$this->assertSame('Yes', $conditional->isSuperTypeOf($conditional)->describe());
+	/**
+	 * @return list<array{Type, Type, TrinaryLogic}>
+	 */
+	private static function provideCases(): array
+	{
+		return [
+			'conditional vs same conditional' => [
+				self::createConditional(),
+				self::createConditional(),
+				TrinaryLogic::createYes(),
+			],
+			'conditional vs union containing it' => [
+				self::createConditional(),
+				new UnionType([new StringType(), self::createConditional()]),
+				TrinaryLogic::createYes(),
+			],
+		];
+	}
 
-		$unionWithConditional = new UnionType([
-			new StringType(),
-			$conditional,
-		]);
+	#[DataProvider('dataIsSuperTypeOf')]
+	public function testIsSuperTypeOf(Type $left, Type $right, TrinaryLogic $expected): void
+	{
+		$actual = $left->isSuperTypeOf($right);
+		$this->assertSame($expected->describe(), $actual->describe());
+	}
 
-		$this->assertSame('Yes', $conditional->isSuperTypeOf($unionWithConditional)->describe());
+	#[DataProvider('dataIsSubTypeOf')]
+	public function testIsSubTypeOf(Type $left, Type $right, TrinaryLogic $expected): void
+	{
+		$actual = $left->isSubTypeOf($right);
+		$this->assertSame($expected->describe(), $actual->describe());
 	}
 
 }

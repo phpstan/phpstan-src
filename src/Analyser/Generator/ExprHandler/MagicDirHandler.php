47b3bfd6ba2601db8c1649d55fc8aec3d5eab641
Markus Staab <markus.staab@redaxo.de>
2025-12-01 11:28:56 +0100

    Implement Magic-File/Dir/Line handler

---
+++ src/Analyser/Generator/ExprHandler/MagicDirHandler.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Analyser\Generator\ExprHandler;
+
+use Generator;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Scalar\MagicConst\Dir;
+use PhpParser\Node\Stmt;
+use PHPStan\Analyser\ExpressionContext;
+use PHPStan\Analyser\Generator\ExprAnalysisResult;
+use PHPStan\Analyser\Generator\ExprHandler;
+use PHPStan\Analyser\Generator\GeneratorScope;
+use PHPStan\Analyser\SpecifiedTypes;
+use PHPStan\DependencyInjection\AutowiredService;
+use PHPStan\Reflection\InitializerExprContext;
+use PHPStan\Reflection\InitializerExprTypeResolver;
+
+/**
+ * @implements ExprHandler<Dir>
+ */
+#[AutowiredService]
+final class MagicDirHandler implements ExprHandler
+{
+
+	public function __construct(private InitializerExprTypeResolver $initializerExprTypeResolver)
+	{
+	}
+
+	public function supports(Expr $expr): bool
+	{
+		return $expr instanceof Dir;
+	}
+
+	public function analyseExpr(Stmt $stmt, Expr $expr, GeneratorScope $scope, ExpressionContext $context, ?callable $alternativeNodeCallback): Generator
+	{
+		yield from [];
+
+		$initializerContext = InitializerExprContext::fromScope($scope);
+		$type = $this->initializerExprTypeResolver->getType($expr, $initializerContext);
+
+		return new ExprAnalysisResult(
+			$type,
+			$type,
+			$scope,
+			hasYield: false,
+			isAlwaysTerminating: false,
+			throwPoints: [],
+			impurePoints: [],
+			specifiedTruthyTypes: new SpecifiedTypes(),
+			specifiedFalseyTypes: new SpecifiedTypes(),
+			specifiedNullTypes: new SpecifiedTypes(),
+		);
+	}
+
+}
--- tests/PHPStan/Analyser/Generator/data/gnsr.php
+++ tests/PHPStan/Analyser/Generator/data/gnsr.php
@@ -462,3 +462,11 @@ function (): void {
 		}
 	};
 };
+
+class MagicConstUser {
+	function doFoo(): void {
+		assertType('literal-string&non-falsy-string', __DIR__);
+		assertType('literal-string&non-falsy-string', __FILE__);
+		assertType('467', __LINE__);
+	}
+}

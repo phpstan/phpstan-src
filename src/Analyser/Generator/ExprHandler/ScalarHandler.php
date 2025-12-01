4806cf005d73f415adb9612e0ec5b05242db25a9
Markus Staab <markus.staab@redaxo.de>
2025-12-01 15:54:33 +0100

    Refactor Scalar*Handlers into single ScalarHandler

--- src/Analyser/Generator/ExprHandler/ScalarHandler.php
+++ src/Analyser/Generator/ExprHandler/ScalarHandler.php
@@ -4,7 +4,9 @@ namespace PHPStan\Analyser\Generator\ExprHandler;
 
 use Generator;
 use PhpParser\Node\Expr;
+use PhpParser\Node\Scalar\Float_;
 use PhpParser\Node\Scalar\Int_;
+use PhpParser\Node\Scalar\String_;
 use PhpParser\Node\Stmt;
 use PHPStan\Analyser\ExpressionContext;
 use PHPStan\Analyser\Generator\ExprAnalysisResult;
@@ -12,18 +14,23 @@ use PHPStan\Analyser\Generator\ExprHandler;
 use PHPStan\Analyser\Generator\GeneratorScope;
 use PHPStan\Analyser\SpecifiedTypes;
 use PHPStan\DependencyInjection\AutowiredService;
+use PHPStan\ShouldNotHappenException;
+use PHPStan\Type\Constant\ConstantFloatType;
 use PHPStan\Type\Constant\ConstantIntegerType;
+use PHPStan\Type\Constant\ConstantStringType;
 
 /**
- * @implements ExprHandler<Int_>
+ * @implements ExprHandler<Float_>
  */
 #[AutowiredService]
-final class ScalarIntHandler implements ExprHandler
+final class ScalarHandler implements ExprHandler
 {
 
 	public function supports(Expr $expr): bool
 	{
-		return $expr instanceof Int_;
+		return $expr instanceof Float_
+			|| $expr instanceof Int_
+			|| $expr instanceof String_;
 	}
 
 	public function analyseExpr(
@@ -35,7 +42,17 @@ final class ScalarIntHandler implements ExprHandler
 	): Generator
 	{
 		yield from [];
-		$type = new ConstantIntegerType($expr->value);
+
+		if ($expr instanceof Float_) {
+			$type = new ConstantFloatType($expr->value);
+		} elseif ($expr instanceof Int_) {
+			$type = new ConstantIntegerType($expr->value);
+		} elseif ($expr instanceof String_) {
+			$type = new ConstantStringType($expr->value);
+		} else {
+			throw new ShouldNotHappenException();
+		}
+
 		return new ExprAnalysisResult(
 			$type,
 			$type,

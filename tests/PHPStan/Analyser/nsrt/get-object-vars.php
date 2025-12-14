767a07eb27d25fb205a6fa8c935df5f92cfbd886
VojtaB <triplkrypl@centrum.cz>
2025-12-14 15:57:33 +0100

    added test for get_object_vars()

---
+++ tests/PHPStan/Analyser/nsrt/get-object-vars.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace GetObjectVars;
+
+use function PHPStan\Testing\assertType;
+
+/**
+ * @param object{1: mixed} $object
+ */
+function getObjectVarsWithIntKeyTest(object $object): void
+{
+	assertType('array<array-key, mixed>', get_object_vars($object));
+	assertType('array<array-key, mixed>', get_object_vars(json_decode('{"1": "test"}')));
+}

e0aac66100e0ff4365b1b3bc3820bdd6ce6ae539
Ondrej Mirtes <ondrej@mirtes.cz>
2025-12-29 17:29:37 +0100

    Preparation for turbo-ext PoC

--- src/Reflection/BetterReflection/SourceLocator/OptimizedDirectorySourceLocatorFactory.php
+++ src/Reflection/BetterReflection/SourceLocator/OptimizedDirectorySourceLocatorFactory.php
@@ -7,36 +7,24 @@ use PHPStan\DependencyInjection\AutowiredParameter;
 use PHPStan\DependencyInjection\AutowiredService;
 use PHPStan\File\FileFinder;
 use PHPStan\Php\PhpVersion;
-use PHPStan\Reflection\ConstantNameHelper;
 use function array_key_exists;
-use function count;
+use function array_keys;
 use function hash_file;
-use function in_array;
-use function ltrim;
-use function php_strip_whitespace;
-use function preg_match_all;
-use function preg_replace;
 use function sprintf;
-use function strtolower;
 
 #[AutowiredService]
 final class OptimizedDirectorySourceLocatorFactory
 {
-
-	private PhpFileCleaner $cleaner;
-
-	private string $extraTypes;
 
 	public function __construct(
 		private FileNodesFetcher $fileNodesFetcher,
 		#[AutowiredParameter(ref: '@fileFinderScan')]
 		private FileFinder $fileFinder,
 		private PhpVersion $phpVersion,
+		private SymbolFinderInFiles $symbolFinderInFiles,
 		private Cache $cache,
 	)
 	{
-		$this->extraTypes = $this->phpVersion->supportsEnums() ? '|enum' : '';
-		$this->cleaner = new PhpFileCleaner();
 	}
 
 	public function createByDirectory(string $directory): OptimizedDirectorySourceLocator
@@ -63,8 +51,11 @@ final class OptimizedDirectorySourceLocatorFactory
 	{
 		$variableCacheKey = 'v1';
 
+		$originalFileHashes = $fileHashes;
+
 		/** @var array<string, array{string, string[], string[], string[]}>|null $cached */
 		$cached = $this->cache->load($cacheKey, $variableCacheKey);
+		$findInFiles = [];
 		if ($cached !== null) {
 			foreach ($cached as $file => [$hash, $classes, $functions, $constants]) {
 				if (!array_key_exists($file, $fileHashes)) {
@@ -77,17 +68,21 @@ final class OptimizedDirectorySourceLocatorFactory
 					continue;
 				}
 
-				[$newClasses, $newFunctions, $newConstants] = $this->findSymbols($file);
-				$cached[$file] = [$newHash, $newClasses, $newFunctions, $newConstants];
+				$findInFiles[] = $file;
 			}
 		} else {
 			$cached = [];
 		}
 
-		foreach ($fileHashes as $file => $newHash) {
-			[$newClasses, $newFunctions, $newConstants] = $this->findSymbols($file);
+		foreach (array_keys($fileHashes) as $file) {
+			$findInFiles[] = $file;
+		}
+
+		foreach ($this->symbolFinderInFiles->findSymbols($findInFiles, $this->phpVersion->supportsEnums()) as $file => [$newClasses, $newFunctions, $newConstants]) {
+			$newHash = $originalFileHashes[$file];
 			$cached[$file] = [$newHash, $newClasses, $newFunctions, $newConstants];
 		}
+
 		$this->cache->save($cacheKey, $variableCacheKey, $cached);
 
 		[$classToFile, $functionToFiles, $constantToFile] = $this->changeStructure($cached);
@@ -146,80 +141,6 @@ final class OptimizedDirectorySourceLocatorFactory
 			$classToFile,
 			$functionToFiles,
 			$constantToFile,
-		];
-	}
-
-	/**
-	 * Inspired by Composer\Autoload\ClassMapGenerator::findClasses()
-	 * @link https://github.com/composer/composer/blob/45d3e133a4691eccb12e9cd6f9dfd76eddc1906d/src/Composer/Autoload/ClassMapGenerator.php#L216
-	 *
-	 * @return array{string[], string[], string[]}
-	 */
-	private function findSymbols(string $file): array
-	{
-		$contents = @php_strip_whitespace($file);
-		if ($contents === '') {
-			return [[], [], []];
-		}
-
-		$matchResults = (bool) preg_match_all(sprintf('{\b(?:(?:class|interface|trait|const|function%s)\s)|(?:define\s*\()}i', $this->extraTypes), $contents, $matches);
-		if (!$matchResults) {
-			return [[], [], []];
-		}
-
-		$contents = $this->cleaner->clean($contents, count($matches[0]));
-
-		preg_match_all(sprintf('{
-			(?:
-				\b(?<![\$:>])(?:
-					(?: (?P<type>class|interface|trait%s) \s++ (?P<name>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff\-]*+) )
-					| (?: (?P<function>function) \s++ (?:&\s*)? (?P<fname>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff\-]*+) \s*+ [&\(] )
-					| (?: (?P<constant>const) \s++ (?P<cname>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff\-]*+) \s*+ [^;] )
-					| (?: (?:\\\)? (?P<define>define) \s*+ \( \s*+ [\'"] (?P<dname>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*+(?:[\\\\]{1,2}[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*+)*+) )
-					| (?: (?P<ns>namespace) (?P<nsname>\s++[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*+(?:\s*+\\\\\s*+[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*+)*+)? \s*+ [\{;] )
-				)
-			)
-		}ix', $this->extraTypes), $contents, $matches);
-
-		$classes = [];
-		$functions = [];
-		$constants = [];
-		$namespace = '';
-
-		for ($i = 0, $len = count($matches['type']); $i < $len; $i++) {
-			if (isset($matches['ns'][$i]) && $matches['ns'][$i] !== '') {
-				$namespace = preg_replace('~\s+~', '', strtolower($matches['nsname'][$i])) . '\\';
-				continue;
-			}
-
-			if ($matches['function'][$i] !== '') {
-				$functions[] = strtolower(ltrim($namespace . $matches['fname'][$i], '\\'));
-				continue;
-			}
-
-			if ($matches['constant'][$i] !== '') {
-				$constants[] = ConstantNameHelper::normalize(ltrim($namespace . $matches['cname'][$i], '\\'));
-			}
-
-			if ($matches['define'][$i] !== '') {
-				$constants[] = ConstantNameHelper::normalize($matches['dname'][$i]);
-				continue;
-			}
-
-			$name = $matches['name'][$i];
-
-			// skip anon classes extending/implementing
-			if (in_array($name, ['extends', 'implements'], true)) {
-				continue;
-			}
-
-			$classes[] = strtolower(ltrim($namespace . $name, '\\'));
-		}
-
-		return [
-			$classes,
-			$functions,
-			$constants,
 		];
 	}
 
---
+++ src/Reflection/BetterReflection/SourceLocator/SymbolFinderInFiles.php
@@ -0,0 +1,123 @@
+<?php declare(strict_types = 1);
+
+namespace PHPStan\Reflection\BetterReflection\SourceLocator;
+
+use function count;
+use function in_array;
+use function ltrim;
+use function php_strip_whitespace;
+use function preg_match_all;
+use function preg_replace;
+use function sprintf;
+use function strtolower;
+
+final class SymbolFinderInFiles
+{
+
+	private PhpFileCleaner $cleaner;
+
+	public function __construct()
+	{
+		$this->cleaner = new PhpFileCleaner();
+	}
+
+	/**
+	 * @param string[] $files
+	 * @return array<string, array{string[], string[], string[]}>
+	 */
+	public function findSymbols(array $files, bool $supportsEnums): array
+	{
+		$result = [];
+		foreach ($files as $file) {
+			$result[$file] = $this->findSymbolsInFile($file, $supportsEnums);
+		}
+
+		return $result;
+	}
+
+	/**
+	 * Inspired by Composer\Autoload\ClassMapGenerator::findClasses()
+	 * @link https://github.com/composer/composer/blob/45d3e133a4691eccb12e9cd6f9dfd76eddc1906d/src/Composer/Autoload/ClassMapGenerator.php#L216
+	 *
+	 * @return array{string[], string[], string[]}
+	 */
+	private function findSymbolsInFile(string $file, bool $supportsEnums): array
+	{
+		$contents = @php_strip_whitespace($file);
+		if ($contents === '') {
+			return [[], [], []];
+		}
+
+		$extraTypes = $supportsEnums ? '|enum' : '';
+		$matchResults = (bool) preg_match_all(sprintf('{\b(?:(?:class|interface|trait|const|function%s)\s)|(?:define\s*\()}i', $extraTypes), $contents, $matches);
+		if (!$matchResults) {
+			return [[], [], []];
+		}
+
+		$contents = $this->cleaner->clean($contents, count($matches[0]));
+
+		preg_match_all(sprintf('{
+			(?:
+				\b(?<![\$:>])(?:
+					(?: (?P<type>class|interface|trait%s) \s++ (?P<name>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff\-]*+) )
+					| (?: (?P<function>function) \s++ (?:&\s*)? (?P<fname>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff\-]*+) \s*+ [&\(] )
+					| (?: (?P<constant>const) \s++ (?P<cname>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff\-]*+) \s*+ [^;] )
+					| (?: (?:\\\)? (?P<define>define) \s*+ \( \s*+ [\'"] (?P<dname>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*+(?:[\\\\]{1,2}[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*+)*+) )
+					| (?: (?P<ns>namespace) (?P<nsname>\s++[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*+(?:\s*+\\\\\s*+[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*+)*+)? \s*+ [\{;] )
+				)
+			)
+		}ix', $extraTypes), $contents, $matches);
+
+		$classes = [];
+		$functions = [];
+		$constants = [];
+		$namespace = '';
+
+		for ($i = 0, $len = count($matches['type']); $i < $len; $i++) {
+			if (isset($matches['ns'][$i]) && $matches['ns'][$i] !== '') {
+				$namespace = preg_replace('~\s+~', '', strtolower($matches['nsname'][$i])) . '\\';
+				continue;
+			}
+
+			if ($matches['function'][$i] !== '') {
+				$functions[] = strtolower(ltrim($namespace . $matches['fname'][$i], '\\'));
+				continue;
+			}
+
+			if ($matches['constant'][$i] !== '') {
+				$constants[] = self::normalizeConstantName(ltrim($namespace . $matches['cname'][$i], '\\'));
+			}
+
+			if ($matches['define'][$i] !== '') {
+				$constants[] = self::normalizeConstantName($matches['dname'][$i]);
+				continue;
+			}
+
+			$name = $matches['name'][$i];
+
+			// skip anon classes extending/implementing
+			if (in_array($name, ['extends', 'implements'], true)) {
+				continue;
+			}
+
+			$classes[] = strtolower(ltrim($namespace . $name, '\\'));
+		}
+
+		return [
+			$classes,
+			$functions,
+			$constants,
+		];
+	}
+
+	private static function normalizeConstantName(string $name): string
+	{
+		if (!str_contains($name, '\\')) {
+			return $name;
+		}
+
+		$nameParts = array_filter(explode('\\', $name), static fn ($part) => $part !== '');
+		return strtolower(implode('\\', array_slice($nameParts, 0, -1))) . '\\' . end($nameParts);
+	}
+
+}

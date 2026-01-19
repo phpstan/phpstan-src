a2d6b6359a52d2ac66c3409a117da3061ae4b20b
Ondrej Mirtes <ondrej@mirtes.cz>
2026-01-19 17:34:22 +0100

    Revert "Reapply "Attempt to fix hard-to-reproduce bug""
    This reverts commit b84de67e59d8a24d2487b4ba7451894e58065120.

--- src/Analyser/MutatingScope.php
+++ src/Analyser/MutatingScope.php
@@ -3909,18 +3909,6 @@ class MutatingScope implements Scope, NodeCallbackInvoker
 			$mergedExpressionTypes,
 		);
 
-		$filter = static function (ExpressionTypeHolder $expressionTypeHolder) {
-			if ($expressionTypeHolder->getCertainty()->yes()) {
-				return true;
-			}
-
-			$expr = $expressionTypeHolder->getExpr();
-
-			return $expr instanceof Variable || $expr instanceof VirtualNode;
-		};
-
-		$mergedExpressionTypes = array_filter($mergedExpressionTypes, $filter);
-
 		$ourNativeExpressionTypes = $this->nativeExpressionTypes;
 		$theirNativeExpressionTypes = $otherScope->nativeExpressionTypes;
 		$mergedNativeExpressionTypes = [];
@@ -3954,7 +3942,7 @@ class MutatingScope implements Scope, NodeCallbackInvoker
 			$this->getFunction(),
 			$this->getNamespace(),
 			$mergedExpressionTypes,
-			array_merge($mergedNativeExpressionTypes, array_filter($this->mergeVariableHolders($ourNativeExpressionTypes, $theirNativeExpressionTypes), $filter)),
+			array_merge($mergedNativeExpressionTypes, $this->mergeVariableHolders($ourNativeExpressionTypes, $theirNativeExpressionTypes)),
 			$conditionalExpressions,
 			$this->inClosureBindScopeClasses,
 			$this->anonymousFunctionReflection,
@@ -4090,6 +4078,11 @@ class MutatingScope implements Scope, NodeCallbackInvoker
 				$intersectedVariableTypeHolders[$exprString] = $variableTypeHolder->and($theirVariableTypeHolders[$exprString]);
 			} else {
 				$expr = $variableTypeHolder->getExpr();
+
+				if (!$expr instanceof Variable && !$expr instanceof VirtualNode) {
+					continue;
+				}
+
 				if (!$expr->hasAttribute(self::CONTAINS_SUPER_GLOBAL_ATTRIBUTE_NAME)) {
 					$expr->setAttribute(self::CONTAINS_SUPER_GLOBAL_ATTRIBUTE_NAME, $nodeFinder->findFirst($expr, $globalVariableCallback) !== null);
 				}
@@ -4107,6 +4100,11 @@ class MutatingScope implements Scope, NodeCallbackInvoker
 			}
 
 			$expr = $variableTypeHolder->getExpr();
+
+			if (!$expr instanceof Variable && !$expr instanceof VirtualNode) {
+				continue;
+			}
+
 			if (!$expr->hasAttribute(self::CONTAINS_SUPER_GLOBAL_ATTRIBUTE_NAME)) {
 				$expr->setAttribute(self::CONTAINS_SUPER_GLOBAL_ATTRIBUTE_NAME, $nodeFinder->findFirst($expr, $globalVariableCallback) !== null);
 			}

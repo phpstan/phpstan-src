ae420d5fbb827fb1de212aa7d00568f7340415ae
Markus Staab <markus.staab@redaxo.de>
2026-01-20 14:58:18 +0100

    SplObjectStorage iterates over objects

---
+++ tests/PHPStan/Analyser/nsrt/bug-13985.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bug13985;
+
+use SplObjectStorage;
+use function PHPStan\Testing\assertType;
+
+function example(mixed $param): void
+{
+	if ($param instanceof SplObjectStorage) {
+		foreach ($param as $key => $value) {
+			assertType('int', $key);
+			assertType('object', $value);
+		}
+	}
+}
+
+class X {}
+
+/**
+ * @param SplObjectStorage<X, int> $splObjectStorage
+ * @return void
+ */
+function genericExample(SplObjectStorage $splObjectStorage): void
+{
+	foreach ($splObjectStorage as $key => $value) {
+		assertType('int', $key);
+		assertType('Bug13985\X', $value);
+	}
+	assertType('int', $splObjectStorage->getInfo());
+
+}

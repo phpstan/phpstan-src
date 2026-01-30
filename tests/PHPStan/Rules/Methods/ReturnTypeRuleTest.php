d83f50c2846726b683849716355077789b53bb7a
Ondrej Mirtes <ondrej@mirtes.cz>
2026-01-30 12:39:49 +0100

    Regression tests
    Closes https://github.com/phpstan/phpstan/issues/10771

--- tests/PHPStan/Rules/Methods/ReturnTypeRuleTest.php
+++ tests/PHPStan/Rules/Methods/ReturnTypeRuleTest.php
@@ -1286,4 +1286,9 @@ class ReturnTypeRuleTest extends RuleTestCase
 		$this->analyse([__DIR__ . '/data/bug-8438.php'], []);
 	}
 
+	public function testBug10771(): void
+	{
+		$this->analyse([__DIR__ . '/data/bug-10771.php'], []);
+	}
+
 }

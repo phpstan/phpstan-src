c1acd69dc4f699ac58c42bc374620ca7a1e4c554
Markus Staab <markus.staab@redaxo.de>
2026-02-03 16:56:15 +0100

    added regression test

--- tests/PHPStan/Rules/Methods/ReturnTypeRuleTest.php
+++ tests/PHPStan/Rules/Methods/ReturnTypeRuleTest.php
@@ -1291,4 +1291,9 @@ class ReturnTypeRuleTest extends RuleTestCase
 		$this->analyse([__DIR__ . '/data/bug-10771.php'], []);
 	}
 
+	public function testBug8636(): void
+	{
+		$this->analyse([__DIR__ . '/data/bug-8636.php'], []);
+	}
+
 }

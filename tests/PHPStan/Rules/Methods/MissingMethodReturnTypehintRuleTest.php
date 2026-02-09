f062bc3872e9e8bc591aac745fe10abdace5282b
Flyingmana <flyingmana@googlemail.com>
2026-02-10 00:26:30 +0100

    extend detected error in test

--- tests/PHPStan/Rules/Methods/MissingMethodReturnTypehintRuleTest.php
+++ tests/PHPStan/Rules/Methods/MissingMethodReturnTypehintRuleTest.php
@@ -55,8 +55,9 @@ class MissingMethodReturnTypehintRuleTest extends RuleTestCase
 				99,
 			],
 			[
-				'Method MissingMethodReturnTypehint\CallableSignature::doFoo() return type specified in iterable type Traversable..',
+				'Method MissingMethodReturnTypehint\IterableIntersection::doFoo() return type has no value type specified in iterable type Traversable.',
 				110,
+				MissingTypehintCheck::MISSING_ITERABLE_VALUE_TYPE_TIP,
 			],
 			[
 				'Method MissingMethodReturnTypehint\Baz::returnsGenericWithSomeDefaults() return type with generic class MissingMethodReturnTypehint\GenericClassWithSomeDefaults does not specify its types: T, U (1-2 required)',

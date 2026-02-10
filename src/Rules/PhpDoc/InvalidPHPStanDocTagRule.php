ba8fae8027fbd88fc323e5bfc1ebe08ed7a5bee1
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2026-02-10 16:23:48 +0100

    Introduce `@phpstan-all-methods-(im)pure`

--- src/Rules/PhpDoc/InvalidPHPStanDocTagRule.php
+++ src/Rules/PhpDoc/InvalidPHPStanDocTagRule.php
@@ -62,6 +62,8 @@ final class InvalidPHPStanDocTagRule implements Rule
 		'@phpstan-param-immediately-invoked-callable',
 		'@phpstan-param-later-invoked-callable',
 		'@phpstan-param-closure-this',
+		'@phpstan-all-methods-pure',
+		'@phpstan-all-methods-impure',
 	];
 
 	public function __construct(

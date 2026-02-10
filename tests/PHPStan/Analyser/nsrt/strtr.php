62fd66e466736300ad79bfa46d26f7b9c4ff8832
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2026-02-10 08:12:07 +0100

    Fix strtr inferences (#4466)

--- tests/PHPStan/Analyser/nsrt/strtr.php
+++ tests/PHPStan/Analyser/nsrt/strtr.php
@@ -7,8 +7,9 @@ use function PHPStan\Testing\assertType;
 /**
  * @param non-empty-string $nonEmptyString
  * @param non-falsy-string $nonFalseyString
+ * @param mixed $mixed
  */
-function doFoo(string $s, $nonEmptyString, $nonFalseyString) {
+function doFoo(string $s, $nonEmptyString, $nonFalseyString, $mixed) {
 	assertType('string', strtr($s, 'f', 'b'));
 	assertType('string', strtr($s, ['f' => 'b']));
 	assertType('string', strtr($s, ['f' => 'b', 'o' => 'a']));
@@ -24,4 +25,31 @@ function doFoo(string $s, $nonEmptyString, $nonFalseyString) {
 	assertType('non-empty-string', strtr($nonFalseyString, $s, $nonEmptyString));
 	assertType('non-falsy-string', strtr($nonFalseyString, $nonEmptyString, $nonFalseyString));
 	assertType('non-falsy-string', strtr($nonFalseyString, $nonFalseyString, $nonFalseyString));
+
+	assertType('string', strtr($s, [$s => $nonEmptyString]));
+	assertType('string', strtr($s, [$nonEmptyString => $nonEmptyString]));
+	assertType('string', strtr($s, [$nonFalseyString => $nonFalseyString]));
+
+	assertType('non-empty-string', strtr($nonEmptyString, [$s => $nonEmptyString]));
+	assertType('non-empty-string', strtr($nonEmptyString, [$nonEmptyString => $nonEmptyString]));
+	assertType('non-empty-string', strtr($nonEmptyString, [$nonFalseyString => $nonFalseyString]));
+
+	assertType('non-empty-string', strtr($nonFalseyString, [$s => $nonEmptyString]));
+	assertType('non-falsy-string', strtr($nonFalseyString, [$nonEmptyString => $nonFalseyString]));
+	assertType('non-falsy-string', strtr($nonFalseyString, [$nonFalseyString => $nonFalseyString]));
+
+	// see https://3v4l.org/jCUhW/rfc#vgit.master
+	assertType('string', strtr($nonFalseyString, [$nonFalseyString => $s]));
+	assertType('string', strtr($nonEmptyString, [$nonEmptyString => $s]));
+
+	assertType('non-empty-string', strtr($nonEmptyString, rand(0, 1) ? [$s => $nonEmptyString] : null));
+	assertType('non-empty-string', strtr($nonEmptyString, rand(0, 1) ? [$nonEmptyString => $nonEmptyString] : null));
+	assertType('non-empty-string', strtr($nonEmptyString, rand(0, 1) ? [$nonFalseyString => $nonFalseyString] : null));
+
+	assertType('non-empty-string', strtr($nonFalseyString, rand(0, 1) ? [$s => $nonEmptyString] : null));
+	assertType('non-falsy-string', strtr($nonFalseyString, rand(0, 1) ? [$nonEmptyString => $nonFalseyString] : null));
+	assertType('non-falsy-string', strtr($nonFalseyString, rand(0, 1) ? [$nonFalseyString => $nonFalseyString] : null));
+
+	assertType('string', strtr($nonEmptyString, $mixed));
+	assertType('string', strtr($nonFalseyString, $mixed));
 }

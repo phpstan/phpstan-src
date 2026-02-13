7693f5b7e7a5eecea1955ad564be2c9818eb01e4
Ondrej Mirtes <ondrej@mirtes.cz>
2026-02-13 08:59:50 +0100

    Improve array_count_values

--- src/Type/Php/ArrayCountValuesDynamicReturnTypeExtension.php
+++ src/Type/Php/ArrayCountValuesDynamicReturnTypeExtension.php
@@ -6,6 +6,7 @@ use PhpParser\Node\Expr\FuncCall;
 use PHPStan\Analyser\Scope;
 use PHPStan\DependencyInjection\AutowiredService;
 use PHPStan\Reflection\FunctionReflection;
+use PHPStan\Type\Accessory\AccessoryNumericStringType;
 use PHPStan\Type\Accessory\NonEmptyArrayType;
 use PHPStan\Type\ArrayType;
 use PHPStan\Type\Constant\ConstantArrayType;
@@ -54,12 +55,12 @@ final class ArrayCountValuesDynamicReturnTypeExtension implements DynamicFunctio
 				continue;
 			}
 
-			$keyType = $itemType->toArrayKey();
-
 			// PHP casts numeric strings to integer keys, so a general string
 			// that might be numeric should produce int|string keys
 			if ($itemType->isString()->yes() && $itemType->isNumericString()->maybe()) {
-				$keyType = TypeCombinator::union($keyType, new IntegerType());
+				$keyType = TypeCombinator::union($itemType->toArrayKey(), TypeCombinator::intersect($itemType, new StringType(), new AccessoryNumericStringType())->toArrayKey());
+			} else {
+				$keyType = $itemType->toArrayKey();
 			}
 
 			$outputTypes[] = new IntersectionType([

71c81d42b430b5cc3a86a7de1006a9d06dc1ff32
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-02-15 19:16:14 +0100

    More tests

--- tests/PHPStan/Analyser/nsrt/bug-13561.php
+++ tests/PHPStan/Analyser/nsrt/bug-13561.php
@@ -35,3 +35,16 @@ function more_test(MyInterface $i): void {
 	assertType("non-empty-array<string, 42|string|true>&hasOffsetValue('always_here', 42|true)", $b);
 	assertType('true', isset($b['always_here']));
 }
+
+/**
+ * @param array{thing?: 'do', always_here: 42|true} $x
+ */
+function more_test_2(MyInterface $i, array $x): void {
+	$a = $i->getArray() + $x;
+	assertType("non-empty-array<string, 42|string|true>&hasOffsetValue('always_here', 42|string|true)", $a);
+	assertType('true', isset($a['always_here']));
+
+	$b = $x + $i->getArray();
+	assertType("non-empty-array<string, 42|string|true>&hasOffsetValue('always_here', 42|true)", $b);
+	assertType('true', isset($b['always_here']));
+}

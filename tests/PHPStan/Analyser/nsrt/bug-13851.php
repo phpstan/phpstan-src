a44d54ebc72af9cb68c51747646fedbf94d22f2e
Markus Staab <maggus.staab@googlemail.com>
2026-02-15 10:49:19 +0100

    Add more foreach with by-ref $value tests (#4934)

---
+++ tests/PHPStan/Analyser/nsrt/bug-13851.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+namespace Bug13851;
+
+use function PHPStan\Testing\assertType;
+
+class Transaction
+{
+	public function __construct(public int $value)
+	{
+	}
+
+	public function withValue(int $value): self
+	{
+		return new self($value);
+	}
+}
+
+/**
+ * @return array<string, non-empty-list<Transaction>>
+ */
+function getPositions(): array
+{
+	return ['AAPL' => [new Transaction(50)], 'TSLA' => [new Transaction(50), new Transaction(100)]];
+}
+
+$positions = getPositions();
+
+foreach ($positions as $symbol => &$transactions) {
+	foreach ($transactions as &$transaction) {
+		$transaction = $transaction->withValue(60);
+	}
+}
+
+assertType('array<string, non-empty-list<Bug13851\Transaction>>', $positions);
---
+++ tests/PHPStan/Analyser/nsrt/bug-14084.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bug14084;
+
+use function PHPStan\Testing\assertType;
+
+/**
+ * @param array<string, list<string>> $convert
+ */
+function example(array $convert): void
+{
+	foreach ($convert as &$inner) {
+		foreach ($inner as &$val) {
+			$val = strtoupper($val); // https://github.com/phpstan/phpstan/issues/14083
+		}
+	}
+	assertType('array<string, list<string>>', $convert);
+}
+
+/**
+ * @param array<string, list<string>> $convert
+ */
+function example2(array $convert): void
+{
+	foreach ($convert as &$inner) {
+		foreach ($inner as $key => $val) {
+			$inner[$key] = strtoupper($val);
+		}
+	}
+	assertType('array<string, list<string>>', $convert);
+}
+
+/**
+ * @param array<string, list<string>> $convert
+ */
+function example3(array &$convert): void
+{
+	foreach ($convert as $outerKey => $inner) {
+		foreach ($inner as $key => $val) {
+			$convert[$outerKey][$key] = strtoupper($val);
+		}
+	}
+	assertType('array<string, list<string>>', $convert);
+}

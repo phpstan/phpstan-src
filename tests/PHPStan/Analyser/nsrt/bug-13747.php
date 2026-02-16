d58b3da885907fdb41a8b46c2699bf507b427e31
Markus Staab <maggus.staab@googlemail.com>
2026-02-16 07:52:18 +0100

    add tests

--- tests/PHPStan/Analyser/nsrt/bug-13747.php
+++ tests/PHPStan/Analyser/nsrt/bug-13747.php
@@ -74,6 +74,64 @@ class HelloWorld
 		}
 	}
 
+	/**
+	 * @param list<int> $listA
+	 * @param non-empty-list<string> $listB
+	 */
+	public function doMaybeBar($listA, $listB): void
+	{
+		if (rand(0,1)) {
+			$list = $listA;
+		} else {
+			$list = $listB;
+		}
+
+		if (2 <= count($list)) {
+			assertType('non-empty-list<int|string>&hasOffsetValue(1, int|string)', $list);
+			assertType('int<2, max>', count($list));
+		} else {
+			assertType('list<int|string>', $list);
+			assertType('int<0, 1>', count($list));
+		}
+	}
+
+	/**
+	 * @param array<int> $aArray
+	 * @param non-empty-list<string> $aList
+	 */
+	public function doMaybeArray($aArray, $aList): void
+	{
+		if (rand(0,1)) {
+			$listOrArray = $aArray;
+		} else {
+			$listOrArray = $aList;
+		}
+
+		if (2 <= count($listOrArray)) {
+			assertType('non-empty-array<int|string>', $listOrArray);
+			assertType('int<2, max>', count($listOrArray));
+		} else {
+			assertType('array<int|string>', $listOrArray);
+			assertType('int<0, 1>', count($listOrArray));
+		}
+	}
+
+	/** @param list<int> $list */
+	public function doMaybeEmpty($list): void
+	{
+		if (rand(0,1)) {
+			$list = [1, 2, 3];
+		}
+
+		if (2 <= count($list)) {
+			assertType('non-empty-list<int>&hasOffsetValue(1, int)', $list);
+			assertType('int<2, max>', count($list));
+		} else {
+			assertType('list<int>', $list);
+			assertType('int<0, 1>', count($list));
+		}
+	}
+
 	/** @param list<int> $list */
 	public function checkLimit($list): void
 	{
@@ -95,5 +153,42 @@ class HelloWorld
 			assertType('non-empty-list<int>', $list);
 		}
 
+	}
+
+	/** @param array<int> $array */
+	public function countArray($array): void
+	{
+		if (count($array) === 0) {
+			return;
+		}
+
+		if (count($array) > 2) {
+			assertType('bool', array_key_exists(-1, $array));
+			assertType('bool', array_key_exists(0, $array));
+			assertType('bool', array_key_exists(1, $array));
+			assertType('bool', array_key_exists(2, $array));
+			assertType('bool', array_key_exists(3, $array));
+
+			assertType('non-empty-array<int>', $array);
+			assertType('int<3, max>', count($array));
+		} else {
+			assertType('non-empty-array<int>', $array);
+		}
+		assertType('non-empty-array<int>', $array);
+
+		if (count($array, COUNT_NORMAL) > 2) {
+			assertType('non-empty-array<int>', $array);
+			assertType('int<3, max>', count($array, COUNT_NORMAL));
+		} else {
+			assertType('non-empty-array<int>', $array);
+		}
+
+		assertType('non-empty-array<int>', $array);
+		if (count($array, COUNT_RECURSIVE) > 2) { // COUNT_RECURSIVE on non-recursive array
+			assertType('non-empty-array<int>', $array);
+			assertType('int<3, max>', count($array, COUNT_RECURSIVE));
+		} else {
+			assertType('non-empty-array<int>', $array);
+		}
 	}
 }

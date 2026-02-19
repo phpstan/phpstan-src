0dc6560555e74a55cc96360c84c2080d46432f27
Markus Staab <maggus.staab@googlemail.com>
2026-02-19 10:03:55 +0100

    Implement ArrayKeyFirstLastTypeSpecifyingExtension

---
+++ tests/PHPStan/Analyser/nsrt/bug-14081.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Bug14081;
+
+use function PHPStan\Testing\assertType;
+
+/** @param list<string> $array */
+function first(array $array): mixed
+{
+	if (($key = array_key_first($array))) {
+		assertType('int<1, max>', $key);
+		assertType('non-empty-list<string>', $array);
+		assertType('string', $array[$key]);
+		return $array[$key];
+	}
+	return null;
+}
+
+/** @param list<string> $array */
+function last(array $array): mixed
+{
+	if (($key = array_key_last($array))) {
+		assertType('int<1, max>', $key);
+		assertType('non-empty-list<string>', $array);
+		assertType('string', $array[$key]);
+		return $array[$key];
+	}
+	return null;
+}

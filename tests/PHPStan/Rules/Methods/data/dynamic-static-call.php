d41f4f857c74427822af178e48a3528a1bec2e9b
Markus Staab <maggus.staab@googlemail.com>
2026-02-20 17:19:33 +0100

    Cover dynamic static calls in tests

---
+++ tests/PHPStan/Rules/Methods/data/dynamic-static-call.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace DynamicStaticCall;
+
+class SomeClass {}
+
+class Foo {
+	/** @phpstan-pure */
+	static public function doFoo():int
+	{
+		return 42;
+	}
+}
+
+final class FinalFoo {
+	/** @phpstan-pure */
+	static public function doFoo():int
+	{
+		return 42;
+	}
+}
+
+class Bar {
+	/** @phpstan-pure */
+	final static public function finalFoo():int
+	{
+		return 42;
+	}
+}
+
+
+class Baz {
+	function doBaz(Foo $foo, FinalFoo $finalFoo, Bar $bar):void {
+		$foo::doFoo();
+		$finalFoo::doFoo();
+		$bar::finalFoo();
+	}
+}

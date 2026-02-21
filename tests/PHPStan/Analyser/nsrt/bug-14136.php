518cb27d7997df7b6bd6e85d1f874f75d36374c6
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-02-21 18:48:40 +0100

    Nsrt

---
+++ tests/PHPStan/Analyser/nsrt/bug-14136.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Bug14136Nsrt;
+
+use function PHPStan\Testing\assertType;
+
+$xml = <<<'EOT'
+<?xml version="1.0"?><item>3</item>
+EOT;
+
+$xml = simplexml_load_string($xml);
+assertType('int', intval($xml));
+assertType('int', intval(gmp_init(42)));
+assertType('int', (int) ($xml));
+assertType('int', (int) (gmp_init(42)));
+
+assertType('float', floatval($xml));
+assertType('float', floatval(gmp_init(42)));
+assertType('float', (float) ($xml));
+assertType('float', (float) (gmp_init(42)));
+
+assertType('string', strval($xml));
+assertType('non-empty-string&numeric-string', strval(gmp_init(42)));
+assertType('non-empty-string&numeric-string', strval(new \BCMath\Number(99)));
+assertType('string', (string) ($xml));
+assertType('non-empty-string&numeric-string', (string) (gmp_init(42)));
+assertType('non-empty-string&numeric-string', (string) new \BCMath\Number(99));
+
+assertType('bool', boolval($xml));
+assertType('bool', boolval(gmp_init(0)));
+assertType('bool', boolval(new \BCMath\Number(0)));
+assertType('bool', (bool) ($xml));
+assertType('bool', (bool) (gmp_init(0)));
+assertType('bool', (bool) (new \BCMath\Number(0)));

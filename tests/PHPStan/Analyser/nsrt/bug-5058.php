aa8d2a5f3f9b0ec2aacf082c99a691d3d1d8a8e0
Markus Staab <markus.staab@redaxo.de>
2026-02-27 15:32:19 +0100

    Create bug-5058.php

---
+++ tests/PHPStan/Analyser/nsrt/bug-5058.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Bug5058;
+
+use function PHPStan\Testing\assertType;
+
+class test{
+
+	private string $properString;
+
+	public function doSomething(mixed $string): void
+	{
+
+		$errors = [];
+		if(is_string($string) === false){
+			$errors['string'] = 'fail';
+		}
+
+		if(empty($errors) === false){
+			throw new Exception('Epic fail');
+		}
+
+		assertType('string', $string);
+		$this->properString = $string;
+	}
+
+	public function getProperString(): string
+	{
+		return $this->properString;
+	}
+
+}

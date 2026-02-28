e4b026a5f004041d03cb8b6da9d52ddfc152cc03
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-02-28 13:25:53 +0100

    Add test

--- tests/PHPStan/Rules/Classes/data/bug-13440.php
+++ tests/PHPStan/Rules/Classes/data/bug-13440.php
@@ -35,3 +35,30 @@ class Box
         return new self($this->val, $cb);
     }
 }
+
+/**
+ * @template TVal
+ * @template TReturn
+ */
+class Box2
+{
+	/**
+	 * @param TVal $val
+	 * @param callable(Foo<TVal>): TReturn $cb
+	 */
+	public function __construct(
+		private mixed $val,
+		private $cb,
+	) {
+	}
+
+	/**
+	 * @template TNewReturn
+	 * @param callable(Foo<TVal>): TNewReturn $cb
+	 * @return self<TVal, TNewReturn>
+	 */
+	public function test($cb): self
+	{
+		return new self($this->val, $cb);
+	}
+}

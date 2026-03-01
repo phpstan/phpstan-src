6c3a01e49ca7a1ccea4ba9694442afe2bb52f267
Markus Staab <maggus.staab@googlemail.com>
2026-03-01 08:47:05 +0100

    test mix of undefined and int-ranges

--- tests/PHPStan/Analyser/nsrt/bug-14213.php
+++ tests/PHPStan/Analyser/nsrt/bug-14213.php
@@ -24,4 +24,25 @@ class HelloWorld
 
 		assertType('int<0, 5>|null', $x);
 	}
+
+	public static function coalesce_int_range_after_maybe_defined(): void
+	{
+		$x0 = $x1 = $x2 = null;
+
+		if (rand(0, 1)) {
+			$maybeDefined = 10;
+			$x0 = rand(0, 1);
+			$x1 = rand(2, 3);
+			$x2 = rand(4, 5);
+		}
+
+		$x = (
+			$maybeDefined ??
+			$x0 ??
+			$x1 ??
+			$x2
+		);
+
+		assertType('int<0, 5>|10|null', $x);
+	}
 }

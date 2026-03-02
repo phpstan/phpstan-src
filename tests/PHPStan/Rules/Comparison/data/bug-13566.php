f5f97764261aad5c80cdf32f37efe5c3f74983fe
Markus Staab <markus.staab@redaxo.de>
2026-03-02 10:38:13 +0100

    more tests

--- tests/PHPStan/Rules/Comparison/data/bug-13566.php
+++ tests/PHPStan/Rules/Comparison/data/bug-13566.php
@@ -2,7 +2,7 @@
 
 namespace Bug13566;
 
-class HelloWorld
+class ReturnViaBool
 {
 	/** @return ($exit is true ? never : void) */
 	public static function notFound(bool $exit = true): void
@@ -19,5 +19,72 @@ class HelloWorld
 	{
 		// send 404 header without exiting
 		self::notFound(false);
+	}
+}
+
+class ReturnViaInt
+{
+	/** @return ($exit is 1 ? never : void) */
+	public static function notFound(int $exit = 1): void
+	{
+		header('HTTP/1.1 404 Not Found', true, 404);
+
+		if ($exit == 1) {
+			echo '404 Not Found';
+			exit;
+		}
+	}
+
+
+	public function test(): void
+	{
+		// send 404 header
+		self::notFound(0);
+	}
+}
+
+class ReturnsMaybeNever
+{
+	/** @return ($exit is true ? never : 1) */
+	public static function notFound(bool $exit = true)
+	{
+		header('HTTP/1.1 404 Not Found', true, 404);
+
+		if ($exit) {
+			echo '404 Not Found';
+			exit;
+		}
+		return 1;
+	}
+
+
+	public function test(): void
+	{
+		// send 404 header
+		self::notFound(false);
+
+	}
+}
+
+class ReturnsMaybeVoid
+{
+	/** @return ($exit is true ? void : 1) */
+	public static function notFound(bool $exit = true)
+	{
+		header('HTTP/1.1 404 Not Found', true, 404);
+
+		if ($exit) {
+			echo '404 Not Found';
+			return;
+		}
+		return 1;
+	}
+
+
+	public function test(): void
+	{
+		// send 404 header
+		self::notFound(false);
+
 	}
 }

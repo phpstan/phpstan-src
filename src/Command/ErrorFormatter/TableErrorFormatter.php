541e9d3e9691b49c629c843b388333421e3ee1e8
Markus Staab <markus.staab@redaxo.de>
2026-03-05 15:06:21 +0100

    Update TableErrorFormatter.php

--- src/Command/ErrorFormatter/TableErrorFormatter.php
+++ src/Command/ErrorFormatter/TableErrorFormatter.php
@@ -29,7 +29,7 @@ use function str_replace;
 final class TableErrorFormatter implements ErrorFormatter
 {
 
-	private const MAX_ERRORS_TO_SHOW = 1000;
+	private const DEFAULT_MAX_ERRORS_TO_SHOW = 1000;
 
 	public function __construct(
 		private RelativePathHelper $relativePathHelper,
@@ -90,7 +90,7 @@ final class TableErrorFormatter implements ErrorFormatter
 
 		$errorsBudget = getenv('PHPSTAN_ERRORS_LIMIT');
 		if ($errorsBudget === false) {
-			$errorsBudget = self::MAX_ERRORS_TO_SHOW;
+			$errorsBudget = self::DEFAULT_MAX_ERRORS_TO_SHOW;
 		}
 		$errorsBudget = (int) $errorsBudget;
 

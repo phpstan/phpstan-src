11d1bbb6c6b90fa1f7c0812e385fa5d5bd9a29e8
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-03-07 12:44:58 +0100

    Fix

--- tests/PHPStan/Type/ArrayTypeTest.php
+++ tests/PHPStan/Type/ArrayTypeTest.php
@@ -362,7 +362,7 @@ class ArrayTypeTest extends PHPStanTestCase
 				new ConstantIntegerType(0),
 				new ConstantIntegerType(0),
 				new ArrayType(new MixedType(), new MixedType()),
-				'array<int<0, max>|string, mixed>',
+				'array<(int<0, max>|string), mixed>',
 			],
 		];
 	}

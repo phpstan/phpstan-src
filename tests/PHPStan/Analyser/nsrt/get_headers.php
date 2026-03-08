60b0b1a990a226d34290fcd7ef422f3cda4407e3
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-03-08 10:14:06 +0100

    Add test

---
+++ tests/PHPStan/Analyser/nsrt/get_headers.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types = 1);
+
+namespace Bug13835;
+
+use function PHPStan\Testing\assertType;
+
+assertType('list<string>|false', get_headers('http://example.com'));
+assertType('array<int|string, list<string>>|string|false', get_headers('http://example.com', true));
+assertType('list<string>|false', get_headers('http://example.com', false));

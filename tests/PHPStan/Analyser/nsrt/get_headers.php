0ccc707a680edf20ecbc3e0e0bd229fb80759b48
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-03-08 10:32:30 +0100

    Fix

--- tests/PHPStan/Analyser/nsrt/get_headers.php
+++ tests/PHPStan/Analyser/nsrt/get_headers.php
@@ -5,5 +5,5 @@ namespace Bug13835;
 use function PHPStan\Testing\assertType;
 
 assertType('list<string>|false', get_headers('http://example.com'));
-assertType('array<int|string, list<string>>|string|false', get_headers('http://example.com', true));
+assertType('array<int|string, list<string>|string>|false', get_headers('http://example.com', true));
 assertType('list<string>|false', get_headers('http://example.com', false));

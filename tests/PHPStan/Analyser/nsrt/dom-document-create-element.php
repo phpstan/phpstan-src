824bfa835ae104907393c78b102f1373c61320cc
Jan Nedbal <janedbal@gmail.com>
2026-03-10 16:50:05 +0100

    Narrow DOMDocument::createElement() return type for valid constant names
    Fixes phpstan/phpstan#13792

---
+++ tests/PHPStan/Analyser/nsrt/dom-document-create-element.php
@@ -0,0 +1,48 @@
+<?php declare(strict_types = 1);
+
+namespace DomDocumentCreateElement;
+
+use DOMDocument;
+use function PHPStan\Testing\assertType;
+
+class Foo
+{
+
+	public function dynamicName(DOMDocument $doc, string $name): void
+	{
+		assertType('DOMElement|false', $doc->createElement($name));
+	}
+
+	public function validConstantNames(DOMDocument $doc): void
+	{
+		assertType('DOMElement', $doc->createElement('div'));
+		assertType('DOMElement', $doc->createElement('my-element'));
+		assertType('DOMElement', $doc->createElement('ns:tag'));
+		assertType('DOMElement', $doc->createElement('_private'));
+		assertType('DOMElement', $doc->createElement('h1'));
+	}
+
+	public function invalidConstantNames(DOMDocument $doc): void
+	{
+		assertType('DOMElement|false', $doc->createElement(''));
+		assertType('DOMElement|false', $doc->createElement('123element'));
+		assertType('DOMElement|false', $doc->createElement('my element'));
+	}
+
+	/**
+	 * @param 'div'|'span' $validUnion
+	 * @param 'div'|'' $mixedUnion
+	 */
+	public function unions(DOMDocument $doc, string $validUnion, string $mixedUnion): void
+	{
+		assertType('DOMElement', $doc->createElement($validUnion));
+		assertType('DOMElement|false', $doc->createElement($mixedUnion));
+	}
+
+	public function localVariable(DOMDocument $doc): void
+	{
+		$name = 'paragraph';
+		assertType('DOMElement', $doc->createElement($name));
+	}
+
+}

89b7e85a1b99d8df4ff36f4997813c9ecf7c1cb7
Markus Staab <maggus.staab@googlemail.com>
2026-03-11 10:05:05 +0100

    re-add tests

--- tests/PHPStan/Analyser/data/bug-12246.php
+++ tests/PHPStan/Analyser/data/bug-12246.php
@@ -1,18 +1,54 @@
-<?php declare(strict_types = 1);
+@ -2,7 +2,7 @@
 
 namespace Bug12246;
 
 final class SkipFirstClassCallableInDo
+final class FirstClassCallableInDo
+{
+	public function getSubscribedEvents(): void
+	{
+@ -16,3 +16,44 @@ final class SkipFirstClassCallableInDo
+		return 1;
+	}
+}
+
+final class StaticFirstClassCallableInDo
 {
 	public function getSubscribedEvents(): void
 	{
 		do {
 
-		} while ($this->textElement(...));
+		} while (self::textElement(...));
 	}
 
-	public function textElement(): int
+	static public function textElement(): int
 	{
 		return 1;
 	}
 }
+
+final class FunctionFirstClassCallableInDo
+{
+	public function getSubscribedEvents(): void
+	{
+		do {
+
+		} while (doFoo(...));
+	}
+}
+
+function doFoo():int {
+	return 1;
+}
+
+final class InstationFirstClassCallableInDo
+{
+	public function getSubscribedEvents(): void
+	{
+		do {
+
+		} while (new Foo(...));
+	}
+}
+
+class Foo {}

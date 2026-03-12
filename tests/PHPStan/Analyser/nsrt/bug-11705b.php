2603236b4862ff5efcb5a6d5d3864b797a36b1fb
Markus Staab <markus.staab@redaxo.de>
2026-03-12 11:09:06 +0100

    added failling test

---
+++ tests/PHPStan/Analyser/nsrt/bug-11705b.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types = 1);
+
+namespace Bug11705;
+
+use function PHPStan\Testing\assertType;
+
+/**
+ * @param array{'name':string,'owners':array<int,string>, 'optional'?: string} $theInput
+ * @param array<int,string> $theTags
+ */
+function example(array $theInput, array $theTags): void
+{
+	foreach ($theTags as $tag) {
+		if (!array_key_exists($tag, $theInput)) {
+			continue;
+		}
+		switch ($tag) {
+			case 'name':
+				assertType("'name'", $tag);
+				assertType('string', $theInput[$tag]);
+				if ($tag === 'name') {
+					echo "Of course it is...";
+				}
+				assertType("'name'", $tag);
+				assertType('string', $theInput[$tag]);
+				break;
+			default:
+				// fall out
+		}
+	}
+}

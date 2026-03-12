14a21546b891127a47e067495f1acc6f6bf52112
Markus Staab <maggus.staab@googlemail.com>
2026-03-12 07:23:52 +0100

    Revert "Apply symmetric fix for integer range generalization in gotGreater && gotSmaller case"
    This reverts commit 92fe3884cc99f10061ecd97ad39f78a2540b8528.

--- src/Analyser/MutatingScope.php
+++ src/Analyser/MutatingScope.php
@@ -4204,19 +4204,7 @@ class MutatingScope implements Scope, NodeCallbackInvoker
 					}
 
 					if ($gotGreater && $gotSmaller) {
-						$newMin = $min;
-						$newMax = $max;
-						foreach ($integerRanges['b'] as $range) {
-							$rangeMin = $range->getMin();
-							$rangeMax = $range->getMax();
-							if ($newMin !== null && ($rangeMin === null || $rangeMin < $newMin)) {
-								$newMin = $rangeMin;
-							}
-							if ($newMax !== null && ($rangeMax === null || $rangeMax > $newMax)) {
-								$newMax = $rangeMax;
-							}
-						}
-						$resultTypes[] = IntegerRangeType::fromInterval($newMin, $newMax);
+						$resultTypes[] = new IntegerType();
 					} elseif ($gotGreater) {
 						$resultTypes[] = IntegerRangeType::fromInterval($min, null);
 					} elseif ($gotSmaller) {

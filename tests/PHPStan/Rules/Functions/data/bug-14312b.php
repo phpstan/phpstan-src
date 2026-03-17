ab8c5579b7a4e7b3d2a93023693fcfe09388e4a0
Markus Staab <markus.staab@redaxo.de>
2026-03-17 16:48:43 +0100

    fix test

--- tests/PHPStan/Rules/Functions/data/bug-14312b.php
+++ tests/PHPStan/Rules/Functions/data/bug-14312b.php
@@ -7,7 +7,7 @@ function get_something(): mixed { return null; }
 function test(string $needle): bool {
 	$o = get_something();
 	if (array_search($needle, $o) !== false) {
-		if (array_key_exists($needle, $o)) {
+		if (array_search($needle, $o)) {
 			return true;
 		}
 	}

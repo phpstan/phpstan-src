f99e3b397c3d4e1dd43e1aef73f52730a38439e3
Markus Staab <maggus.staab@googlemail.com>
2026-03-18 08:05:05 +0100

    simplify test

--- tests/PHPStan/Rules/Variables/data/bug-14318.php
+++ tests/PHPStan/Rules/Variables/data/bug-14318.php
@@ -115,57 +115,25 @@ class HelloWorld8
 }
 
 /**
- * @throws \RuntimeException
+ * @param array<string>|null $reasons
+ * @throws \Exception
  */
-function maybeThrows9(string $s): void
-{
-	if (random_int(0, 1) === 1) {
-		throw new \RuntimeException();
+function check1(array &$reasons = null): void {
+	$fileName = time() % 2 ? "abc":null;
+	if (!$fileName) {
+		$reasons[] = sprintf("Dependency check fail");
+		throw new \Exception("check failed");
 	}
 }
 
-/**
- * @throws \RuntimeException
- */
-function maybeThrows10(int $s): void
-{
-	if (random_int(0, 1) === 1) {
-		throw new \RuntimeException();
-	}
-}
-
-class HelloWorld9
-{
-	public function test9(): void
-	{
-		global $pdo;
-
-		try {
-			maybeThrows9($sql = "SELECT * FROM foo");
-			$rs = $pdo->query($sql);
-			if ($result = $rs->fetch(\PDO::FETCH_ASSOC)) {
-				// do something
-			}
-		} catch (\PDOException $e) {
-			var_dump($sql);
+function test1():void {
+	try {
+		check1($reasons);
+		printf("ok\n");
+	} catch (\Exception $e) {
+		if (!empty($reasons)) {
+			$e = new \Exception("Dependency check failed: " . implode(', ', $reasons), 0, $e);
 		}
-	}
-}
-
-class HelloWorld10
-{
-	public function test10(): void
-	{
-		global $pdo;
-
-		try {
-			maybeThrows10(strlen($sql = "SELECT * FROM foo"));
-			$rs = $pdo->query($sql);
-			if ($result = $rs->fetch(\PDO::FETCH_ASSOC)) {
-				// do something
-			}
-		} catch (\PDOException $e) {
-			var_dump($sql);
-		}
+		throw new \Exception("Failed", 0, $e);
 	}
 }

b15cf90e93037f09e2b25aef2b2dc43c4cb6d6de
Markus Staab <maggus.staab@googlemail.com>
2026-03-18 08:06:56 +0100

    move test

--- tests/PHPStan/Rules/Variables/data/bug-14318.php
+++ tests/PHPStan/Rules/Variables/data/bug-14318.php
@@ -113,27 +113,3 @@ class HelloWorld8
 		}
 	}
 }
-
-/**
- * @param array<string>|null $reasons
- * @throws \Exception
- */
-function check1(array &$reasons = null): void {
-	$fileName = time() % 2 ? "abc":null;
-	if (!$fileName) {
-		$reasons[] = sprintf("Dependency check fail");
-		throw new \Exception("check failed");
-	}
-}
-
-function test1():void {
-	try {
-		check1($reasons);
-		printf("ok\n");
-	} catch (\Exception $e) {
-		if (!empty($reasons)) {
-			$e = new \Exception("Dependency check failed: " . implode(', ', $reasons), 0, $e);
-		}
-		throw new \Exception("Failed", 0, $e);
-	}
-}
--- tests/PHPStan/Rules/Variables/data/bug-7806.php
+++ tests/PHPStan/Rules/Variables/data/bug-7806.php
@@ -2,7 +2,7 @@
 
 namespace Bug7806;
 
-class Test {
+class TestMethod {
 	/**
 	 * @param array<string>|null $reasons
 	 * @throws \Exception
@@ -27,3 +27,28 @@ class Test {
 		}
 	}
 }
+
+/**
+ * @param array<string>|null $reasons
+ * @throws \Exception
+ */
+function check1(array &$reasons = null): void {
+	$fileName = time() % 2 ? "abc":null;
+	if (!$fileName) {
+		$reasons[] = sprintf("Dependency check fail");
+		throw new \Exception("check failed");
+	}
+}
+
+function test1():void {
+	try {
+		check1($reasons);
+		printf("ok\n");
+	} catch (\Exception $e) {
+		if (!empty($reasons)) {
+			$e = new \Exception("Dependency check failed: " . implode(', ', $reasons), 0, $e);
+		}
+		throw new \Exception("Failed", 0, $e);
+	}
+}
+

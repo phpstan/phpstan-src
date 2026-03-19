889361fd54259de96568ddc42e6baee00cae5c08
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2026-03-19 16:57:53 +0100

    Add non regression test (#5252)

---
+++ tests/PHPStan/Analyser/nsrt/shopware-connection-profiler.php
@@ -0,0 +1,43 @@
+<?php declare(strict_types=1);
+
+namespace Shopware\Core\Profiling\Doctrine;
+
+use function PHPStan\Testing\assertType;
+
+class ParameterType {}
+
+/**
+ * @phpstan-type SanitizedQueryInfo array{sql: string, executionMS: float, types: array<(int | string), ParameterType|int>}
+ */
+abstract class ConnectionProfiler
+{
+	/**
+	 * @return array<string, array<int, SanitizedQueryInfo>>
+	 */
+	abstract public function getQueries(): array;
+
+	public function getGroupedQueries(): void
+	{
+		foreach ($this->getQueries() as $queries) {
+			$connectionGroupedQueries = [];
+			foreach ($queries as $i => $query) {
+				$key = $query['sql'];
+				if (!isset($connectionGroupedQueries[$key])) {
+					$connectionGroupedQueries[$key] = $query;
+					$connectionGroupedQueries[$key]['executionMS'] = 0;
+					$connectionGroupedQueries[$key]['count'] = 0;
+					$connectionGroupedQueries[$key]['index'] = $i; // "Explain query" relies on query index in 'queries'.
+				}
+
+				assertType("array<string, array{sql: string, executionMS: 0, types: array<int|string, int|Shopware\Core\Profiling\Doctrine\ParameterType>, count: 0, index: int}|array{sql: string, executionMS: float, types: array<int|string, int|Shopware\Core\Profiling\Doctrine\ParameterType>, count: int<1, max>, index: int}>", $connectionGroupedQueries);
+				$connectionGroupedQueries[$key]['executionMS'] += $query['executionMS'];
+				assertType("non-empty-array<string, array{sql: string, executionMS: float, types: array<int|string, int|Shopware\Core\Profiling\Doctrine\ParameterType>, count: int<0, max>, index: int}>", $connectionGroupedQueries);
+				++$connectionGroupedQueries[$key]['count'];
+			}
+
+			assertType("array<string, array{sql: string, executionMS: float, types: array<int|string, int|Shopware\Core\Profiling\Doctrine\ParameterType>, count: int<1, max>, index: int}>", $connectionGroupedQueries);
+			usort($connectionGroupedQueries, static fn (array $a, array $b): int => $b['executionMS'] <=> $a['executionMS']);
+		}
+	}
+
+}

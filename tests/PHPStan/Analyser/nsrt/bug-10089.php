d57d50b0095eecc9352793a08363fe52d0d1160f
Markus Staab <maggus.staab@googlemail.com>
2026-03-21 16:21:30 +0100

    more asserts

--- tests/PHPStan/Analyser/nsrt/bug-10089.php
+++ tests/PHPStan/Analyser/nsrt/bug-10089.php
@@ -11,8 +11,10 @@ class Test
 	protected function create_matrix(int $size): array
 	{
 		$size = min(8, $size);
+		assertType('int<min, 8>', $size);
 		$matrix = [];
 		for ($i = 0; $i < $size; $i++) {
+			assertType('int<0, 7>', $i);
 			$matrix[] = array_fill(0, $size, 0);
 		}
 

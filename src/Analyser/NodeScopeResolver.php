677ca44fe6c0da039daf9e9e46636c5b89d99191
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-03-30 13:07:11 +0200

    Preserve vacuously true conditional expressions during scope merging

--- src/Analyser/NodeScopeResolver.php
+++ src/Analyser/NodeScopeResolver.php
@@ -1146,7 +1146,7 @@ class NodeScopeResolver
 					$throwPoints = array_merge($throwPoints, $branchScopeStatementResult->getThrowPoints());
 					$impurePoints = array_merge($impurePoints, $branchScopeStatementResult->getImpurePoints());
 					$branchScope = $branchScopeStatementResult->getScope();
-					$finalScope = $branchScopeStatementResult->isAlwaysTerminating() ? $finalScope : $branchScope->mergeWith($finalScope);
+					$finalScope = $branchScopeStatementResult->isAlwaysTerminating() ? $finalScope : $branchScope->mergeWith($finalScope, true);
 					$alwaysTerminating = $alwaysTerminating && $branchScopeStatementResult->isAlwaysTerminating();
 					if (count($branchScopeStatementResult->getEndStatements()) > 0) {
 						$endStatements = array_merge($endStatements, $branchScopeStatementResult->getEndStatements());
@@ -1170,7 +1170,7 @@ class NodeScopeResolver
 
 			if ($stmt->else === null) {
 				if (!$ifAlwaysTrue && !$lastElseIfConditionIsTrue) {
-					$finalScope = $scope->mergeWith($finalScope);
+					$finalScope = $scope->mergeWith($finalScope, true);
 					$alwaysTerminating = false;
 				}
 			} else {
@@ -1182,7 +1182,7 @@ class NodeScopeResolver
 					$throwPoints = array_merge($throwPoints, $branchScopeStatementResult->getThrowPoints());
 					$impurePoints = array_merge($impurePoints, $branchScopeStatementResult->getImpurePoints());
 					$branchScope = $branchScopeStatementResult->getScope();
-					$finalScope = $branchScopeStatementResult->isAlwaysTerminating() ? $finalScope : $branchScope->mergeWith($finalScope);
+					$finalScope = $branchScopeStatementResult->isAlwaysTerminating() ? $finalScope : $branchScope->mergeWith($finalScope, true);
 					$alwaysTerminating = $alwaysTerminating && $branchScopeStatementResult->isAlwaysTerminating();
 					if (count($branchScopeStatementResult->getEndStatements()) > 0) {
 						$endStatements = array_merge($endStatements, $branchScopeStatementResult->getEndStatements());
--- tests/PHPStan/Rules/Variables/DefinedVariableRuleTest.php
+++ tests/PHPStan/Rules/Variables/DefinedVariableRuleTest.php
@@ -911,12 +911,7 @@ class DefinedVariableRuleTest extends RuleTestCase
 		$this->polluteScopeWithLoopInitialAssignments = false;
 		$this->checkMaybeUndefinedVariables = true;
 		$this->polluteScopeWithAlwaysIterableForeach = true;
-		$this->analyse([__DIR__ . '/data/bug-4173.php'], [
-			[
-				'Variable $value might not be defined.', // could be fixed
-				30,
-			],
-		]);
+		$this->analyse([__DIR__ . '/data/bug-4173.php'], []);
 	}
 
 	public function testBug5805(): void
@@ -1119,28 +1114,12 @@ class DefinedVariableRuleTest extends RuleTestCase
 				18,
 			],
 			[
-				'Variable $foo might not be defined.',
-				36,
-			],
-			[
-				'Variable $foo might not be defined.',
-				37,
-			],
-			[
-				'Variable $bar might not be defined.',
+				'Undefined variable: $bar',
 				38,
 			],
 			[
-				'Variable $bar might not be defined.',
-				40,
-			],
-			[
-				'Variable $foo might not be defined.',
+				'Undefined variable: $foo',
 				41,
-			],
-			[
-				'Variable $bar might not be defined.',
-				42,
 			],
 			[
 				'Undefined variable: $buz',
@@ -1156,14 +1135,6 @@ class DefinedVariableRuleTest extends RuleTestCase
 			],
 			[
 				'Undefined variable: $buz',
-				49,
-			],
-			[
-				'Variable $bar might not be defined.',
-				49,
-			],
-			[
-				'Variable $foo might not be defined.',
 				49,
 			],
 			[
@@ -1487,6 +1458,24 @@ class DefinedVariableRuleTest extends RuleTestCase
 		$this->analyse([__DIR__ . '/data/bug-13920.php'], []);
 	}
 
+	public function testBug12992(): void
+	{
+		$this->cliArgumentsVariablesRegistered = true;
+		$this->polluteScopeWithLoopInitialAssignments = false;
+		$this->checkMaybeUndefinedVariables = true;
+		$this->polluteScopeWithAlwaysIterableForeach = true;
+		$this->analyse([__DIR__ . '/data/bug-12992.php'], []);
+	}
+
+	public function testBug14227(): void
+	{
+		$this->cliArgumentsVariablesRegistered = true;
+		$this->polluteScopeWithLoopInitialAssignments = false;
+		$this->checkMaybeUndefinedVariables = true;
+		$this->polluteScopeWithAlwaysIterableForeach = true;
+		$this->analyse([__DIR__ . '/data/bug-14227.php'], []);
+	}
+
 	public function testBug14117(): void
 	{
 		$this->cliArgumentsVariablesRegistered = true;
@@ -1495,10 +1484,6 @@ class DefinedVariableRuleTest extends RuleTestCase
 		$this->polluteScopeWithAlwaysIterableForeach = true;
 
 		$this->analyse([__DIR__ . '/data/bug-14117.php'], [
-			[
-				'Variable $value might not be defined.',
-				33,
-			],
 			[
 				'Variable $value might not be defined.',
 				49,

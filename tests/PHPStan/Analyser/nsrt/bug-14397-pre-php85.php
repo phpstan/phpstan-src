0608ee0307d3c71e5d1597cc46a0bc9d3b2c7b7c
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-03-30 12:17:25 +0200

    Update tests

--- tests/PHPStan/Analyser/nsrt/bug-14397-pre-php85.php
+++ tests/PHPStan/Analyser/nsrt/bug-14397-pre-php85.php
@@ -4,13 +4,21 @@ declare(strict_types = 1);
 
 namespace Bug14397PrePhp85;
 
+use PHPStan\TrinaryLogic;
+
 use function filter_var;
 use function PHPStan\Testing\assertType;
+use function PHPStan\Testing\assertVariableCertainty;
 
 function test(mixed $mixed): void
 {
-	// On PHP < 8.5, FILTER_THROW_ON_FAILURE doesn't truly exist
-	// so it shouldn't remove false from the return type
-	assertType('int|false', filter_var($mixed, FILTER_VALIDATE_INT, FILTER_THROW_ON_FAILURE));
-	assertType('int|false', filter_var($mixed, FILTER_VALIDATE_INT, ['flags' => FILTER_THROW_ON_FAILURE]));
+	try {
+		filter_var($mixed, FILTER_VALIDATE_INT, FILTER_FLAG_GLOBAL_RANGE);
+		$foo = 1;
+	} catch (\Filter\FilterFailedException $e) {
+		assertVariableCertainty(TrinaryLogic::createYes(), $foo);
+	}
+
+	assertType('int|false', filter_var($mixed, FILTER_VALIDATE_INT, FILTER_FLAG_GLOBAL_RANGE));
+	assertType('int|false', filter_var($mixed, FILTER_VALIDATE_INT, ['flags' => FILTER_FLAG_GLOBAL_RANGE]));
 }
--- tests/PHPStan/Rules/Functions/data/filter_var_null_and_throw_pre_php85.php
+++ tests/PHPStan/Rules/Functions/data/filter_var_null_and_throw_pre_php85.php
@@ -2,15 +2,13 @@
 
 namespace FilterVarNullAndThrowPrePhp85;
 
-// On PHP < 8.5, FILTER_THROW_ON_FAILURE doesn't truly exist
-// so combining it with FILTER_NULL_ON_FAILURE should not be an error
-filter_var('foo@bar.test', FILTER_VALIDATE_EMAIL, FILTER_THROW_ON_FAILURE|FILTER_NULL_ON_FAILURE);
+filter_var('foo@bar.test', FILTER_VALIDATE_EMAIL, FILTER_FLAG_GLOBAL_RANGE|FILTER_NULL_ON_FAILURE);
 
-$flag = FILTER_NULL_ON_FAILURE|FILTER_THROW_ON_FAILURE;
+$flag = FILTER_NULL_ON_FAILURE|FILTER_FLAG_GLOBAL_RANGE;
 filter_var(100, FILTER_VALIDATE_INT, $flag);
 
 filter_var(
 	'johndoe',
 	FILTER_VALIDATE_REGEXP,
-	['options' => ['regexp' => '/^[a-z]+$/'], 'flags' => FILTER_THROW_ON_FAILURE|FILTER_NULL_ON_FAILURE]
+	['options' => ['regexp' => '/^[a-z]+$/'], 'flags' => FILTER_FLAG_GLOBAL_RANGE|FILTER_NULL_ON_FAILURE]
 );

a5c5252519e1d14b03d3831ce2c1772264235ca8
Markus Staab <maggus.staab@googlemail.com>
2026-04-02 11:00:07 +0200

    add failling test

---
+++ tests/PHPStan/Analyser/nsrt/pr-5379.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PR5379;
+
+use function PHPStan\Testing\assertType;
+
+class AggregationParser
+{
+	/**
+	 * @param array<string, mixed> $aggregation
+	 */
+	private function parseAggregation(array $aggregation)
+	{
+		$type = $aggregation['type'] ?? null;
+		if (!\is_string($type) || empty($type) || is_numeric($type)) {
+			return null;
+		}
+
+		if (empty($aggregation['field']) && $type !== 'filter') {
+			return null;
+		}
+
+		$field = '';
+		if ($type !== 'filter') {
+			$field = self::buildFieldName();
+		}
+
+		assertType('non-empty-string', $type);
+	}
+
+	private static function buildFieldName(): string
+	{
+		return 'field';
+	}
+}
+

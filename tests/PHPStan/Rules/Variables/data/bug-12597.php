dde1b67dffd90b041d6a821c16c589d2c2ba5abf
Markus Staab <maggus.staab@googlemail.com>
2026-04-02 14:53:41 +0200

    move test

--- tests/PHPStan/Rules/Variables/data/bug-12597.php
+++ tests/PHPStan/Rules/Variables/data/bug-12597.php
@@ -20,33 +20,3 @@ class HelloWorld
 
 	public function message(string $message): void {}
 }
-
-class HelloWorld2
-{
-	public function test(mixed $type): void
-	{
-		if (is_int($type) || is_object($type)) {
-			$message = 'Hello!';
-		}
-
-		if (is_int($type)) {
-			$this->message($message);
-		}
-	}
-
-	public function test2(mixed $type): void
-	{
-		if ($type instanceof Foo || $type instanceof Bar) {
-			$message = 'Hello!';
-		}
-
-		if ($type instanceof Foo) {
-			$this->message($message);
-		}
-	}
-
-	public function message(string $message): void
-	{
-	}
-}
-

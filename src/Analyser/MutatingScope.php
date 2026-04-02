9ede8088ed54c464e32bd1912a60881ef44e86e6
Markus Staab <maggus.staab@googlemail.com>
2026-04-02 14:46:55 +0200

    Update MutatingScope.php

--- src/Analyser/MutatingScope.php
+++ src/Analyser/MutatingScope.php
@@ -3233,7 +3233,7 @@ class MutatingScope implements Scope, NodeCallbackInvoker
 							$conditionalExpression->getTypeHolder()->getCertainty()->yes()
 							&& $specifiedHolder->getCertainty()->yes()
 							&& $conditionalTypeHolder->getCertainty()->yes()
-							&& ($guardType->isArray()->no())
+							&& $guardType->isArray()->no()
 							&& $guardType->isSuperTypeOf($specifiedType)->yes()
 						) {
 							$subtypeMatch = true;

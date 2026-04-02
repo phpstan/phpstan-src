642648209f2211811a41e1d1c9d614e70e849cd4
shmax <shmax@shmax.com>
2026-04-02 07:49:32 -0700

    remove

--- src/Type/TypeAliasApplicationType.php
+++
@@ -1,5 +0,0 @@
-<?php declare(strict_types = 1);
-
-// @deprecated Use GenericTypeAliasType instead.
-class_alias(\PHPStan\Type\GenericTypeAliasType::class, \PHPStan\Type\TypeAliasApplicationType::class);
-

ac40a6fde166b1821e249a37112526fef12a9fcb
VincentLanglet <9052536+VincentLanglet@users.noreply.github.com>
2026-04-08 14:50:02 +0000

    Add regression test for readonly class property modification via clone()
    - New test in tests/PHPStan/Rules/Properties/data/bug-14063.php covering readonly class with clone() property overrides
    - Tests AccessPropertiesInAssignRule correctly reports "Assign to protected(set) property" when modifying readonly class properties via clone() from outside the declaring class
    - Tests ReadOnlyPropertyAssignRule does not double-report (defers to AccessPropertiesInAssignRule)
    - The underlying fix was in BetterReflection 6.70 which correctly sets isProtectedSet() for properties in readonly classes
    
    Closes https://github.com/phpstan/phpstan/issues/14063

---
+++ tests/PHPStan/Rules/Properties/data/bug-14063.php
@@ -0,0 +1,39 @@
+<?php // lint >= 8.5
+
+declare(strict_types = 1);
+
+namespace Bug14063;
+
+final readonly class Obj
+{
+	public function __construct(public string $value) {}
+
+	public function withValue(string $newValue): self
+	{
+		return clone($this, ['value' => $newValue]);
+	}
+}
+
+readonly class Base
+{
+	public function __construct(public string $value) {}
+}
+
+class Child extends Base
+{
+	public function withValue(string $newValue): self
+	{
+		return clone($this, ['value' => $newValue]);
+	}
+}
+
+$obj = new Obj('val');
+$newObj = clone($obj, ['value' => 'newVal']);
+
+function test(Obj $obj): void {
+	clone($obj, ['value' => 'newVal']);
+}
+
+function testBase(Base $base): void {
+	clone($base, ['value' => 'newVal']);
+}

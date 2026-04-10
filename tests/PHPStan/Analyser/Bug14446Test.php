19038be0ffc7d64d803dc5507cd342f1e2c2a9be
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-04-10 10:18:24 +0200

    Simplify

--- tests/PHPStan/Analyser/Bug14446Test.php
+++ tests/PHPStan/Analyser/Bug14446Test.php
@@ -2,7 +2,6 @@
 
 namespace PHPStan\Analyser;
 
-use PHPStan\File\FileHelper;
 use PHPStan\Testing\TypeInferenceTestCase;
 use PHPUnit\Framework\Attributes\DataProvider;
 
@@ -32,21 +31,6 @@ class Bug14446Test extends TypeInferenceTestCase
 		return [
 			__DIR__ . '/bug-14446.neon',
 		];
-	}
-
-	public function testRule(): void
-	{
-		$file = self::getContainer()->getByType(FileHelper::class)->normalizePath(__DIR__ . '/data/bug-14446-rule.php');
-
-		$analyser = self::getContainer()->getByType(Analyser::class);
-		$finalizer = self::getContainer()->getByType(AnalyserResultFinalizer::class);
-		$errors = $finalizer->finalize(
-			$analyser->analyse([$file], null, null, true),
-			false,
-			true,
-		)->getErrors();
-
-		$this->assertNoErrors($errors);
 	}
 
 }

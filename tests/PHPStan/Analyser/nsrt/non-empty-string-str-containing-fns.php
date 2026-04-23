98590b0ccc91aa8994ba7110e97929104a28154a
Markus Staab <markus.staab@redaxo.de>
2026-04-23 15:43:01 +0200

    Improve inference for truethy comparisons

--- tests/PHPStan/Analyser/nsrt/non-empty-string-str-containing-fns.php
+++ tests/PHPStan/Analyser/nsrt/non-empty-string-str-containing-fns.php
@@ -100,7 +100,7 @@ class Foo {
 		assertType('string', $s);
 
 		if (strpos($s, ':') === 5) {
-			assertType('string', $s); // could be non-empty-string
+			assertType('non-empty-string', $s);
 		}
 		assertType('string', $s);
 		if (strpos($s, ':') !== 5) {
@@ -152,7 +152,7 @@ class Foo {
 		assertType('string', $s);
 
 		if (mb_strpos($s, ':') === 5) {
-			assertType('string', $s); // could be non-empty-string
+			assertType('non-falsy-string', $s);
 		}
 		assertType('string', $s);
 		if (mb_strpos($s, ':') !== 5) {

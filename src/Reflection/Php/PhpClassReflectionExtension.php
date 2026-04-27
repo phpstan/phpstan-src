fe750c5587a75b175150feeade5894c89918134f
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2026-04-27 10:25:14 +0200

    Declare Memcached methods as impure (#5536)

--- src/Reflection/Php/PhpClassReflectionExtension.php
+++ src/Reflection/Php/PhpClassReflectionExtension.php
@@ -618,6 +618,12 @@ final class PhpClassReflectionExtension
 			$acceptsNamedArguments = true;
 			$selfOutType = null;
 			$phpDocComment = null;
+
+			$isPure = null;
+			if ($this->signatureMapProvider->hasMethodMetadata($declaringClassName, $methodReflection->getName())) {
+				$isPure = !$this->signatureMapProvider->getMethodMetadata($declaringClassName, $methodReflection->getName())['hasSideEffects'];
+			}
+
 			$methodSignaturesResult = $this->signatureMapProvider->getMethodSignatures($declaringClassName, $methodReflection->getName(), $methodReflection);
 			foreach ($methodSignaturesResult as $signatureType => $methodSignatures) {
 				if ($methodSignatures === null) {
@@ -693,6 +699,7 @@ final class PhpClassReflectionExtension
 
 						$asserts = Assertions::createFromResolvedPhpDocBlock($currentResolvedPhpDoc);
 						$acceptsNamedArguments = $currentResolvedPhpDoc->acceptsNamedArguments();
+						$isPure ??= $currentResolvedPhpDoc->isPure();
 
 						$selfOutTypeTag = $currentResolvedPhpDoc->getSelfOutTag();
 						if ($selfOutTypeTag !== null) {
@@ -727,11 +734,15 @@ final class PhpClassReflectionExtension
 				}
 			}
 
-			if ($this->signatureMapProvider->hasMethodMetadata($declaringClassName, $methodReflection->getName())) {
-				$hasSideEffects = TrinaryLogic::createFromBoolean($this->signatureMapProvider->getMethodMetadata($declaringClassName, $methodReflection->getName())['hasSideEffects']);
-			} else {
-				$hasSideEffects = TrinaryLogic::createMaybe();
+			if ($isPure === null) {
+				$classResolvedPhpDoc = $declaringClass->getResolvedPhpDoc();
+				if ($classResolvedPhpDoc !== null && $classResolvedPhpDoc->areAllMethodsPure()) {
+					$isPure = true;
+				} elseif ($classResolvedPhpDoc !== null && $classResolvedPhpDoc->areAllMethodsImpure()) {
+					$isPure = false;
+				}
 			}
+
 			return new NativeMethodReflection(
 				$this->reflectionProviderProvider->getReflectionProvider(),
 				$declaringClass,
@@ -739,7 +750,7 @@ final class PhpClassReflectionExtension
 				$currentResolvedPhpDoc ?? null,
 				$variantsByType['positional'],
 				$variantsByType['named'] ?? null,
-				$hasSideEffects,
+				$isPure !== null ? TrinaryLogic::createFromBoolean(!$isPure) : TrinaryLogic::createMaybe(),
 				$throwType,
 				$asserts,
 				$acceptsNamedArguments,

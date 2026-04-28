6159843e047b571c709951e52ce11fb8f349a646
Nicolai Ehrhardt <245527909+predictor2718@users.noreply.github.com>
2026-04-28 21:39:02 +0200

    Fix phpstan/phpstan#13539: property.notFound in chained isset() with checkDynamicProperties

--- tests/PHPStan/Rules/Properties/AccessPropertiesRuleTest.php
+++ tests/PHPStan/Rules/Properties/AccessPropertiesRuleTest.php
@@ -1272,4 +1272,18 @@ class AccessPropertiesRuleTest extends RuleTestCase
 		$this->analyse([__DIR__ . '/data/bug-13537.php'], $errors);
 	}
 
+	public function testBug13539(): void
+	{
+		$this->checkThisOnly = false;
+		$this->checkUnionTypes = true;
+		$this->checkDynamicProperties = true;
+		$this->analyse([__DIR__ . '/data/bug-13539.php'], [
+			[
+				'Access to an undefined property object::$baz.',
+				26,
+				'Learn more: <fg=cyan>https://phpstan.org/blog/solving-phpstan-access-to-undefined-property</>',
+			],
+		]);
+	}
+
 }
---
+++ tests/PHPStan/Rules/Properties/data/bug-13539.php
@@ -0,0 +1,28 @@
+<?php
+
+function broken(string $x): void {
+	$tmp = json_decode($x, false);
+	if (!isset($tmp->foo) || !isset($tmp->bar)) {
+	}
+}
+
+function works(string $x): void {
+	$tmp = json_decode($x, false);
+	if (!isset($tmp->foo, $tmp->bar)) {
+	}
+}
+
+function works_too(string $x): void {
+	/** @var stdClass $tmp */
+	$tmp = json_decode($x, false);
+	if (!isset($tmp->foo) || !isset($tmp->bar)) {
+	}
+}
+
+function also_ok(mixed $tmp): void {
+	if (isset($tmp->foo) && isset($tmp->bar)) {
+		echo $tmp->foo;
+		echo $tmp->bar;
+		echo $tmp->baz; // intentional: baz not checked by isset
+	}
+}

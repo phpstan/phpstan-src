810f7d3e35e9edcbce550eb9d063b83934362dec
Markus Staab <markus.staab@redaxo.de>
2026-04-29 08:30:44 +0200

    fix tests

--- tests/PHPStan/Analyser/nsrt/bug-14550.php
+++ tests/PHPStan/Analyser/nsrt/bug-14550.php
@@ -46,28 +46,18 @@ function testStrlen(): void
 {
 	$fn = strlen(...);
 	assertType('Closure(string): int<0, max>', $fn);
-
-	if (strlen(...) < 1) {}
-	if (0 < strlen(...)) {}
 }
 
 function testMbStrlen(): void
 {
 	$fn = mb_strlen(...);
 	assertType('Closure(string, string|null=): int<0, max>', $fn);
-
-	if (mb_strlen(...) < 1) {}
-	if (0 < mb_strlen(...)) {}
 }
 
 function testCount(): void
 {
 	$fn = count(...);
 	assertType('Closure(array|Countable, 0|1=): int<0, max>', $fn);
-
-	if (count(...) < 1) {}
-	if (0 < count(...)) {}
-	if (count(...) === 0) {}
 }
 
 function testSizeof(): void
@@ -80,66 +70,48 @@ function testPregMatch(): void
 {
 	$fn = preg_match(...);
 	assertType('Closure(string, string, array<string>|null=, TFlags=, int=): (0|1|false)', $fn);
-
-	if (preg_match(...) < 1) {}
-	if (1 <= preg_match(...)) {}
 }
 
 function testGettype(): void
 {
 	$fn = gettype(...);
 	assertType('Closure(mixed): string', $fn);
-
-	if (gettype(...) === 'string') {}
-	if (gettype(...) == 'string') {}
 }
 
 function testGetClass(): void
 {
 	$fn = get_class(...);
 	assertType('Closure(object=): class-string', $fn);
-
-	if (get_class(...) == 'stdClass') {}
 }
 
 function testGetDebugType(): void
 {
 	$fn = get_debug_type(...);
 	assertType('Closure(mixed): string', $fn);
-
-	if (get_debug_type(...) == 'string') {}
 }
 
 function testGetParentClass(): void
 {
 	$fn = get_parent_class(...);
 	assertType('Closure(object|string=): (class-string|false)', $fn);
-
-	if (get_parent_class(...) === 'stdClass') {}
 }
 
 function testTrim(): void
 {
 	$fn = trim(...);
 	assertType('Closure(string, string=): string', $fn);
-
-	if (trim(...) !== '') {}
 }
 
 function testLtrim(): void
 {
 	$fn = ltrim(...);
 	assertType('Closure(string, string=): string', $fn);
-
-	if (ltrim(...) !== '') {}
 }
 
 function testRtrim(): void
 {
 	$fn = rtrim(...);
 	assertType('Closure(string, string=): string', $fn);
-
-	if (rtrim(...) !== '') {}
 }
 
 /**
@@ -151,13 +123,4 @@ function testArrayKeysInForeach(array $list): void
 	}
 	$fn = array_keys(...);
 	assertType('Closure(array, mixed=, bool=): list<int|string>', $fn);
-}
-
-/**
- * @param list<string> $list
- */
-function testCountInForLoop(array $list): void
-{
-	for ($i = 0; $i < count(...); $i++) {
-	}
 }

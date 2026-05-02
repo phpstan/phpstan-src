dea87661c2631bddcb8ee2b6bc9c25c96e450035
Vincent Langlet <vincentlanglet@hotmail.fr>
2026-05-02 20:05:13 +0200

    Fix test

--- tests/PHPStan/Rules/Methods/CallToStaticMethodStatementWithoutSideEffectsRuleTest.php
+++ tests/PHPStan/Rules/Methods/CallToStaticMethodStatementWithoutSideEffectsRuleTest.php
@@ -52,6 +52,10 @@ class CallToStaticMethodStatementWithoutSideEffectsRuleTest extends RuleTestCase
 				12,
 			],
 			[
+				'Call to static method DateTimeImmutable::createFromFormat() on a separate line has no effect.',
+				13,
+			],
+			[
 				'Call to method DateTime::format() on a separate line has no effect.',
 				23,
 			],

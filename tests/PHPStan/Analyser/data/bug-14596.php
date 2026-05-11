3dac4788dbee38b636dcc0fbeb9b3b2166b2028d
Markus Staab <markus.staab@redaxo.de>
2026-05-11 09:46:32 +0200

    Revert "Split bug-14596 test data to avoid autoloader crash on invalid PHP syntax"
    This reverts commit 1845afd0cfe5bb34af107843f21c8e1766ef956c.

--- tests/PHPStan/Analyser/data/bug-14596.php
+++ tests/PHPStan/Analyser/data/bug-14596.php
@@ -2,6 +2,14 @@
 
 namespace Bug14596;
 
+function foo(int $a, int $b, int $c, string ...$rest): void {}
+
+class Foo {
+	public function bar(int $a, int $b, int $c, string ...$rest): void {}
+	public static function baz(int $a, int $b, int $c, string ...$rest): void {}
+	public function __construct(int $a, int $b, int $c, string ...$rest) {}
+}
+
 // built-in function
 \PHPStan\dumpType(1, 2, 3, d: 'foo', 5);
 

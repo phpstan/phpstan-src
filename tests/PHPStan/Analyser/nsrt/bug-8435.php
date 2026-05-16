28cf00cc01f0ea3276a0bf58dea4ed524da5de29
Takuya Aramaki <takaram71@gmail.com>
2026-05-16 18:15:18 +0900

    Add `@implements` to SPL iterator stubs

---
+++ tests/PHPStan/Analyser/nsrt/bug-8435.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types = 1);
+
+namespace Bug8435;
+
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use function PHPStan\Testing\assertType;
+
+class HelloWorld
+{
+	public function sayHello1(string $path): void
+	{
+		$iterator = new RecursiveDirectoryIterator($path);
+		foreach ($iterator as $fileinfo) {
+			assertType('SplFileInfo|string', $fileinfo);
+		}
+	}
+
+	public function sayHello2(string $path): void
+	{
+		$iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+		foreach ($iterator as $fileinfo) {
+			assertType('SplFileInfo|string', $fileinfo);
+		}
+	}
+}

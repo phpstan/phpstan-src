2afc0a461184a83e65dd44c671e42b51784ba67d
Markus Staab <maggus.staab@googlemail.com>
2026-05-24 14:18:43 +0200

    Test array_key_exists() on subtracted enum

---
+++ tests/PHPStan/Analyser/nsrt/array-key-exists-on-subtracted.php
@@ -0,0 +1,20 @@
+<?php // lint >= 8.0
+
+namespace ArrayKeyExistsSubtracted;
+
+use function PHPStan\Testing\assertType;
+
+enum IntBacked: int {
+	case A = 1;
+	case B = 2;
+}
+
+function test(IntBacked $i, array $arr): void {
+	if ($i !== IntBacked::A) {
+		assertType('2', $i->value);
+
+		if (array_key_exists($i->value, $arr)) {
+			assertType('non-empty-array&hasOffset(2)', $arr);
+		}
+	}
+}

20143f8438a166e24dd33952bd8b5f7c0211ae5a
Markus Staab <maggus.staab@googlemail.com>
2026-05-24 12:46:44 +0200

    Update ClassExistsFunctionTypeSpecifyingExtension.php

--- src/Type/Php/ClassExistsFunctionTypeSpecifyingExtension.php
+++ src/Type/Php/ClassExistsFunctionTypeSpecifyingExtension.php
@@ -49,14 +49,30 @@ final class ClassExistsFunctionTypeSpecifyingExtension implements FunctionTypeSp
 		$args = $node->getArgs();
 		$argType = $scope->getType($args[0]->value);
 		if ($argType instanceof ConstantStringType) {
-			$funcCall = new FuncCall(new FullyQualified($functionReflection->getName()), [
-				new Arg(new String_(ltrim($argType->getValue(), '\\'))),
-			]);
 			return $this->typeSpecifier->create(
-				new AlwaysRememberedExpr($funcCall, new BooleanType(), new BooleanType()),
+				new AlwaysRememberedExpr(
+					new FuncCall(new FullyQualified($functionReflection->getName()), [
+						new Arg(new String_(ltrim($argType->getValue(), '\\'))),
+					]),
+					new BooleanType(),
+					new BooleanType(),
+				),
 				new ConstantBooleanType(true),
 				$context,
 				$scope,
+			)->unionWith(
+				$this->typeSpecifier->create(
+					new AlwaysRememberedExpr(
+						new FuncCall(new FullyQualified('class_exists'), [
+							new Arg(new String_(ltrim($argType->getValue(), '\\'))),
+						]),
+						new BooleanType(),
+						new BooleanType(),
+					),
+					new ConstantBooleanType(true),
+					$context,
+					$scope,
+				),
 			);
 		}
 

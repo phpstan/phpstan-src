ea44c482045e13cb429beddb3b0179ca2186e165
Markus Staab <maggus.staab@googlemail.com>
2026-05-25 11:05:38 +0200

    Cleanup `instanceof ConstantStringType` calls (#5753)

--- src/Type/Php/DefinedConstantTypeSpecifyingExtension.php
+++ src/Type/Php/DefinedConstantTypeSpecifyingExtension.php
@@ -10,7 +10,6 @@ use PHPStan\Analyser\TypeSpecifierAwareExtension;
 use PHPStan\Analyser\TypeSpecifierContext;
 use PHPStan\DependencyInjection\AutowiredService;
 use PHPStan\Reflection\FunctionReflection;
-use PHPStan\Type\Constant\ConstantStringType;
 use PHPStan\Type\FunctionTypeSpecifyingExtension;
 use PHPStan\Type\MixedType;
 use function count;
@@ -48,15 +47,14 @@ final class DefinedConstantTypeSpecifyingExtension implements FunctionTypeSpecif
 		TypeSpecifierContext $context,
 	): SpecifiedTypes
 	{
-		$constantName = $scope->getType($node->getArgs()[0]->value);
-		if (
-			!$constantName instanceof ConstantStringType
-			|| $constantName->getValue() === ''
-		) {
+		$args = $node->getArgs();
+		$constantNames = $scope->getType($args[0]->value)->getConstantStrings();
+
+		if (count($constantNames) !== 1 || $constantNames[0]->getValue() === '') {
 			return new SpecifiedTypes([], []);
 		}
 
-		$expr = $this->constantHelper->createExprFromConstantName($constantName->getValue());
+		$expr = $this->constantHelper->createExprFromConstantName($constantNames[0]->getValue());
 		if ($expr === null) {
 			return new SpecifiedTypes([], []);
 		}

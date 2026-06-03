a37479ed8f6a9681c33e06b9246a04d040105f41
Markus Staab <maggus.staab@googlemail.com>
2026-06-03 09:30:11 +0200

    Update bug-14758.php

--- tests/PHPStan/Rules/Arrays/data/bug-14758.php
+++ tests/PHPStan/Rules/Arrays/data/bug-14758.php
@@ -6,7 +6,7 @@ namespace Bug14758;
  * @param string $s
  * @param array<string, string> $arr
  */
-function doBar(string $s, $arr) {
+function doBarBug14758(string $s, $arr) {
 	if (ctype_digit($s)) {
 		var_dump($arr[$s]);
 	}

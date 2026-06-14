7650ac8910df398534540dd5043e0ddaa1e00429
Markus Staab <maggus.staab@googlemail.com>
2026-06-14 08:10:24 +0200

    Consider `is_file($path)` before checking `require $path`

---
+++ tests/PHPStan/Rules/Keywords/data/include-in-file-exists.php
@@ -0,0 +1,9 @@
+<?php
+
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+	require __DIR__ . '/../vendor/autoload.php';
+}
+
+if (is_file(__DIR__ . '/../vendor/autoload.php')) {
+	require __DIR__ . '/../vendor/autoload.php';
+}

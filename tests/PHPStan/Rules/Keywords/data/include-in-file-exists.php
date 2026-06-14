b976332b34982bc0bba99a51c6d248cf89924365
Markus Staab <maggus.staab@googlemail.com>
2026-06-14 08:54:21 +0200

    add test

--- tests/PHPStan/Rules/Keywords/data/include-in-file-exists.php
+++ tests/PHPStan/Rules/Keywords/data/include-in-file-exists.php
@@ -7,3 +7,9 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
 if (is_file(__DIR__ . '/../vendor/autoload.php')) {
 	require __DIR__ . '/../vendor/autoload.php';
 }
+
+foreach ([__DIR__ . '/../../autoload.php', __DIR__ . '/../autoload.php', __DIR__ . '/vendor/autoload.php'] as $file) {
+	if (file_exists($file)) {
+		require $file;
+	}
+}

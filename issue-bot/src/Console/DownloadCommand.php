0581ca564ae55cba9c55391a6d535aaff178afde
Markus Staab <markus.staab@redaxo.de>
2026-06-16 14:17:53 +0200

    test with reduced shard size

--- issue-bot/src/Console/DownloadCommand.php
+++ issue-bot/src/Console/DownloadCommand.php
@@ -123,7 +123,7 @@ class DownloadCommand extends Command
 			if (count($phpVersionHashes) === 0) {
 				continue;
 			}
-			$chunkSize = (int) ceil(count($phpVersionHashes) / 25);
+			$chunkSize = (int) ceil(count($phpVersionHashes) / 20);
 			if ($chunkSize < 1) {
 				throw new Exception('Chunk size less than 1');
 			}

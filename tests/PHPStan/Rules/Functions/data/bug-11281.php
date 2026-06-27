fe09fa42c4aac4a1cf10d7e73beb260625b6a324
Markus Staab <maggus.staab@googlemail.com>
2026-06-27 10:59:38 +0200

    Update bug-11281.php

--- tests/PHPStan/Rules/Functions/data/bug-11281.php
+++ tests/PHPStan/Rules/Functions/data/bug-11281.php
@@ -4,7 +4,7 @@ declare(strict_types = 1);
 
 namespace Bug11281Functions;
 
-function sayHello(int $i): void
+function takesInt(int $i): void
 {
 }
 
@@ -15,7 +15,7 @@ function test(array $values): void
 {
 	// The ternary's resulting type normalizes to mixed (mixed|string),
 	// but the else branch is definitely a string passed to an int parameter.
-	sayHello(array_key_exists('key', $values) ? $values['key'] : ' a string');
+	takesInt(array_key_exists('key', $values) ? $values['key'] : ' a string');
 }
 
 /**
@@ -24,7 +24,7 @@ function test(array $values): void
 function noError(array $values): void
 {
 	// Numeric-ish coercible branches must not be flagged.
-	sayHello(array_key_exists('key', $values) ? $values['key'] : 5);
+	takesInt(array_key_exists('key', $values) ? $values['key'] : 5);
 }
 
 /**
@@ -32,7 +32,7 @@ function noError(array $values): void
  */
 function nested(array $values, bool $other, bool $another): void
 {
-	sayHello($other ? $values['key'] : ($another ? 1 : ' nested string'));
+	takesInt($other ? $values['key'] : ($another ? 1 : ' nested string'));
 }
 
 function expectsString(string $s): void

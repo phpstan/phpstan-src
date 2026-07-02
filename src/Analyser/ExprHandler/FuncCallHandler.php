59626188b569acc8adeddea18eef6f542d3307e0
Markus Staab <markus.staab@redaxo.de>
2026-07-02 08:04:27 +0200

    simplify

--- src/Analyser/ExprHandler/FuncCallHandler.php
+++ src/Analyser/ExprHandler/FuncCallHandler.php
@@ -576,8 +576,8 @@ final class FuncCallHandler implements ExprHandler
 			$scope = OutputBufferHelper::applyLevelDelta($scope, $outputBufferDelta);
 		} elseif (OutputBufferHelper::isLevelTracked($scope)) {
 			if ($functionReflection === null) {
-				$forgetOutputBufferLevel = !$parametersAcceptor instanceof CallableParametersAcceptor
-					|| count($parametersAcceptor->getImpurePoints()) > 0;
+				$forgetOutputBufferLevel =
+					count($parametersAcceptor->getImpurePoints()) > 0;
 			} else {
 				$forgetOutputBufferLevel = (!$functionReflection->isBuiltin() && !$functionReflection->hasSideEffects()->no());
 			}

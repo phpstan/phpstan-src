c42e108f07168ab139ea5422d79b31991995849e
USAMI Kenta <tadsan@zonu.me>
2026-07-10 16:54:22 +0900

    Cover -PHP_INT_MAX and a partially overflowing union

--- tests/PHPStan/Analyser/data/unary-minus-64bit.php
+++ tests/PHPStan/Analyser/data/unary-minus-64bit.php
@@ -8,6 +8,9 @@ use function PHPStan\Testing\assertType;
 assertType('9.223372036854776E+18', -(-9223372036854775807 - 1));
 assertType('2147483648|9.223372036854776E+18', -PHP_INT_MIN);
 
+// Negating the largest integer stays an integer.
+assertType('-9223372036854775807|-2147483647', -PHP_INT_MAX);
+
 $min = -9223372036854775807 - 1;
 assertType('9.223372036854776E+18', -$min);
 
@@ -27,4 +30,15 @@ function constantUnion(int $int): void
 {
 	/** @var -1|-2 $int */
 	assertType('1|2', -$int);
+
+	/** @var 9223372036854775807|25 $int */
+	assertType('-9223372036854775807|-25', -$int);
+}
+
+// The union has one member that overflows and one that does not.
+function partiallyOverflowingUnion(bool $bool): void
+{
+	$int = $bool ? -9223372036854775807 - 1 : 25;
+	assertType('-9223372036854775808|25', $int);
+	assertType('-25|9.223372036854776E+18', -$int);
 }

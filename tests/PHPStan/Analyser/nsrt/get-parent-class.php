fc781b91befde17799037f307904ebe4e9fbd1fb
USAMI Kenta <tadsan@zonu.me>
2026-07-14 18:09:17 +0900

    Cover get_parent_class(self::class) inside a trait
    self::class is not late static bound: inside a trait it is already the using class as a
    constant class-string, indistinguishable from writing Bar::class, so it keeps the exact
    parent while $this / static::class stay conservative.

--- tests/PHPStan/Analyser/nsrt/get-parent-class.php
+++ tests/PHPStan/Analyser/nsrt/get-parent-class.php
@@ -49,6 +49,12 @@ trait FooTrait
 		assertType('class-string|false', get_parent_class());
 		assertType('class-string|false', get_parent_class($this));
 		assertType('class-string|false', get_parent_class(static::class));
+
+		// self::class is not late static bound - it is the using class, so it is already a
+		// constant class-string here, indistinguishable from writing Bar::class.
+		assertType('\'ParentClass\\\\Bar\'', self::class);
+		assertType('\'ParentClass\\\\Foo\'', get_parent_class(self::class));
+		assertType('\'ParentClass\\\\Foo\'', get_parent_class(Bar::class));
 	}
 
 }

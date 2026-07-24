6e85ec93395e47742e86b195fa6c8f20a2ef67a1
Markus Staab <maggus.staab@googlemail.com>
2026-07-24 10:20:18 +0200

    Fix "ob_get_*() === false will always evaluate to false" false positive (#6097)

--- src/Type/Php/OutputBufferingDynamicReturnTypeExtension.php
+++ src/Type/Php/OutputBufferingDynamicReturnTypeExtension.php
@@ -8,11 +8,10 @@ use PHPStan\Analyser\Scope;
 use PHPStan\DependencyInjection\AutowiredService;
 use PHPStan\Reflection\FunctionReflection;
 use PHPStan\Reflection\ParametersAcceptorSelector;
-use PHPStan\Type\Constant\ConstantBooleanType;
 use PHPStan\Type\DynamicFunctionReturnTypeExtension;
 use PHPStan\Type\IntegerRangeType;
 use PHPStan\Type\Type;
-use PHPStan\Type\TypeCombinator;
+use PHPStan\Type\TypeUtils;
 use function in_array;
 
 /**
@@ -48,7 +47,10 @@ final class OutputBufferingDynamicReturnTypeExtension implements DynamicFunction
 
 		$outputBufferLevelType = $scope->getType(new FuncCall(new Name('ob_get_level'), []));
 		if (IntegerRangeType::createAllGreaterThanOrEqualTo(1)->isSuperTypeOf($outputBufferLevelType)->yes()) {
-			return TypeCombinator::remove($defaultReturnType, new ConstantBooleanType(false));
+			// checking error state return values of ob_* functions is essentially useless
+			// as this usually means that your system is out of memory and your process is going to die anyway.
+			// that's why error states oftentimes are not checked.
+			return TypeUtils::toBenevolentUnion($defaultReturnType);
 		}
 
 		return $defaultReturnType;
--- tests/PHPStan/Rules/Comparison/StrictComparisonOfDifferentTypesRuleTest.php
+++ tests/PHPStan/Rules/Comparison/StrictComparisonOfDifferentTypesRuleTest.php
@@ -1261,6 +1261,11 @@ class StrictComparisonOfDifferentTypesRuleTest extends RuleTestCase
 		$this->analyse([__DIR__ . '/../../Analyser/nsrt/bug-14878.php'], []);
 	}
 
+	public function testBug14985(): void
+	{
+		$this->analyse([__DIR__ . '/data/bug-14985.php'], []);
+	}
+
 	public function testBug14847(): void
 	{
 		$this->analyse([__DIR__ . '/data/bug-14847.php'], [

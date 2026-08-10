e4f4452dfaa0ff8f5747d9051596396064198775
Markus Staab <maggus.staab@googlemail.com>
2026-08-10 14:38:50 +0200

    fix

--- src/Type/ExtensionClassHelper.php
+++ src/Type/ExtensionClassHelper.php
@@ -2,6 +2,8 @@
 
 namespace PHPStan\Type;
 
+use PhpParser\Node;
+use PHPStan\Collectors\Collector;
 use PHPStan\Reflection\ReflectionProvider;
 use function array_key_exists;
 use function class_implements;
@@ -12,6 +14,9 @@ final class ExtensionClassHelper
 
 	/** @var array<string, array<string>> */
 	private static array $extensionClassNames = [];
+
+	/** @var array<class-string<Node>, array<class-string<Node>>> */
+	private static array $extensionClassNamesRuntimeReflections = [];
 
 	/**
 	 * @return string[]
@@ -27,15 +32,16 @@ final class ExtensionClassHelper
 	}
 
 	/**
-	 * @return string[]
+	 * @param class-string<Node> $className
+	 * @return array<class-string<Node>>
 	 */
 	public static function getExtensionClassNamesByRuntimeReflection(string $className): array
 	{
-		if (!array_key_exists($className, self::$extensionClassNames)) {
-			self::$extensionClassNames[$className] = [$className] + class_parents($className) + class_implements($className);
+		if (!array_key_exists($className, self::$extensionClassNamesRuntimeReflections)) {
+			self::$extensionClassNamesRuntimeReflections[$className] = [$className] + class_parents($className) + class_implements($className);
 		}
 
-		return self::$extensionClassNames[$className];
+		return self::$extensionClassNamesRuntimeReflections[$className];
 	}
 
 }

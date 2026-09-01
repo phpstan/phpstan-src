2aca59b947d029380119cf9ef43e093d1863910d
Ondrej Mirtes <ondrej@mirtes.cz>
2026-09-01 10:51:37 +0200

    Bump expected turbo version

--- src/Turbo/TurboExtensionEnabler.php
+++ src/Turbo/TurboExtensionEnabler.php
@@ -21,7 +21,7 @@ final class TurboExtensionEnabler
 	 * version is the short SHA of the last commit touching turbo-ext/src/,
 	 * enforced by the phar.yml turbo-version job.
 	 */
-	public const EXPECTED_EXTENSION_VERSION = 'dee1d70';
+	public const EXPECTED_EXTENSION_VERSION = '4df6ab1';
 
 	private static bool $typeCombinatorCacheEnabled = false;
 

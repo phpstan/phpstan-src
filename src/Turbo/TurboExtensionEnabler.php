89b86fe308308a6e41180b376bfa10cf0bc4f1b9
Markus Staab <maggus.staab@googlemail.com>
2026-09-08 10:15:47 +0200

    Bump expected turbo version

--- src/Turbo/TurboExtensionEnabler.php
+++ src/Turbo/TurboExtensionEnabler.php
@@ -22,7 +22,7 @@ final class TurboExtensionEnabler
 	 * version is the short SHA of the last commit touching turbo-ext/src/,
 	 * enforced by the phar.yml turbo-version job.
 	 */
-	public const EXPECTED_EXTENSION_VERSION = '91db796';
+	public const EXPECTED_EXTENSION_VERSION = 'b48812e';
 
 	private static bool $typeCombinatorCacheEnabled = false;
 

65e6adb4264f11d29256494916b8787d94dd8d59
Ondrej Mirtes <ondrej@mirtes.cz>
2026-09-14 09:03:45 +0200

    Allow PHP 8.6 as max version and run CI on it

--- tests/PHPStan/Analyser/nsrt/filter-var-array.php
+++ tests/PHPStan/Analyser/nsrt/filter-var-array.php
@@ -302,39 +302,3 @@ function dynamicVariables(array $input): void
 		'missing' => $filter,
 	], false));
 }
-
-/**
- * @param array{exists: int, optional?: int, extra: int} $input
- * @param array<mixed> $arrayFilter
- * @param FILTER_VALIDATE_* $intFilter
- */
-function dynamicFilter(array $input, array $arrayFilter, int $intFilter): void
-{
-	// filter array with add_empty=default
-	assertType('array|false|null', filter_var_array($input, $arrayFilter));
-	// filter array with add_empty=true
-	assertType('array|false|null', filter_var_array($input, $arrayFilter, true));
-	// filter array with add_empty=false
-	assertType('array|false|null', filter_var_array($input, $arrayFilter, false));
-
-	// filter flag with add_empty=default
-	assertType('array|false|null', filter_var_array($input, $intFilter));
-	// filter flag with add_empty=true
-	assertType('array|false|null', filter_var_array($input, $intFilter, true));
-	// filter flag with add_empty=false
-	assertType('array|false|null', filter_var_array($input, $intFilter, false));
-
-	// filter array with add_empty=default
-	assertType('array|false|null', filter_var_array([], $arrayFilter));
-	// filter array with add_empty=true
-	assertType('array|false|null', filter_var_array([], $arrayFilter, true));
-	// filter array with add_empty=false
-	assertType('array|false|null', filter_var_array([], $arrayFilter, false));
-
-	// filter flag with add_empty=default
-	assertType('array|false|null', filter_var_array([], $intFilter));
-	// filter flag with add_empty=true
-	assertType('array|false|null', filter_var_array([], $intFilter, true));
-	// filter flag with add_empty=false
-	assertType('array|false|null', filter_var_array([], $intFilter, false));
-}

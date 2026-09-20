83b0304db935e92191112a56640d476785247dcb
maruyama <taku2000maru@icloud.com>
2026-09-20 22:37:10 +0900

    test : Add RoundingMode constant evaluation tests

---
+++ tests/PHPStan/Analyser/nsrt/round-php84.php
@@ -0,0 +1,18 @@
+<?php // lint >= 8.4
+
+namespace RoundFamilyTestPHP84;
+
+use function PHPStan\Testing\assertType;
+
+function constantRoundingMode(): void
+{
+	assertType('10.0', round(9.5, 0, \RoundingMode::HalfAwayFromZero));
+	assertType('9.0', round(9.5, 0, \RoundingMode::HalfTowardsZero));
+	assertType('10.0', round(9.5, 0, \RoundingMode::HalfEven));
+	assertType('9.0', round(9.5, 0, \RoundingMode::HalfOdd));
+
+	assertType('float', round(9.5, 0, \RoundingMode::TowardsZero));
+	assertType('float', round(9.5, 0, \RoundingMode::AwayFromZero));
+	assertType('float', round(9.5, 0, \RoundingMode::NegativeInfinity));
+	assertType('float', round(9.5, 0, \RoundingMode::PositiveInfinity));
+}

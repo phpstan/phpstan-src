cad5a85a8496134d4f18c9495a9797049dda21f9
Ondrej Mirtes <ondrej@mirtes.cz>
2026-09-22 09:23:51 +0200

    Bump expected turbo version

--- src/Turbo/TurboExtensionEnabler.php
+++ src/Turbo/TurboExtensionEnabler.php
@@ -33,7 +33,7 @@ use function phpversion;
 final class TurboExtensionEnabler
 {
 
-	public const EXPECTED_EXTENSION_VERSION = 'cb469d0';
+	public const EXPECTED_EXTENSION_VERSION = '500bbe7';
 
 	private static bool $active = false;
 
